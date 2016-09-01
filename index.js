2f278fb7c1742daf97f30a76bf37df3ab0bd7305
Anthony Zeitlin <zeitlin1@gmail.com>
2016-09-01 23:24:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+var katzDeli = []
+var katzDeliLine = katzDeli.length + 1
+
+function takeANumber(katzDeli, name) {
+  katzDeli.push(name)
+  return "Welcome, " + name + ". You are number " + katzDeliLine + " in line."
+}
+
+function nowServing(katzDeli){
+  if (katzDeli.length = 0){
+    return "The line is empty"
+  }else
+}
