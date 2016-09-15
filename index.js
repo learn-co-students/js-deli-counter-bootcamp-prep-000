87216abd94f28cd6c1240a7934ff67b0c892d7d3
Jonathan Kemp <jonathan_kemp@talk21.com>
2016-09-15 17:47:32 +0000

    Done.

--- index.js
+++ index.js
@@ -18,14 +18,13 @@ function nowServing(deliLine, name) {
 }
 
 
-
 function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty."
   }
   var newLine = [];
-  for (let i = 0; i < line.length; ++i) {
-    newLine.push( `${i + 1}. ${line[i]}, `); ++i;
-    return `The line is currently: ${newLine}`
-}
+  for (var i = 0; i < line.length; i++) {
+    newLine.push(` ${i + 1}. ${line[i]}`);
+  }
+  return `The line is currently:${ newLine}`
 }
