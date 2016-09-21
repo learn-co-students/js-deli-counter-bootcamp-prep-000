e19d0ae6d34af51079c7473d5e85409a95a22983
Poka Mayson <smayson94@gmail.com>
2016-09-21 18:41:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeli = []
+
+function takeANumber(katzDeliLine,name){
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(deliLine) {
+  if (!deliLine.length) {
+    return "There is nobody waiting to be served!"
+  }
+
+return `Currently serving ${deliLine.shift()}.`
+};
+
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  }
+
+  const numbersAndNames = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
