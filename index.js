b3b7d90f3e6321fa32d4373a7f72668f8087d73d
Ethan Ryan <ethanryan@gmail.com>
2016-09-25 20:26:27 +0000

    Done.

--- index.js
+++ index.js
@@ -28,17 +28,29 @@ function nowServing(katzDeli) {
 
 var line = [];
 
+//for (i = 0; i < cars.length; i++) {
+
 function currentLine(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
+  while (i = 0; i < katzDeli.length; i++) {
     line.push(` `+[i+1]+`. `  + katzDeli[i])
-    i++;
   }
   if (katzDeli.length === 0) {
     return "The line is currently empty.";
   } else
   return(`The line is currently:` + line);
 }
+
+// function currentLine(katzDeli) {
+//   let i = 0;
+//   while (i < katzDeli.length) {
+//     line.push(` `+[i+1]+`. `  + katzDeli[i])
+//     i++;
+//   }
+//   if (katzDeli.length === 0) {
+//     return "The line is currently empty.";
+//   } else
+//   return(`The line is currently:` + line);
+// }
 
 //WORKS:::::
 
