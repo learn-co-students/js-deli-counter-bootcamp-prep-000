d6d684f4f4511b0e594b43e9468df230ff9b0181
Vnay Kumr <chvnay@gmail.com>
2016-10-07 09:01:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function takeANumber(katzDeliLine, name)
+{
+katzDeliLine.push(name)
+return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine)
+{
+  if (katzDeliLine.length === 0)
+  {return 'There is nobody waiting to be served!'}
+  else {
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+}
+
+function currentLine(katzDeliLine)
+{
+  const nw = []
+  for (let i = 0, l = katzDeliLine.length; i < l; i++)
+  {nw.push(`${i + 1}. ${katzDeliLine[i]}`)}
+
+  if (katzDeliLine.length === 0)
+  {return "The line is currently empty."}
+
+  else
+  {return `The line is currently: ${nw.join(', ')}`}
+}
