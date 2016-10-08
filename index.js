31f4adc5a2f8d0b14cb6e56c1ff53512707b9520
Sydney Chun <schun08@gmail.com>
2016-10-08 15:49:28 +0000

    Done.

--- index.js
+++ index.js
@@ -4,17 +4,31 @@ function takeANumber(katzDeliLine,name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing(){
-  if(katzDeliLine.length !== 0) {
-    var name = katzDeliLine[0];
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length > 0) {
+    var dupe = katzDeliLine;
+    var name = dupe[0];
     katzDeliLine = katzDeliLine.shift();
     return `Currently serving ${name}.`;
   }
   else {return "There is nobody waiting to be served!";}
 }
 
-function currentLine() {
-  if (katzDeliLine !== 0) {
-    return `The line is currently: ${Number(katzDeliLine.length - katzDeliLine -1)}.`
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length > 1) {
+    var a = "The line is currently: ";
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      if (i === katzDeliLine.length-1) {
+        a += `${i+1}. ${katzDeliLine[i]}`
+      }
+      else { a += `${i+1}. ${katzDeliLine[i]}, `; }
+    }
+    return a;
+  }
+  else if (katzDeliLine.length === 1) {
+    return `Currently serving ${katzDeliLine[0]}.`;
+  }
+  else {
+    return `The line is currently empty.`
   }
 }
