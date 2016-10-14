ce22d3eef89ea18238d21bcc9d5b056dcba33520
Chris Hansen <chris.hansen@modernmi.com>
2016-10-14 21:49:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function takeANumber(line, name) {
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return `Currently serving ${line.shift()}.`
+  }
+}
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return "The line is currently empty."
+  }
+
+  const result = []
+
+  for(let i = 0, l = line.length; i < l; i++) {
+    result.push(`${i+1}. ${line[i]}`)
+  }
+
+  return `The line is currently: ${result.join(', ')}`
+}
