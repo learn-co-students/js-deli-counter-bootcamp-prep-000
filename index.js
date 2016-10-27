af0b405bd94a7db50b5b3d648451265024985778
Bharath Kumar <bharathkumar.a20@gmail.com>
2016-10-27 23:35:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var katzDeli = []
+
+function takeANumber(katzDeliLine, newCustomer){
+  katzDeliLine.push(newCustomer)
+  return `Welcome, ${newCustomer}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length > 0){
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+  return "There is nobody waiting to be served!"
+}
+
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length > 0){
+    var line = []
+    for(var i = 0; i < katzDeliLine.length; i++){
+      line.push(`${i+1}. ${katzDeliLine[i]}`)
+    }
+    return `The line is currently: ${line.join(", ")}`
+  }
+  return "The line is currently empty."
+}
