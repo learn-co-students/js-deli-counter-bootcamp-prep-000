1742e9e7ab7c9baee62c9df9d58bd04becf20ad1
colombianito66 <colombianito66@gmail.com>
2016-10-29 00:38:59 +0000

    Done.

--- index.js
+++ index.js
@@ -5,17 +5,22 @@ function takeANumber(katzDeliLine, name){
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing() {
+function nowServing(katzDeli) {
 
-
-  for(let i = 0; i >= katzDeli.length; i++){
-
-     if (i != 0) {
-      return `Currently serving ${katzDeli[i]}`;
+     if (katzDeli.length === 0 ) {
+      return `There is nobody waiting to be served!`;
      }
      else {
-       return `There is nobody waiting to be served!`;
-
+       var serving = `Currently serving ${katzDeli[0]}.`
+       katzDeli.shift()
+       return serving
      }
+  }
+
+function currentLine(katzDeli){
+  if (katzDeli.length === 0) {
+    return `The line is currently empty.`
+  }
+  else { return `The line is currently: ${katzDeli.slice(0)}`;
   }
 }
