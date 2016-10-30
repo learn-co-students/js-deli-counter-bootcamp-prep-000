7b75670c973d0a291dec0fc9a77956fa46e91d0d
Laziza Kodirova <kodirova.laziza@gmail.com>
2016-10-30 14:14:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 var katzDeli = [];
+var otherDeli = ["Steven", "Blake", "Avi"];
 
-function takeANumber(katzDeliLine,name){
-  katzDeli.push("Welcome, " + name + " . You are number " + katzDeliLine + " in line.") ;
-  return katzDeli;
-}
+function takeANumber(katzDeli,name){
+  katzDeli.push(name);
+  return("Welcome, "+ name +". You are number "+ katzDeli.length +" in line.");
+};
+
+function nowServing(katzDeli){
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+   }
+  //otherDeli.push(name);
+  //return (" Welcome, "+ name +". You are number "+ otherDeli.length +" in line.")
+  //if (otherDeli===0) return ("There is nobody waiting to be served!");
+};
+
+
+function currentLine(katzDeliLine){
+  for (var i=0; i<otherDeli.length ; i++)
+if (otherDeli>0) {katzDeli.push("The line is currently:"+i+"."+otherDeli[i]+",") }
+else {return "The line is currently empty."};
+return katzDeli;
+};
