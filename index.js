ebd9941e98ebcade94c45f268e4f30b96cd2216b
jason brown <j.grant.brown@gmail.com>
2016-11-14 19:37:44 +0000

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ return ("Welcome, " + newperson + ". You are number " + (katzDeliLine.indexOf(ne
 function nowServing(katzDeliLine){
 if (katzDeliLine.length>=1){
   katzDeliLine.shift;
-  return ( "Currently serving " + (katzDeliLine[0]);
+  return ( "Currently serving " + (katzDeliLine.indexOf(0));
 }
 else{
   return ("There is nobody waiting to be served!");
