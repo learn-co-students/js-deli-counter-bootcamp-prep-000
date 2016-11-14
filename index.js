cef1c457eb7b95d4369aad89f0c4b203165a563a
jason brown <j.grant.brown@gmail.com>
2016-11-14 20:02:27 +0000

    Done.

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ function nowServing(katzDeliLine){
 if (katzDeliLine.length>=1){
 
   return ( "Currently serving " + (katzDeliLine.shift());
-
+}
 else{
   return ("There is nobody waiting to be served!");
-}
 }
 }
