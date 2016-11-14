e6c4a2b07d4be40bbc814c190e0f95ce59360c79
Brittany Hill <brit.hill10@gmail.com>
2016-11-14 16:54:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-var katzDeliLine=[];
-function takeANumber(katzDeliLine, name) {
-  for (let i=0, 1=line.length;i<1;i++) {
-    katzDeliLine.push(`${i + 1}. ${line[i]}`)
-  }
-  return "The line is: "${katzDeliLine.join(", ")}""
-}
-function nowServing(katzDeliLine) {
-  if(katzDeliLine===0) {
+var katzDeli = []
+
+function takeANumber(katzDeli, name) {
+   katzDeli.push(name)
+   return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
+ }
+
+function nowServing(katzDeli) {
+  if(katzDeli.length===0) {
     return "There is nobody waiting to be served!"
-  } else{
-  console.log("Now serving: ${katzDeliLine[0]}")
+  } else {
+    return `Currently serving ${katzDeli.shift()}.`
+  }
 }
+
+function currentLine(katzDeli) {
+  if (katzDeli.length===0) {
+    return "The line is currently empty."
+  } else {
+    const line = []
+    for (let i = 0, l = katzDeli.length; i < l; i++) {
+      line.push(`${i+1}. ${katzDeli[i]}`)
+    }
+    return `The line is currently: ${line.join(", ")}`
+  }
 }
