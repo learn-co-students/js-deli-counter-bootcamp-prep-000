9d5c33f5e04dcca5ff791a2f9d3abd0d0595afdc
jason brown <j.grant.brown@gmail.com>
2016-11-14 18:58:10 +0000

    Done.

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ katzDeliLine.push(newperson);
 return ("Welcome, " + newperson + ". You are number " + (katzDeliLine.indexOf(newperson)+1) +" in line." )
 }
 
-function nowServing(){
+function nowServing(katzDeliLine){
 if (katzDeliLine.length>1){
-return katzDeliLine.shift[0];
+return katzDeliLine.shift;
 }
 else{
   return ("There is nobody waiting to be served!");
