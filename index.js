c203a9e87aa90938062125b65958e25bf0c3eae2
jason brown <j.grant.brown@gmail.com>
2016-11-14 17:57:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 function takeANumber(katzDeliLine,newperson){
 var katzDeliLine = new Array();
-katzDeliLine.push(newperson)
+katzDeliLine.push(newperson)}
 return ("Welcome," + newperson + ". You are " (katzDeliLine[]+1) +"in line." )
-}
