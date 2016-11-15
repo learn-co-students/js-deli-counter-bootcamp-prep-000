b228e76a575a7d7ecd6d530e1223be4a528af475
jason brown <j.grant.brown@gmail.com>
2016-11-15 14:57:47 +0000

    Done.

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ else{
 function currentLine(katzDeliLine){
 for (var i=1; katzDeliLine.length >= i; i++)
 {
-  while(katzDeliLine.length>=i) {
-    return ( "The line is currently: " + i + ". " + katzDeliLine[i] + ", ")
+  while(katzDeliLine.length>=0) {
+    return ( "The line is currently: " + i + ". " + katzDeliLine + ", ")
 }
 
     return ("The line is currently empty.");
