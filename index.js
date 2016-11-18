511166794556397fedddfa675c4c53da3cad806b
Justin Zuchowski <justin.zuchowski@gmail.com>
2016-11-18 01:50:58 +0000

    Done.

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ function currentLine(katzDeli) {
     } if (katzDeli.length === 0) {
       return "The line is currently empty.";
       } else {
-          return(`The line is currently:` + line);
-        }       
+        return `The line is currently:` + line;
+        }
 }
