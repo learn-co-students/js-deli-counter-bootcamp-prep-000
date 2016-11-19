529a93facb1626a797412d67776d46c222ce2e7d
Joanne C <jocchann@gmail.com>
2016-11-19 23:17:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var katzDeli = [];
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+    katzDeliLine.push(name);
+    return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if(!katzDeliLine.length){
+    return "The line is currently empty."
+  }
+
+  var updatedLine = [];
+
+  for (let i = 0; i < katzDeliLine.length; i++) {
+      updatedLine.push(`${i+1}. ${katzDeliLine[i]}`)
+      }
+
+  return `The line is currently: ${updatedLine.join(', ')}`
+}
