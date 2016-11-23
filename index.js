b7d1559581cf89b98daced11b0a6771a01cc9cec
Elizabeth Hall <elizabethhall205@gmail.com>
2016-11-23 02:30:02 +0000

    Done.

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ line.push(name) //adds a person (name) to the line (line)
   return `Welcome, ${name}. You are number ${line.length} in line.`;
   //Error: Expected undefined to equal 'Welcome, Ada. You are number 1 in line.' iterates over
   // the array returns the string welcome ${name template literal placeholder}. You are ${template literal array.length}
-  //
 }
+  //Pass an array as a function parameter
+  function nowServing(line) {
+    if() {
+    return(//the first person in line and then remove that person)
+    }
+    return  "There is nobody waiting to be served!"
+  }
