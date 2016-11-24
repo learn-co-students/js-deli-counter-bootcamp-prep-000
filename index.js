11d3e7c06ef4bf580b0c2e03a7ea910fb885b92a
Alexis Vazquez <alexisvaz93@yahoo.com>
2016-11-24 01:28:14 +0000

    Done.

--- index.js
+++ index.js
@@ -3,52 +3,42 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
 
-  for(let n = 0; n <= katzDeliLine.length; n++){
-  katzDeliLine.push[name];
-  return(`Welcome, ${name}. You are number ${katzDeliLine.length+1} in line.`)
+  katzDeliLine.push(name)
+  return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+//length+1 WORKED
 
-//  katzDeliLine++;
-  }
-
-
-/*  var n = 0;
-
-  while (n <= katzDeliLine.length){
-    katzDeliLine.push[name];
-    return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
-  }
-*/
 
   return katzDeliLine;
-//  console.log(`Welcome ${name}. You are number ${katzDeliLine.length} in line.`)
 
 }
 
 function nowServing(katzDeliLine){
 
-  for (let i = 0; i < katzDeliLine.length; i++){
-  if (katzDeliLine.length == 0){
-    return("There is nobody waiting to be served!");
-  } else if(katzDeliLine.length > 0){
-    return(`Currently serving ${katzDeliLine[i]}.`)
-    katzDeliLine = katzDeliLine.slice(0)
-    return katzDeliLine;
-  }
-}
 
+  if(katzDeliLine.length > 0){
+    return(`Currently serving ${katzDeliLine[0]}.`)
+    katzDeliLine = katzDeliLine.slice(1)
+    return katzDeliLine
+  } else {
+      return "There is nobody waiting to be served!";
+  }
+
+//katzDeliLine = katzDeliLine.shift(0)
+
+return katzDeliLine;
 }
 
 
 function currentLine(){
 
-  for (let i = 0; i < katzDeliLine.length; i++){
-    if (katzDeliLine.length == 0){
+//  for (let i = 0; i < katzDeliLine.length; i++){
+    if (katzDeliLine.length === 0){
       return("The line is currently empty.");
   } else if(katzDeliLine.length > 0){
       return(`The line is currently ${katzDeliLineline[i]}. ${katzDeliLine[i]},`)
   }
 
-  }
+//  }
 
 
   //return katzDeliLine;
