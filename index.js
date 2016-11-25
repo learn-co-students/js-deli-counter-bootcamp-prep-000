6f73516d91150d7b6782d419f863bb7fe22b937b
Ian McNair <imcnair13@gmail.com>
2016-11-25 20:22:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 function takeANumber(katzDeliLine, name){
-  var check = katzDeliLine.length+ 1
-  return(`Welcome, ${name}. you are number ${check} in line.`)
+  var position = katzDeliLine.length + 1
+  katzDeliLine.push(name)
+  return(`Welcome, ${name}. You are number ${position} in line.`)
 }
 
 
 function nowServing(katzDeliLine){
   if(katzDeliLine.length===0){
     return "There is nobody waiting to be served!"
-  }
-  return(`Currently serving ${katzDeliLine[0]}.`)
+  } else
+  var annoucement = `Currently serving ${katzDeliLine[0]}.`
+  katzDeliLine.shift()
+  return annoucement
 }
 
-/*
+
 function currentLine(katzDeliLine){
-	if(katzDeliLine === 0){
-	return (“The line is currently empty”)
+	if(katzDeliLine.length === 0){
+	return ("The line is currently empty.")
 	}
-	else return(
-
+	else {
+    var current = "The line is currently: "
+    for(var i = 0; i < katzDeliLine.length; i++){
+      if(i === (katzDeliLine.length-1)){
+        current += `${i+1}. ${katzDeliLine[i]}`
+      } else{
+        current += `${i+1}. ${katzDeliLine[i]}, `
+      }
+    }
+    return current
+  }
 }
-*/
