da08e4cbb2edd94acae924eb6e4d6945e323b279
Elizabeth Hall <elizabethhall205@gmail.com>
2016-11-25 17:47:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var katzDeli = [];
-function takeANumber(line, name) {
-line.push(name) //adds a person (name) to the line (line)
-  return `Welcome, ${name}. You are number ${line.length} in line.`;
-  //Error: Expected undefined to equal 'Welcome, Ada. You are number 1 in line.' iterates over
-  // the array returns the string welcome ${name template literal placeholder}. You are ${template literal array.length}
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
-  //Pass an array as a function parameter
-  function nowServing(line) {
-    if() {
-    return(//the first person in line and then remove that person)
-    }
-    return  "There is nobody waiting to be served!"
-  }
+
+function nowServing(katzDeliLine) {
+    for (let i = 0; i < katzDeliLine.length; i++)
+
+    if(katzDeliLine.length > 0) {
+    return `Currently serving ${katzDeliLine.shift()}.`
+
+      } else { (katzDeliLine.length < 1 )
+        
+      }
+    return "There is nobody waiting to be served!"
+  };
