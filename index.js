bdc6c3a549df1f619621a794846bee925b9e2360
Elizabeth Hall <elizabethhall205@gmail.com>
2016-11-26 05:02:43 +0000

    Done.

--- index.js
+++ index.js
@@ -11,7 +11,23 @@ function nowServing(katzDeliLine) {
     return `Currently serving ${katzDeliLine.shift()}.`
 
       } else { (katzDeliLine.length < 1 )
-        
+
       }
     return "There is nobody waiting to be served!"
   };
+
+function currentLine(katzDeliLine) {
+    if(katzDeliLine.length < 1 ) {
+    return "The line is currently empty."
+
+  //  var numbersNames = []
+
+  } else {
+    for(let i = 0; i < katzDeliLine.length; i ++) {
+    //([initialization]; [condition]; [iteration])
+    //[loopBody] block of code to be executed loops
+    placesNames.push(`${}. ${}`)
+    }
+
+
+ }
