962afb71a53d80877516f2e174cb5e14608ce49d
Rishi Solanki <rishi.solanki92@gmail.com>
2016-12-03 00:19:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+var katzDeli = [];
+
+// accept current line of people and new name
+// return their line number
+var takeANumber = (katzDeliLine, name) => {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+var nowServing = (katzDeliLine) => {
+  if(katzDeliLine.length > 0)
+    return `Currently serving ${katzDeliLine.shift()}.`
+  else {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+var currentLine = (katzDeliLine) => {
+  var str = "The line is currently: ";
+  if(katzDeliLine.length == 0)
+    return "The line is currently empty."
+
+  for(var i = 0; i < katzDeliLine.length - 1; i++){
+    str += `${i+1}. ${katzDeliLine[i]}, `
+  }
+  str += `${i+1}. ${katzDeliLine[i]}`
+
+  return str;
+
+
+}
