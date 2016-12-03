676d3ae51529d149e47d8d0798dcfffdf07bec8a
Kevin Adkins <kevinladkins@gmail.com>
2016-12-03 16:08:14 +0000

    Done.

--- index.js
+++ index.js
@@ -2,5 +2,28 @@ var katzDeli = []
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+      return (`Currently serving ${katzDeliLine.shift()}.`);
+    } else {
+     return "There is nobody waiting to be served!";
+   }
+  }
+
+  function currentLine(line) {
+    var lineNow = [];
+    var i;
+    i = 0
+    if (line.length > 0) {
+      while (i < line.length) {
+        lineNow.push(`${lineNow.length + 1}. ${line[i]}`)
+        i++
+      }
+      return `The line is currently: ${lineNow}`;
+    } else {
+      return "The line is currently empty."
+    }
+  }
