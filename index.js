2ef98b129ef89023faabd0c649ba124e5d8f798a
Carlota Bindner <cev.bindner@gmail.com>
2016-12-11 04:52:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return "Currently serving " + katzDeliLine.shift() + ".";
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var customerlist = []
+  if (katzDeliLine.length === 0) {
+  return 'The line is currently empty.';
+  } else {
+    for(var i = 0; i < katzDeliLine.length; i++) {
+      var linenum = i+1;
+      var name = katzDeliLine[i];
+      customerlist.push(' ' + linenum + '. ' + name);
+    }
+    return 'The line is currently:' + customerlist.toString();
+  }
+}
