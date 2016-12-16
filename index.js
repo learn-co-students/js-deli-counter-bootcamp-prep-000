3b8e8ca2701ca53e00cb3cf01e3ba0f0ad44598b
Kandis Tagliabue <kandistag@gmail.com>
2016-12-16 20:25:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+  katzDeliLine.push(`${name}`);
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
 }
 
-
-  //return the first person in line and then remove that individual from the line. 
-  //If there is nobody in line, it should return "There is nobody waiting to be served!"
-
-  function nowServing(katzDeliLine){
-    if(!katzDeliLine.length){
-      return "There is nobody waiting to be served!";
-    }
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length == 0){
+    return ("There is nobody waiting to be served!");
+  }
     return `Currently serving ${katzDeliLine.shift()}.`
 }
 
 function currentLine(line){
-  if(!line.length){
-    return "The line is currently empty.";
+  if (line.length == 0){
+    return ("The line is currently empty.");
   }
-  const numbersAndNames = [];
-
-    for (let i = 0, l = line.length; i < l; i++) {
-      numbersAndNames.push(`${i + 1}. ${line[i]}`)
-    }
-
-    return `The line is currently: ${numbersAndNames.join(', ')}`
+  var waitingQ = [];
+  for(var i=0; i<line.length; i++){
+    waitingQ.push(`${i + 1}. ${line[i]}`);
+  }
+  return `The line is currently: ${waitingQ.join(', ')}`
 }
