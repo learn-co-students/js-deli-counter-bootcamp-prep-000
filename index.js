b6f147ed6c8ebd34dbbe3dd2c390a0b036286a32
zeina sadaldeen <zeina.sadal.deen@gmail.com>
2016-12-20 14:51:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,47 +1,29 @@
-var katzDeli = []; // At the beginning of the day,
- //the deli is empty and is represented by an empty array
+'use strict';
 
+var katzDeliLine = [];
 
- function takeANumber(katzDeli, name) {
-   katzDeli.push(`${name}`);
-   return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
- }
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  var position = katzDeliLine.length;
+  return `Welcome, ${name}. You are number ${position} in line.`;
+}
 
- function takeANumber(katzDeliLine, nowServing) {		
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return `Currently serving ${katzDeliLine.splice(0, 1)}.`;
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
 
- +function nowServing(katzDeli) {
- +  let i = 0;
- +  while (i < katzDeli.length) {
- +    i++;
- +  }
- +  if (katzDeli.length === 0) {
- +    return "There is nobody waiting to be served!";
- +  }
- +  else
- +  return (`Currently serving ${katzDeli.shift()}.`);
-  }		  }
- +
- +// Build a function currentLine that returns the current line.
- +// For example, if katzDeliLine is currently ["Ada", "Grace"],
- +// currentLine(katzDeliLine) would return "The line is currently: 1. Ada 2. Grace".
- +// If there is nobody in line, it should return "The line is currently empty."
- +
- +
- +var line = [];
- +
- +
- +
- +
- +
- +function currentLine(katzDeli) {
- +  let i = 0;
- +  while (i < katzDeli.length) {
- +    line.push(` `+[i+1]+`. `  + katzDeli[i])
- +    i++;
- +  }
- +  if (katzDeli.length === 0) {
- +    return "The line is currently empty.";
- +  } else
- +  return(`The line is currently:` + line);
- +}
- +
+function currentLine(katzDeliLine) {
+  var inLine = [];
+  if (katzDeliLine.length > 0) {
+    katzDeliLine.forEach(function(name, index){
+      inLine.push(`${index + 1}. ${name}`)
+    });
+    return "The line is currently: " + inLine.join(', ');
+  } else {
+    return "The line is currently empty."
+  }
+}
