bdef0b0ed88a7ef0c48f8a441c56f42ff28eab68
Vagelis Kalimeris <aka9080@yahoo.gr>
2016-12-22 00:40:06 +0000

    Done.

--- index.js
+++ index.js
@@ -4,21 +4,25 @@ function takeANumber(array, name) {
 }
 
 function nowServing(array) {
-  if (array.length > 0)
-    result = `Currently serving ${array[0]}`
+  if (array.length > 0) {
+    var result = `Currently serving ${array[0]}.`
     array.shift()
     return result
-  else
+  }
+  else {
     return "There is nobody waiting to be served!"
+  }
 }
 
 function currentLine(array) {
-  if (array.length > 0)
-    result = []
-    for (n = 1; n < array.length, n++) {
-      result.push(`The line is currently: ${n}. ${array[n-1]}`)
-      return result
+  if (array.length > 0) {
+    var result = "The line is currently: "
+    for (var n = 1; n <= array.length; n++) {
+      result+= n + ". " + array[n-1] + ", "
     }
-  else
+    return result.substr(0, result.length - 2)
+  }
+  else {
     return "The line is currently empty."
+  }
 }
