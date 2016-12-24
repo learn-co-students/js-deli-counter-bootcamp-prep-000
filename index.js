562ecd7d00212f8fb214468829577ed4cca5e0ce
IKSHVAKU SINGAPURAM <ixs21870@ucmo.edu>
2016-12-24 21:03:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+var katzDeli=[];
+
+function takeANumber(katzDeliLine,name){
+
+katzDeliLine.push(name);
+return `Welcome, ${katzDeliLine[katzDeliLine.length-1]}. You are number ${katzDeliLine.length} in line.`;
+
+
+}
+
+function nowServing(katzDeliLine){
+
+if(katzDeliLine.length!==0){
+return `Currently serving ${katzDeliLine.shift()}.`;
+}
+return `There is nobody waiting to be served!`;
+
+}
+
+function currentLine(line){
+  let li= `The line is currently:`
+  if(line.length==0){
+
+    return "The line is currently empty.";
+  }
+
+for (let i=0;i<line.length;i++){
+
+
+  li+=` ${i+1} ${line[i]}, `;
+
+
+}
+return li;
+}
