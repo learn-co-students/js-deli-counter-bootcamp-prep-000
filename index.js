78f5529bd256f3b76c623f0920268b95d09bfb67
Derrick Chu <dchu74@gmail.com>
2016-12-29 02:50:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-var katzDeli = [];
-
-function takeANumber(katzDeliLine,"Ada"){
-
-
+function takeANumber(katzDeli,name) {
+  katzDeli.push(name);
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
 }
-takeANumber(katzDeliLine, "Ada");
+
+function nowServing(katzDeli) {
+
+  var i = 0;
+
+  while (i < katzDeli.length) {
+    i++;
+
+  }
+  if (katzDeli.length != 0) {
+    return `Currently serving ${katzDeli.shift()}.`
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
+  return katzDeli;
+}
+
+
+function currentLine(katzDeliLine){
+
+
+  var i = 0;
+  while (i < katzDeliLine.length) {
+    i++
+  }
+
+  if (katzDeliLine.length !=0) {
+    return (`The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]}`);
+  }
+  else {
+    return "The line is currently empty.";
+  }
+
+  return katzDeliLine;
+}
