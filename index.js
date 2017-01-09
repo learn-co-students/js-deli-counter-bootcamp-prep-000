a147dbd8557b889557183f5b3cb3a691178a981d
Ursh Khan <urshkhan@gmail.com>
2017-01-09 09:38:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-var katzDeli = [];
+var katzDeliLine = [];
 
-function takeANumber(katzDeliLine, name) {
-   let num = katzDeliLine.push(name);
-   return `Welcome, ${name}. You are number ${num} in line.`;
+function takeANumber(katzDeliLine, name)
+{
+  katzDeliLine.push(name)
+
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing(katzDeliLine){
-  if (katzDeliLine.length === 0) {
+
+function nowServing(line)
+{
+  if (line.length === 0)
+  {
     return "There is nobody waiting to be served!"
   }
-  let name = katzDeliLine.shift();
-  return `Currently serving ${name}.`;
+  return `Currently serving ${line.shift()}.`
 }
 
-function currentLine(katzDeliLine){
-  var line = 'The line is currently:'
-  // always declare var OUTSIDE of the loop
-  if (katzDeliLine.length === 0) {
+
+function currentLine(line)
+{
+  if (line.length === 0 ){
     return "The line is currently empty."
   }
-  else {
-    for (let i = 0; i < katzDeliLine.length; i++)
-    line += ` ${i+1}. ${katzDeliLine[i]},`
+
+  var namesAndNumbers = []
+
+  for (let i = 0; i < line.length; i++)
+  {
+    namesAndNumbers.push(`${i + 1}. ${line[i]}`)
   }
-  return line.slice(0,-1)
+
+  return `The line is currently: ${namesAndNumbers.join(', ')}`
+
 }
