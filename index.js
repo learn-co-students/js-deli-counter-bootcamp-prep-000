3a7e44f9c7a27c831256c08e6d3a842d99c8154f
John Ballon <ballonj1@tcnj.edu>
2017-01-11 01:46:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length) {
+    return `Currently serving ${katzDeliLine.shift()}.`;
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(katzDeliLine){
+  var currLine = "The line is currently: "
+    if (katzDeliLine.length) {
+      for (let i = 0; i < katzDeliLine.length - 1; i++){
+        currLine += `${i + 1}. ${katzDeliLine[i]}, `
+      }
+    } else {
+      return "The line is currently empty."
+    }
+     currLine += `${katzDeliLine.length}. ${katzDeliLine[katzDeliLine.length - 1]}`;
+     return currLine;
+}
