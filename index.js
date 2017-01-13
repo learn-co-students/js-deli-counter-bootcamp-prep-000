899b84f67a12a90799ef558697d223fe0479d1d1
Mike Tangaro <mtangaro@gmail.com>
2017-01-13 15:53:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+var katzDeli[];
+
+function takeANumber(katzDeliLine, name); {
+  katzDeliLine.push(name);
+  position = katzDeliLine.index(name);
+  console.log('Welcome, ' + name + 'you are ' + position + 'in line.');
+  return (name, position);
+}
