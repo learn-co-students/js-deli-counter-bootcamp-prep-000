1e987ace9712eec0bb55ee8d38df1270ffac726c
Mike Tangaro <mtangaro@gmail.com>
2017-01-13 17:12:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-var katzDeli[];
+var katzDeli = [];
 
-function takeANumber(katzDeliLine, name); {
+function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  position = katzDeliLine.index(name);
-  console.log('Welcome, ' + name + 'you are ' + position + 'in line.');
-  return (name, position);
+  var position = katzDeliLine.indexOf(name) + 1;
+  return ('Welcome, ' + name + '. You are number ' + (position) + ' in line.');
+}
+
+function nowServing(katzDeli) {
+  if (katzDeli.length === 0) {
+    return ('There is nobody waiting to be served!')
+  } else {
+    var name = katzDeli.shift();
+    return ('Currently serving ' + name + '.');
+  }
+}
+
+function currentLine(katzDeli) {
+  if (katzDeli.length === 0) {
+    return ('The line is currently empty.')
+  } else {
+    return ('The line is currently: 1. ' + katzDeli[0] + ', 2. ' + katzDeli[1] + ', 3. ' + katzDeli[2]);
+  }
 }
