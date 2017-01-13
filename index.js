8896f350a13fd0b6d0752b39a75755408f4dc660
Amanda Rager <mandarage@gmail.com>
2017-01-13 07:12:14 +0000

    Done.

--- index.js
+++ index.js
@@ -6,23 +6,20 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${parseInt(placeInLine) + 1} in line.`;
 };
 
-function nowServing() {
-  var first = katzDeliLine.shift();
-    if (first === undefined) {
-      return "There is nobody waiting to be served!";
-    } else {
-      return `Currently serving ${first}`;
-      katzDeliLine.shift();
-    }
-};
-
-function currentLine() {
-  if (katzDeliLine.length >= 1) {
-    var len = katzDeliLine.length;
-    var text = "The line is currently: ";
-    for (var i = 0; i < len; i++) {
-    text += `${i + 1}. ${name[i]}`;
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return `Currently serving ${katzDeliLine.shift()}.`;
   } else {
     return "There is nobody waiting to be served!";
+  }
+};
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    var len = katzDeliLine.length;
+    var text = "The line is currently: ";
+      for (var i = 0; i < len; i++) {
+      text += `${i + 1}. ${katzDeliLine[i]}`;
+  } else {
+    return "The line is currently empty.";
   }
 };
