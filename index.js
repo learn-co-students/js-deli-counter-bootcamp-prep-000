7a0cc56749ec4cc45fbd5497cf67823f6f6ddc29
John Cibula <johnjcibula@gmail.com>
2017-01-14 03:56:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function takeANumber(katzDeliLine,name){
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+  return  "There is nobody waiting to be served!";
+  }
+  /*
+  else{
+    return `Currently serving ${katzDeliLine[0]}.`;
+  }
+  */
+return katzDeliLine.shift();
+}
