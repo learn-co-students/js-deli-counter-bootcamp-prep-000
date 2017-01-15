9eb36f360324378e6b3d2f336e2bc519a8a9b4c5
Isaac Peifer <iwpeifer@gmail.com>
2017-01-15 00:08:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 var katzDeliLine = [];
 
-function takeANumber(KatzDeliLine, name){
-  katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+function takeANumber(line, name){
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+ if (katzDeliLine.length > 0) {
+    return "Currently serving " + katzDeliLine.shift(0) + ".";
+
+} else {
+  return "There is nobody waiting to be served!"
+}
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    var line = []
+    var lineL = katzDeliLine.length;
+    for (var i = 0; i < lineL; i++) {
+     line.push(" " + [i+1] + ". " + katzDeliLine[i])
+    }
+    return "The line is currently:" + line
+  } else {
+    return "The line is currently empty."
+  }
 }
