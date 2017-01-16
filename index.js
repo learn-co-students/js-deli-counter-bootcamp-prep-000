4ee8b19cff277fedb8fa0990b1b8f624b5ce9583
github username <github email address>
2017-01-16 18:34:21 -0500

    refactor

--- index.js
+++ index.js
@@ -17,10 +17,8 @@ function currentLine(katzDeliLine){
 	if (katzDeliLine.length === 0) {return "The line is currently empty."}
 	else {
 		var result = "The line is currently: " ;
-		var count = 0;
 		for (var i = 1; i <= katzDeliLine.length; i++){
-			result += `${i}. ${katzDeliLine[count]}, `
-			count++
+			result += `${i}. ${katzDeliLine[i-1]}, `
 		} return result.slice(0,result.length-2);
 	}
 
