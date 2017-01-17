c03c5e0c01827ab1ca5ceb572cf784a36eac7f03
Art Mour <artmour@gmail.com>
2017-01-17 04:25:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function takeANumber(katzDeli, name) {
+  katzDeli.push(name)
+  return `Welcome, ${name}. You are number ${katzDeli.indexOf(name)+1} in line.`
+}
+
+function currentLine(katzDeli) {
+if (katzDeli.length === 0) {
+  return "The line is currently empty."
+} else {
+  var myString = "The line is currently:"
+  for (let i=0; i<katzDeli.length-1; i++) {
+    myString = myString + ` ${i+1}. ${katzDeli[i]},`
+    }
+  }
+  return myString + ` ${katzDeli.length}. ${katzDeli[katzDeli.length-1]}`
+}
+
+function nowServing(katzDeli) {
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var currentlyServing = katzDeli[0]
+    katzDeli.shift()
+    return `Currently serving ${currentlyServing}.`
+  }
+}
