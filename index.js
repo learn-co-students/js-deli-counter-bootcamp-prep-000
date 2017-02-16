da77c6828235c6f08b58f1bcc565137e0a4aed29
Matthew Buckner <bucknermr@gmail.com>
2017-02-16 04:53:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return `Currently serving ${katzDeliLine.shift()}.`
+  } else {
+    return "There is nobody waiting to be served!"}
+}
+
+function currentLine(katzDeliLine) {
+  let lineArray = []
+  if (katzDeliLine.length > 0) {
+    for (let n = 0; n < katzDeliLine.length; n += 1) {
+      lineArray.push(` ${n + 1}. ${katzDeliLine[n]}`)
+    }
+    return `The line is currently:${lineArray}`
+  } else {
+    return "The line is currently empty."
+  }
+}
