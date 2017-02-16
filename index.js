907f2fee4d7ada05b40cde648476f04870194dba
Kyle Andrews <kylemandrews@gmail.com>
2017-02-16 04:17:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(deliLine) {
+  var newPerson = deliLine.shift()
+  if  (deliLine.length > 0) {
+    return `Currently serving ${newPerson}.`
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(line) {
+  if (line.length > 0) {
+    var lines = ''
+    for ( var i = 0; i < line.length ; i++) {
+      if (i < line.length -1){
+        var lineOrder = `${i + 1}. ${line[i]}, `
+      }
+      else { 
+        var lineOrder = `${i + 1}. ${line[i]}`
+      }
+      lines = lines + lineOrder
+}  return (`The line is currently: ${lines}`)
+}
+else {
+  return "The line is currently empty."
+  }
+}
