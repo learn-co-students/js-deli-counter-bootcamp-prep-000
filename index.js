920ed1f10b99ed54965cf7173cb309858af4e7cd
Hersh Patel <hershe.91@gmail.com>
2017-02-20 21:44:22 +0000

    Done.

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ function takeANumber(katzDeli,name) {
   return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
 }
 
-function nowServing (katzDeli) {
-  var number = katzDeli.length
-  if(number>0) {
+function nowServing(katzDeli) {
+  var l = katzDeli.length
+  if(l>0) {
     return `Currently serving ${katzDeli.shift()}.`
   }
   else {
-    return "There is nobody waiting to be served!"
+    return `There is nobody waiting to be served!`
+  }
+  }
+
+  function currentLine(katzDeli) {
+    var l = katzDeli.length
+    var array = []
+    if(l>0) {
+      for(var i=0;i<l;i++) {
+        array.push(` ${i+1}. ${katzDeli[i]}`)
+      }
+      return `The line is currently:${array}`
+    }  
+  else {
+    return `The line is currently empty.`
   }
 }
-
-function currentLine(katzDeli) {
-  var number = katzDeli.length
-  var array = []
-  if(number>0) {
-    for(var i=0;i<number;i++) {
-      array.push(` ${i+1}. ${katzDeli[i]}`)}
-      return `The line is currently:${array}`
-    }
-    else {
-      return "The line is currently empty."
-    }
-  }
-  
