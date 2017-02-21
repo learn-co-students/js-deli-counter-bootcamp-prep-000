a46daeafba8c7c8d2bd0365995e7ca1ea79e9a6f
Max Fortis <max.r.fortis@gmail.com>
2017-02-21 17:21:53 +0000

    Done.

--- index.js
+++ index.js
@@ -3,20 +3,34 @@ function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
-function nowServing(){
-  if (katzDeliLine.length > 0){
-    var katz = katzDeliLine.slice(0, 1);
-    return "Currently Serving " + katz + "."
+function nowServing(line){
+  if (line.length > 0){
+    var katz = line.shift();
+    return "Currently serving " + katz + "."
 
   }
-else if (katzDeliLine.length === 0){
-  return "There is nobody waiting to be served!"
+else {
+  if (line.length === 0){
+    return "There is nobody waiting to be served!"}
+}
 }
 
 
-}
 function currentLine(line) {
   if(line.length === 0) {
     return "The line is currently empty."
   }
+  else {
+    var statement = "The line is currently: "
+    for (var i = 0; i < line.length; i++){
+
+      if (i === line.length - 1){
+         statement +=   [i + 1] + ". " + line[i] }
+        else{
+
+       statement += [i + 1] + ". " + line[i] + ", "}
+    }
+  }
+  return statement
+
 }
