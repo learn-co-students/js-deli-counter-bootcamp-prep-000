b0db64ff8bf5512b09b39757d367f06c896015d3
Andrea Graham <andrea.f.graham@gmail.com>
2017-02-23 17:04:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-var katzDeli = []; // beginning of the day when line is empty
+var katzDeliLine = []; // With names of individuals in line
 
 function takeANumber(katzDeliLine, name){ //accepts the current line of people
     katzDeliLine.push(name); //adds name to the end of the array
     return "Welcome, ${name},  You are number ${katzDeliLine.length} in line."
 };
 
-function nowServing(array) { //should return the first person in line and then remove that individual from line
-  if(array.length > 1){
-    return "Currently serving ${line.shift()}." //returns the removed element. In this case it would be the person being served
+function nowServing(katzDeliLine) { //should return the first person in line and then remove that individual from line
+  if (katzDeliLine.length > 0) {
+    return "Currently serving" + ${katzDeliLine.shift()} + "." //returns the removed element. In this case it would be the person being served
   } else {
     return "There is nobody waiting to be served!" //if no one is in line
   }
 };
 
-function currentLine(array){ //returns the current line.
-  if (array.length > 1) {  //.length returns the number of characters left in a string
-    for
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length > 0) { //if the line is greater than 0
+    const katzDeliLineAndNumbers = []
+    for (let i=0, l = katzDeliLine.length; i<l; i++) { // for loop
+      katzDeliLineAndNumbers.push(`${i + 1}. ${katzDeliLine[i]}`) // the i+1 is purely to start counting at 1 rather than counting at 0.
+      //And at the end here, we'll see the the number "pushed" before the line name
+    }
+    return "The line is currently: " + ${katzDeliLineAndNumbers.join(", ")}
+  } else {
+    return "The line is currently empty";
   }
-
-const numbersAndNames =[]
-
-  for (let i=0, i < array.length; i++) { // for loop
-    array[i] = ' ${i + 1}. ' + array[i];
-  }
-
-return "the line is currently: ${array}"; //joins all elements of the array into a string
-} else return "The line is currently empty."; // if no one is in line
 }
