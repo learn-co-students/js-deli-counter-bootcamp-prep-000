2a09d0749588d5b7b51d52fc69e2f84e4ca3e8d2
Andrea Graham <andrea.f.graham@gmail.com>
2017-02-23 15:15:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+function takeANumber(line, name){ //accepts the current line of people
+    line.push(name); //adds name to the end of the array
+    return "Welcome, ${name},  You are number ${line.length} in line."
+};
+
+function nowServing(line) { //should return the first person in line and then remove that individual from line
+  if(!line.length){
+    return "There is nobody waiting to be served!" //if no one is in line
+  }
+  return "Currently serving ${line.shift()}." //returns the removed element. In this case it would be the person being served
+};
+
 function currentLine(line){ //returns the current line.
   if (!line.length) {  //.length returns the number of characters left in a string
     return "The line is currently empty." // if no one is in line
@@ -10,16 +22,4 @@ const numbersAndNames =[]
   }
 
 return "the line is currently : ${numbersAndNames.join(", ")}" //joins all elements of the array into a string
-};
-
-function nowServing(line) { //should return the first person in line and then remove that individual from line
-  if(!line.length){
-    return "There is nobody waiting to be served!" //if no one is in line
-  }
-  return "Currently serving ${line.shift()}." //returns the removed element. In this case it would be the person being served
-};
-
-function takeANumber(line, name){ //accepts the current line of people
-    line.push(name); //adds name to the end of the array 
-    return "Welcome, ${name},  You are number ${line.length} in line."
 };
