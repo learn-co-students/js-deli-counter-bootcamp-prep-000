98130d00b7d8df0d59f53cea25bf9f477c10fec2
Andrea Graham <andrea.f.graham@gmail.com>
2017-02-23 14:31:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-function currentLine(line){
-  if (!line.length) {
-    return "The line is currently empty."
+function currentLine(line){ //returns the current line.
+  if (!line.length) {  //.length returns the number of characters left in a string
+    return "The line is currently empty." // if no one is in line
   }
 
 const numbersAndNames =[]
 
-  for (let i=0, l=line.length; i,l; i++){
+  for (let i=0, l=line.length; i,l; i++){ // for loop
     numbersAndNames.push("${i+1}, ${line[i]}")
   }
 
-return "the line is currently : ${numbersAndNames.join(", ")}"
+return "the line is currently : ${numbersAndNames.join(", ")}" //joins all elements of the array into a string
 };
 
-function nowServing(line) {
+function nowServing(line) { //should return the first person in line and then remove that individual from line
   if(!line.length){
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!" //if no one is in line
   }
-  return "Currently serving ${line.shift()}."
+  return "Currently serving ${line.shift()}." //returns the removed element. In this case it would be the person being served
 };
 
-function takeANumber(line, name){
-    line.push(name);
+function takeANumber(line, name){ //accepts the current line of people
+    line.push(name); //adds name to the end of the array 
     return "Welcome, ${name},  You are number ${line.length} in line."
 };
