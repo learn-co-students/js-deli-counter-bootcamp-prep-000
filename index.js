e66126c3f67e28e110a361a16cd60763ffec5573
Leah Tabush <leahtabush@gmail.com>
2017-02-26 03:39:10 +0000

    Done.

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ function takeANumber (line, name) {
   line.push(name);
   var position = line.indexOf(name) + 1;
   return "Welcome, " + name + ". You are number " + position + " in line.";
-}
+};
 
 function nowServing(line) {
-  // if (line.length > 0) {
-  // return "Currently serving " + katzDeliLine[0] + ".";
-  // line.splice(0, 1);
-  // } else {
-  //   return "There is nobody waiting to be served!";
-  // }
-  if (!line.length) {
+  if (line.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-    return "Currently serving " + line.shift();
+    return `Currently serving ${line.shift()}.`;
   }
-}
+};
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    var numbersAndNames = [];
+    for (let i = 0; i < line.length; i++) {
+      numbersAndNames.push(`${i + 1}. ${line[i]}`);
+    }
+    return `The line is currently: ${numbersAndNames.join(", ")}`;
+  }
+};
