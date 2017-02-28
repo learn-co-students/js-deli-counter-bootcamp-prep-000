be9b13b3d1021ecb8d67d7f0d008ab01ff938fa0
Saaeedur Rahman <rahman.saaeed@gmail.com>
2017-02-28 18:35:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+var katzDeli = [];
+
+function takeANumber(katzDeli, name) {
+  katzDeli.push(name)
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
+}
+
+function nowServing(katzDeli) {
+  if (katzDeli.length == 0) {
+    return "There is nobody waiting to be served!"
+  } else return `Currently serving ${katzDeli.shift()}.`
+  }
+
+function currentLine(line) {
+  var i = 0
+  if (line.length == 0) {
+    return "The line is currently empty."
+  } else return `The line is currently: 1. ${line[0]}, 2. ${line[1]}, 3. ${line[2]}`
+}
