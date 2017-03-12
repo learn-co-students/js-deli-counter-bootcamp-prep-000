7a6a3f4e9cbc9a7bae93c884fac0998a72d3bc62
Tzvi Hamerman <16guitar@gmail.com>
2017-03-12 00:47:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name)  {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!';
+  } else {
+    var removed = katzDeliLine.shift();
+    return `Currently serving ${removed}.`;
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'The line is currently empty.';
+  } else {
+    var lineStatus = 'The line is currently:';
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      if (i !== katzDeliLine.length - 1) {
+        lineStatus += ` ${i + 1}. ${katzDeliLine[i]},`;
+      } else {
+        lineStatus += ` ${i + 1}. ${katzDeliLine[i]}`;
+      }
+    }
+    return lineStatus;
+  }
+}
