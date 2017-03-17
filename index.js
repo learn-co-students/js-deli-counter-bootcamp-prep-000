f1e77df8aa5801f999117f75a3c04ee14281dbf8
Mollie Stein <mollie.stein@gmail.com>
2017-03-17 01:45:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine,name) {
+  katzDeliLine.push(name)
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+}
+
+function nowServing(katzDeliLine) {
+
+  if (katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!";
+  } else if (katzDeliLine.length > 0){
+  var first = katzDeliLine[0]
+  katzDeliLine.shift();
+  return "Currently serving " + first + "."
+}
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0){
+    return "The line is currently empty."
+  } else if (katzDeliLine.length > 0){
+    var deliString = "The line is currently:"
+    for (var i=1;i<=katzDeliLine.length;i++) {
+      deliString = deliString + " " + i + ". " + katzDeliLine[i-1]+ ","
+    }
+    deliString.remove(deliString.length)
+    return deliString
+  }
+}
