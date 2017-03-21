b212b8a09e513ec6f92c16ef7083c4ee5d64b892
Justin Kahn <jdavidkahn@gmail.com>
2017-03-21 23:03:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function takeANumber(katzDeliLine, name){
+  var index=katzDeliLine.length;
+  var  position=index +1;
+  return position;
+}
+
+function nowServing(katzDeliLine){
+
+}
+
+function currentLine(katzDeliLine){
+
+}
