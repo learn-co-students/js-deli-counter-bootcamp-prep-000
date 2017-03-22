13d5fadc6fc7a560927cb1b2049deb6b9e5a79ad
David Tomczyk <davidjtomczyk@gmail.com>
2017-03-22 01:46:51 +0000

    Done.

--- index.js
+++ index.js
@@ -23,12 +23,10 @@ function currentLine(line) {
 
   if (line.length > 0){
     for (var i = 0, l = line.length; i<l; i++){
-      lineString += ` ${parseInt(i)+1}. ${line[i]},`;
+      lineString += ` ${i+1}. ${line[i]},`;
     };
-  } else if (line.length === 0){
-    return "The line is currently empty."
   } else {
-    return "Something went very wrong."
+    return "The line is currently empty."
   };
 
   return lineString.slice(0,lineString.length-1);
