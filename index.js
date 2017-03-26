fc59d9da07ef7e3f73e3a86dbab2d2114b9b29ab
Nicholas D'Amico <nickalan82@icloud.com>
2017-03-26 12:57:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+const katzDeli = [];
+
+function takeANumber( katzDeliLine, name ) {
+    // Push new Customer to DeliLine array
+    katzDeliLine.push(name);
+    // Return msg to customer on their position in line, using .length
+    return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing( katzDeliLine ) {
+
+  return katzDeliLine.length === 0 ? "There is nobody waiting to be served!" : `Currently serving ${katzDeliLine.splice(0,1)}.`;
+}
+
+function currentLine( katzDeliLine ) {
+  // declare string variable for return.
+  let string;
+  // Check if array is empty and return msg if it is.
+  if( katzDeliLine.length === 0 ) {
+    return "The line is currently empty.";
+  } else {
+    // Build String with opening statement
+    string = `The line is currently:`;
+    // for loop through array and build msg.
+    for( let i = 0; i < katzDeliLine.length; i++ ) {
+      // Ternary coniditon checks if we are at the end of the array, if so. It will not append a comma to the end of the msg.
+      katzDeliLine.length - 1 === i ? string += ` ${i+1}. ${katzDeliLine[i]}` : string += ` ${i+1}. ${katzDeliLine[i]},`;
+    }
+  }
+  // Returns finished built msg.
+  return string;
+
+}
