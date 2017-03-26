21fefeb40c9b5b26efb809a3931d79efa7897cc6
Nicholas D'Amico <nickalan82@icloud.com>
2017-03-26 15:39:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-function takeANumber( katzDeliLine, name ) {
+function takeANumber( DeliLine, name ) {
 
-    katzDeliLine.push(name);
+    DeliLine.push(name);
 
-    return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+    return `Welcome, ${name}. You are number ${DeliLine.length} in line.`
 }
 
-function nowServing( katzDeliLine ) {
+function nowServing( DeliLine ) {
 
-  return katzDeliLine.length === 0 ? "There is nobody waiting to be served!" : `Currently serving ${katzDeliLine.splice(0,1)}.`;
+  return DeliLine.length === 0 ? "There is nobody waiting to be served!" : `Currently serving ${DeliLine.splice(0,1)}.`;
 }
 
-function currentLine( katzDeliLine ) {
-  // declare string variable for return.
+function currentLine( DeliLine ) {
+  // declare string variable for return msg.
   let string;
   // Check if array is empty and return msg if it is.
-  if( katzDeliLine.length === 0 ) {
+  if( DeliLine.length === 0 ) {
     return "The line is currently empty.";
   } else {
     // Build String with opening statement
     string = `The line is currently:`;
     // for loop through array and build msg.
-    for( let i = 0; i < katzDeliLine.length; i++ ) {
+    for( let i = 0; i < DeliLine.length; i++ ) {
       // Ternary coniditon checks if we are at the end of the array, if so. It will not append a comma to the end of the msg.
-      katzDeliLine.length - 1 === i ? string += ` ${i+1}. ${katzDeliLine[i]}` : string += ` ${i+1}. ${katzDeliLine[i]},`;
+      DeliLine.length - 1 === i ? string += ` ${i + 1}. ${DeliLine[i]}` : string += ` ${i + 1}. ${DeliLine[i]},`;
     }
   }
   // Returns finished built msg.
