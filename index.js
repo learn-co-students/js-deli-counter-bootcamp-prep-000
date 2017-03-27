a065bbb732ba0c269e18db4da50926e6048de6a6
Jeanne Erickson Cooley <cool0020@gmail.com>
2017-03-27 02:05:54 +0000

    Done.

--- index.js
+++ index.js
@@ -14,53 +14,27 @@ function nowServing(katzDeli) {
     }
 }
 
-function currentLIne(array) {
-    if (array.length === 0) {
-        return "The line is currently empty";
+function currentLine(array) {
+    // returns current line
+    // if array is empty, return "The line is currently empty."
+    // if array is populated, return "The line is currently: "
+        // if 1: "1. " {name}
+        // if more than 1: "1. " {name}, "2. " {name}
+
+    if (array.length < 1) {
+        return "The line is currently empty.";
+    } else if (array.length === 1) {
+        return "The line is currently: 1. " + array[0];
+
     } else {
-        for (var i = 1; i < array.length; i++) {
-            return "The line is currently: " + i + ". ";
+        var leng = array.length;
+        var text = "The line is currently: ";
+        // loop through array
+        for (var i = 0; i < (leng - 1); i++) {
+            // var name = array[i];
+            // return "The line is currently: 1. " + array[0] + ", " + (i + 1) + ". " + name + ", ";
+            text += ((i + 1) + ". " + array[i] + ", ");
         }
+        return text + ((leng) + ". " + array[leng-1]);
     }
 }
-
-// var i = 0;
-// function currentLine(katzDeliLine) {
-//     // for array taken in,
-//     // returns all of the elements in the array, test if array is empty,
-//         // if array is not empty
-//             // if first element in array, no comma at the beginning, go to next condition
-//             // if subsequent, comma at beginning, go back through loop
-//         // until array is empty
-//             // different message
-//     if (katzDeliLine.length > 0) {
-//         // moved var i = 0; outside of function
-//         // if more
-//         if (katzDeliLine.length > 1) {
-//             i++;
-//             return "I need to figure this out" + [i];
-//         } else if (katzDeliLine.length == 1) {
-//
-//             return "The line is currently: 1. " + katzDeliLine[i];
-//         } else {
-//             return "The line is currently empty.";
-//         }
-//     }
-// }
-//         // if 1 element
-//
-//         // if no elements
-//
-//     //     while (i < katzDeliLine.length) {
-//     //
-//     //         // i++;
-//     //         // return text + i + ". " + katzDeliLine[i -1] + ", ";
-//     //
-//     //         if (i = 0; i < katzDeliLine.length; i++) {
-//     //             return ("The line is currently: " + i + ". " + katzDeliLine[i]);
-//     //         }
-//     //
-//     //     } else {
-//     //         return "The line is currently empty.";
-//     //     }
-//     // }
