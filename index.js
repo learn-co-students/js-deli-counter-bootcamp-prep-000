55b01d8dfb7280e62ea635d1938d3811e6b6b32e
Joel Schaeffer <joel.schaeffer@gmail.com>
2017-03-27 03:24:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+var katzDeli = []
+
+function takeANumber(katzDeliLine,name)
+{
+  var lineNumber = katzDeliLine+1   
+  var response = `Welcome, ${name}. You are number ${lineNumber} in line.`
+  katzDeli.push(name)
+
+  return response
+  return katzDeli
+}
