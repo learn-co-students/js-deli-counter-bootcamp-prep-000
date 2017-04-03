134799b4daedf8fc6f2dc44870fb92022d6eb9c7
Venessa  Stewart <stewart.venessa@gmail.com>
2017-04-03 02:02:40 +0000

    Done.

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ function nowServing (katzDeliLine) {
      return "There is nobody waiting to be served!";
   }
 }
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  }
+  else {
+    let list = "The line is currently: ";
+    for (let i = 0; i < katzDeliLine.length; i++) {
+      list = list + `${i + 1}. ${katzDeliLine[i]}, `;
+    }
+    return list.slice(0, list.length - 2);
+  }
+}
