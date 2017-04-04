5285a3186ef96003acf6dbad8b34261cd9f3c211
Justin Lee <leejwg.3@gmail.com>
2017-04-04 16:31:24 +0000

    Done.

--- index.js
+++ index.js
@@ -7,18 +7,48 @@
 
 function takeANumber(katzDeliLine, name){
     katzDeliLine.push(name);
-    return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line. "
+    return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
 
 //return person being served then remove from line
 function nowServing(katzDeliLine){
-    if(katzDeliLine.length >= 0){
-    katzDeliLine.shift(katzDeliLine);
-    return katzDeliLine;
-}}
+  var outPutServing = "Currently serving "
+    if(katzDeliLine.length > 0){
+      return outPutServing + katzDeliLine.shift() + ".";
+} else{
+  return 'There is nobody waiting to be served!'
+ }
+}
 
 // returns current line , if empty return msg
 function currentLine(katzDeliLine){
-    for(i = 0; i < katzDeliLine.length; i++)
-    return `the current line is ${i+1} ${katzDeliLine[i]}`
+
+  var output = "The line is currently: ";
+  var arrayOfStrings = [];
+
+
+      if(katzDeliLine.length === 0){
+        return "The line is currently empty."
+      }
+
+    for(var i = 0; i < katzDeliLine.length; i++){
+
+      arrayOfStrings.push(`${i+1}. ${katzDeliLine[i]}`)
+
+    }
+    //debugger
+
+    return  output + arrayOfStrings.join(", ")  //method2:
+    //return arrayOfStrings joined together with commas
 }
+
+// function lookForDeadbeat(allDaGuys){
+//   var foundHim = false;
+//   for (var i = 0; i < =allDaGuys.length; i++){
+//     if (allDaGuys[i] == "steve"){
+//       foundHim = true;
+//       return foundHim;
+//     }
+//   }
+//   return foundHim;
+// }
