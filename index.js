93ffb5814d276103f460e127fc24643fadf17087
Rena Sha <linkena@gmail.com>
2017-04-09 20:08:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var katzDeli=[];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length } in line.`;
+
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length===0){
+    return "There is nobody waiting to be served!";
+  }else{
+    //var person = katzDeliLine.pop();
+    return `Currently serving ${katzDeliLine.shift()}.`;
+  }
+}
+
+function currentLine(line){
+  if(line.length===0){
+    return "The line is currently empty."
+  }else{
+    var linedesc = "The line is currently: "
+    for(var i=0;i<line.length;i++){
+      linedesc = linedesc + `${i+1}. ${line[i]}${(i+1)===line.length ? '':', '}`
+      
+    }
+    return linedesc;
+  }
+}
