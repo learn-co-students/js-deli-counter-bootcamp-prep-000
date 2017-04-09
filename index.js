bb19686352c1b97588a2d4e3373e81be366aed26
Chinmay Lonkar <clonkified@gmail.com>
2017-04-09 23:25:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function takeANumber(katzDeliLine, newPerson) {
+  katzDeliLine.push(newPerson);
+  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  }
+  var line = "The line is currently: ";
+  for(var i = 0; i < katzDeliLine.length; i++) {
+    line += `${i+1}. ${katzDeliLine[i]}`;
+    if(i < katzDeliLine.length - 1) {
+      line += ", ";
+    }
+  }
+  return line;
+}
+
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  var announcement = `Currently serving ${katzDeliLine[0]}.`;
+  katzDeliLine.shift();
+  return announcement;
+}
