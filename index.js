6973cf1332e0df4cc27cebaca5002d49e0fa2df3
Alejandra Campero <alejandracamperovega@gmail.com>
2017-04-17 02:02:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeliLine = []
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+    if (katzDeliLine.length === 0) {
+        return "There is nobody waiting to be served!"
+    } else {
+		return `Currently serving ${katzDeliLine.shift()}.`
+    }
+}
+
+function currentLine(katzDeliLine) {
+	if(katzDeliLine.length === 0) {
+		return "The line is currently empty."
+    }
+
+var listOfPeople = [];
+
+for (let i = 0; i < katzDeliLine.length; i++) {
+	listOfPeople.push(`${i + 1}. ${katzDeliLine[i]}`)
+}
+
+return `The line is currently: ${listOfPeople.join(", ")}`
+}
