fd43581de1208e28354dcdee846d4cf76b7d452b
Jin Cheng <jincheng88@outlook.com>
2017-04-23 12:53:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var katzDeli = [];
+function takeANumber(deli,name){
+  deli.push(name);
+  return `Welcome, ${name}. You are number ${deli.length} in line.`;
+}
+function nowServing(deli){
+  if(deli.length == 0){
+    return "There is nobody waiting to be served!";
+  }else{
+    var serving = deli[0];
+    deli.shift();
+    return `Currently serving ${serving}.`;
+  }
+}
+function currentLine(line){
+  if(line.length == 0){
+    return "The line is currently empty."
+  }else{
+    var list = "The line is currently:";
+    for(var i = 0; i<line.length; i++){
+      list = list + line[i];
+    }
+    return list;
+}
