5a1714a929b2496f75ac860fddd7da41f63fcb56
Douglas Horne <dough370@gmail.com>
2017-04-25 03:58:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 var katzDeliLine = [];
-
-//should accept the current line of people and person's name
 function takeANumber(katzDeliLine,name) {
   katzDeliLine.push(name)
   return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
-  //return person's posiiton in line, not index!
-  //console.log(katzDeliLine);
 }
 
 function nowServing(katzDeliLine) {
   if(katzDeliLine.length > 0) {
-    ("Currently serving " + katzDeliLine.shift() + ".")
+    return ("Currently serving " + katzDeliLine.shift() + ".")
   } else {
     return ("There is nobody waiting to be served!")
   }
-
-  //Display "There is nobody waiting to be served!" if noone is in line
 }
+
+//Need to review this one
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  }
+  var remainingLine = []
+  for (let i = 0, l = katzDeliLine.length; i < l; i++) {
+    remainingLine.push(`${i + 1}. ${katzDeliLine[i]}`)
+  }
+  return `The line is currently: ${remainingLine.join(', ')}`
+};
