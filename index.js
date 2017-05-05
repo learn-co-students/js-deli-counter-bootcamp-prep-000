95c8e364361acf80089539b742234dd7ea87b224
Jennifer Watson <jennifer.watson@me.com>
2017-05-05 20:40:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var katzDeli = [];
+var otherDeli = [];
+
+function takeANumber(katzDeli,name) {
+  while (katzDeli.length > 0) {
+    otherDeli.push(name);
+  var position = otherDeli.length;
+  return `Welcome, ${name}. You are number ${position} in line.`;
+  }
+}
+
+function nowServing(deliLine){
+  for (var i=0; i < (line.length -1); i++) {
+  var name = otherDeli[i];
+  return `Currently serving ${name}.`;
+  }
+}
+
+function currentLine(line){
+  if(line.length > 0){
+    for (var i = 0; i <= (line.length -1); i++){
+      var position = i + 1;
+      currentLine = [...currentLine,`${position}. ${line[i]}`];
+    }
+    return "The line is currently: " + currentLine;
+  } else {
+    return "The line is currently empty."
+  }
+}
