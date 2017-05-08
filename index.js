42c601c51b390e2c7100468529832978a04e04dc
Jennifer Watson <jennifer.watson@me.com>
2017-05-08 13:53:22 +0000

    Done.

--- index.js
+++ index.js
@@ -9,35 +9,23 @@ function takeANumber(katzDeli,name) {
   }
 }
 
-<<<<<<< HEAD
 function nowServing(deliLine){
+  if (otherDeli.length > 1) {
   for (var i=0; i < (deliLine.length -1); i++) {
   var name = deliLine[i];
+  deliLine.shift();
   return `Currently serving ${name}.`;
-  }
-=======
-function nowServing(otherDeli){
-  if (otherDeli.length > 1) {
-    for (var i=0; i < (otherDeli.length -1); i++) {
-      var name = otherDeli[i];
-      otherDeli.shift();
-      return `Currently serving ${name}.`;
-        }
-      } else {
-      return 'There is nobody waiting to be served!'
     }
->>>>>>> 120f4fbbe821d9e80cbe0950e082a614967fa395
+  } else {
+    return 'There is nobody waiting to be served!'
+  } 
 }
 
 function currentLine(otherDeli){
   if(otherDeli.length > 0){
     for (var i = 0; i <= (otherDeli.length - 1); i++){
       var position = i + 1;
-<<<<<<< HEAD
-      currentLine = [...currentLine,` ${position}. ${line[i]}`];
-=======
       var currentLine = [...currentLine,` ${position}. ${otherDeli[i]}`];
->>>>>>> 120f4fbbe821d9e80cbe0950e082a614967fa395
     }
     return `The line is currently: ${currentLine}`;
   } else {
