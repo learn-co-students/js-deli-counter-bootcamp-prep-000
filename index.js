21c3862d4218c024009a157b025bcc9545c0fe18
Arthur Miguel <retiredinfantry80@outlook.com>
2017-05-10 22:47:04 +0000

    Done.

--- index.js
+++ index.js
@@ -5,22 +5,30 @@ function takeANumber(katzDeli, name){
   return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
 }
 
-function nowServing(katzDeli, name){
+function nowServing(katzDeli){
+  //var i = 0;
   if(katzDeli < 1){
     return "There is nobody waiting to be served!"
   }else{
-    katzDeli.push(name);
+    // ['Steven', 'Blake', 'Avi']
+    var name = katzDeli[0];
+    katzDeli.shift()
     return `Currently serving ${name}.`
-    katzDeli.shift(name);
+    //katzDeli.shift[0];
   }
-  return katzDeli
 }
 
 function currentLine(line){
-  if (katzDeli < 1){
+  if (line.length < 1){
     return "The line is currently empty."
   }else{
-    return `The line is currently ${katzDeli.length}. ${name}`
+    var array = []
+    for (var i = 0; i < line.length; i++) {
+      var name = line[i] // "Bill"
+      var l = i + 1 // 1
+      array.push(`${l}. ${name}`)
+    }
+    // "1. Bill, 2. Jane, 3. Ann"
+    return `The line is currently: ${array.join(', ')}`
   }
-  return katzDeli
 }
