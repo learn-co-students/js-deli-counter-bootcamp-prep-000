90252ae74a624182ec4351aa0970d76300560349
Kenny Lin <kenny.lin.91@gmail.com>
2017-05-15 18:28:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 var katzDeliLine = [];
 
 function takeANumber(array, name) {
-  array.push(name)
-  return `Welcome, ${name}. You are number ${array.length} in line.`
+  array.push(name);
+  return `Welcome, ${name}. You are number ${array.length} in line.`;
 }
 
 
 function nowServing(array) {
   if (array.length > 0) {
-    var person = array.shift()
-    return `Currently serving ${person}.`
+    return `Currently serving ${array.shift()}.`;
   } else {
-    return 'There is nobody waiting to be served!'
+    return 'There is nobody waiting to be served!';
   }
 }
 
 function currentLine(array) {
   if (array.length === 0) {
-    return "The line is currently empty."
+    return "The line is currently empty.";
   }
   var positions = [];
   for(var i=0; i < array.length; i++) {
-    positions.push(` ${i+1}. ${array[i]}`)
+    positions.push(` ${i+1}. ${array[i]}`);
   }
-  return `The line is currently:${positions}`
+  return `The line is currently:${positions}`;
 }
