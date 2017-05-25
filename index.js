4f27649f2f61b9bca7ff053ff2541e6714e726b0
elle lee <eelzil77@gmail.com>
2017-05-25 05:04:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,6 @@
+var katzDeli = [];
+function takeANumber(katzDeliLine, name){
+  var position = katzDeliLine.length+1
+  katzDeliLine.push(name);
+  return "Welcome, "+ name +". You are number " + position + "in line."
+}
