2c87db2ecd49fe25cff716a8df1028a65b8f9b13
Felipe Valencia <jfvalencia@gmail.com>
2017-06-14 23:37:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 var katzDeli = [];
 
-function takeANumber(katzDeliLine, person) {
-  for (var i = 0; i < katzDeliLine.length; i++) {
-    katzDeli[i] = katzDeliLine[i]
-    return `Welcome, ${person}. You are number ${i} in line.`
+function takeANumber(katzDeli, person) {
+  for (var i = 0; i <= katzDeli.length; i++) {
+    katzDeli[i] = person
+    var placeInLine = i + 1
+    return `Welcome, ${person}. You are number ${placeInLine} in line.`
   }
 }
+
+function nowServing() {
+  if (katzDeli.length < 1) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    return `Currently serving ${katzDeli[0]}.`
+  }
+}
+
+function currentLine() {
+  if (katzDeli.length < 1) {
+    return 'The line is currently empty.'
+  } else {
+    return `The line is currently: ${katzDeli[0]}`
+  }
+}
+/*
+function theBeatlesPlay (musicians, instruments) {
+  var whoPlaysWhat = [];
+  for (var i = 0; i < musicians.length; i++) {
+    whoPlaysWhat[i] = (`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return whoPlaysWhat
+}
+*/
