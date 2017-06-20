0036a513f9c0c1a107abf46782098bffbac4b986
Joanna Sese <jmgsese@gmail.com>
2017-06-20 04:30:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function takeANumber(katzDeliLine,newPerson){
+  // return person's position in line
+  katzDeliLine.push(newPerson)
+  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine[0]===undefined){
+    return 'There is nobody waiting to be served!'
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+}
+
+function currentLine(katzDeliLine){
+  if (katzDeliLine[0]===undefined){
+    return 'The line is currently empty.'
+  } else {
+    // for (let i=0; i<katzDeliLine.length; i++){
+    return `The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]}`
+  }
+}
