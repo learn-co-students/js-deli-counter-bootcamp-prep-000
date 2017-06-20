6143bee7aaeeae4ca7f0724ec011e6273b0652bd
Joanna Sese <jmgsese@gmail.com>
2017-06-20 05:01:00 +0000

    Done.

--- index.js
+++ index.js
@@ -12,11 +12,15 @@ function nowServing(katzDeliLine){
   }
 }
 
-function currentLine(katzDeliLine){
+function currentLine(katzDeliLine
+  var longLine = [];
   if (katzDeliLine[0]===undefined){
     return 'The line is currently empty.'
   } else {
-    // for (let i=0; i<katzDeliLine.length; i++){
-    return `The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]}`
+    for (let i=0; i<katzDeliLine.length; i++){
+      longLine.push(${i+1}. ${katzDeliLine[i]})
+    }
+    return `The line is currently: ${longLine.join(', ')}`
   }
 }
+    // return `The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]}`
