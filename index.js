3d66562b56708e87886e761b453e494c519cbddf
Sophia L <soph915@gmail.com>
2017-06-28 15:10:04 +0000

    Done.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function nowServing(katzDeliLine){
 function currentLine(line){
   var loop = []
   if (line.length === 0){
-       return "The line is currently empty."}
+       return "The line is currently empty. "}
   else for(var i=0; i<line.length; i++){
     loop.push(`${i+1}. ${line[i]}`)}
     return `The line is currently: ${loop.join(', ')}`
