446feba79d051ab920c79f5ab4d1ae9372c6ea3a
Mohamad Khayat <moek91@outlook.com>
2017-07-02 15:51:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-var someVariable = 7;
+var katzDeliLine =[] ;
+
+function takeANumber(customername, katzDeliLine){
+customername.push(katzDeliLine)
+return `Welcome, ${customername}, You are number in line.`
+}
