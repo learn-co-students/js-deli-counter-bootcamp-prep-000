5965aa0db7a023838dc81157fa9cb14362100cfe
Mohamad Khayat <moek91@outlook.com>
2017-07-05 01:06:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var katzDeliLine =[] ;
 
-function takeANumber(customername, katzDeliLine){
-katzDeliLine.push(customername)
-return `Welcome, ${customername.push}, You are number in line.`
+function takeANumber(katzDeliLine, customername){
+  katzDeliLine.push (customername)
+  return `Welcome, ${customername}. You are number ${katzDeliLine.length} in line.`
 }
+
+
+//function nowServing (line){
+// console.log(); the next person in line and remove it from the line
+
+ //return "There is nobody waiting to be served!"
+//}
