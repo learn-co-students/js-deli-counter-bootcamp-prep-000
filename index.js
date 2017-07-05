ed187d368ed5b6b0727e3276a2363f2a37cfe115
Matt Milton <matt.milton@gmail.com>
2017-07-05 15:14:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var katzDeliLine = [];
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+}
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length>0){
+    return (`Currently serving ${katzDeliLine.shift()}.`)
+  }
+  else{
+    return ('There is nobody waiting to be served!')
+  }
+}
+function currentLine(katzDeliLine){
+  var currentline =[];
+  if(!katzDeliLine.length){
+    return ('The line is currently empty.')
+  }
+  for (let i =0; i<katzDeliLine.length; i++){
+    currentline.push(`${i+1}. ${katzDeliLine[i]}`);}
+  return (`The line is currently: ${currentline.join(', ')}`)
+}
