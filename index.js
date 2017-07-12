740409c3eb3a6f54116c55819c5d5dc7d512ac29
giang ha <gi4ngh4_95@yahoo.com>
2017-07-12 03:56:48 +0000

    Done.

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ function nowServing(katzDeliLine)
   if(katzDeliLine.length ===0)
   {return 'There is nobody waiting to be served!'}
 
-  return `Currently serving ${katzDeliLine.shift()}.`
+  return `Currently serving ${katzDeliLine.shift()}.`}
   function currentLine(line)
   {
     if(line.length===0)
-    {return 'The line is currently empty'}
+    {return 'The line is currently empty.'}
     var a = []
-    for(var i = 1 ;i<line.length+1;i++)
-    {a.push(`${i}. ${line[i]}, `)}
+    a.push(`1. ${line[0]}`)
+    for(var i = 0 ;i<line.length-1;i++)
+    {a.push(` ${i+2}. ${line[i+1]}` )}
 
-  return 'the line is currently: '+a}
+  return ('The line is currently: '+a)}
