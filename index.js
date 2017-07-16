90700990a83d187ab0035f2559dde09eaf156e59
Asha R <asharoopireddy94@gmail.com>
2017-07-16 20:37:42 +0000

    Done.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing(line){
 
 function currentLine(line){
   if(!line.length){
-    return "The line is currently empty."
+    return `The line is currently empty.`
   }
   const numbersAndNames = []
   for(let i = 0, l = line.length; i < l; i++){
