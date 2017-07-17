0fe2456df2d52dcdf25538fdbbf446c391a889bc
Ian Percy <ipercy20@gmail.com>
2017-07-17 22:47:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-var katzDeliLine = ['ada', 'ian'];
-//var katzDeliLine = [];
-var name = 'ada'
+
+var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
-  katzDeliLine.unshift(name)
-  console.log(katzDeliLine)
-  return (katzDeliLine.length)
+  katzDeliLine.push(name)
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
-
-console.log(takeANumber(katzDeliLine, name))
 
 
 function nowServing(katzDeliLine){
 if (katzDeliLine.length === 0){
-  return "The line is currently empty."
+  return "There is nobody waiting to be served!"
   //console.log('The line is currently empty.')
 }
 else {
-  string = "Currently serving " + katzDeliLine[0] + "."
-  katzDeliLine.shift()
-  console.log(katzDeliLine)
-  console.log(string)
-  return string
+  return "Currently serving " + katzDeliLine.shift() + "."
 }
 }
 
-//nowServing(katzDeliLine)
+function currentLine(katzDeliLine) {
+  var i = 0
+  var string = "The line is currently:"
+  if (katzDeliLine.length === 0){
+    return "The line is currently empty."
+  }
+  else {
+    for ( i = 0; i <= katzDeliLine.length - 1; i++) {
+      if (i === katzDeliLine.length - 1){
+        string = string + " " + (parseInt(i, 10) + 1) + ". " + katzDeliLine[i]
+      }
+      else{
+        string = string + " " + (parseInt(i, 10) + 1) + ". " + katzDeliLine[i] + ","
+        }
+      }
+      return string
+    }
+}
