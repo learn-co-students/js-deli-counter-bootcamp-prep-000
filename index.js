50354ea0aefafd8bc06983a7a98b75fb13249817
Will Middlebrook <will.d.middlebrook@gmail.com>
2017-07-17 20:08:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function takeANumber (katzDeliLine, name) {
+  katzDeliLine.push (name)
+  return "Welcome, " + name + "." + " You are number " + katzDeliLine.length + " in line."
+}
+
+function nowServing (katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return "Currently serving " + katzDeliLine.shift() + "."
+  }
+   else {return "There is nobody waiting to be served!"
+   }
+}
+
+function currentLine (katzDeliLine) {
+  var katzDeliLine2 = []
+  if (katzDeliLine.length > 0) {
+for (var i = 0; i < katzDeliLine.length; i++) {
+  katzDeliLine2.push (" " + [i+1] + ". " + katzDeliLine[i])
+}
+return ("The line is currently:" + katzDeliLine2)
+  }
+  else {
+    return ("The line is currently empty.")
+  }
+}
