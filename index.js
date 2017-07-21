51789f95dcd6e26e0879e5f2429d535eafde1ee5
David Tannenbaum <david.tannenbaum@gmail.com>
2017-07-21 01:54:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, newName) {
+  katzDeliLine.push(newName)
+  var position
+  position = katzDeliLine.length
+  return ('Welcome, ' + newName + '. You are number ' + position + ' in line.')
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length == 0) {
+    return ('There is nobody waiting to be served!')
+  } else {
+    return ('Currently serving ' + katzDeliLine.shift() + '.')
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length == 0) {
+    return ('The line is currently empty.')
+  } else {
+    var allPeople = [];
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      if (i == katzDeliLine.length - 1) {
+        allPeople.push(' ' + (i + 1) + '. ' + katzDeliLine[i])
+      } else {
+        allPeople.push(' ' + (i + 1) + '. ' + katzDeliLine[i])
+      }
+    }
+  }
+  return ('The line is currently:' + allPeople)
+}
