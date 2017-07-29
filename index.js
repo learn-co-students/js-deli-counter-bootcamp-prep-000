bfb924a6b2765567790250015c61be21a79431eb
Araba Taylor <araba.taylor20@gmail.com>
2017-07-29 15:41:00 +0000

    Done.

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ function takeANumber(katzDeliLine, newName){
   katzDeliLine.push(newName)
   return(`Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`)
 }
-function nowServing(){
-katzDeliLine.shift();
-  return katzDeliLine;
-  if (katzDeliLine.length===0) {
-    return("There is nobody waiting to be served!")
+function nowServing(deliLine){
+  if (deliLine.length===0) {
+  return("There is nobody waiting to be served!");
+}
+else{
+       return(`Currently serving ${deliLine.shift()}.`);
+}
+}
+function currentLine(line){
+  if(line.length===0){
+    return("The line is currently empty.");
+  }
+  else{
+    return(`The line is currently: 1. ${line[0]}, 2. ${line[1]}, 3. ${line[2]}`);
   }
 }
