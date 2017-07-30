aaa3af0feb3f60d8a7a7278ac409ed75226dc5e1
ff18qd <ff18qd@gmail.com>
2017-07-30 21:44:30 +0000

    Done.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var katzDeli = [];
 
 function takeANumber(array, name) {
   array.push(name);
-  return ('Welcome, '+ name +". You are number "+ array.length + " in line.")
-  // return 'Welcome, $name. You are number ${array.length} in line.'
+  // return ('Welcome, '+ name +". You are number "+ array.length + " in line.")
+  return 'Welcome, ${name}. You are number ${array.length} in line.'
 }
 
 function nowServing(array) {
