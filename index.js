fdee7cc2293f5784d4ab3da05d4dde8069e3f150
Malachi Price <malachiprice1@gmail.com>
2017-08-01 14:24:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+var katzDeli = [];
+
+function takeANumber(katzDeli, customerName) {
+  katzDeli.push(customerName);
+  return `Welcome, ${customerName}. You are number ${katzDeli.indexOf(customerName) + 1} in line.`;
+}
+
+function nowServing(katzDeli) {
+   //let i = 0;
+   //while (i < katzDeli.length) {
+//     i++;
+  // }
+   if (katzDeli.length === 0) {
+     return "There is nobody waiting to be served!";
+   }
+   else
+  return (`Currently serving ${katzDeli.shift()}.`);
+}
+
+function currentLine(katzDeli) {
+  var line = [];
+  let i = 0;
+  while (i < katzDeli.length) {
+    line.push(` ${i+1}. ${katzDeli[i]}`);
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return `The line is currently empty.`
+  } else {
+    return "The line is currently:" + line;
+  }
+}
