af935019fa559dd18e93307f44df890d84ef45da
Blake Nelson <l.blake.nelson@gmail.com>
2017-08-03 19:02:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+var katzDeli = []
+function takeANumber(katzDeli, name){
+  katzDeli.push(name)
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
+}
+
+function nowServing(katzDeli){
+  if (!katzDeli.length){
+    return "There is nobody waiting to be served!"
+  }
+  else{
+    return `Currently serving ${katzDeli.shift()}.`
+  }
+}
+
+function currentLine(katzDeli){
+  if(!katzDeli.length){
+    return "The line is currently empty."
+  }
+  else{
+const line = []
+for (let i = 0, l = katzDeli.length; i < l; i++) {
+  line.push(`${i + 1}. ${katzDeli[i]}`)
+}
+
+return `The line is currently: ${line.join(', ')}`
+}}
