6ff3393c3e5c6c950c4506c2fff2750af03138aa
MARIA HRNDZ <maria.hrndz14@gmail.com>
2017-08-05 03:25:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,57 +1,14 @@
+function currentLine(line) {     if (!line.length) {   return "The line is currently empty."       }
 
-var katzDeliLine = [];
-var katzDeli = [];
-function takeANumber(katzDeliLine, Name)
-{
-  var n = katzDeliLine.length + 1;
+  const numbersAndNames = [ ]
 
-  var LineInfo = 'info';
-    LineInfo = ("Welcome, "+ Name +". You are number " + n + " in line." );
-    console.log(LineInfo);
+  for (let i = 0, l = line.length; i < l; i++)   { numbersAndNames.push(`${i + 1}. ${line[i]}`)     }
 
-    { if  (Name = 'Ada')
-        {  function appendkatzDeliLine1(Name)
-          { return [...katzDeliLine, 'Ada'];  }    }   };
+  return `The line is currently: ${numbersAndNames.join(', ')}`    };
 
-    { if  (katzDeliLine = ['Steven','Blake','Avi'] )
-       {  function appendkatzDeliLine2(Name)
-          { return  [...katzDeliLine, 'Grace'];   }   }   };
+function nowServing(line) {  if (!line.length) {     return "There is nobody waiting to be served!"    }
 
-      var x = katzDeliLine.length;
-      { if  (x === 0)
-        {  function appendkatzDeliLine3()
-           { katzDeliLine = ['Ada','Grace','Kent']; }
-            { return katzDeliLine };   }    };
+  return `Currently serving ${line.shift()}.`   };
 
-
- return LineInfo };
-
-function nowServing()
-  { var LineInfo = 'info';
-   var n = katzDeliLine.length ;
-     { if  (n === 0)
-           {LineInfo = ("There is nobody waiting to be served!"); }
-        else
-          {LineInfo = ("Currently serving " + katzDeliLine[1] +".");
-            function removeFirstinLine()
-                { return katzDeliLine.slice(1)  } }
-        }
-              console.log(LineInfo);
-return LineInfo };
-
-function  currentLine(katzDeliLine)
-{  var LineInfo = 'info';
-   var n = katzDeliLine.length ;
-
-   { if (n === 0)
-        {LineInfo = ("The line is currently empty.");  }
-     else
-        {LineInfo = ("The line is currently: "+
-        "1. "  + katzDeliLine[0] + ","+
-        " 2. "+ katzDeliLine[1] + ","+
-        " 3. " + katzDeliLine[2]  ) ;  }
-
-        console.log(LineInfo);
-    }
-
-return LineInfo};
+function takeANumber(line, name) {    line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`   };
