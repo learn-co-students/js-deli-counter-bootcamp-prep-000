581c85e76a45454ad0639786b042900c9d2a536f
David Hopkins <dlhopkins2@gmail.com>
2017-08-05 02:57:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
+function takeANumber(line, name) {
+  line.push(name)
 
-var katzDeli = []
-
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name);
-    return ('Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.');
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
-  return ('There is nobody waiting to be served!');
-  } else {
-    return ('Currently serving ' + katzDeliLine.shift() + '.');
-    }
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }
+
+  return `Currently serving ${line.shift()}.`
 }
 
-function currentLine(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
-    return 'The line is currently empty.';
-  } else {
-    var lineStatus = 'The line is currently: ';
-      for (let i = 0; i < katzDeliLine.length; i++) {
-          if (i === 0) {
-            lineStatus = lineStatus + (i+1) + '. ' + katzDeliLine[i];
-          } else {
-              lineStatus = lineStatus + ', ' + (i+1) + '. ' + katzDeliLine[i];
-            }
-      }
-        return lineStatus;
-     }
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  }
+
+  const numbersAndNames = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`
 }
