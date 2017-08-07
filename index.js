82d7354965db7713d5b273c0435f307118d6c4f9
lgreenberg23 <lgreenberg23@gmail.com>
2017-08-07 17:08:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,3 +30,68 @@ function currentLine(katzDeliLine){
   return "The line is currently empty.";
 }
 }
+
+
+
+```katzDeli = []
+
+
+
+// katzDeliLineObject = {Luis: 1, Ivan: 2, Sho: 3, Steven: 4, Lauren: 5, Ethan: 6, Yakov: 7}
+
+// katzDeliLineObject = {1: "Luis", 2: "Ivan", 3: "Sho", 4: "Steven", 5: "Lauren", 6: "Ethan", 7: "Yakov"}
+
+katzDeliLine = [{Luis: 1}, {Ivan: 2}, {Sho: 3}, {Steven: 4}, {Lauren: 5}, {Ethan: 6}, {Yakov: 7}]
+
+n = katzDeliLine.length
+
+function nowServing(katzDeliLine) {
+ return Object.values(katzDeliLine.shift())[0]
+}
+
+function takeANumber(katzDeliLine, name) {
+n++
+return katzDeliLine.push({name: n})
+}
+
+function currentLine(katzDeliLine){
+ let line = "The line is currently: "
+ for (let i=0; i<katzDeliLine.length; i++){
+   line += `${Object.values(katzDeliLine[i])[0]}: ${Object.keys(katzDeliLine[i])[0]}, `
+ }
+  return line.slice(0, length-2)
+}
+
+
+[4:25] 
+```katzDeli = []
+
+katzDeliLine = ["Luis", "Ivan", "Sho", "Steven", "Lauren", "Ethan", "Yakov"]
+
+
+// function nowServing(katzDeliLine) {
+//   return katzDeliLine.shift()
+// }
+
+nowServing = katzDeliLine => {
+ return katzDeliLine.shift()
+}
+
+// function takeANumber(katzDeliLine, name) {
+//   katzDeliLine.push(name)
+//   return katzDeliLine.length
+// }
+
+takeANumber = (katzDeliLine, name) => {
+ katzDeliLine.push(name)
+ return katzDeliLine.length
+}
+
+currentLine = (katzDeliLine) => {
+ let line = "The line is currently: "
+ for (let i=0; i<katzDeliLine.length; i++){
+   line += `${i+1}: ${katzDeliLine[i]}, `
+ }
+
+   return line.slice(0, length-2)
+}
