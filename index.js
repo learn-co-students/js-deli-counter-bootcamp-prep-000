688954f8c2b46423fc72248a9edb8cdc8546a1d6
Mehreen Rahman <mimirahman2013@gmail.com>
2017-08-11 13:31:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,37 +1,75 @@
 var katzDeli = [];
-var takeANumber = function(katzDeliLine,name){
-katzDeliLine.push(name);
-var position = katzDeliLine.indexOf(name)+1;
-return "Welcome, "+ name + ". You are number " + position + " in line.";
-};
 
-
-var nowServing = function(katzDeliLine){
-var now_serving_msg = "";
-if (katzDeliLine.length === 0)
-  {now_serving_msg = "There is nobody waiting to be served!"}
-  else {
-    now_serving_msg = "Currently serving "+ katzDeliLine[0] + ".";
-    katzDeliLine.shift(0);
-  }
-  return now_serving_msg;
-};
-
-var currentLine = function(katzDeliLine){
-var current_line = "";
-var current_line_msg = "";
-if (katzDeliLine.length === 0){
-  current_line_msg = "The line is currently empty.";
-}else {
-  current_line_msg = "The line is currently: ";
-  for (var idx = 0; idx < katzDeliLine.length; idx++){
-  var position = idx + 1;
-  if (idx === katzDeliLine.length-1) {
-    current_line = current_line + position.toString() + "." + " " + katzDeliLine[idx];
-  } else {current_line = current_line + position.toString() + "." + " "+ katzDeliLine[idx] + ", ";
-
-  }
+//Add new customers to line and greet customers
+function takeANumber(katzDeliLine, name) {
+	katzDeliLine.push(name);
+	return (
+		'Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.'
+	);
 }
+
+//test takeANumber
+function testTakeANumber() {
+	katzDeli = ['Farah', 'Sami', 'Summer'];
+	var response = takeANumber(katzDeli, 'Mimi');
+	var expectedResponse = 'Welcome, Mimi. You are number 4 in line.';
+	return expectedResponse === response && katzDeli.length === 4;
 }
-return current_line_msg + current_line;
-};
+
+//Indicate which customer you will be serving
+function nowServing(katzDeliLine) {
+	if (katzDeliLine.length === 0) {
+		return 'There is nobody waiting to be served!';
+	} else {
+		return 'Currently serving ' + katzDeliLine.shift() + '.';
+	}
+}
+
+//test nowServing
+function testNowServingWithPeopleOnLine() {
+  katzDeli = ['Farah', 'Sami', 'Summer','Mimi'];
+  var response = nowServing(katzDeli);
+  var expectedResponse = "Currently serving Farah.";
+  return response === expectedResponse;
+}
+function testNowServingWithNoPeopleOnLine() {
+  katzDeli = [];
+  var response = nowServing(katzDeli);
+  var expectedResponse = "There is nobody waiting to be served!";
+  return response === expectedResponse;
+}
+
+//Return the current line
+function currentLine(katzDeliLine) {
+	if (katzDeliLine.length === 0) {
+		return 'The line is currently empty.';
+	} else {
+		var currentLineStrings = [];
+		for (var i = 0; i < katzDeliLine.length; i++) {
+			currentLineStrings.push(i + 1 + '. ' + katzDeliLine[i]);
+		}
+		return 'The line is currently: ' + currentLineStrings.join(', ');
+	}
+}
+
+function testCurrentLineWithPeopleOnLine(){
+  katzDeli = ["Mimi","Farah"];
+  var response = currentLine(katzDeli);
+  var expectedResponse = "The line is currently: 1. Mimi, 2. Farah";
+  return expectedResponse === response;
+}
+
+function testCurrentLineWithNoPeopleOnLine(){
+  katzDeli = [];
+  var response = currentLine(katzDeli);
+  var expectedResponse = "The line is currently empty.";
+  return expectedResponse === response;
+}
+
+function testAll(){
+console.log("testCurrentLineWithNoPeopleOnLine() = ", testCurrentLineWithNoPeopleOnLine());
+console.log("testCurrentLineWithPeopleOnLine() = ", testCurrentLineWithPeopleOnLine());
+console.log("testTakeANumber() = ",testTakeANumber());
+console.log("testNowServingWithNoPeopleOnLine() = ",testNowServingWithNoPeopleOnLine());
+console.log("testNowServingWithPeopleOnLine() = ",testNowServingWithPeopleOnLine());
+}
