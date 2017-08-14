6dc1ae5cec80975628c76ec5a70d1bf5f981f3f3
LG-STEM <gatesinfo@gmail.com>
2017-08-14 12:58:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-var katzDeliLine = [];
-
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
+  if (!katzDeliLine.length) {
     return "There is nobody waiting to be served!"
-  } else {
-    return `Currently serving ${katzDeliLine[0]}.`;
-    katzDeliLine.shift();
   }
+  return `Currently serving ${katzDeliLine.shift()}.`;
 }
 
 function currentLine(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
+  if (!katzDeliLine.length) {
     return "The line is currently empty."
-  } else {
-    var sentence = [`The line is currently: ${1}. ${katzDeliLine[0]}`];
-    var i = 1;
-    while (i < katzDeliLine.length -1) {
-      sentence.push(`${i + 1}. ${katzDeliLine[i]} `)
-    }
-    return sentence.split(", ");
   }
+
+  const numbersAndNames = []
+
+  for(var i = 0, l = katzDeliLine.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${katzDeliLine[i]}`)
+  }
+  return `The line is currently: ${numbersAndNames.join(', ')}`
 }
