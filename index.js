b21d606588a7da94107f4dac85ad03168d40baae
Dave Ege <teacher.dave.ege@gmail.com>
2017-08-14 01:40:00 +0000

    Done.

--- index.js
+++ index.js
@@ -2,26 +2,32 @@ var katzdeli = []
 
 function takeANumber(katzdeli,newname) {
   katzdeli[katzdeli.length]=newname;
-  return `Welcome ${newname}, you are ${katzdeli.length} in line`;
+  return `Welcome, ${newname}. You are number ${katzdeli.length} in line.`;
 }
 
-function nowserving() {
-  if katzdeli[0] === null {
+function nowServing(katzdeli) {
+  if (katzdeli[0] == null) {
     var nowserving = "There is nobody waiting to be served!"
-  }else {var nowserving = katzdeli[0];
-  katzdeli.unshift;}
-  return nowserving;
+    return nowserving;
+  }else {
+ return (`Currently serving ${katzdeli.shift()}.`)
+
+    //var nowserving = katzdeli[0];
+  //katzdeli.shift;
+  //console.log(`Currently serving ${nowserving}.`);
+  //return `Currently serving ${nowserving}.`;
+}
 
 }
 
-function currentLine(katzDeli) {
-  if katzdeli[0] === null {
+function currentLine(katzDeliLine) {
+  if (katzDeliLine[0] == null) {
     return "The line is currently empty."
 } else {
 var LineArrayInSentences = [];
-for (var i=0; i<katzdeli.length;i++){
-  LineArrayInSentences[0] = '${i}. ${katzdeli[i]}'
+for (var i=0; i<katzDeliLine.length;i++){
+  LineArrayInSentences[i] = ` ${i+1}. ${katzDeliLine[i]}`
 }
-return `The line is currently: ${LineArrayInSentences}``
+return `The line is currently:${LineArrayInSentences}`
 }
 }
