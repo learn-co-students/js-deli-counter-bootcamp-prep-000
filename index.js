014ed8c600cf6130c99114ddd95b3853d5363574
lauralaura14 <laurao148@gmail.com>
2017-08-18 21:15:09 +0000

    Done.

--- index.js
+++ index.js
@@ -6,12 +6,24 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(katzDeliLine) {
-var i = 0;
-
-  while (i < katzDeliLine.length) {
-    i++ };
-
-  if(katzDeliLine[]) {
-    return 'The line is empty.'
+  if(katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
+}
+
+function currentLine(line) {
+
+var numberLine = [];
+
+
+    if(line.length === 0) {
+      return 'The line is currently empty.';
+    } else {
+        for(var i = 0; i < line.length; i++) {
+          numberLine.push(`${i + 1}. ${line[i]}` );
+          }
+        return `The line is currently: ${numberLine.join(', ')}`;
+}
 }
