9524cd6edb2e78fcfeb4b70accb17d9bcfb073bf
lauralaura14 <laurao148@gmail.com>
2017-08-18 15:18:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeli = katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeli} in line.`
+}
+
+function nowServing(katzDeliLine) {
+var i = 0;
+
+  while (i < katzDeliLine.length) {
+    i++ };
+
+  if(katzDeliLine[]) {
+    return 'The line is empty.'
+  }
+}
