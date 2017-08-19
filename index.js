96d0ebf6ac4a944f71491bb907b6e40f1e5c6720
Tyler Chan <tcflux@gmail.com>
2017-08-19 07:26:16 +0000

    Done.

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function currentLine(line) {
   } else {
     let string = "The line is currently: ";
     for (let i=0; i < line.length - 1; i++) {
-      string = string + (i+1) + ". " + line[i] + ", ";
+      string += (i+1) + ". " + line[i] + ", ";
     }
-    string = string + line.length + ". " + line[line.length - 1];
+    string += line.length + ". " + line[line.length - 1];
     return string;
   }
 }
