77e8075acbfb13e5454d743b245a6178bc9c3cbb
Zsófia Dalnoki-Papp <dalnokipapp.zsofia@gmail.com>
2017-08-19 12:39:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, newPersonName) {
+ var positionInLine = katzDeliLine + 2;
+
+ return "You are "+positionInLine+" in line, "+newPersonName+"!"
+
+
+
+
+}
