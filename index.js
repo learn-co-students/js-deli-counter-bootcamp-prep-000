9f6f035a650481c71d0fdcff26b3ca0687fd6b41
John Day <itisjohnday@gmail.com>
2017-08-24 00:16:57 +0000

    Done.

--- index.js
+++ index.js
@@ -17,3 +17,34 @@ function currentLine (arr) {
   }
   return 'The line is currently: 1. ' + arr[0] + ', 2. ' + arr[1] + ', 3. ' + arr[2];
 }
+
+/*
+
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  }
+
+  const numbersAndNames = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
+
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }
+
+  return `Currently serving ${line.shift()}.`
+};
+
+function takeANumber(line, name) {
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+};
+*/
