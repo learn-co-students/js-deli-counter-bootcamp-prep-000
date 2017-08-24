91f1dcf1ec95536d84411f230e05edffdcd12167
Devon Kincaid <devon.kincaid@gmail.com>
2017-08-24 17:54:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var katzDeli = [];
+
+
+function takeANumber(katzDeliLine, name){
+
+  katzDeliLine.push(`${name}`);
+
+  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`;
+}
+
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length > 0){
+
+    return `Currently serving ${katzDeliLine.shift()}.`
+
+  } else {
+
+    return "There is nobody waiting to be served!"
+  }
+
+
+}
