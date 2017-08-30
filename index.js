66039ac33cd0529cfdfe05ee7ac9cdbbbbe19dde
dsbondin <dsbondin@gmail.com>
2017-08-30 00:55:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length == 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else {
+    var message = "Currently serving " + katzDeliLine[0] +".";
+    katzDeliLine.shift();
+    return message;
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length == 0) {
+    return "The line is currently empty."
+  }
+  else {
+    var line = "";
+    for (var i = 0; i < katzDeliLine.length; i++){
+      line = line + (i + 1) + ". " + katzDeliLine[i] + ", ";
+    }
+    line = line.slice(0, -2);
+    return "The line is currently: " + line;
+  }
+}
