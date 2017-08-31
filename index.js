2f4fcff5f1f71ea8d6fe2d784fe052aca53f968d
Henry Zhang <henryyihengzhang@gmail.com>
2017-08-31 19:36:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeliLine = [];
+
+function currentLine(line) {
+  if (!line.length) {
+    return `The line is currently empty.`
+  }
+
+var numbersAndNames = []
+
+for (let i = 0, l = line.length; i < l; i++) {
+  numbersAndNames.push(`${i + 1}. ${line[i]}`)
+}
+return `The line is currently: ${numbersAndNames.join(', ')}`
+}
+
+function takeANumber(line, name) {
+line.push(name)
+
+return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing(line) {
+  if (!line.length) {
+    return `There is nobody waiting to be served!`
+  }
+
+  return `Currently serving ${line.shift()}.`
+};
