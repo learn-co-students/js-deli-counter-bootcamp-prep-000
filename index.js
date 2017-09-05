b47ac4543c2ed2c049c853cb9fd3290e0b7a48f0
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-05 17:58:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+}
+
+function nowServing(katzDeliLine) {
+    if (katzDeliLine.length > 0) {
+    return `Currently serving ${katzDeliLine.shift()}.`;
+  } else {
+      return `There is nobody waiting to be served!`
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var array = [];
+if (katzDeliLine.length !== 0) {
+for (var i = 0; i < katzDeliLine.length; i++) {
+ array.push(" " + `${i + 1}` + ". " + `${katzDeliLine[i]}` + "");
+  }
+ } else {
+  return "The line is currently empty.";
+ }
+ return "The line is currently:" + `${array}`;
+}
