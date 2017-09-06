159e7d50d1a760aed5f4adece3aa07eb07b11ad6
webdgirl <emmapadbury@gmail.com>
2017-09-06 19:11:48 +0000

    Done.

--- index.js
+++ index.js
@@ -8,21 +8,23 @@ function takeANumber (katzDeliLine, person) {
 }
 
 function nowServing (katzDeliLine) {
-  var nextCustomer = katzDeliLine[0];
-  if (katzDeliLine.length > 0) {
-    return "Currently serving " + nextCustomer +".";
-    katzDeliLine.shift();
+    if (katzDeliLine.length > 0) {
+    return "Currently serving " + katzDeliLine.shift() +".";
+
   } else {
     return "There is nobody waiting to be served!";
   }
 }
-function currentLine (katzDeliLine) {
-  var currentLine = [];
-  for (var i = 0; i < katzDeliLine.length; i++) {
-    currentLine.push(katzDeliLine[i]);
+function currentLine(katzDeliLine) {
+  if (!katzDeliLine.length) {
+    return "The line is currently empty.";
   }
-  if (katzDeliLine.length > 0) {
-    return "The line is currently: 1. " + katzDeliLine[1] + ;
-  } else {
-  return "The line is currently empty.";
-}
+
+  const numbersAndNames = [];
+
+  for (let i = 0; i < katzDeliLine.length; i++) {
+    numbersAndNames.push(`${i + 1}. ${katzDeliLine[i]}`)
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
