5275ff90d80fab094c8fd6757cab8555ab39e7ec
Hazelin Ngan <hazelin.ngan@gmail.com>
2017-09-09 13:44:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 var katzDeli = []
 
-var katzDeliLine = []
-
-function takeANumber (katzDeliLine, name){
-
+function currentLine (line){
+  if (!line.length){
+  return "The line is currently empty."
 }
 
-function nowServing {
+  const numberAndNames = []
 
-}
+  for (let i=0, l=line.length; i<l; i++){
+    numberAndNames.push (`${i+1}. ${line[i]}`)
+  }
+  return `The line is currently: ${numberAndNames.join(', ')}`
+};
 
-function currentLine (katzDeliLine){
+function nowServing (line){
+  if (!line.length){
+    return "There is nobody waiting to be served!"
+  }
+  return `Currently serving ${line.shift()}.`
+};
 
-}
+function takeANumber (line, name){
+    line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+};
