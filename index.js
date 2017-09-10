ad24dcfd390bddec17082c4d09b3cc2c625e34f0
jasmine Magdi <jesojolay@yahoo.com>
2017-09-10 20:57:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`;
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var wholeLine = "The line is currently: ";
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      if (i == katzDeliLine.length - 1) {
+        wholeLine += `${i+1}. ${katzDeliLine[i]}`
+      } else {
+        wholeLine += `${i+1}. ${katzDeliLine[i]}, `
+      }
+    }
+  }
+  return wholeLine;
+}
+console.log(currentLine(katzDeliLine));
