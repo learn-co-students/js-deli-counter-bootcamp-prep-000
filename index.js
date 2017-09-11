f06c6e768accd3f45ed1793495049843e91dcdff
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-11 22:55:52 +0000

    Done.

--- index.js
+++ index.js
@@ -4,9 +4,48 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
 
-  katzDeliLine.push(name);
+  katzDeliLine.push(`${name}`);
 
-  console.log('Welcome ${name} you are ' + katzDeliLine[i] + ' in line')
+  return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 
+}
+
+function nowServing(katzDeliLine) {
+
+  var i = 0;
+
+  while (i < katzDeliLine.length) {
+
+    i++;
+
+  }
+
+if (katzDeliLine.length === 0) {
+
+  return "There is nobody waiting to be served!";
+
+} else return (`Currently serving ${katzDeliLine.shift()}.`)
+
+}
+
+var custLine = [];
+
+function currentLine(katzDeliLine) {
+
+  var i = 0;
+
+  while (i < katzDeliLine.length) {
+
+    custLine.push(` ` + [i + 1] + `. ` + katzDeliLine[i]);
+
+    i++;
+
+  }
+
+  if (katzDeliLine.length == 0) {
+
+    return "The line is currently empty."
+
+  } else return (`The line is currently:` + custLine)
 
 }
