bd2f154e2b92b152e2591ef0630b37126a059bf5
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-11 00:01:43 +0000

    Done.

--- index.js
+++ index.js
@@ -20,14 +20,18 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty."
   }
-  else {
-    var lineSentence = "The line is currently: "
-    for (var i = 0; i < line.length; i++) {
-      lineSentence += `${i + 1}. ${line[i]}`
-      if (i < line.length - 1) {
-        lineSentence += `, `
-      }
-    }
+
+  const numbersAndNames = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
   }
-  return lineSentence
-}
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
+
+
+takeANumber(katzDeli, 'Ada')
+takeANumber(katzDeli, 'Jon')
+takeANumber(katzDeli, 'Joe')
+console.log(currentLine(katzDeli))
+console.log(katzDeli)
