1e1e37eca57a01456ecf64242862a76bf953ab68
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-11 22:16:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+var katzDeli = [];
+
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+
+  katzDeliLine.push(name);
+
+  console.log('Welcome ${name} you are ' + katzDeliLine[i] + ' in line')
+
+
+}
