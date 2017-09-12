2c58f04ac8a2f1fa73efd6ec68fa57ff60365934
Cayla Schwartz <cayschwartz@gmail.com>
2017-09-12 17:42:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-var katzDeli = []
+//var katzDeli = []
 
 function takeANumber(katzDeliLine, name){
  katzDeliLine.push(name)
- var position = katzDeliLine.length + 1
+ var position = katzDeliLine.length
  return `Welcome, ${name}. You are number ${position} in line.`
+}
 
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length==0){
+    return "There is nobody waiting to be served!"
+  }
+  else{
+    var name = katzDeliLine.shift()
+    return `Currently serving ${name}.`
+  }
+}
 
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length==0){
+    return "The line is currently empty."
+  }
+  else{
+    var line = "The line is currently: "
+    for(var i=0; i<katzDeliLine.length; i++){
+      line+=`${i+1}. ${katzDeliLine[i]}, `
+    }
+    return line.slice(0,-2)
+  }
 }
