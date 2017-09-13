2631ba49dec4e03d3379571709629c19be0ea771
Jenn Cacace <jenn.cacace@gmail.com>
2017-09-13 09:09:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+var katzDeliLine = []
+function takeANumber (katzDeliLine, otherDeli) {
+katzDeliLine.push(otherDeli);
+  return `Welcome, ${otherDeli}. You are number ` + katzDeliLine.length + " in line."
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  } else {
+    return "Currently serving " + katzDeliLine.shift() + ".";
+  }
+}
+
+function currentLine(katzDeliLine){
+var line = []
+  for(var i=0; i < katzDeliLine.length; i++){
+  line.push(i+1 + ". " + katzDeliLine[i])
+  }
+  if (line.length === 0){
+    return "The line is currently empty."
+} else {
+  return "The line is currently: " + line.join(', ');
+}
+}
