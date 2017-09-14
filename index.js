0d12ed8381d5843ebbd9e9679575b55bf2b6f877
Sandy Yeung <sandynism@gmail.com>
2017-09-14 02:06:26 +0000

    Done.

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
-    return "There is nobody waiting to be served!"
-  }
+  if (katzDeliLine.length === 0)
+  return "There is nobody waiting to be served!"
 }
 
 function currentLine(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
-  return ("The line is currently empty")
-}
+  if (katzDeliLine.length === 0)
+  return ("The line is currently empty.")
+
 }
