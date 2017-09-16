5829e1118cedac159d677aa0e8fedcac823628b1
Franco Aiello <steed430@hotmail.com>
2017-09-16 15:14:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function takeANumber(katzDeliLine,name){
+    katzDeliLine.push(name)
+    return "Welcome, "+name+". You are number "+katzDeliLine.length+" in line."
+}
+function nowServing(katzDeliLine){
+    if(katzDeliLine.length==0){
+        return "There is nobody waiting to be served!"
+    }else{
+        return "Currently serving "+katzDeliLine.shift()+"."
+    }
+}
+function currentLine (katzDeliLine){
+    if (katzDeliLine==0){
+        var announcement="The line is currently empty."
+        }else{
+            var line =[]
+            var i=0
+            for (i=0;i<katzDeliLine.length;i++){
+                line.push(" "+(i+1)+". "+katzDeliLine[i])
+            }
+            var announcement= "The line is currently:"+line
+            }
+    return announcement
+}
