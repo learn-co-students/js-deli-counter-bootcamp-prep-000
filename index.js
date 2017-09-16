ae23c11e69d4d18cf74bd5134d6c3a9b2bab10eb
Jennifer Stewart <jlgregg1@gmail.com>
2017-09-16 21:39:46 +0000

    Done.

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ function currentLine(KatzDeliLine){
   if (KatzDeliLine.length===0) {
     return ("The line is currently empty.")
   } else {
-    // The line is curretly: 1. Jennifer, 2. Mikio, 3. Max
     var retVal = "The line is currently: "
     for (let i=0; i < KatzDeliLine.length; i++) {
       //let number = i+1
