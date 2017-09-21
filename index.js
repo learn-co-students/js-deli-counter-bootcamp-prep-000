b2ec5b9ff91c5379702d1b96db8f790fd31fa834
Eric Johnson <0216johnsoneric@gmail.com>
2017-09-21 15:26:44 +0000

    Done.

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return 'Welcome,' + name + 'You are number' + katzDeliLine.length;
+  return 'Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.';
+  //      `Welcome, ${name} you are number ${katzDeliLine.length}.``
 }
 
-function nowServing(line) {
-  if(!line.length) {
-    console.log("There is nobody waiting to be served!")
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length == 0) {
+    // console.log("There is nobody waiting to be served!")
     return "There is nobody waiting to be served!"
   } else {
     //console.log("Currently serving " + line.shift());
-    return "Currently serving " + line.shift();
+    return "Currently serving " + katzDeliLine.shift() + '.';
   }
 }
+
+//
+// function currentLine(katzDeliLine){
+//
+// }
