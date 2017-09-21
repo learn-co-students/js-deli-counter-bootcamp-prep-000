86be00fd4cb937892d7d1d9c26f9da823b129716
Eric Johnson <0216johnsoneric@gmail.com>
2017-09-21 16:00:53 +0000

    Done.

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ function nowServing(katzDeliLine) {
   }
 }
 
-//
 // function currentLine(katzDeliLine){
-//
+//   if(katzDeliLine.length == 0) {
+//     return "The line is currently empty."
+//   } else {
+//     for(var i =0; i < katzDeliLine.length; i++) {
+//        katzDeliLine.push('The line is currently:' + name[i])
+//     }
+//   }
 // }
