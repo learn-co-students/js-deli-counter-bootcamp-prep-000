401e8ce1eb1e0987b3957b50b84cd300cd2d957e
NolanHughes <nhughes987@gmail.com>
2017-09-22 17:53:13 +0000

    Done.

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ function currentLine(line) {
   if (line.length < 1) {
     return "The line is currently empty."
   } else {
-    return "The line is currently: 1. " + line[0] + ", 2. " + line[1] + ", 3. " + line[2]
+    const numbersAndNames = []
+
+    for (var i = 0; i < line.length; i++){
+      numbersAndNames(i + 1 + " line[i]")
+    }
+    // return "The line is currently: 1. " + line[0] + ", 2. " + line[1] + ", 3. " + line[2]
+    return "The line is currently: " + numbersAndNames
   }
 }
