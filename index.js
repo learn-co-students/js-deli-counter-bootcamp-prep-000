8fbf9b90137e9998782ad3cb5dc36f24fee64cb8
Alex Levine <aalexlevine@gmail.com>
2017-09-23 18:59:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+var katzDeli = [];
+
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+return 'Welcome, ${name}. You are number ${katzDeliLine.length +1} in line.' ;
+}
+
+function nowServing(name){
+ katzDeliLine.shift(name);
+if katzDeliLine = 0;
+console.log "There is nobody waiting to be served!"
+  return katzDeliLine
+}
