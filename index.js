3414cfa34da32d82f10dd902ee2fc4d7d97b66b2
Nolan Byrnes <nolanehs@gmail.com>
2017-09-24 19:31:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+var katzDeli = [];
+var line = [];
+
+
+function takeANumber(katzDeli, name) {
+   katzDeli.push(`${name}`);
+return "Welcome, " + name + ". You are number " + katzDeli.length + " in line.";
+}
+
+function currentLine (katzDeli) {
+  console.log("The line is currently:") + katzDeli;
+}
+
+function nowServing(katzdeli) {
+
+  if (katzdeli.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  if (katzDeli.length > 0)  {
+  return "Currently serving " + katzDeli.shift() + ".";
+}
+}
+
+
+
+
+function currentLine(katzDeli) {
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+}
+    for (let i = 0; i < katzDeli.length; i++) {
+      line.push(` `+[i+1]+`. `  + katzDeli[i])
+    }
+    return(`The line is currently:` + line);
+  }
