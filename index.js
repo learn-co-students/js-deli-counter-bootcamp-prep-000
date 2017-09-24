1d026822dd2738550d2bc2a0d9cb2e0056ee852d
Alex Levine <aalexlevine@gmail.com>
2017-09-24 18:20:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var katzDeli = [];
 
-
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
-return 'Welcome, ${name}. You are number ${katzDeliLine.length +1} in line.' ;
+return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.` ;
 }
 
-function nowServing(name){
- katzDeliLine.shift(name);
-if katzDeliLine = 0;
-console.log "There is nobody waiting to be served!"
-  return katzDeliLine
+function nowServing(katzDeliLine, name) {
+  if (katzDeliLine.length === 0)
+return "There is nobody waiting to be served!"
+  else   var array = katzDeliLine.shift(name);
+return `Currently serving ${array}.`
+}
+
+function currentLine(katzDeliLine) {
+var lineRightNow = []
+if (katzDeliLine.length === 0)
+  return "The line is currently empty."
+for (let i =0; i < katzDeliLine.length; i++ ) {
+   lineRightNow.push(` `+[i+1]+`. ` + katzDeliLine[i])}
+return ("The line is currently:" + [lineRightNow]);
 }
