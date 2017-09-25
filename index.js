aa28b650453516d15933ff8a361c3cd4875de7aa
James Royer <walkingalchemy@gmail.com>
2017-09-25 20:20:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 var katzDeli = [];
-var katzDeliLine = []
+var deliLine = []
 
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name);
-  return katzDeliLine.length
+function takeANumber(deliLine, name) {
+  deliLine.push(name);
+  return `Welcome, ${name}. You are number ${deliLine.length} in line.`
+}
+
+function nowServing(deliLine) {
+  if (deliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    let announce = `Currently serving ${deliLine[0]}.`
+    deliLine.shift();
+    return announce
+  }
+}
+
+function currentLine(deliLine) {
+  if (deliLine.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    let announce = `The line is currently: 1. ${deliLine[0]}`;
+    for (let i = 1; i < deliLine.length; i++) {
+      announce += `, ${i+1}. ${deliLine[i]}`
+    }
+    return announce
+  }
 }
