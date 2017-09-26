4a35d977d1039684a47301b40193ca468405bfdc
James Abels <abelsj60@gmail.com>
2017-09-26 18:40:38 +0000

    Done.

--- index.js
+++ index.js
@@ -21,19 +21,19 @@ function nowServing(arr) {
 }
 
 function currentLine(arr) {
-  var visitorStatus = `The line is currently empty.`;
+  var lineStatus = `The line is currently empty.`;
 
   if(arr.length > 0) {
-    visitorStatus = `The line is currently: `;
+    lineStatus = `The line is currently: `;
 
     for(var i = 0; i < arr.length; i++) {
       if(i < arr.length - 1) {
-        visitorStatus += `${(i + 1)}. ${arr[i]}, `;
+        lineStatus += `${(i + 1)}. ${arr[i]}, `;
       } else {
-        visitorStatus += `${(i + 1)}. ${arr[i]}`;
+        lineStatus += `${(i + 1)}. ${arr[i]}`;
       }
     }
   }
 
-  return visitorStatus;
+  return lineStatus;
 }
