fb725dd77cafdab4145924800bf5dcaea41c24ed
Fernando Gomez <fhergomez37@gmail.com>
2017-09-27 23:06:03 +0000

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nowServing (katzDeliLine, name) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-    return `Currently serving ${katzDeliLine.shift()}.`;
+    return `Currently serving ${katzDeliLine.splice(0,1)}.`;
   }
 }
 
