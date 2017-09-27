78895945b75e072b301acb2e9061ec964e8735c7
Michael Wong <mik9080@gmail.com>
2017-09-27 15:54:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-function takeANumber(katzDeliLine,name){
-  for (var i=0;i<katzDeliLine.length;i++){
-    if(katzDeliLine[i]==name){
-      return i+1
-    }
-  }
+function takeANumber(line,name){
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 function nowServing(katzDeliLine){
   if (katzDeliLine.length==0) {
     return "There is nobody waiting to be served!"
   } else{
-  return katzDeliLine.shift()
-}
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
 }
 function currentLine(line){
   if (line.length==0){
     return "The line is currently empty."
   } else{
     var str = "The line is currently:"
-    for (var i=1;i<=line.length;i++){
-      str+=` ${i}. ${line[i]},`
+    for (var i=0;i<line.length;i++){
+      str+=` ${i+1}. ${line[i]},`
     }
     return str.slice(0,str.length-1)
   }
