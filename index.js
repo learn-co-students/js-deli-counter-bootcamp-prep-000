04e8a1cf1f779b4b16c6cafe8a8f3cc92a881648
Jarrod Dyer <jarrod@gasganno.com>
2017-09-27 20:16:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var katzDeli = [];
+
+function takeANumber(line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+
+}
+
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return `Currently serving ${line.shift()}.`
+  }
+}
+
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  } else {
+    var lineup = []
+      for (let n = 0; n < line.length; n++)
+        lineup.push(`${n + 1}. ${line[n]}`)
+        }
+  return `The line is currently: ${lineup.join(', ')}`
+}
