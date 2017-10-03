683c92dbe7cb83a91ff102b12f7f409ca56a1441
davidjkim810 <davidjkim810@gmail.com>
2017-10-03 18:05:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
 function takeANumber (katzDeliLine, name) {
-   katzDeliLine.push(name);
-   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
-}
-
-function nowServing (katzDeliLine) {
-  if (katzDeliLine.length == 0) {
-    return `There is nobody waiting to be served!`;
-  }else{
-    return `Currently serving ${katzDeliLine.shift()}.`
-  }
-}
-
-function currentLine (katzDeliLine) {
-  if (katzDeliLine.length == 0) {
-    return `The line is currently empty.`
-  }else{
-    var lineupdate = [];
-    for (let x = 0; x < katzDeliLine.length; x++) {
-      var numbering = x + 1;
-      lineupdate.push(` ${numbering}. ${katzDeliLine[x]}`);
-    }
-    return `The line is currently:${lineupdate}`
-  }
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
