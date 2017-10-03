da30dff355151d312e083db3b1b57f3dcd62ec37
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-03 14:45:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 function takeANumber( katzDeliLine, name ) {
-  katzDeliLine.push(name)
-  var num = katzDeliLine.length
-  return `Welcome, ${name}. You are number ${num} in line.`
+  katzDeliLine.push(name);
+  var num = katzDeliLine.length;
+  return `Welcome, ${name}. You are number ${num} in line.`;
 }
 
 function nowServing( katzDeliLine ) {
-  if( katzDeliLine.length === 0 ) {
-    return "There is nobody waiting to be served!"
+  if ( katzDeliLine.length === 0 ) {
+    return "There is nobody waiting to be served!";
   } else {
-    return `Currently serving ${katzDeliLine.shift(0)}.`
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
 
-function currentLine(line) {
-  if( line.length === 0 ) {
-    return "The line is currently empty."
+function currentLine( line ) {
+  if ( line.length === 0 ) {
+    return "The line is currently empty.";
   } else {
-    var saysWho = `The line is currently: 1. ${line[0]}`
+    var saysWho = `The line is currently: 1. ${line[0]}`;
     for ( var i = 1; i < line.length; i++ ) {
-      saysWho += `, ${i + 1}. ${line[i]}`
+      saysWho += `, ${i + 1}. ${line[i]}`;
     }
-    return saysWho
+    return saysWho;
   }
 }
