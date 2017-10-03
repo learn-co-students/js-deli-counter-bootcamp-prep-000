83f5c975b76af81a08df06bf0d50cb74844e71eb
davidjkim810 <davidjkim810@gmail.com>
2017-10-03 18:35:52 +0000

    Done.

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ function nowServing (katzDeliLine) {
   if (katzDeliLine.length == 0) {
     return `There is nobody waiting to be served!`
   } else {
-    return `Currently serving ${katzDeliLine.shift()}`
+    return `Currently serving ${katzDeliLine.shift()}.`
   }
+}
+
+function currentLine (katzDeliLine) {
+  if (katzDeliLine.length == 0) {
+    return `The line is currently empty.`
+  } else {
+    var newline = [];
+    for (let x = 0; x < katzDeliLine.length; x++) {
+      var y = x + 1;
+      newline.push(` ${y}. ${katzDeliLine[x]}`)
+    }
+  } return `The line is currently:${newline}`
 }
