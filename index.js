bf11bf72ef3f6cfe203c7efbd8f033c09e7317e5
James Fernandez <jffernan@comcast.net>
2017-10-05 10:44:31 -0700

    Re-do & added pseudocode // comments

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-var katzDeliLine = [];
-
 /* 
 1. Declare a function named takeANumber.
 2. Function accepts 2 parameters named katzDeliLine & name, whose values are number and string. 
 3. When call function, return position in line.
 */
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
-  //return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
+function takeANumber(line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
+}
+
+/* 
+1. Declare a function named currentLine.
+2. Function accepts parameter named katzDeliLine, whose value is an array.
+3. When call function, returns the current line.
+*/
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty.";
+  }
+  const numbersAndNames = [];
+  for (let i=0; i<line.length; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`);
+  }
+  return `The line is currently: ${numbersAndNames.join(', ')}`;
 }
 
 /* 
@@ -17,30 +30,9 @@ function takeANumber(katzDeliLine, name) {
 3. Return the first person in line and then remove that individual from the line. 
 4. If there is nobody in line, it should return "There is nobody waiting to be served!"
 */
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
+function nowServing(line) {
+  if (!line.length) {
     return "There is nobody waiting to be served!";
-  } else {
-    var name = katzDeliLine[0];
-    katzDeliLine.splice(0, 1);
-    return `Currently serving ${name}.`;
   }
-}
-
-/* 
-1. Declare a function named currentLine.
-2. Function accepts parameter named katzDeliLine, whose value is an array.
-3. When call function, returns the current line.
-*/
-function currentLine(katzDeliLine) {
-    katzDeliLine = [];
-    if (katzDeliLine.length === 0) {
-      return "The line is currently empty.";
-    } else {
-      for(var i = 0; i < katzDeliLine.length; i++) {
-        katzDeliLine += (i + 1) + ". " + katzDeliLine[i] + ", ";
-      }
-      katzDeliLine = katzDeliLine.slice(0, katzDeliLine.length-2);
-      return "The line is currently: " + katzDeliLine;
-    }
+  return `Currently serving ${line.shift()}.`;
 }
