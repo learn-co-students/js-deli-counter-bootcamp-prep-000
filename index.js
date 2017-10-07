0a808b82f6217375b8055e6d422a409275740f06
hmelo1 <heriberto@melo.nyc>
2017-10-07 15:31:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var katzDeliLine = []
+function takeANumber(array, person){
+  array.push(person)
+  var spotInLine = parseInt(array.indexOf(person))
+  return (`Welcome, ${person}. You are number ${spotInLine+1} in line.`)
+}
+function nowServing(array){
+  var customer = array.shift()
+  if (array.length > 0){
+    //console.log(`Currently serving ${array[0]}`)
+    return (`Currently serving ${customer}.`)
+  }
+  else{
+    return ("There is nobody waiting to be served!")
+  }
+}
+function currentLine(array){
+  if (array.length === 0){
+    return "The line is currently empty."
+  }
+  else{
+    for (var i = 0; i < 1; i++){
+      return ("The line is currently: "+`${i+1}. ${array[i]},`+` ${i+2}. ${array[i+1]},`+` ${i+3}. ${array[i+2]}`)
+    }
+  }
+}
