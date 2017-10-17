a9f6dc804acc0b2edb86e621ffc295b5fc1aadbd
Karen G <kgraves0701@gmail.com>
2017-10-17 00:43:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var katzDeli = []
+
+function takeANumber(katzDeliLine,name) {
+  katzDeliLine.push(name)
+  return "Welcome, "+ name + ". You are number " + katzDeliLine.length + " in line."
+}
+
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  }else{
+    return "Currently serving " + katzDeliLine.shift() + "."
+  }
+}
+
+function currentLine(line) {
+  if (line.length === 0){
+    return "The line is currently empty."
+  }else{
+    var lineNames = ["The line is currently: "]
+    for (var i = 0; i<line.length-1; i++){
+      var n = i + 1
+      lineNames+= (n + ". " + line[i] + ", ")
+    }
+    n=line.length
+    lineNames+=(n + ". " + line[n-1])
+  }
+  return lineNames
+}
