50418ce7b9200e3ff0abc01d9d5b0adb79b8e27a
Adil Asif <asifadilanis@gmail.com>
2017-10-20 09:33:15 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
+return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
 function nowServing(katzDeliLine) {
@@ -20,8 +20,8 @@ function currentLine(katzDeliLine) {
     return "The line is currently empty.";
   } else {
     for(var i = 0; i < katzDeliLine.length; i++) {
-      array.push(" " + (i + 1) + ". " + katzDeliLine[i])
+      array.push(` ${i + 1}. ${katzDeliLine[i]}`)
     }
-    return "The line is currently:" + array;
+    return `The line is currently:${array}`;
   }
 }
