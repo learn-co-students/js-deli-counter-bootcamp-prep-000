c3347fe0067a2832fcd3bcd165a3f17aea9b4ef2
Noelle <noelle.legrain@gmail.com>
2017-10-20 15:23:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function takeANumber(katzDeli, string) {
+  katzDeli.push(string)
+  return `Welcome, ${string}. You are number ${katzDeli.length} in line.`
+}
+function nowServing(katzDeli) {
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+  else {
+    return `Currently serving ${katzDeli.shift()}.`
+  }
+}
+function currentLine(line) {
+  if (line.length === 0) {
+    return "The line is currently empty."
+  }
+  else {
+    var array = []
+    for (var i = 0; i < line.length; i++)
+    array.push(` ${i+1}. ${line[i]}` )
+    return `The line is currently:` +  array
+  }
+}
