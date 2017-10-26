479e00d749af3db3e71caa9735787d5d25ea0905
Christian Ahn <kwanghoonahn@gmail.com>
2017-10-26 22:44:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, newName) {
+  katzDeliLine.push(newName);
+  var lineNumber = katzDeliLine.length;
+  return `Welcome, ${newName}. You are number ${lineNumber} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length <= 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    var firstPerson = katzDeliLine.shift();
+    return `Currently serving ${firstPerson}.`;
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    var string = "The line is currently:";
+    for (var i = 0, n = katzDeliLine.length; i < n; i++) {
+      var turn = i + 1;
+      if (i != katzDeliLine.length - 1) {
+        string += ` ${turn}. ${katzDeliLine[i]},`
+      } else {
+        string += ` ${turn}. ${katzDeliLine[i]}`
+      }
+    }
+    return string;
+  }
+}
