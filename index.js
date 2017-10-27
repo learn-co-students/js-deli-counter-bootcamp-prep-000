9b237b5b1d41dd2c80c3a85bb5060f73311f28f8
Alishaan Asaria <alishaan.asaria@gmail.com>
2017-10-27 08:08:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine,name) {
-  return 'Welcome, ${name} You are number ${katzDeliLine.length + 2} in line.';
+  var number = katzDeliLine.length + 1;
+  katzDeliLine.push(name);
+  return "Welcome, " + name +". You are number " + number + " in line.";
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  }else {
+    return "Currently serving " + katzDeliLine.shift() + ".";
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  }else {
+    var result = [];
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      var temp = i + 1;
+      var construct = temp + ". " + katzDeliLine[i];
+      result.push(construct);
+    }
+    return "The line is currently: " + result.join(", ");
+  }
 }
