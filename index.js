9a7586413437ef65463ea40ae05d247d20141dea
Wenjie <xuwenjie8866@gmail.com>
2017-10-27 21:05:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+var katzDeli = [];
+var katzDeLine = [];
+function takeANumber(katzDeLine,name) {
+  katzDeLine.push(name);
+  return (`Welcome, ${name}. You are number ${katzDeLine.length} in line.`)
+  return katzDeLine;
+}
