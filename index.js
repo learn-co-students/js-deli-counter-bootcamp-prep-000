274bd6a00665002c7432edb21e0bf9c92221f8f2
Christian Ahn <kwanghoonahn@gmail.com>
2017-10-27 17:59:46 +0000

    Done.

--- index.js
+++ index.js
@@ -17,15 +17,10 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
-    var string = "The line is currently:";
+    var array = [];
     for (var i = 0, n = katzDeliLine.length; i < n; i++) {
-      var turn = i + 1;
-      if (i != katzDeliLine.length - 1) {
-        string += ` ${turn}. ${katzDeliLine[i]},`
-      } else {
-        string += ` ${turn}. ${katzDeliLine[i]}`
-      }
+      array.push(`${i + 1}. ${katzDeliLine[i]}`);
     }
-    return string;
+    return "The line is currently: " + array.join(", ");
   }
 }
