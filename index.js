469e81d0623e128a2d382b015cc324cab3c135c7
Julien Tregoat <jtregoatlearn@gmail.com>
2017-10-27 00:18:50 +0000

    Done.

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ function takeANumber(katzDeliLine, name){
 }
 
 function nowServing(array){
-  if (array = []) {
+  if (array.length === 0) {
     return `There is nobody waiting to be served!`
   }
   else {
-    return `Currently serving ${array[0]}.`;
-    array.shift;
+    return `Currently serving ${array.shift()}.`;
+  }
+}
+
+function currentLine(array){
+  if (array.length === 0) {
+    return "The line is currently empty."
+  }
+  else {
+    var line = "The line is currently: "
+    for (var n = 0; n < (array.length - 1); n++){
+      line = line + `${n + 1}. ${array[n]}, `
+    }
+    line = line + `${array.length}. ${array[array.length-1]}`
+    return line
   }
 }
