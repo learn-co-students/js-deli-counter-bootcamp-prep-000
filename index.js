3842c519175079ce5c83c534ce654d7f0c8d978b
Robert Meis <robert.meis@colorado.edu>
2017-10-30 22:28:15 +0000

    Done.

--- index.js
+++ index.js
@@ -3,9 +3,42 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
 
-  var position = katzDeli.length+1;
+  var position = katzDeliLine.length+1;
 
-  katzDeliLine.push("Welcome, " + name + "." + " You are number " + position + " in line.")
+  katzDeliLine.push(name);
 
-return katzDeliLine
+  return ("Welcome, " + name + "." + " You are number " + position + " in line.")
+
+
+}
+
+
+function nowServing(katzDeliLine) {
+
+ if (katzDeliLine.length === 0) {
+   return "There is nobody waiting to be served!"
+ } else {
+
+   return "Currently serving " + katzDeliLine.shift() + "."
+ }
+
+}
+
+
+
+function currentLine(line) {                                               var sent = "The line is currently: "
+  if (line.length === 0) {
+    return "The line is currently empty."
+  } else {
+    for (var i = 0; i < line.length; i++) {
+
+      sent += i+1;                                                             sent += ". ";
+      sent += line[i];                                                         sent += ", ";
+
+    }
+  }
+    var newSent = sent.split('');
+    newSent.pop();
+    newSent.pop();
+    return newSent.join('');
 }
