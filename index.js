a80267c443a9503805b9bfccbd75aeaf97f7ad18
Houston Green <greenhouston@gmail.com>
2017-11-06 21:26:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var katzDeli = [];
+
+function takeANumber(katzDeli, name) {
+      katzDeli.push(name)
+      return "Welcome, " + name + ". You are number " + katzDeli.length + " in line."
+}
+
+function nowServing(deliLine) {
+    if (deliLine.length === 0) {
+      return 'There is nobody waiting to be served!'
+    } else {
+      return "Currently serving " + deliLine.shift() + "."
+    }
+}
+
+var array = ["The line is currently:"];
+function currentLine(line) {
+  for (let i = 0; i < line.length; i++) {
+    if (line.length >= 0) {
+      var string = (i + 1) + ". " + line[i]
+      string.slice(beginIndex[, endIndex])
+      array.push(string)
+    } else {
+      var otherStr = 'The line is currently empty.'
+      array.push(otherStr)
+    }
+  }
+  return array.join(" ")
+}
