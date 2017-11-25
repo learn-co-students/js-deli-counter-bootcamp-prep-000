fde41685f2b6401d519fc9dd9b7bf902fcff0d4b
Mikkel Ridley <mikkel250@gmail.com>
2017-11-25 23:43:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  var lineHash = {name: katzDeliLine};
+  for (i = 0; i < name; i++) {
+  `Welcome, ${name}. You are number ${katzDeliLine + 1} in line.`;
+    
+  }
+}
