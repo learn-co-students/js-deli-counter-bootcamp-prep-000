e05c7b08ee76b266c3dca80aea98d38133eafe23
Mikkel Ridley <mikkel250@gmail.com>
2017-11-26 01:03:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
-  var lineHash = {name: katzDeliLine};
-  for (i = 0; i < name; i++) {
+  
+  for (let i = 0; i < katzDeliLine.length; i++) {
+  katzDeliLine.push;
+  
   `Welcome, ${name}. You are number ${katzDeliLine + 1} in line.`;
-    
   }
 }
