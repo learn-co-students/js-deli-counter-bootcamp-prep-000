36f64eb2226d8d54c11b39f9d692cce34a17fc94
osama miro <o.miro650@gmail.com>
2017-11-27 18:25:50 +0000

    Done.

--- index.js
+++ index.js
@@ -7,15 +7,27 @@ function takeANumber(katzDeliLine,name){
  }
 
 function nowServing(katzDeliLine){
+  if(katzDeliLine[0] === undefined){
+    return 'There is nobody waiting to be served!';
+  }
   for(var i = 0; i < katzDeliLine.length; i++){
     if(katzDeliLine[i] !== 0){
       return "Currently serving " + katzDeliLine.shift(katzDeliLine[i]) + ".";
-    } else if(katzDeliLine[0] === undefined){
-      return 'There is nobody waiting to be served!';
     }
   }
 }
 
+function currentLine(line){
+var numbersAndNames = [];
+  if (line.length === 0){
+    return "The line is currently empty.";
+  } else {
+    for (var i = 0, l = line.length; i < l; i++) {
+   numbersAndNames.push( i + 1 + ". " + line[i])
+    }
+  }
+return "The line is currently: " + numbersAndNames.join(", ");
+}
 
 
 
