03394b0dd81d3f77ab4c2eccfaf81c355b9d1c2f
Mikkel Ridley <mikkel250@gmail.com>
2017-11-27 03:51:11 +0000

    Done.

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
   
-  /*
-  var i = 0;
-  var welcomeMsg = `Welcome, ${katzDeliLine[i]}. You are number ${i + 1} in line.`;
-  for (let i = 0; i < katzDeliLine.length; i++) {
-  katzDeliLine.push(name);
-  return welcomeMsg;
-  }
-  */
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
