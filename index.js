ae24f5093f2e8050e6bde1327886464f959efb44
Angela <wang.angela90@gmail.com>
2017-11-27 20:33:59 +0000

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ function takeANumber (katzDeliLine, newPerson){
   katzDeliLine.push(newPerson);
   return takeANumber;
 }
+
+function nowServing () {
+  do {
+    katzDeliLine.shift();
+    i++
+  } while (katzDeliLine[0]);
+
+  let i = 0
+  while (i < katzDeliLine.length) {
+  return ("There is nobody waiting to be served!");
+}
+}
+//first person in line
+//remove that person form the line
+//If no one in line, return"Nobody in line"
