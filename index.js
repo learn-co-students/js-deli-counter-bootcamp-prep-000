b6b054e090e5a8943995af22adf31e312e53775d
Mikkel Ridley <mikkel250@gmail.com>
2017-11-27 03:50:39 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,29 @@ function takeANumber(katzDeliLine, name) {
   return welcomeMsg;
   }
   */
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    var firstInLine = katzDeliLine[0];
+    katzDeliLine.shift();
+    return `Currently serving ${firstInLine}.`;
+  }
+  else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(line) {
+  var lineList = [];
+  for(let i = 0; i < line.length; i++) {
+      lineList.push(` `+[i+1]+`. ` + line[i]);
+  }
+      if (line.length === 0) {
+        return "The line is currently empty.";
+      }
+      else {
+    return `The line is currently:` + lineList;
+    
+    }
+  
+}
