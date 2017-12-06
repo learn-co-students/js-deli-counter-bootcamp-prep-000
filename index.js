0cc5939c0159e91dbc2a8d4002c0ed347eb32619
Mariel Schlaefer <mschlaefer00@gmail.com>
2017-12-06 20:59:53 +0000

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function takeANumber(katzDeliLine, newName) {
 
 function nowServing(katzDeliLine) {
   function nameServed() {
-    return katzDeliLine[0];
+    return "Currently serving " + katzDeliLine[0] + ".";
   }
   if (katzDeliLine = []) {
     return "There is nobody waiting to be served!"
