bbd5cce9292150d9f60c8cbfe5259f933dd14e4d
Peter Coughlin <coughlin1966@gmail.com>
2017-12-07 16:47:46 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function takeANumber(katzDeliLine, nPerson) {
   return `Welcome, ${nPerson}. You are number ${katzDeliLine.length} in line.`
 }
 takeANumber(Ada)
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) { 'The line is currently empty.'}
+  else {katzDeliLine.slice(0,1)}
+  katzDeliLine.shift
+}
+nowServing()
