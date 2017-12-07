43f0661b7fdae240f09660c3a2e196b2e7b6f80b
Mariel Schlaefer <mschlaefer00@gmail.com>
2017-12-07 01:26:49 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, newName) {
    katzDeliLine.push(newName)
-   return "Welcome, " + (katzDeliLine.length - 1) + ". You are number " + katzDeliLine.length + " in line.";
+   return "Welcome, " + katzDeliLine[-1] + ". You are number " + katzDeliLine.length + " in line.";
 }
 
 function nowServing(katzDeliLine) {
