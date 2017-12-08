34f368c919d563a6af46b0dba6cd161eb83028e5
Peter Coughlin <coughlin1966@gmail.com>
2017-12-08 17:12:26 +0000

    Done.

--- index.js
+++ index.js
@@ -13,14 +13,16 @@ function nowServing(katzDeliLine) {
 }
 nowServing()
 
+//var linePosition = []
+
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length == 0)
   {return 'The line is currently empty.'}
 
-  var linePosition = []
+var linePosition = []
 
-  for (let i = 0; katzDeliLine.length > i; i++)
-    {linePosition.push(`The line is currently: ${i+1}. ${katzDeliLine[i]}, `)}
-  return linePosition
+  for (let i = 0; i < katzDeliLine.length; i++)
+    {linePosition.push( ` ${i+1}. ${katzDeliLine[i]}`)}
+  return 'The line is currently:' + linePosition
 }
 currentLine()
