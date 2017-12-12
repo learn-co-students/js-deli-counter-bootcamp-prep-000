2d3b0130bb91615da791b9ce0bfc2d133f184b2f
Ryan Shur <ryanshur32@yahoo.com>
2017-12-12 04:45:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+function takeANumber(katzDeliLine,NewPerson){
+katzDeliLine.push(NewPerson);
+return 'Welcome, ' + NewPerson + '. You are number '+ katzDeliLine.length + ' in line.';
+}
+
+
 function currentLine(katzDeliLine) {
   var newLine = [];
+    if (katzDeliLine.length === 0){
+      return "The line is currently empty.";
+    }
   for(var i = 0; i < katzDeliLine.length; i++){
      newLine.push((i + 1) + '. ' +  katzDeliLine[i]);
   }  
-
-  return ('The line is currently: '+ newLine.join(', '));
+    return ('The line is currently: '+ newLine.join(', '));
 }
