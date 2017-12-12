b86619ecb0c55c50dda0b5d2548a5b86380b147a
Ryan Shur <ryanshur32@yahoo.com>
2017-12-12 05:15:13 +0000

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ katzDeliLine.push(NewPerson);
 return 'Welcome, ' + NewPerson + '. You are number '+ katzDeliLine.length + ' in line.';
 }
 
+function nowServing(katzDeliLine){
+  for (var i = 0; i < katzDeliLine.length; i++){ 
+    katzDeliLine.shift(i)
+  
+  }
+   return 'currently serving ' + katzDeliLine.shift(i) +'.' ;
+}
+  if(katzDeliLine.length === 0){
+  return 'There is nobdy waiting to be served!';
+  }
+}
 
 function currentLine(katzDeliLine) {
   var newLine = [];
