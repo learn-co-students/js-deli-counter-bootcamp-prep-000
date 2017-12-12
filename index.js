3b09f31b644cd98e8e412f6f92666167cccc3cc2
Quentin <q.smith1@yahoo.com>
2017-12-12 20:11:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+var katzDeli = [];
+var otherDeli = ["Steven", "Blake", "Avi"];
+
+function takeANumber(katzDeliLine, newCustomer) {
+  newCustomer = "Ada"
+  let i = 0
+  return "Welcome, " + newCustomer[i] + ". You are number " + [i+1] + " in line."
+  if (otherDeli.length > 0) {
+    otherDeli.push(newCustomer)
+    return "Welcome, " + newCustomer[i] + ". You are number " + [i+1] + " in line."
+  }
+}
+
+
+function nowServing(katzDeliLine) {
+  var currentCustomer = katzDeliLine.shift()
+  if(katzDeliLine.length > 0){
+     return 'Currently serving ' + currentCustomer +'.' ;
+}    else if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      return "The line is currently: " + (i+1) +  ". " + katzDeliLine;
+    } 
+  }
+}
