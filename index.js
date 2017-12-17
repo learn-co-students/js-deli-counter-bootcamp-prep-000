9ae7f2d490deee8179e5178e00722c671dc5fbee
jencekade <gaephuoc@gmail.com>
2017-12-17 03:10:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var katzDeliLine = [];
 
-function takeANumber(array, name){
+function takeANumber(array, name) {
   array.push(name);
   return `Welcome, ${name}. You are number ${array.length} in line.`;
 }
 
 function currentLine(array) {
-  if (array.length ==0) {
+  if (array.length == 0) {
     return `The line is currently empty.`;
   }
   let stringerfy = "";
-  for (let index =0; index< array.length; index++) {
-    stringerfy = stringerfy + (index+1) +". "+array[index]+", "
+  for (let index = 0; index < array.length; index++) {
+    stringerfy = stringerfy + (index + 1) + ". " + array[index] + ", ";
+    //stringerfy = stringerfy +`${index+1}, ${array.index}, `;
   }
   stringerfy = stringerfy.toString();
   stringerfy = stringerfy.slice(0, -2);
@@ -19,11 +20,13 @@ function currentLine(array) {
 }
 
 function nowServing(array) {
-  if (array.length==0){
+  if (array.length == 0) {
     return `There is nobody waiting to be served!`;
   }
   return `Currently serving ${array[0]}.`;
-  array.shift();
-  return array;
+  //array.shift();
+  array.splice(0, 1);
 
+  //delete array[0];
+  return array;
 }
