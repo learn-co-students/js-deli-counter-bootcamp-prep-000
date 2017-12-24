2e8614377af937df40df934b5fae9da860ea0776
Justin K <justin-kim1@hotmail.com>
2017-12-24 04:31:30 +0000

    Done.

--- index.js
+++ index.js
@@ -9,16 +9,12 @@ function nowServing(line,name){
   return "There is nobody waiting to be served!"
 }
 function currentLine(line, name){
-var peopleOnLine = ""
-if (line.length !== 0){
-  for (let i = 0; i < line.length - 1; i++){
-    (peopleOnLine += `${i + 1}. ${line[i]}`)
-    }
-    
-    return `The line is currently: ${peopleOnLine} ${line.length}. line[line.length]`
-}
-  else {
+if (line.length === 0){ 
   return "The line is currently empty."
-  
+}
+var lineNow = [];
+for(var i=0; i < line.length; i++) {
+    lineNow.push(i+1 + ". "+ line[i]);
   }
+  return "The line is currently: " + lineNow.join(', ');
 }
