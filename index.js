0669331dc15a360ebc183fc514627be9ee59ae0d
Justin K <justin-kim1@hotmail.com>
2017-12-24 01:38:19 +0000

    Done.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function takeANumber(line,name){
   line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
-function nowServing(line){
-  if(i = 0; i < line.length; i++){
-  return `Currently serving ${line.shift}`
+function nowServing(line,name){
+  for (let i = 0; i < line.length; i++){
+  return `Currently serving ${line.shift(name)}.`
   }
   return "There is nobody waiting to be served!"
 }
