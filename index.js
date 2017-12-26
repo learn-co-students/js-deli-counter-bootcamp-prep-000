12424ccc452fa350905d8215125cca4bcc1a82e2
Daniel Cotic <d.cotic26@gmail.com>
2017-12-26 00:00:42 +0000

    Done.

--- index.js
+++ index.js
@@ -14,10 +14,16 @@ function nowServing(deliLine){
   }
 }
 
-function currentLine(line){
-  if (line.length > 0) {
-    return "The line is currently: 1. Bill, 2. Jane, 3. Ann"
-  } else {
+function currentLine(line) {
+  if (!line.length) {
     return "The line is currently empty."
   }
-}
+
+  const numbersAndNames = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
