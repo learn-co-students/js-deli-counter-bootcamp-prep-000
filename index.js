904bab582cffed64637559b8ec498f77fe7d1c1b
Shirley Zhang <shirleyz9402@yahoo.com>
2017-12-28 22:59:55 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function takeANumber (katzDeliLine,name) {
   katzDeliLine.push(name)
   return (`Welcome, ${name}. You are number ${n} in line.`)
 }
+function nowServing(katzDeliLine) {
+  if (katzDeliLine[0] === undefined)
+  return (`There is nobody waiting to be served!`)
+  else
+  return (`Currently serving ${katzDeliLine[0]}.`)
+  katzDeliLine.shift()
+}
