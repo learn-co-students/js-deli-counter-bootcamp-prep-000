2d1f288b0a582444630a6f8e6e7bece574a84fb4
Shirley Zhang <shirleyz9402@yahoo.com>
2017-12-28 23:01:54 +0000

    Done.

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine[0] === undefined)
   return (`There is nobody waiting to be served!`)
   else
-  return (`Currently serving ${katzDeliLine[0]}.`)
-  katzDeliLine.shift()
+  return (`Currently serving ${katzDeliLine.shift()}.`)
+ 
 }
