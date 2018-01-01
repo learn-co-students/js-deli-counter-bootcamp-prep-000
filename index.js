d2f173dfa0c70860a386690b6657a5a4ad2cceb6
Carl Cliver <ccliver@gmail.com>
2018-01-01 00:54:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+var katzDeli = []
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  }
+  else {
+    var name = katzDeliLine[0]
+    katzDeliLine.shift()
+    return `Currently serving ${name}.`
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'The line is currently empty.'
+  }
+  else {
+    var line = 'The line is currently:'
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      line += `${i > 0 ? ',' : ''} ${i + 1}. ${katzDeliLine[i]}`
+    }
+  }
+  return line
+}
