db5e30952bda9963aef5071b66aa7ce474d504a7
craig sperlazza <craig.sperlazza@gmail.com>
2018-01-08 17:22:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-var katzDeliLine = [];
+var katzDeli = [];
 
 function takeANumber (katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length == 0) {
-    return `There is nobody waiting to be served!`
+function nowServing (katzDeliLine) {
+  if (katzDeliLine.length == 0){
+    return ("There is nobody waiting to be served!")
   } else {
-    return `Currently serving ${katzDeliLine.shift()}.`
+    return (`Currently serving ${katzDeliLine.shift()}.`)
   }
 }
 
-function currentLine(katzDeliLine) {
+function currentLine (katzDeliLine){
   var newArray = [];
 
   if (katzDeliLine.length == 0) {
-    return "The line is currently empty."
+    return `The line is currently empty.`
   } else {
-    for (var i = 0; i < katzDeliLine.length; i++) {
-      newArray.push (i+1 + ". " + katzDeliLine[i])
+    for (var i = 0; i < katzDeliLine.length; i++){
+      newArray.push(i + 1 + ". " + katzDeliLine[i])
     }
     return "The line is currently: " + newArray.join(', ');
   }
