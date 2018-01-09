16cc218711ae9d75cda75064e928683240ccef4b
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-09 19:05:04 +0000

    Done.

--- index.js
+++ index.js
@@ -9,16 +9,13 @@ function currentLine(line){
   for(var i=0; i<line.length; i++) {
     lineNamesandNumbers.push(i+1 + ". "+ line[i]);
   }
-  console.log("The line is currently: " + lineNamesandNumbers)
   return "The line is currently: " + lineNamesandNumbers.join(', ');
 }
 
 function nowServing(line) {
   if(!line.length) {
-    console.log("There is nobody waiting to be served!")
     return "There is nobody waiting to be served!"
   } else {
-    //console.log("Currently serving " + line.shift());
     return "Currently serving " + line.shift();
   }
 }
@@ -26,10 +23,9 @@ function nowServing(line) {
 function takeANumber(line, name){
   line.push(name);
   
-  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
-  
   return "Welcome, " + name + ". You are number " + line.length + " in line.";
 }
+
 takeANumber(katzDeli, "Ada");
 takeANumber(katzDeli, "Grace");
 takeANumber(katzDeli, "Kent");
