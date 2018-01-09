d55bcc9755d948591c9d349022658a3384c1d2ec
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-09 18:31:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
- var katzDeli = [];
-function takeANumber(katzDeliLine, katzDeli) {
+
+function takeANumber(katzDeliLine) {
+   var katzDeli = [];
   var nextGuest = ['Ada', 'Grace', 'Kent'];
   for (var i = 1; i < katzDeli.length; i++);
   katzDeli.push(nextGuest[i]);
