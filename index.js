8aabf1026bd3498abc77d8a3a92dcdfbe10d24fa
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-09 19:03:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
+var katzDeli = [];
 
-function takeANumber(katzDeliLine) {
-   var katzDeli = [];
-  var nextGuest = ['Ada', 'Grace', 'Kent'];
-  for (var i = 1; i < katzDeli.length; i++);
-  katzDeli.push(nextGuest[i]);
-  return ('Welcome, ' + nextGuest[i] + '. You are number ' + i + ' in line.');
-
+function currentLine(line){
+  if(!line.length) {
+    return "The line is currently empty.";
+  }
+  var lineNamesandNumbers = [];
+  
+  for(var i=0; i<line.length; i++) {
+    lineNamesandNumbers.push(i+1 + ". "+ line[i]);
+  }
+  console.log("The line is currently: " + lineNamesandNumbers)
+  return "The line is currently: " + lineNamesandNumbers.join(', ');
 }
+
+function nowServing(line) {
+  if(!line.length) {
+    console.log("There is nobody waiting to be served!")
+    return "There is nobody waiting to be served!"
+  } else {
+    //console.log("Currently serving " + line.shift());
+    return "Currently serving " + line.shift();
+  }
+}
+
+function takeANumber(line, name){
+  line.push(name);
+  
+  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
+  
+  return "Welcome, " + name + ". You are number " + line.length + " in line.";
+}
+takeANumber(katzDeli, "Ada");
+takeANumber(katzDeli, "Grace");
+takeANumber(katzDeli, "Kent");
+currentLine(katzDeli);
+nowServing(katzDeli);
+takeANumber(katzDeli, "Matz"); 
+currentLine(katzDeli); 
+nowServing(katzDeli);
+currentLine(katzDeli);
