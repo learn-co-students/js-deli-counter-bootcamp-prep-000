33611d581b1b296e79e2c8f7ca208cbd2345ed83
dokinqs <22225317+dokinqs@users.noreply.github.com>
2018-01-09 12:37:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,74 @@
-var katzDeli = [];
-
-function takeANumber(katzDeli,name){
-  var numba = katzDeli.length + 1;
-  katzDeli.push(name);
-  return 'Welcome, ' + name + '. You are number ' + numba + ' in line.';
-  // katzDeli.concat(name);
-}
-
-function nowServing(katzDeli){
-  if (katzDeli.length > 0) {
-    var servin = katzDeli[0];
-    katzDeli.shift();
-    return "Currently serving " + servin + ".";
-
-  } else {
-    return "There is nobody waiting to be served!";
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
   }
-  // katzDeli.splice(0,1);
-  // return katzDeli;
+  const numbersAndNames = []
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
+
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }
+  return `Currently serving ${line.shift()}.`
+};
+
+function takeANumber(line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
+};
+
+// super fail interview, seppuku
+
+function takeANumber(line) {
+  if (!line.length) {
+      if (!i) {
+         return `You are number 1.`; 
+      }
+    return `You are number {i++}.`; 
+  }
+  
+  let lastIndex = line.length - 1;
+  let i = line[lastIndex] + 1;
+  
+	return `You are number ${i}.`; 
 }
 
-function currentLine(katzDeli){
-  if (katzDeli.length > 0) {
-    var arr = ':';
-    for (var i=0; i<katzDeli.length; i++) {
-      var numb = i + 1;
-      var arr2 = ' ' + numb + '. ' + katzDeli[i] + ',';
-      arr = arr + arr2;
-    }
-    arr = arr.slice(0, -1);
-    return 'The line is currently' + arr;
-  } else {
-    return 'The line is currently empty.';
-  }
-}
+// first person enters empty line
+// initial line [], first person of the day
+// 1
+
+// first person enters empty line
+// initial line [], last number that left was 16
+// 16++ = 17
+// does the value of i get saved? if not then I'm wrong here
+
+// a second person enters a one person line
+// initial line [1]
+// 0 = 1 - 1
+// 2 = 1 + 1 
+
+// a third person enters a two person line
+// initial line [4, 5]
+// 1 = 2 - 1
+// 6 = 5 + 1
+
+// a fourth person enters a three person line
+// initial line [5, 6, 7]
+// 1 = 3 - 1
+// 8 = 7 + 1
+
+// a fifth person enters a six person line
+// initial line [10, 11, 12, 13, 14, 15]
+// 5 = 6 - 1
+// 16 = 15 + 1 
+
+//
+
+
+
+
+
