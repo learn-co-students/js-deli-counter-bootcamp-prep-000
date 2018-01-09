aff7e343b3d381ca6377c7113a89022f55185b78
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-09 18:21:20 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function takeANumber(katzDeliLine) {
   var katzDeli = [];
   var nextGuest = ['Ada', 'Grace', 'Kent'];
   for (var i = 1; i < katzDeli.length; i++);
-  katzDeli.push(nextGuest);
-  return ('Welcome, ' + nextGuest[i-1] + '. You are number ' + i + ' in line.');
+  katzDeli.push(nextGuest[i]);
+  return ('Welcome, ' + nextGuest[i] + '. You are number ' + i + ' in line.');
 
 }
