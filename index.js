10159d4075f9841746f43c256866bd3b5018499c
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-09 19:13:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var katzDeli = [];
 
+takeANumber(katzDeli, "Ada");
+takeANumber(katzDeli, "Grace");
+takeANumber(katzDeli, "Kent");
+currentLine(katzDeli);
+nowServing(katzDeli);
+takeANumber(katzDeli, "Matz"); 
+currentLine(katzDeli); 
+nowServing(katzDeli);
+currentLine(katzDeli);
+
 function currentLine(line){
-  if(!line.length) {
+  if (line.length === 0) {
     return "The line is currently empty.";
   }
   var lineNamesandNumbers = [];
@@ -13,25 +23,16 @@ function currentLine(line){
 }
 
 function nowServing(line) {
-  if(!line.length) {
+  if(line.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
     return "Currently serving " + line.shift() + '.';
   }
 }
 
-function takeANumber(line, name){
+function takeANumber(katzDeliLine, name){
   line.push(name);
   
-  return "Welcome, " + name + ". You are number " + line.length + " in line.";
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
 }
 
-takeANumber(katzDeli, "Ada");
-takeANumber(katzDeli, "Grace");
-takeANumber(katzDeli, "Kent");
-currentLine(katzDeli);
-nowServing(katzDeli);
-takeANumber(katzDeli, "Matz"); 
-currentLine(katzDeli); 
-nowServing(katzDeli);
-currentLine(katzDeli);
