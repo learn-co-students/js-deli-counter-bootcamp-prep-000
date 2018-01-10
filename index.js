b92d737324f2369da7f6b2e6611aae80eb7bc1f1
Daniel Baamonde <dannyb21892@gmail.com>
2018-01-10 07:22:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+var katzDeliLine = [];
+
+function takeANumber(line, next) {
+  line.push(next);
+  return (`Welcome, ${next}. You are number ${line.length} in line.`)
+}
+
+function nowServing(line) {
+  if (line.length > 0) {
+    return (`Currently serving ${line.shift()}`);
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(line) {
+  var output = "The line is currently"
+  var original = output
+  for (let i = 0; i === line.length; i++) {
+    if (i === 0) {
+      output = output + ":"
+    }
+    output = output + ` ${i+1}. ${line[i]}`
+    if (i !== line.length - 1) {
+         output = output + "," 
+    }
+  }
+  if (output === original) {
+    output = output + " empty."
+  }
+  return output
+}
