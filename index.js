ec19678fddb352eff380d8418c398cea9c9c5e51
Daniel Baamonde <dannyb21892@gmail.com>
2018-01-10 07:25:59 +0000

    Done.

--- index.js
+++ index.js
@@ -2,32 +2,32 @@ var katzDeliLine = [];
 
 function takeANumber(line, next) {
   line.push(next);
-  return (`Welcome, ${next}. You are number ${line.length} in line.`)
+  return (`Welcome, ${next}. You are number ${line.length} in line.`);
 }
 
 function nowServing(line) {
   if (line.length > 0) {
-    return (`Currently serving ${line.shift()}`);
+    return (`Currently serving ${line.shift()}.`);
   }
   else {
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
   }
 }
 
 function currentLine(line) {
-  var output = "The line is currently"
-  var original = output
-  for (let i = 0; i === line.length; i++) {
+  var output = "The line is currently";
+  var original = output;
+  for (let i = 0; i >= line.length; i++) {
     if (i === 0) {
-      output = output + ":"
+      output = output + ":";
     }
     output = output + ` ${i+1}. ${line[i]}`
     if (i !== line.length - 1) {
-         output = output + "," 
+         output = output + "," ;
     }
   }
   if (output === original) {
-    output = output + " empty."
+    output = output + " empty.";
   }
-  return output
+  return output;
 }
