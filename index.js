a48df79b5e85a30c7947c025f6b42242ca152d25
Virginia Herron <inadream@gmail.com>
2018-01-12 04:28:51 +0000

    Done.

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ function currentLine(katzDeliLine) {
   if(katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
-    namePostion = [];
-    for (i = 0; i < katzDeliLine.length; i++) {
-      namePostion.push(`${i + 1}. ${katzDeliLine[1]}`)
+    var namePostion = [];
+    for (let i = 0; i < katzDeliLine.length; i++) {
+      namePostion.push(` ${i + 1}. ${katzDeliLine[i]}`)
     }
-  return "The line is currently: "
+  return "The line is currently:" + namePostion
   }
 }
