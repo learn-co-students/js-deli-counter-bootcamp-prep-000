3ef0327d4ed510b0655d2d90e7df3acdd1616688
Virginia Herron <inadream@gmail.com>
2018-01-12 02:43:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine,name) {
-katzDeli.push(katzDeliLine);
-  return katzDeli;
+ katzDeliLine.push(name);
+ var postion = katzDeliLine.length;
+ return `Welcome, ${name}. You are number ${postion} in line.`
+}
+
+function nowServing() {
+  if (katzDeliLine.length > 0) {
+    return katzDeliLine[1]
+    katzDeliLine.unshift()
+  } else {
+    return "The line is currently empty."
+  }
 }
