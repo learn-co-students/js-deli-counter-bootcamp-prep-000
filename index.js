d6ebd4cc8e4a5b13b1e0fee827bf0cb4b0c7b587
rosie w <rosie.wilt@gmail.com>
2018-01-23 04:36:30 +0000

    Done.

--- index.js
+++ index.js
@@ -35,9 +35,22 @@ function nowServing(line){
 //
 
 
+// function currentLine(line) {
+//   var linearray = []
+//   if (line.length == 0) {
+//   console.log(`The line is currently: empty`)};
+//   for (var i = 0; i < line.length; i++) {
+//
+// }
+
+
 function currentLine(line) {
-  if (line.length > 0) {
-  console.log(`The line is currently: ${line}`)}
-  else
-  return `The line is currently empty.`
-}
+  var linearray = []
+  if (line.length == 0) {
+  return `The line is currently empty.`}
+  else {
+  for (var i = 0; i < line.length; i++) {
+    linearray.push(` ${i+1}`+ `.` + ` ${line[i]}`)
+    }
+    return `The line is currently:` + `${linearray}`}
+  }
