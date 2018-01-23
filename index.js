47327730ebb3ff6f69dcfe9025485ce6c72b851c
Nic Travis <nptravis@yahoo.com>
2018-01-23 12:08:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 var katzDeliLine = [];
 
-   	function takeANumber(katzDeliLine, name) {
-   		katzDeliLine.push(name);
-   		return("Welcome, " + name + ". You are number " + (katzDeliLine.length) + " in line.");
-   	}
+    function takeANumber(katzDeliLine, name) {
+      katzDeliLine.push(name);
+      return("Welcome, " + name + ". You are number " + (katzDeliLine.length) + " in line.");
+    }
 
-   	function nowServing(katzDeliLine) {
-   		if (katzDeliLine.length > 0) {
-        return ("Currently serving " + katzDeliLine[0] + ".");
-        katzDeliLine.shift();
-        debugger;
-   			}
-   		else {
+    function nowServing(katzDeliLine) {
+      if (katzDeliLine.length > 0) {
+        var up = katzDeliLine.splice(0,1);
+        return ("Currently serving " + up + ".");
+        
+        }
+      else {
         return ("There is nobody waiting to be served!");
-   	      }
-  	}
+          }
+    }
 
 
-	function currentLine(katzDeliLine) {
-    	var line = 0
-
-     while (line < katzDeliLine.length); {
-        return ("The line is currently " + (line + 1) + ". " + katzDeliLine[line]);
-  			line++;
+  function currentLine(katzDeliLine) {
+    var line = 0;
+    
+    if (katzDeliLine.length === 0) {
+      return "The line is currently empty.";
       }
 
-		if (katzDeliLine.length == 0) {
-      return "The line is currently empty."
-			}
-	}
+    var current = "The line is currently: ";
+    while (line < katzDeliLine.length) {
+      if (line+1 === katzDeliLine.length){
+        current += (line + 1) + ". " + katzDeliLine[line];
+        line++;
+      }
+      else{
+        current += (line + 1) + ". " + katzDeliLine[line] + ", ";
+        line++;
+      }
+    }
+      return current;
+  }
