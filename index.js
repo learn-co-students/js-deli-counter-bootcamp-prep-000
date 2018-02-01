e6c212105368621109b0b0e5e0bc186d037ad568
Nik <shaione3@gmail.com>
2018-02-01 17:51:53 +0000

    Done.

--- index.js
+++ index.js
@@ -8,45 +8,86 @@ function takeANumber(line, name){
 
 function nowServing(line){
 
-  if (line.length !== 0){
+  if (!line.length) {
+    //if line.length is evaluated as "falsey", aka "0" or empty, make it true and return the following
 
-    let name = line[0];
-
-    line.shift();
-
-    return `Currently serving ${name}.`
+    return "There is nobody waiting to be served!";
 
   } else {
 
-    return "There is nobody waiting to be served!"
+    return `Currently serving ${line.shift()}.`;
 
   }
 }
 
+    // * NOTE to SELF: Below is my code, which passed, but was less efficient than the final solution above
+//
+// function nowServing(line){
+//
+//   if (line.length !== 0){
+//
+    // * NOTE: easier to just do "if(!line.length){}".. it'll run the "if()" if line.length is equal to "0" or is empty (i.e. is "falsey"), otherwise it will run what's inside the "else" block
+//
+//     let name = line[0];
+//
+//     line.shift();
+//
+//     return `Currently serving ${name}.`
+//
+    // * NOTE: Didn't need to do all that crap above. line.shift() not only removes the first element from the array (line[0]) but it also RETURNS that removed element, so no need to create that extra "name" variable
+//
+//   } else {
+//
+//     return "There is nobody waiting to be served!"
+//
+//   }
+// }
 
-function currentLine(line) {
+function currentLine(line){
 
-  if (line.length !== 0) {
+  if(!line.length) {
 
-    let lineList = "The line is currently: ";
+    return "The line is currently empty."
 
-    for (let i = 0; i < line.length; i++) {
-      if (i === line.length - 1) {
+  } else {
 
-        lineList += `${i + 1}. ${line[i]}`;
+    const numbersAndNames = [];
 
-      } else {
-
-        lineList += `${i + 1}. ${line[i]}, `;
-
+      for (let i = 0; i < line.length; i++) {
+        numbersAndNames.push(`${i + 1}. ${line[i]}`);
       }
-    }
 
-    return lineList;
-
-  } else {
-
-    return "The line is currently empty.";
-
+      return `The line is currently: ${numbersAndNames.join(', ')}`;
+      // can use array.join(); to add the ", " between each of the array elements when returned, so no need to do that if/else within the for loop
   }
+
 }
+
+// * NOTE to Self: Again, my code below, which passed, but not as efficient as final solution above
+
+// function currentLine(line) {
+//
+//   if (line.length !== 0) {
+//
+//     let lineList = "The line is currently: ";
+//
+//     for (let i = 0; i < line.length; i++) {
+//       if (i === line.length - 1) {
+//
+//         lineList += `${i + 1}. ${line[i]}`;
+//
+//       } else {
+//
+//         lineList += `${i + 1}. ${line[i]}, `;
+//
+//       }
+//     }
+//
+//     return lineList;
+//
+//   } else {
+//
+//     return "The line is currently empty.";
+//
+//   }
+// }
