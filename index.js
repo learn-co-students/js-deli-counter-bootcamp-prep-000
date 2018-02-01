0f067fceb7b3dc7d503a8fc09e3097317b10f897
James Montoya <jhenrymontoya@gmail.com>
2018-02-01 20:48:23 +0000

    Done.

--- index.js
+++ index.js
@@ -3,27 +3,32 @@ function takeANumber(line, name) {
   line.push(name);
   return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
+
 function nowServing(line) {
-  let i = 0;
-  while (i < line.length) {
-   i++;
-}
-if (line.length === 0) {
-  return "There is nobody waiting to be served!";
-} else {
-  return `Currently serving ${line.shift()}.`;
-}
-}
-var lineup = [];
-function currentLine(line) {
-  let i = 0;
-  while (i < line.length) {
-    line.push(` ` + [i+1] + `. ` + line[i]);
-    i++;
+  if (line.length === 0) {
+    var response = "There is nobody waiting to be served!";
+    console.log(response);
+    return response;
+  } else {
+    var person = line.shift();
+    var response1 = "Currently serving " + person + ".";
+    console.log(response1);
+    return response1;
   }
-if (line.length === 0) {
-  return "The line is currently empty.";
-} else {
-  return `The line is currently: ` + line;
 }
+
+function currentLine(array) {
+  if (array.length < 1) {
+    return "The line is currently empty.";
+  } else {
+    var queue = [];
+    array.forEach(function(name, index) {
+      queue.push((index + 1).toString() + ". " + name);
+    });
+  var response = "The line is currently: " + queue.join(", ");
+  return response;
+  }
 }
+  
+  
+  
