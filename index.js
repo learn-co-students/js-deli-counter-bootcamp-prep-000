3961a76ebbae887f62330490e64d1fb4ab0b61e5
carlitos321 <ca.cabrera321@gmail.com>
2018-02-02 21:31:21 +0000

    Done.

--- index.js
+++ index.js
@@ -27,17 +27,4 @@ function currentLine(deliLine){
   }
 }
 
-// --------
-// function currentLine(line) {
-//   if (!line.length) {
-//     return "The line is currently empty."
-//   }
-//
-//   const numbersAndNames = []
-//
-//   for (let i = 0, l = line.length; i < l; i++) {
-//     numbersAndNames.push(`${i + 1}. ${line[i]}`)
-//   }
-//
-//   return `The line is currently: ${numbersAndNames.join(', ')}`
-// };
+
