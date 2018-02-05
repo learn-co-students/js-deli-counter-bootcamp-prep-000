d69c6c1dd3387ceac6cb1b33b286af4e26c6a9ce
Betsy Sallee <ensallee@gmail.com>
2018-02-05 19:24:25 +0000

    Done.

--- index.js
+++ index.js
@@ -6,20 +6,21 @@ function takeANumber(katzDeliLine, name){
 }
 
 function nowServing(katzDeliLine){
-  if (katzDeliLine.length!==0){
-  return `Currently serving ${katzDeliLine.shift()}.`;}
-  else{
+  if (katzDeliLine.length===0){
     return "There is nobody waiting to be served!";
+  }else{
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
 
 function currentLine(katzDeliLine){
-   if (katzDeliLine.length===0){
-      return "The line is currently empty.";
-   }
-   let array=[];
-  for (let i=0; i<katzDeliLine.length; i++){ 
-    array.push(` ${i+1}. ${katzDeliLine[i]}`);
+var array=[];
+  if (katzDeliLine.length===0){
+    return "The line is currently empty.";
+  }else{
+   for (let i=0; i<katzDeliLine.length; i++){
+     array.push(` ${i+1}. ${katzDeliLine[i]}`);
+   }    
+   return `The line is currently:${array}`;
   }
-  return "The line is currently:" + array;
 }
