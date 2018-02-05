6d7addae1a99aca11aa98cac68d48fb02d7706b6
Przemyslaw Wiczolek <przemyslaw.wk@gmail.com>
2018-02-05 14:59:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length === 0)
+    return "There is nobody waiting to be served!";
+
+  let current = katzDeliLine[0];
+  katzDeliLine.shift();
+
+  return `Currently serving ${current}.`;
+}
+
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length === 0)
+    return "The line is currently empty.";
+
+  let str = "The line is currently:";
+
+  for(let i = 0; i < katzDeliLine.length; ++i){
+    str += " " + (i+1) + ". " + katzDeliLine[i] + ",";
+  }
+
+  str = str.slice(0, str.length - 1);
+  return str;
+}
