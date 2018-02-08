e070893b506b167cdab1b7185edb6d5a6cc51cac
Troy Lawrence <redirect@gmail.com>
2018-02-08 01:29:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function takeANumber (katzDeliLine, name) {
-  return `Welcome, ${name}. You are number ${katzDeliLine.length+1} in line.`;
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
 function nowServing (katzDeliLine, name) {
   if (katzDeliLine>0) {
+    katzDeliLine.shift(name);
     return `Currently serving ${name}`;
   } else {
     return `There is nobody waiting to be served!`;
