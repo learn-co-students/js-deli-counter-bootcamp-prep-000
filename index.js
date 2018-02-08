84568645cc4420d2c8d0b64de2db83aec8033c93
Alyssa LaPerna <aalaperna@gmail.com>
2018-02-08 06:55:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var katzDeli = [];
+
+function takeANumber (katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+}
+
+
+function nowServing (katzDeliLine) {
+  if (katzDeliLine.length < 1) {
+  return ("There is nobody waiting to be served!")
+} else {
+var firstPersonInLine = katzDeliLine.shift()
+return (`Currently serving ${firstPersonInLine}.`)
+}
+}
+
+
+function currentLine (katzDeliLine) {
+  var line = []
+  for (let i = 0; i < katzDeliLine.length; i++) {
+   line.push(` ${i + 1}. ${katzDeliLine[i]}`)
+  }
+if (katzDeliLine.length === 0) {
+  return ("The line is currently empty.")
+} else {
+  return (`The line is currently:${line}`)
+}
+}
