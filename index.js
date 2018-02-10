a6e3bbefced5bb27a2ad9d5052d3c96584528efe
Betsy Sallee <ensallee@gmail.com>
2018-02-10 22:55:05 +0000

    Done.

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
-var array=[];
+var numberedArray=[];
   if (katzDeliLine.length===0){
     return "The line is currently empty.";
   }else{
    for (let i=0; i<katzDeliLine.length; i++){
      array.push(` ${i+1}. ${katzDeliLine[i]}`);
-   }    
+   }
    return `The line is currently:${array}`;
   }
 }
