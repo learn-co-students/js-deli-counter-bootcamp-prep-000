e21bde058c1110918a2b31b3f2817db2bdfedc48
Riyaad Azad <azad.riyaad@gmail.com>
2018-02-18 19:48:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+var katzDeliLine = [];
+ 
+takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
+takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
+takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
+ 
+currentLine(katzDeliLine); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
+ 
+nowServing(katzDeliLine); // "Currently serving Ada."
+ 
+currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent"
+ 
+takeANumber(katzDeliLine, "Matz"); // "3"
+ 
+currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent, 3. Matz"
+ 
+nowServing(katzDeliLine); // "Currently serving Grace."
+ 
+currentLine(katzDeliLine); // "The line is currently: 1. Kent, 2. Matz"
