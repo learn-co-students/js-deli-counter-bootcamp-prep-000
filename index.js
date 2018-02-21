deb7aacf9537bcb7b760479418e1ed45122b7709
Adam Towers <hello@adamtowers.design>
2018-02-21 04:13:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-var katzDeliLine = [];
-
-function takeANumber (katzDeliLine, name) {
-  katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`
+function takeANumber (line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.indexOf(name) + 1} in line.`
 }
 
-function nowServing (katzDeliLine) {
-  if (katzDeliLine.length == 0) {
+function nowServing (line) {
+  if (line.length == 0) {
     return 'There is nobody waiting to be served!'
   } else {
-    var nowServingName = katzDeliLine.shift()
+    var nowServingName = line.shift()
     return `Currently serving ${nowServingName}.`
   }
 }
 
-function currentLine (katzDeliLine) {
-  if (katzDeliLine.length == 0) {
+function currentLine (line) {
+  if (line.length == 0) {
     return `The line is currently empty.`
   } else {
     var placesInLine = [];
-    for (let i = 0; i < katzDeliLine.length; i++) {
-      placesInLine.push(`${i + 1}. ${katzDeliLine[i]}`)
+    for (var i = 0; i < line.length; i++) {
+      placesInLine.push(`${i + 1}. ${line[i]}`)
     }
     return `The line is currently: ${placesInLine.join(`, `)}`
   }
