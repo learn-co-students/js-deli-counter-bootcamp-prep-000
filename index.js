b7afafec4d4a7afc24d69f79937a78c824e9f3a7
adam N <adam.nawroc@gmail.com>
2018-02-21 18:41:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+
+  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`
+
+}
+function nowServing(line) {
+  if (line.length === 0) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    var current = line.shift()
+    return `Currently serving ${current}.`
+  }
+}
+
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return "The line is currently empty."
+  } else {
+    var string = `The line is currently: `
+    for (var i = 0; i < line.length; i++) {
+
+      if (i !== line.length - 1) {
+      string += `${i+1}. ${line[i]}, `
+    } else {
+      string += `${i+1}. ${line[i]}`
+
+    }
+  }
+
+    return string
+  }
+}
