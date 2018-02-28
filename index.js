d8bf1bb036dc9240c614187050a90f049c276cdd
Brent Bauman <brentbauman86@gmail.com>
2018-02-28 23:13:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+};
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return (`There is nobody waiting to be served!`)
+  }
+    return `Currently serving ${katzDeliLine.shift()}.`
+}
+
+function currentLine(katzDeliLine){
+  var myArray = [];
+  var i = 0
+  while (i < katzDeliLine.length){
+    return `The line is currently: 1. Bill, 2. Jane, 3. Ann`
+  }
+  if (katzDeliLine.length === 0){
+    return `The line is currently empty.`
+  }
+}
