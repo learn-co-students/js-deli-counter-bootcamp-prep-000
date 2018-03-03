fe5be01c3a18c3fe8ba823ca4f1f34b484810afc
Travis Dockter <travisdock@gmail.com>
2018-03-03 02:51:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+
 function takeANumber(katzDeliLine, name) {
-  katzDeliLine = katzDeliLine.length + 1
-  return katzDeliLine + 1
+  katzDeliLine.push(name)
+  var position = katzDeliLine.length
+  return "Welcome, " + name + ". You are number " + position + " in line.";
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return "Currently serving " + katzDeliLine.shift() + "."
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var linestring = []
+  if (katzDeliLine.length > 0) {
+    for (let i = 0; i < katzDeliLine.length; ++i) {
+      var position = i + 1
+      linestring.push(" " + position + ". " + katzDeliLine[i])
+      
+    }
+    return "The line is currently:" + linestring
+  } else {
+    return "The line is currently empty."
+  }
 }
