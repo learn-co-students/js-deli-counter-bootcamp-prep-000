38bd71a2c61b736569b230f4c28ab50d6eef40b0
Dan  Pereda <dpereda@gmail.com>
2018-03-04 22:50:44 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function takeANumber (katzDeliLine,name){
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)+1} in line.`
 }
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length >=1){
+  return `Currently serving ${}.`
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+    
+  }
+}
