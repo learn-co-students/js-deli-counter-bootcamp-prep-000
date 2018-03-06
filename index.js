856d727cd2979989c1e0bcc068ef3c5c4c4185bf
Hany Rostom <hanyrostom@gmail.com>
2018-03-06 20:52:09 +0000

    Done.

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ var katzDeliLine = []
 
 function takeANumber(katzDeliLine, name){
 
-  katzDeliLine.push(name);
+  katzDeliLine.push(name); //we're adding the customer names to que array (line)
 
   var number = 1 + (katzDeliLine.indexOf(name));
+  // (grabbing the index and because arrays and strings are zero indexed we add 1 to give customer actual number in que)
   return 'Welcome, '+ name + '. You are number ' + number + ' in line.'
 }
 
 
 function nowServing(katzDeliLine){
 
-  if(katzDeliLine.length === 0){
+  if(katzDeliLine.length === 0){ //checking if line empty
     return "There is nobody waiting to be served!";
   } else {
-  return "Currently serving " + katzDeliLine.shift()+ ".";
+  return "Currently serving " + katzDeliLine.shift()+ "."; // if not we return first element from beginning of array and shift it out
   }
 
 }
@@ -25,21 +26,22 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine){
 
-  var statement = "The line is currently: "
+  var statement = "The line is currently: " // same statement in all cases if line not empty
 
-  if(katzDeliLine.length === 0){
+  if(katzDeliLine.length === 0){ //checking if line is empty
     return "The line is currently empty.";
   } else {
-
-    statement += "1. " + katzDeliLine[0];
+  // if line not empty we do the following
+    statement += "1. " + katzDeliLine[0]; //first element in line
 
     for (var i = 1; i < katzDeliLine.length; i++) {
       var customer = katzDeliLine[i];
 
-      statement +=  ", " + (1 + i) + ". " + customer
+      statement +=  ", " + (1 + i) + ". " + customer  // adding to the string statement
+      // created a loop so regardless of the number of people inside it always prints the right que
     }
 
-    return statement;
+    return statement; // returns the whole que;
 
   }
 
