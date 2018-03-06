094610620056eaa9473f3677f06599000307f12b
max behling <elmachomax123@gmail.com>
2018-03-06 22:09:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function takeANumber(katzDeliLine, max) {
+  let takeANumber = ['jeff', 'bill', 'pedro']
+  return takeANumber
+}
+
+function nowServing() {
+  takeANumber.shift()
+  return 'There is nobody waiting to be served!'
+}
+
+function currentLine() {
+  if takeANumber = ['jeff', 'bill', 'pedro'] {
+    return ('The line is currrently: 1. jeff, 2. bill, 3. pedro')
+  } else return ('The line is currently empty.')
+}
+
+
+
