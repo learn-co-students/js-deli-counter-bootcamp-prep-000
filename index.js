b53360d7fbd64e12e137e68053c800465e3453cf
Brad Newman <newmanbradm@gmail.com>
2018-03-11 16:04:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+var katzDeliLine = []
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(`${name}`)
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+}
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
+  return ("There is nobody waiting to be served!")
+  }
+  else {
+    var name = katzDeliLine.shift()
+    return (`Currently serving ${name}.`)
+  } 
+}
+function currentLine(katzDeliLine) {
+  var newLine = []
+  if (katzDeliLine.length === 0) {
+    return ("The line is currently empty.")
+  }
+  for (var i = 0; i < katzDeliLine.length; i++) {
+    newLine.push(` ${i + 1}. ${katzDeliLine[i]}`)
+  }
+  return "The line is currently:" + newLine
+}
