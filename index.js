7e57f90cc3fba5f6c74132ce46b7aa5c25fb18e5
Saige Leslie <saigeleslie@gmail.com>
2018-03-16 03:38:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var katzDeli = [];
+function takeANumber(katzDeli , name){
+  katzDeli.push(name);
+  return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
+function nowServing(line){
+  if (line.length === 0){
+    return ('There is nobody waiting to be served!'); 
+  } else {
+    return (`Currently serving ${line.shift()}.`);
+  }
+}
+function currentLine(katzDeliLine){
+  // hard coded, needs revision
+  // "The line is currently: 1. Bill, 2. Ada, 3. Anne"
+  var DeliLine = [];
+  for (var i = 0; i < katzDeliLine.length; i++) {
+      DeliLine.push(' ' + [i + 1] +'. ' + katzDeliLine[i]);
+  }
+  if (katzDeliLine.length === 0){
+      return ('The line is currently empty.');
+    } else {
+      return ('The line is currently:' + DeliLine);
+    }
+}
+
