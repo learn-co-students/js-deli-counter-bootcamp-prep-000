c6e8249bad95e432f623ff14c9703fd0aebd8b26
Christopher <christopherccm@msn.com>
2018-03-23 21:13:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+var katzDeli = [];
+var customerName = [];
+
+function takeANumber(katzDeli, customerName) {
+  for (let i = 0; i < 10; i++) {
+    katzDeli.push(customerName);
+  }
+  return customerName+1;
+}
