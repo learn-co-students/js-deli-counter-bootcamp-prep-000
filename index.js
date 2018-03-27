723b01dd7e487f7ffc1119de10e25bba090a203b
Steven Weiss <weiss.steven@gmail.com>
2018-03-27 18:48:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
+/// PROGRAM TO HANDLE A STORE'S LINE. ///
 
 // Initialize the line of the 'STORE'
 var katzDeli = []
-
 
 // Give status of the line whether empty or for a flood of guests
 function currentLine(line) {
@@ -15,14 +14,13 @@ function currentLine(line) {
   }
   return `The line is currently: ${listNames.join(', ')}`
 }
-
 // Adds guest to the line & returns their place in line.
 function takeANumber(line, name) {
   line.push(name);
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
-// Announces who is being served.
+// Announces who is being served unless there isn't anyone on line.
 function nowServing(line) {
   if (line.length == 0) {
     return "There is nobody waiting to be served!"
