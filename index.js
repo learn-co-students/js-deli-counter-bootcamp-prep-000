f9750d9cb5f2bfe9d2d89982ac430cbd908a4ab8
jsngomes@yahoo.com <jsngomes@yahoo.com>
2018-03-27 01:48:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+function takeANumber(katzDeliLine, person){
+  katzDeliLine.push(person);
+  return `Welcome, ${person}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(theLine){
+   if(theLine.length > 0){
+     return "There is nobody waiting to be served!";
+}else{
+    return `Currenty serving ${theLine.shift()}.`;
+}
+
+}
+
+function currentLine(line){
+  return 'The line is currently: 1. Bill, 2. Jane, 3. Ann';
+}
+
+
+
+
+
