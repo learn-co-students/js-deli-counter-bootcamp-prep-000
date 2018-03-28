7d0d3b2092f6a5eb2df5ce6c2dd9ee330392392f
Alex Edwards <info@acedigitalagency.net>
2018-03-28 04:25:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  return "Currently serving " + katzDeliLine.shift(0) + ".";
+}
+
+function currentLine(katzDeliLine) {
+    if (katzDeliLine.length === 0) {
+      return "The line is currently empty.";
+    }
+    else {
+      var i = 0;
+      var newArray = [];
+        while (i < katzDeliLine.length) {
+        newArray.push(` ` + `${i+1}. ` + katzDeliLine[i]);
+        i++;
+      }
+      return `The line is currently:${newArray}`;
+    }
+}
