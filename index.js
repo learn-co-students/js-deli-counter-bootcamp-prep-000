4b03465d2974e7d6d1249ebb271295ed47265ef4
Richard S <rscheiwe@gmail.com>
2018-03-29 03:41:13 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     return `Currently serving ${katzDeliLine.shift()}.`
   } else {
-    return 'There is nobody waiting to be served!'
+    return "There is nobody waiting to be served!"
   }
 }
 
