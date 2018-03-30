fa786c3be961a4fdf72c3250056fd06d9e0ffbd5
Nancy Jo Chang <enciie@gmail.com>
2018-03-30 23:30:39 +0000

    Done.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing(katzDeliLine){
 }
 function currentLine(katzDeliLine){
   var line = [];
-  if (katzDeliLine === 0){
+  if (katzDeliLine.length === 0){
   return "The line is currently empty.";
   } else {
     for(var i = 0; i < katzDeliLine.length; i++){
