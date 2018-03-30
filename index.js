a786cbe46b8367daf7fc942308172ae83aee6c7b
Nancy Jo Chang <enciie@gmail.com>
2018-03-30 23:01:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  //adding name to the end of the katzDeliLine array
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
+}
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    //if nobody is in line katzDeliLine.length will equal to 0
+  return "There is nobody waiting to be served!";  
+  } else {
+    //return first person in line then remove that individual from line
+    return "Currently serving " + katzDeliLine.shift() + ".";
+  }
+}
+function currentLine(katzDeliLine){
+  if (katzDeliLine === 0){
+  return "The line is currently empty."
+  } else {
+    for(var i = 0; i < katzDeliLine.length; i++){
+      var numberName = [];
+      numberName.push((i+1) + ". " + katzDeliLine[i] + " ,");
+    return "The line is currently: " + numberName;
+  }
+}
+
+ 
+  //returns the current line
+  //ex katzDeliLine ["Ada", "Grace"]
+  //would return "The line is currently: 1. Ada, 2. Grace"
+  //if nobody in line
+  //return "The line is currently empty."
+  
+}
