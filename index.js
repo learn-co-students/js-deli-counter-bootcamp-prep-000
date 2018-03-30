f0f8ec30999272d5d231095746edc00f41722daf
Steven Weiss <weiss.steven@gmail.com>
2018-03-30 19:08:17 +0000

    Done.

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty."
   }
-  var listNames = []
-  for (var i = 0; i < line.length; i++) {
+  let listNames = []
+  for (let i = 0; i < line.length; i++) {
       listNames.push(`${i + 1}. ${line[i]}`)
   }
   return `The line is currently: ${listNames.join(', ')}`
 }
-// Adds guest to the line & returns their place in line.
+// Adds guest to the back of the line (katzDeli array) & returns their place in line.
 function takeANumber(line, name) {
   line.push(name);
   return `Welcome, ${name}. You are number ${line.length} in line.`
