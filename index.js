2710e4bddbc5e9ead85e678cbefbd751beaa8798
richiebonilla01@gmail.com Bonilla <richiebonilla01@gmail.com>
2018-04-02 02:15:05 +0000

    Done.

--- index.js
+++ index.js
@@ -2,22 +2,30 @@ var KatzDeli = []
 
 var takeANumber = (katzDeliLine, name) => {
   katzDeliLine.push(name)
-  return katzDeliLine.length + 1
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 var nowServing = katzDeliLine => {
-  var firstPerson = katzDeliLine.slice(0,1)
-  katzDeliLine.shift()
-  return firstPerson
+  if (katzDeliLine.length > 0) {
+    var firstPerson = katzDeliLine.slice(0,1)
+    katzDeliLine.shift()
+    return `Currently serving ${firstPerson}.`
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+
 }
 
 var currentLine = katzDeliLine => {
-  if (katzDeliLine.length !== 0) {
+  if (katzDeliLine.length > 0) {
     var string = "The line is currently:"
+
     for (var i = 0; i < katzDeliLine.length; i++) {
-      string + `${i}. ${katzDeliLine[i]},`
+      string = string.concat(` ${i + 1}. ${katzDeliLine[i]},`)
     }
-    return string.length - 1
+
+    return string.slice(0, string.length - 1)
+
   } else {
     return "The line is currently empty."
   }
