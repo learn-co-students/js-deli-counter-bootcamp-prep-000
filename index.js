6719b48248d5f91036c0d369f0b3a3210ecf78f4
Rommy Elyan <relyan@email.arizona.edu>
2018-04-05 17:43:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+	katzDeliLine.push(`${name}`);
+	return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function currentLine(katzDeliLine) {
+  var newLine=[];
+	if (katzDeliLine.length === 0) {
+		return 'The line is currently empty.';
+	} else {
+	  for (let i=0;i<katzDeliLine.length;i++){
+	    newLine.push(`${i+1}. ${katzDeliLine[i]}`)
+	  }
+	return `The line is currently: ${newLine.join(', ')}`
+	}
+}
+
+function nowServing(name) {
+	if (name.length === 0) {
+		return 'There is nobody waiting to be served!';
+	} else {
+		return `Currently serving ${name.splice(0,1)}.`;
+	}
+}
