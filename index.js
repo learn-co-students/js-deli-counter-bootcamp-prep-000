17ae8a3e8d86aa44554cc2df88cde292f0177e16
Aviram cohen <justaviram@gmail.com>
2018-04-11 21:31:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,61 @@
+var katzDeliLine = [];
+
+/*Another way to return position
+
 function takeANumber(katzDeliLine, name){
-  var positionInLine = katzDeliLine.length + 1;
   katzDeliLine.push(name);
-  return "Welcome, " + name + ". You are number " + positionInLine + " in line.";
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
-function nowServing(line){
-  if(line.length < 1){
-  return "There is nobody waiting to be served!";
+
+*/
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  var position = katzDeliLine.indexOf(name);
+  return `Welcome, ${name}. You are number ${position + 1} in line.`;
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return `There is nobody waiting to be served!`;
+  }else{
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
-  var next = line.shift();
-  return "Currently serving " + next +".";
 }
+
+/*example with help
 function currentLine(katzDeliLine){
+  var str = "The line is currently: ";
+  var line = [];
   if(katzDeliLine.length < 1){
-    return "The line is currently empty.";
+    return `The line is currently empty.`;
+  }else{
+    for(let i = 0; i <katzDeliLine.length; i++){
+      line[i] = `${[i+1]}. ${katzDeliLine[i]}`;
+    }str += line.join(', ');
+    return str;
   }
-  var result = `The line is currently: 1. ${katzDeliLine[0]}`;
+}*/
+/*Not looping correctly
+function currentLine(katzDeliLine){
+  var str = `The line is currently: 1. ${katzDeliLine[0]}`;
+  if(katzDeliLine.length === 0){
+    return `The line is currently empty.`
+  }else if(katzDeliLine.length === 1){
+    return `${str}`;
+  }else if(katzDeliLine.length > 1){
+    for(let i = 0; i < katzDeliLine.length; i++){
+      return `${str}, ${i+1}. ${katzDeliLine[i+1]}`
+    }
+  }
+}*/
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return `The line is currently empty.`;
+  }
+  var str = `The line is currently: 1. ${katzDeliLine[0]}`;
   for(let i = 1; i < katzDeliLine.length; i++){
-    result += `, ${i+1}. ${katzDeliLine[i]}`;
+    str += `, ${i + 1}. ${katzDeliLine[i]}`;
   }
-  return result;
+  return str
 }
