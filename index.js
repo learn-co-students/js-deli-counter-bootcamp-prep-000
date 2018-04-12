f92a64c3f6bf014c30182b0e1ad7ddca2b38ecc8
Brad Newman <newmanbradm@gmail.com>
2018-04-12 12:12:19 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = []
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 function nowServing(katzDeliLine) {
   if(katzDeliLine.length === 0) {
