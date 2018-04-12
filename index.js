41ce4c474d0568abfc593ffe5c03ae03cfd22330
Brad Newman <newmanbradm@gmail.com>
2018-04-12 12:13:38 +0000

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 function nowServing(katzDeliLine) {
-  if(katzDeliLine.length === 0) {
+  if (katzDeliLine.length === 0) {
   return "There is nobody waiting to be served!"
   }
   else {
