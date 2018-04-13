476e54d6c08c7b7c56a3dafcb06cb8a598d3804c
Anna Dybas <a.m.dybas@gmail.com>
2018-04-13 21:11:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeli.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(){
+  use .pop
+}
+
+function currentLine(){
+  
+}
