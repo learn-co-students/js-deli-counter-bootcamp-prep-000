224f20db53f5a290532183fa7a3edeaef1ae2069
Joseph C <jciv.coding@gmail.com>
2018-04-21 04:46:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeli = [];
 
-function takeANumber(katzDeliLine, person) {
+function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
 
   console.log("Welcome," + `${name}` + ". You are number " + `${katzDeliLine.length}` +" in line.")
