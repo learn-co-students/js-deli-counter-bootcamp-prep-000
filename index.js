60c593fb18cd0f579583821217b1d10c89457ac7
sam-parsons <38405997+sam-parsons@users.noreply.github.com>
2018-04-24 00:08:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(deliLine) {
   if (deliLine.length === 0) {
     return "The line is currently empty.";
   } else {
-    var stg = [];
+    var str = [];
     for (var i = 0; i < deliLine.length; i++) {
       str.push((i+1) + ". " + deliLine[i]);
     }
