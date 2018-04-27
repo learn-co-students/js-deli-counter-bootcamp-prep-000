e7cf8c48aa509c99051d69a2aaabe084e5907ff8
Anthony Lam <al3101@nyu.edu>
2018-04-27 19:41:12 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var katzDeliLine = [];
 function takeANumber(katzDeliLine, person) {
   katzDeliLine.push(person) 
   return "Welcome, " + person + ". " + "You are number " + katzDeliLine.length + " in line." 
+
 }
 
 
