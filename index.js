b22754f8efe0deaae3385d99ef5bde6ae05cad1c
Cindy Wong <cwong690@gmail.com>
2018-04-28 22:21:20 +0000

    Done.

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function currentLine(katzDeliLine) {
     for (var i = 0; i < katzDeliLine.length; i++) {
       arrLine.push(` ${i+1}. ${katzDeliLine[i]}`)
     }
-  return 'The line is currently:' + arrLine
+    return 'The line is currently:' + arrLine
   }
 }
