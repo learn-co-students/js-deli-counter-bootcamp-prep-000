9f5a37ca36e3db504046c75f70f23b384608409a
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-04-30 16:07:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+var katzDeli = [ ]; 
+
+function takeANumber(katzDeliLine, name) {
+    katzDeliLine.push(name);
+ return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+
+function nowServing(katzDeliLine) {
+  
+  if (katzDeliLine.length > 0) {
+  return `Currently serving ${katzDeliLine.shift()}.`;
+ } else {
+    return ("There is nobody waiting to be served!")
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length < 1) {
+  return "The line is currently empty."
+  }
+  let newArray = [] 
+  for(let i=0; i< katzDeliLine.length; i++) {
+    newArray.push(`${i+1}. ${katzDeliLine[i]}`)
+  }
+  return `The line is currently: ${newArray.join(", ")}`
+}
