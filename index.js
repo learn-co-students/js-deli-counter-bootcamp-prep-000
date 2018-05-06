6951b5f3b5976233279306b0f66bca7254bd3b8d
sailboatlarry@gmail.com <sailboatlarry@gmail.com>
2018-05-06 19:32:54 +0000

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   let position =  katzDeliLine.indexOf(name) + 1;
-
   return `Welcome, ${name}. You are number ${position} in line.`
 }
 
