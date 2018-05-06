3c007766aca1897de739a751c992dc1ae618b0ae
Jess H <jessdev83@gmail.com>
2018-05-06 23:59:07 +0000

    Done.

--- index.js
+++ index.js
@@ -20,12 +20,11 @@ var line = []
 
 function currentLine(katzDeli) {
   for (let i = 0; i < katzDeli.length; i++) {
-    line.push(' ' + [i+1] + '.' + katzDeli[i])
+    line.push(' ' + [i+1] + '. ' + katzDeli[i])
   }
-} if (katzDeli.length === 0) {
-  return `The line is currently empty.`
+ if (katzDeli.length === 0) {
+  return `The line is currently empty.`;
 } else {
-  return `The line is currently ` + line
+  return `The line is currently:` + line
 }
-  
 }
