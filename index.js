e82490d9800c5098f973108eec64d7b05cd6191b
lmkirkiii@gmail.com Kirk <lmkirkiii@gmail.com>
2018-05-09 23:20:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+var katzDeliLine = [];
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  var position = katzDeliLine.length;
+  return (`Welcome, ${name}. You are number ${position} in line.`)
+}
+
+function nowServing(katzDeliLine){
+  var index = 0;
+  while(index < katzDeliLine.length){
+    index++;
+  }
+
+  if(katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  }
+  else{
+    return (`Currently serving ${katzDeliLine.shift()}`+".");
+  }
+}
+
+function currentLine(katzDeliLine){
+  var list = [];
+  for(let index= 0; index<katzDeliLine.length; index++){
+    list.push(" " + [index+1] + "." + " " + katzDeliLine[index])
+  }
+  if(katzDeliLine.length === 0){
+    return "The line is currently empty."
+  }
+  else{
+    return ("The line is currently:" + list)
+  }
+}
