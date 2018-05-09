85cbf0dd96736a5cf806997bc0a13e9200afd668
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-05-09 11:40:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var katzDeliLine=["Ada", "Grace", "Ben"];                   //Current Guests already waiting in line
+var katzDeliLine=[];                   //Current Guests already waiting in line
 
-function takeANumber(katzDeliLine,name="Guest"){            //
+function takeANumber(katzDeliLine,name = "Guest"){            //
     katzDeliLine.push(name)
     console.log(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
     return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
@@ -21,21 +21,30 @@ function currentLine(line){
     console.log("Line is currently empty")
     //return "The line is currently empty."
   }else{
-    var newLine = [];
+    var newLine = [],i;
     //line.forEach(function(item,index){newLine.push(` ${index+1}. ${item}`)} )
-    for(var i=0; i<line.length; i++){
-      newLine.push(` ${i}. ${line[i]}`)
+    for(i=0; i<line.length;i++){
+      newLine.push(` ${i+1}. ${line[i]}`)
     }
     console.log(`The line is currently:${newLine}`)
     return `The line is currently:${newLine}`
   }
 }
 
-takeANumber(katzDeliLine,"Kent")
-nowServing(katzDeliLine)
-nowServing(katzDeliLine)
-nowServing(katzDeliLine)
-currentLine(katzDeliLine)
-takeANumber(katzDeliLine,"Jessica")
-nowServing(katzDeliLine)
-currentLine(katzDeliLine)
+takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
+takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
+takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
+
+currentLine(katzDeliLine); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
+
+nowServing(katzDeliLine); // "Currently serving Ada."
+
+currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent"
+
+takeANumber(katzDeliLine, "Matz"); // "3"
+
+currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent, 3. Matz"
+
+nowServing(katzDeliLine); // "Currently serving Grace."
+
+currentLine(katzDeliLine); // "The line is currently: 1. Kent, 2. Matz"
