d139a289c8a49cbd2eac7977931b4d4b492f9405
lmkirkiii@gmail.com Kirk <lmkirkiii@gmail.com>
2018-05-10 18:07:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 var katzDeliLine = [];
-function takeANumber(katzDeliLine, name){
+function takeANumber(katzDeliLine,name){
   katzDeliLine.push(name);
-  var position = katzDeliLine.length;
+  var position = katzDeliLine.length
   return (`Welcome, ${name}. You are number ${position} in line.`)
 }
 
 function nowServing(katzDeliLine){
-  var index = 0;
-  while(index < katzDeliLine.length){
-    index++;
+  var index = 0
+  while(index< katzDeliLine.length){
+    index++
   }
-
-  if(katzDeliLine.length === 0){
+  if (katzDeliLine.length===0){
     return "There is nobody waiting to be served!"
   }
   else{
-    return (`Currently serving ${katzDeliLine.shift()}`+".");
+    return (`Currently serving ${katzDeliLine.shift()}.`)
   }
 }
-
 function currentLine(katzDeliLine){
-  var list = [];
-  for(let index= 0; index<katzDeliLine.length; index++){
-    list.push(" " + [index+1] + "." + " " + katzDeliLine[index])
+  var line= [];
+  for(var index = 0; index<katzDeliLine.length; index++){
+    line.push(" "+[index+1]+"."+" "+[katzDeliLine[index]])
   }
-  if(katzDeliLine.length === 0){
+  if(katzDeliLine.length===0){
     return "The line is currently empty."
   }
   else{
-    return ("The line is currently:" + list)
+    return "The line is currently:"+ line
   }
 }
