df0f6155480c71de384cdb8311a077d31d157280
Marcus Cheung <myc2131@tc.columbia.edu>
2018-05-10 17:20:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var katzDeli = [];
 
-function takeANumber(katzDeli, name) {
-  katzDeli.push(`${name}`);
-  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeliLine .length} in line.`);
 }
 
 function nowServing(katzDeli){
@@ -21,8 +21,10 @@ function nowServing(katzDeli){
 var line = [];
 
 function currentLine(katzDeliLine){
-  for (var i = 0; i < katzDeliLine.length; i++){
+  let i = 0;
+  while(i < katzDeliLine.length){
     line.push(` `+ [1 + i] + `. ` + katzDeliLine[i]);
+    i++;
   }
   if (katzDeliLine.length === 0){
     return `The line is currently empty.`;
