2204c0e79803b114fa0b0d61cc11cd053c4ab2e3
Steven Crouse <sdcrouse91@gmail.com>
2018-05-11 18:46:51 +0000

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ function takeANumber (line, newPerson){
   return `Welcome, ${newPerson}. You are number ${line.length} in line.`;
 }
 
-function nowServing(line) {
+function nowServing(line) { /* Name the next person in line unless it's empty, and remove that person from the line */
+
   if(line.length === 0){
     return "There is nobody waiting to be served!";
   }
@@ -14,22 +15,20 @@ function nowServing(line) {
   }
 }
 
-function currentLine (line) {
+function currentLine (line) { /*Note: according to the solution, this function is made easier with the join() method. */
+
   var lineString = "The line is currently: ";
   if(line.length === 0){
-    return "The line is currently empty.";
+    return "The line is currently empty."; // Return this if the line is empty
   }
   
-  else{
-    for (let i = 1; i <= line.length; i++){
-      if (i === 1){
-        lineString = lineString + `${i}. ${line[i-1]}`;
-      }
-      else {
+  else{ /* The line has at least one person. Add each person's name and place in line to lineString and return lineString. */
+    lineString += "1. " + line[0];
+  
+    for (let i = 2; i <= line.length; i++){
       lineString = lineString + `, ${i}. ${line[i-1]}`;
-      }
     }  
-  }
-  
+   }
+   
   return lineString;
 }
