61b0303728551cbbb8aa6ccd1597fb832c47ed4a
Emily Jennings <emily.jennings@flatironschool.com>
2018-05-13 23:08:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* need an array of names 
 need an array of objects with key value pairs of the person's name and their number
 the function for taking a number:
-grab a person's name and assing them a number 
+grab a person's name and assign them a number 
 
 print a sentence with the name and number
 
@@ -22,41 +22,42 @@ function takeANumber(katzDeliLine, name) {
 
 */
 
+var array = [];
 
-
-var katzDeli = [];
-var katzDeliLine = [];
-
-
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name)
-  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
-}
-
-function nowServing() {
-  if (katzDeliLine.length >= 1) {
-      return "Currently serving " + katzDeliLine[0] + "."
-      katzDeliLine.shift();
-    } else if (katzDeliLine.length === 0) {
-      return "There is nobody waiting to be served!"
-    }
+function takeANumber(array, name) {
+  array.push(name)
+  return "Welcome, " + name + ". You are number " + array.length + " in line."
 }
 
 
 
-  
-function currentLine(katzDeliLine) {
 
-  for (let i = katzDeliLine[0]; i <= katzDeliLine.length; i++) {
-    if (i > 0) {
-      katzDeli.push(katzDeliLine.length + ". " + katzDeliLine[i])
-      return "The line is currently: " + katzDeliLine
-    } else if (i === 0) {
-      return "The line is currently empty."
-    }
+function nowServing(array) {
+  var arrayLength = array.length
+  if (arrayLength > 0) {
+    var person = array[0];
+    return "Currently serving " + person + ".";
+  } else if (arrayLength === 0) {
+    return "There is nobody waiting to be served!"
+    array.shift();
   }
   
 }
 
 
+
+
+var line = [];
+
+function currentLine(array) {
+  var arrayLength = array.length
+  if (arrayLength === 0) {
+      return "The line is currently empty.";
+    } else {
+      for (var i=0; i<arrayLength; i++) {
+      line.push(" " + (i+1) + ". " + array[i])
+    }
+    return "The line is currently:" + line;
+  }
+}
 
