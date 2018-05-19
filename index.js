2ae699c2c37a20591f368b33ca1919518d814530
Peter van Dijk <peter@petervandijk.net>
2018-05-19 19:54:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
-  var place = katzDeliLine.length + 1;
-  return place;
+  katzDeliLine.push(name);
+  var place = katzDeliLine.length;
+  
+  return `Welcome, ${name}. You are number ${place} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    var retStr = "There is nobody waiting to be served!";
+  } else {
+    retStr = `Currently serving ${katzDeliLine[0]}.`;
+    katzDeliLine.shift();
+    
+  }
+  return retStr;
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    var retStr = "The line is currently empty.";
+  } else {
+    retStr = "The line is currently: ";
+    
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      var linePlace = i + 1;
+      retStr += `${linePlace}. ${katzDeliLine[i]}`;
+      if (i !== katzDeliLine.length - 1) {
+        retStr += ", ";
+      }
+    }
+  }
+  return retStr;
 }
