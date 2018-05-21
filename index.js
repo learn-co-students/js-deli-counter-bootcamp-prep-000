929c83276a69d059ecac1b2e7f7ae0668e3744e6
Cassandra Corrales <cmcorrales3@gmail.com>
2018-05-21 01:18:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-var katzDeli = [];
+let katzDeliLine = [];
 
 function takeANumber(katzDeliLine, newCustomer) {
   katzDeliLine.push(newCustomer)
   return `Welcome, ${newCustomer}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing(line) {
-  if(line.length === 0) {
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   }
-  return `Currently serving ${line.shift()}.`;
+  return `Currently serving ${katzDeliLine.shift()}.`;
 }
 
-function currentLine(line) {
-  if(line.length === 0) {
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
     return "The line is currently empty."
   }
   let lineList = [];
-  for(var i=0; i<line.length; i++) {
-    let lineListItem = ` ${i+1}. ${line[i]}`
+  for(let i=0; i<katzDeliLine.length; i++) {
+    let lineListItem = ` ${i+1}. ${katzDeliLine[i]}`
     lineList.push(lineListItem);
   }
   return `The line is currently:${lineList}`
