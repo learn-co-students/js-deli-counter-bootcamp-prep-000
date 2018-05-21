d6134153718208f6e1b555b30e7dfacc45494b01
Michael Peterson <mik3wk@gmail.com>
2018-05-21 17:53:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+var katzDeli = [];
+
+function takeANumber(katzDeli, name)
+{
+  katzDeli.push(name);
+  return `Welcome to Katz Deli ${name}. you are number ${katzdeli.length} in line.`;
+}
+
+function nowServing()
+{
+  
+}
+
+function currentLine(line)
+{
+  
+}
