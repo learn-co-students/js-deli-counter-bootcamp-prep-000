d167827609be47780da885c6edc37861ce1c73ba
Michael Peterson <mik3wk@gmail.com>
2018-05-21 18:19:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 var katzDeli = [];
 
-function takeANumber(katzDeli, name)
-{
-  katzDeli.push(name);
-  return `Welcome to Katz Deli ${name}. you are number ${katzdeli.length} in line.`;
+function takeANumber(katzDeli, name) {
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
 
-function nowServing()
-{
-  
+function nowServing(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else
+  return (`Currently serving ${katzDeli.shift()}.`);
 }
 
-function currentLine(line)
-{
-  
+var line = [];
+
+function currentLine(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i]);
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
 }
