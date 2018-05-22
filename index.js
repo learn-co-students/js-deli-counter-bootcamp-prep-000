e1e179bd7b656686aebda373d3f48c49849b7796
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-05-22 17:54:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, person){
+  
+  for(var i = 0; i < katzDeliLine.length; i++){
+    katzDeliLine.push(person)
+  }return  `Welcome, ${katzDeliLine[i]}. You are number ${i + 1} in line.`
+}
