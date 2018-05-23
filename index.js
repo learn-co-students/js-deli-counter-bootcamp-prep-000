3b20720a307a952e744e48d13809c737a1eb5215
Bryant Torres <brt9029@gmail.com>
2018-05-23 20:12:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-var katzDeli = []
+var katzDeliLine = []
 
-function takeANumber(katzDeliLine, newPerson){
-  katzDeliLine.push(newPerson)
-  return `${newPerson} is ${katzDeliLine[-1] in line}`
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing(){
-  return katzDeli[0]
-    katzDeli.shift()
-}
-
-function currentLine(){
-  if (katzDeli.length > 0){
-    return katzDeli()
-  } else {
-    return "The line is currently empty."
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else { while (katzDeliLine.length > 0) {
+      return `Currently serving ${katzDeliLine.shift()}.`
+    }
   }
+}
+
+function currentLine(katzDeliLine){
+  var i = 0
+  var numberedLine = []
+  var line = "The line is currently: "
+  if (katzDeliLine.length === 0){
+    return "The line is currently empty."
+  } else {
+    while (i < katzDeliLine.length) {
+      numberedLine.push(i+1 + `. ${katzDeliLine[i]}`)
+      i ++
+    }
+  }
+  return line + numberedLine.join(", ")
 }
