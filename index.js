f937d0e4265f666275a351a00237880ea0be0385
alex.swansboro@gmail.com <alex.swansboro@gmail.com>
2018-05-29 21:58:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function takeANumber (katzDeliLine, otherDeli) {
+  var katzDeli = [];
+katzDeliLine.push(otherDeli);
+  return `Welcome, ${otherDeli}. You are number ${katzDeli.length + 1} in line.`
+}
+ 
+
+  
+ 
