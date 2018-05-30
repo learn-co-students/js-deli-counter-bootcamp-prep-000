fd99122c39828172f10771da172ba26ada87e966
alex.swansboro@gmail.com <alex.swansboro@gmail.com>
2018-05-30 00:59:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+
+var katzDeli = []
 function takeANumber (katzDeliLine, otherDeli) {
-  var katzDeli = [];
 katzDeliLine.push(otherDeli);
   return `Welcome, ${otherDeli}. You are number ${katzDeli.length + 1} in line.`
 }
