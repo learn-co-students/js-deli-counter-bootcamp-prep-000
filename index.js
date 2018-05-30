d6fd1353bee1ffafde48a5e08bec36f6181dbf09
jinyuli91@gmail.com <jinyuli91@gmail.com>
2018-05-30 21:20:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 var katzDeli = [];
 
-function takeANumber(katzDeli, name){
+function takeANumber(katzDeliLine, name){
+  let katzDeli = katzDeliLine;
   katzDeli.push(name);
   return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
   }
 
 function nowServing(katzDeli){
   for(let i= 0; i <= katzDeli.length; i++){
-    if(katzDeli.length === 0) return "There is nobody waiting to be served!";
-    else return `Currently serving ${katzDeli.shift()}.`
+    if(katzDeli.length === 0){ 
+      return "There is nobody waiting to be served!";
+    }else return `Currently serving ${katzDeli.shift()}.`
   }
 
 }
-var theLine = [];
 
 function currentLine(katzDeli){
+  var theLine = [];
   for(let i = 0; i < katzDeli.length; i++){
     theLine.push(" " + [i+1] + ". " +  katzDeli[i])
   }
   
   if(katzDeli.length === 0) return "The line is currently empty."
-  else return `The line is currently:` + theLine
+  else return `The line is currently:` + theLine.join(",")
   
 }
 
