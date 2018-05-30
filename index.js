806ed61a004a27ea39b24c2cf278df2376504da9
mary.e.beliveau@gmail.com Beliveau <mary.e.beliveau@gmail.com>
2018-05-30 04:31:23 +0000

    Done.

--- index.js
+++ index.js
@@ -12,10 +12,9 @@ function takeANumber(katzDeliLine, name){
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0) {
     firstperson = katzDeliLine[0]
-    sentence2 = firstperson + " is the first person in line."
+    sentence2 = "Currently serving " + firstperson + "."
     console.log(sentence2)
     katzDeliLine.splice(0, 1) 
-    console.log(katzDeliLine)
     return(firstperson)
   }
   else {
