bc9dad7e4dc6a365d0ca1792bcbe1ebbef878742
alex.swansboro@gmail.com <alex.swansboro@gmail.com>
2018-05-31 19:46:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
 var katzDeli = []
-function takeANumber (katzDeliLine, otherDeli) {
-katzDeliLine.push(otherDeli);
-  return `Welcome, ${otherDeli}. You are number ${katzDeli.length + 1} in line.`
+function takeANumber (line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
- 
-
+ function nowServing(line, name) {
+   var first = line.shift()
+ if (line.length >= 1) {
+   return `Currently serving ${first}.`
+} else {
+  return "There is nobody waiting to be served!"
+} 
+}
   
- 
