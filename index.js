938b7e686301c340d31e95af2f4f6418ad66f864
mary.e.beliveau@gmail.com Beliveau <mary.e.beliveau@gmail.com>
2018-05-31 02:34:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
-otherDeli = ["Steven", "Blake", "Avi"]
-
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name)
-  var position = katzDeliLine.length
-  //position = katzDeliLine.indexOf(name)
+function takeANumber(deliLine, name){
+  deliLine.push(name)
+  var position = deliLine.length
   var sentence1 =  `Welcome, ${name}. You are number ${position} in line.`
   console.log(sentence1)
   return sentence1
 }
 
-function nowServing(line){
-  if (line.length > 0) {
-    var firstperson = line[0]
+function nowServing(deliLine){
+  if (deliLine.length > 0) {
+    var firstperson = deliLine[0]
     var sentence2 = "Currently serving " + firstperson + "."
+    deliLine.splice(0, 1)
     console.log(sentence2)
-    line.splice(0, 1)
     return sentence2
   }
   else {
-    console.log("There is nobody waiting to be served!")
-    return "There is nobody waiting to be served!"
+    var emptyArray = "There is nobody waiting to be served!"
+    console.log(emptyArray)
+    return emptyArray
   }
 }
 
-function currentLine(line){
-   if (line.length > 0) {
-      const sentenceList = []
+function currentLine(deliLine){
+   if (deliLine.length > 0) {
+      const arrayElementList = []
       var num = 1
-      while (num <= line.length){
-        var sentence = ' ' + num + '. ' + line[num - 1]
-        sentenceList.push(sentence)
+      while (num <= deliLine.length){
+        var arrayElement = ' ' + num + '. ' + deliLine[num - 1]
+        arrayElementList.push(arrayElement)
         num += 1 }
-      var finalsentence = "The line is currently:" + sentenceList.toString()
-      console.log(finalsentence)
-      return finalsentence
+      var sentence3 = "The line is currently:" + arrayElementList.toString()
+      console.log(sentence3)
+      return sentence3
       }
   else {
-      var empty = "The line is currently empty."
-      console.log(empty)
-      return empty
+      var emptyArray = "The line is currently empty."
+      console.log(emptyArray)
+      return emptyArray
     }
 }
 
+otherDeli = ["Steven", "Blake", "Avi"]
 var katzDeli = [];
 takeANumber(katzDeli, "Ada")
 takeANumber(otherDeli, "Grace")
