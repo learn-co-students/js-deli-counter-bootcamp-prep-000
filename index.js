64aebdaff558448d080533e06d6044b39cc69a85
Mahwish Riaz-Jamil <mahwishcee@gmail.com>
2018-06-06 16:37:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 var katzDeli = [];
+var katzDeliLine = [];
+function takeANumber(katzDeliLine, name){
+  var lineCount = katzDeliLine.length;
 
-// function takeANumber(katzDeliLine, name){
-
-//   do {
-//     katzDeli.push(...katzDeliLine, name);
-//   }
-//   while (name !== null);
-    
+    katzDeliLine.push(name);
+    lineCount++;
+    return`Welcome, ${name}. You are number ${lineCount} in line.`
   
-//   for (let i = 1; i < katzDeliLine.length; i++){
-//       return (`Welcome, ${name}. You are number ${i} in line.`);
-//   }
+}
 
+function nowServing (katzDeliLine){
+  var counter = 0;
   
+  if (katzDeliLine.length === 0){
+    return `There is nobody waiting to be served!`;
+  }
+  
+  do {
+      return `Currently serving ${katzDeliLine.shift()}.`
+      //katzDeliLine.shift();
+      // counter++;
+  }
+  while (katzDeliLine.length !== 0)
+  
+}
+
+function currentLine(katzDeliLine){
+  
+  var numberInLine = 0;
+  var returnArray = [];
+  if (katzDeliLine.length === 0){
+     return `The line is currently empty.`;
+   }
+  
+  for (let i = 0; i<katzDeliLine.length; i++){
+    numberInLine++;
+    returnArray.push(`${numberInLine}. ${katzDeliLine[i]}`);
+  }
+  
+  return `The line is currently: ${returnArray.join(", ")}`
+
+}
