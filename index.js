ffcaf86b47db2050e87eb72ee8cb9245b89808da
scout.burghardt@gmail.com <scout.burghardt@gmail.com>
2018-06-06 12:25:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-var katzDeliLine = []
+var katzDeliLine = [];
 
-function takeANumber(array, name) {
-  //use array length and add 1 to give the new person their number in the line!
-  var watingPosition = array.length + 1
-  array.push(name)
-  return "Welcome, " + name + ". You are number " + watingPosition + " in line."
+function takeANumber(array, person) {
+  var number = array.length + 1
+  array.push(person)
+  return "Welcome, " + person + ". You are number " + number + " in line."
 }
 
 function nowServing(array) {
-  if (array.length > 0) {
+  if (array.length < 1) {
+    return "There is nobody waiting to be served!"
+  } else {
     return "Currently serving " + array.shift() + "."
-  } else return "There is nobody waiting to be served!"
+  }
 }
 
 function currentLine(array) {
-  var waitingLine = []
+  var waitingLine = [];
 
-  for (var i = 0; i < array.length; i++) {
-    waitingLine.push( " " + [i+1] + ". " +  array[i])
-  }
   if (array.length < 1) {
     return "The line is currently empty."
   } else {
-    return "The line is currently:" + waitingLine
+    for (var i = 0; i < array.length; i++) {
+      waitingLine.push(" " + [i+1] + ". " + array[i])
+    }
+      return "The line is currently:" + waitingLine
   }
 }
