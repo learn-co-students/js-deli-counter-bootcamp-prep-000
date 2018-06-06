beb6ec9efb3d4515e151f17d8226d03f89eb1b6a
Mahwish Riaz-Jamil <mahwishcee@gmail.com>
2018-06-06 00:27:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name){
+
+  do {
+    katzDeli.push(...katzDeliLine, name);
+  }
+  while (name !== null);
+    
+  
+  for (let i = 1; i < katzDeliLine.length; i++){
+      return (`Welcome, ${name}. You are number ${i} in line.`);
+  }
+
+  
+}
