f6fd742ba69e96aba87cb085e7e2a9457cdde0bc
Eddie C <eddiec76@gmail.com>
2018-06-10 22:52:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function takeANumber(katzDeli,name) {
+  katzDeli.push(name);
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
+}
+
+function nowServing (katzDeli) {
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else {
+    var firstInLine = katzDeli[0];
+    katzDeli.shift();
+    return `Currently serving ${firstInLine}.`;
+  }
+}
+
+function currentLine(katzDeli) {
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  }
+  else {
+    var line = "";
+    for (var i=0; i < katzDeli.length-1; i++) {
+      line += `${i+1}. ${katzDeli[i]}, `;
+    }
+    return `The line is currently: ${line}${katzDeli.length}. ${katzDeli[katzDeli.length-1]}`;
+  }
+}
