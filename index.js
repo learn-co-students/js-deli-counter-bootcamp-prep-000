17ee35636e97185b625dc3b45a77c47ea4f00f13
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-06-11 21:30:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var katzDeliLine = ["Ada", "Grace", "Kent", "Matz"]
+
+function takeANumber(katzDeliLine){
+var number1 = ["Ada"];
+var number2 = ["Grace"];
+var number3 = ["Kent"];
+var number4 = ["Matz"]; 
+return array;
+}
+
+currentLine(katzDeliLine);
+
+nowServing(katzDeliLine);
+
+currentLine(katzDeliLine);
+
+takeANumber(katzDeliLine, "Matz");
+
+nowServing(katzDeliLine);
+
+currentLine(katzDeliLine);
