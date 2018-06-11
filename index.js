dca645129523a8e05a31e2771b7b0c79960df631
Brian Wadlow <bwadlow@gmail.com>
2018-06-11 16:26:26 +0000

    Done.

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function takeANumber (line, name) {
 }
 
 function nowServing(line) {
-  return (!line.length) ?'There is nobody waiting to be served!' : 'Currently serving ' + line.shift() + '.'
+  return !line.length ?'There is nobody waiting to be served!' : 'Currently serving ' + line.shift() + '.'
 }
 
  function currentLine(line) {
   let arr=line.map((elem, i) => i+1 +'. ' + elem)
-  return !(line.length) ? 'The line is currently empty.' : 'The line is currently: ' + arr.join(', ') 
+  return !line.length ? 'The line is currently empty.' : 'The line is currently: ' + arr.join(', ') 
 }
