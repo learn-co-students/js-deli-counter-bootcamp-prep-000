d37c065a56d9ef23b5ab436e9e462be65141f37a
Kevin Andrew Johnson <kjohnson5315@hotmail.com>
2018-06-14 12:27:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,6 @@
+var katzDeli = []
+var otherDeli = []
+function takeANumber(katzDeliLine, otherDeli) {
+ katzDeli = "Welcome, katzDeliLine. You are number otherDeli[] in line."
+  return takeANumber
+}
