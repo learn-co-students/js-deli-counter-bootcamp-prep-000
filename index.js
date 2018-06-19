80f3f3d1b9273983e19d234f38da0ed7cb5c8ddb
trisjones04@gmail.com <trisjones04@gmail.com>
2018-06-19 13:59:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function takeANumber(katzDeliLine, newPersonName) {
+  katzDeliLine.push(newPersonName);
+  
+  return `Welcome, ${newPersonName}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  var firstPerson = katzDeliLine.shift();
+  
+  if(firstPerson === undefined) {
+    
+    return `There is nobody waiting to be served!`;
+  }
+  
+  return `Currently serving ${firstPerson}.`;
+}
+
+function currentLine(katzDeliLine) {
+  var order = [];
+  
+  if(katzDeliLine.length === 0) {
+    return `The line is currently empty.`;
+  }
+  
+  for(let i = 0; i < katzDeliLine.length; i++) {
+    order.push(`${i + 1}. ${katzDeliLine[i]}`);
+  }
+  
+  var lineOrder = "The line is currently: " + order.join(", ");
+  return lineOrder;
+}
