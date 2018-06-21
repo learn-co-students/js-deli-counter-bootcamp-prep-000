c937cc3ede5cce421080f3886933088e85dd79ab
Austin Powell <austinpowell58@gmail.com>
2018-06-21 03:15:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+var katzDeli = [];
+function takeANumber(array, person) {
+  array.push(person);
+  return "Welcome, " + person + ". You are number " + array.length + " in line.";
+}
+
+function nowServing (array) {
+  {if (array.length === 0)
+  return "There is nobody waiting to be served!";
+  else
+  var i = "Currently serving " + array[0] + ".";
+  array.shift();
+  return i
+  }
+}
+
+function currentLine (array) {
+   var line = [];
+  if (array.length == 0)
+  return "The line is currently empty.";
+  else
+  var i = 0;
+  while (i < array.length) {
+    if (i === 0)
+  line.push((i + 1) + ". " + array[i])
+  else 
+  line.push(" " + (i+ 1) + ". " + array[i]); 
+  i++;
+ }
+ return "The line is currently: " + line;
+}
+
