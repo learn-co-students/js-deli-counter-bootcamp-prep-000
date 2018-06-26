839387e75e9aafde3e71597723a0fdc8cc18ae65
Daniel Walz <walzdan@gmail.com>
2018-06-26 11:56:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 function takeANumber(line, name){
-line.push(name)
-return `Welcome, ${name}. You are number ${line.length} in line.`
+line.push(name);
+return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
 function nowServing(line){
-  if (!line.length) {
-    return "There is nobody waiting to be served!" }
-    return `Currently serving ${line.shift()}.`}
-    
-function currentLine(line) {
-  if (!line.length){
-    return "The line is currently empty."}
-  var katzDeli = []
-  for (let i = 0, l = line.length; i<l; i++)
-  {katzDeli.push(`${i + 1}. ${line[i]}`)}
-  return `The line is currently: ${katzDeli.join(', ')}`
+  if (line.length === 0){ 
+    return 'There is nobody waiting to be served!';
+  } else {
+    return `Currently serving ${line.shift()}.`;
+  }
+}
+
+function currentLine(line){
+  if (line.length === 0){
+    return 'The line is currently empty.';
+  }
+  let namesAndPositions = []
+  for (let i = 0; i < line.length; i++){
+  namesAndPositions.push(`${i+1}. ${line[i]}`);
+    } 
+return `The line is currently: ${namesAndPositions.join(', ')}`;
 }
