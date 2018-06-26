5831686dd55a32887c91118d7bf87425bc81e58f
Linda Chang <lindachang186@gmail.com>
2018-06-26 14:18:23 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ function takeANumber(katzDeliLine, name) {
     var i = katzDeliLine.length;
     return (`Welcome, ${name}. You are number ${i} in line.`)
   }
+
+function nowServing(deliLine, name){
+  var i = deliLine.length;
+  if (deliLine.length == 0) {
+    return ("There is nobody waiting to be served!")
+  } else {
+    var customer = deliLine.shift();
+    return (`Currently serving ${customer}.`)
+  }
+}
+
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length > 0){
+    return (`The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]}`)
+  } else {
+    return ("The line is currently empty.")
+  }
+}
