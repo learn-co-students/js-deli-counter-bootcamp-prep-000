c89da72b8a921485b13504a68fd7936404d78f40
Jacob Barbot <barbotjacob@gmail.com>
2018-06-27 17:03:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeli = []
+
+function takeANumber(line, name) {
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing(line) {
+  if (line.length === 0) {
+  return "There is nobody waiting to be served!"
+  } else {
+    return `Currently serving ${line.shift()}.`
+  }
+}
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return "The line is currently empty."
+  }
+  
+  const lineOrder = []
+  for (let i = 0, l = line.length; i < l; i++) {
+    lineOrder.push(`${i + 1}. ${line[i]}`)
+    
+  }
+  return `The line is currently: ${lineOrder.join(', ')}`
+  
+  }
