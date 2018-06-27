8071d6df246327b2cb2f82585675bfd187d991c3
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-27 07:51:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
+var katzDeli = [];
 
-var katzDeliLine = [];  //At beginning of day empty line
-
-function takeANumber(katzDeliLine,Name) {
-  //Accepts currentLine of people held in variable katzDeli, and the name of the new person's name as parameter. Then adds the new person's name to end of katzDeliLine and return their position in the line.
-   katzDeliLine.push(Name);  
-   var placeInLine = katzDeliLine.length;
-   var message = `Welcome, ${Name}. You are number ${placeInLine} in line.`
-   return message
+function takeANumber(line, name) {
+  line.push(name)       
+  var posInLine = line.length     
+  return `Welcome, ${name}. You are number ${posInLine} in line.`
 }
 
-function nowServing(LineArray) {
-  //Returns first person in line (katzDeliLine) then removes that individual from line (katzDeliLine)
-  if (LineArray.length === 0) {
-    return "There is nobody waiting to be served!"}
-    else {
-    var beingServed = LineArray[0];
-    var message = "Currently serving " + beingServed +".";
-    LineArray.shift();
-    return message;
+function nowServing(line) {
+  if(line.length === 0) {
+   return "There is nobody waiting to be served!"
+  }
+  else {
+    var personAtFront = line[0]
+    line.shift()
+    return `Currently serving ${personAtFront}.`
     }
 }
 
-function currentLine(LineArray) {
-  //Returns current line, i.e. if current line is ["Ada", "Grace"], the it would return "The line is currently: 1. Ada, 2. Grace"
-  //If empty will return "The line is currently empty."
-  if (LineArray.length === 0) {
+function currentLine(line) {
+  var numsAndNames = [];
+  if(line.length === 0) {
     return "The line is currently empty."
   }
   else {
-    var listarray = [];
-    for (let i=0; i<LineArray.length;i++) {
-     var lineNo = i+1;
-     var name = LineArray[i];
-     listarray.push(`${lineNo}. ${name}`);       
+    for(let i = 0; i<line.length; i++) {
+      var currPos = i+1;
+      var person = line[i]
+      numsAndNames.push(` ${currPos}. ${person}`)
     }
-    var message = listarray.join(', ');
-    return `The line is currently: ${message}`;
-  }
+    var list = numsAndNames.toString()
+    return `The line is currently:${list}`
+    }
 }
+
+
