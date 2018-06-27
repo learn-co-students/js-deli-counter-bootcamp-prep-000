e7421d0bc50d0794748bc24e23109e5387928a5b
Daniel Walz <walzdan@gmail.com>
2018-06-27 12:20:23 +0000

    Done.

--- index.js
+++ index.js
@@ -4,20 +4,14 @@ return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
 function nowServing(line){
-  if (line.length === 0){ 
-    return 'There is nobody waiting to be served!';
-  } else {
-    return `Currently serving ${line.shift()}.`;
-  }
-}
+  if (line.length === 0) {return 'There is nobody waiting to be served!'}
+  return `Currently serving ${line.shift()}.`;
+ }
 
 function currentLine(line){
-  if (line.length === 0){
-    return 'The line is currently empty.';
-  }
-  let namesAndPositions = []
-  for (let i = 0; i < line.length; i++){
-  namesAndPositions.push(`${i+1}. ${line[i]}`);
-    } 
-return `The line is currently: ${namesAndPositions.join(', ')}`;
+  if (line.length === 0) {return 'The line is currently empty.'}
+  let namesAndPlaces = [];
+  for (let i = 0, l = line.length; i<l; i++)
+  { namesAndPlaces.push(`${i+1}. ${line[i]}`)}
+  return`The line is currently: ${namesAndPlaces.join(', ')}`;
 }
