36ccff55c7fa7a5a62ab8e134b3e7620bf1a4beb
weijun huang <luck938938@hotmail.com>
2018-06-28 06:01:58 +0000

    Done.

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ var katzDeliLine =[];
 
 function takeANumber(katzDeliLine, customer)
 {
-  katzDeliLine.push(`${customer}`)
- 
+  katzDeliLine.push(customer)
   return `Welcome, `+ customer + `. You are number `+ katzDeliLine.length +' in line.'
 }
 
