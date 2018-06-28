3bbf2fbdc3703990872eda207b90b0512e52b115
weijun huang <luck938938@hotmail.com>
2018-06-28 06:01:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+
+var katzDeliLine =[];
+
+
+function takeANumber(katzDeliLine, customer)
+{
+  katzDeliLine.push(`${customer}`)
+ 
+  return `Welcome, `+ customer + `. You are number `+ katzDeliLine.length +' in line.'
+}
+
+function nowServing(katzDeliLine)
+{
+  if(katzDeliLine.length===0)
+  {
+    return `There is nobody waiting to be served!`
+  }else
+  {
+    
+    return `Currently serving ` + katzDeliLine.shift()+`.`
+  }
+}
+
+
+
+function currentLine(katzDeliLine)
+{
+    var line=[]
+  
+   if(katzDeliLine.length===0)
+   {
+     return `The line is currently empty.`
+   }
+   else
+   {
+     for(let i=0; i<katzDeliLine.length; i++)
+     {
+       line.push(` `+[i+1]+`. `+katzDeliLine[i])
+     }
+     
+     return `The line is currently:`+ line
+   }
+}
