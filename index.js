6fe599cd50a795505a20a9004787fc8d7ffd8ae2
Carie Pigeon <cariepigeon81@gmail.com>
2018-07-01 19:09:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 function takeANumber(deliLine, name) {
-  deliLine.push(name);
+  deliLine.push(name); //adds person to line-array will have //existing names plus new name
   return `Welcome, ${name}. You are number ${deliLine.length} in line.`;
+}
+
+function nowServing(deliLine) {
+  if (deliLine[0] === undefined) {
+    return "There is nobody waiting to be served!";
+  }
+  else {return `Currently serving ${deliLine.shift()}.`}
 }
