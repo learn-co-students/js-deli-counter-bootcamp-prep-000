ba262905fffbab253ea09e1bf8f51e1f9aebdcea
TylerNodell <tyler.l.nodell@gmail.com>
2018-07-03 19:47:57 +0000

    Done.

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function nowServing(line) {
 function currentLine(line) {
   var lineString = "The line is currently: ";
   if (line.length != 0) {
-    for (var i = 1; i <= line.length ; i++) {
-        lineString = lineString + i + ". " + line[i-1] + ", ";
+    for (var i = 0; i < line.length ; i++) {
+        lineString = lineString + (i+1) + ". " + line[i] + ", ";
     }
   } else {
     return "The line is currently empty."
