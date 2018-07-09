6ae8e7f30b9483a9c0464a1b0661bcba77eb5a49
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-09 19:55:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+var katzDeli=[];
+function takeANumber(katzDeliLine, name){
+  var katzDeliLine=[]
+  while (katzDeliLine!==0){
+  
+  return `Welcome, ${name}. You are number ${katzDeliLine} in line.`
+  }
+}
+
+function nowServing (katzDeliLine, name){
+  if (katzDeliLine>0) {`${katzDeliLine}. ${name}`}
+  else "There is nobody waiting to be served!"
+}
+
+function currentLine (katzDeliLine, name){
+  if (katzDeliLine){`The line is currently: ${katzDeliLine}`}
+  else {"The line is currently empty"}
+}
