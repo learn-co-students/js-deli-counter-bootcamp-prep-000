cd3145358a19e8a61cb8868f1f1884b44ef7ebc0
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-10 11:11:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 var katzDeli=[];
-function takeANumber(katzDeliLine, name){
-  var katzDeliLine=[]
-  while (katzDeliLine!==0){
+function takeANumber(katzDeli, name){
+ katzDeli.push(name);
+ var position=katzDeli.length;
+ return `Welcome, ${name}. You are number ${position} in line.`}
   
-  return `Welcome, ${name}. You are number ${katzDeliLine} in line.`
-  }
+ 
+
+function nowServing (katzDeli){
+  let i=0;
+  while (i < katzDeli.length){
+  i++;}
+//} after i++ closes the while loop, yet the remaining oft the function repeats
+  if (katzDeli.length ===  0) {
+    return "There is nobody waiting to be served!"}
+  else {return `Currently serving ${katzDeli.shift()}.`}
 }
 
-function nowServing (katzDeliLine, name){
-  if (katzDeliLine>0) {`${katzDeliLine}. ${name}`}
-  else "There is nobody waiting to be served!"
-}
 
-function currentLine (katzDeliLine, name){
-  if (katzDeliLine){`The line is currently: ${katzDeliLine} ${name}`}
-  else {"The line is currently empty"}
+function currentLine (katzDeli, name){
+  let i=0;
+  while (i < katzDeli.length){
+  i++;}
+  if (katzDeli===0){return "The line is currently empty"}
+  else {`The line is currently: ${katzDeli.length[name]}. ${name}`}
 }
