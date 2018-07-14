53cd7f8aec0fbeb95f7f5b0d495b781624eda7ef
Felix Chan <felixchan29@gmail.com>
2018-07-14 03:44:24 +0000

    Done.

--- index.js
+++ index.js
@@ -18,9 +18,12 @@ function currentLine(katzDeliLine){
   if(katzDeliLine.length < 1){
     return "The line is currently empty.";
   }
-  var lineString = `The line is currently: 1. ${katzDeliLine[0]}`;
-  for(let i = 1; i < katzDeliLine.length; i++){
-    lineString += `, ${i+1}. ${katzDeliLine[i]}`;
+  var lineString = `The line is currently: `;
+  for(let i = 0; i < katzDeliLine.length; i++){
+    lineString += `${i+1}. ${katzDeliLine[i]}`;
+    if(i < katzDeliLine.length - 1){
+      lineString += `, `;
+    }
   }
   return lineString;
 }
