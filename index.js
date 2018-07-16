9c144a175b439c227a58bf4dec3c4cd9c9815f77
brianhkang1@gmail.com <brianhkang1@gmail.com>
2018-07-16 16:29:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 var katzDeliLine = [];  
 
 function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name)
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
-    var firstInLine = katzDeliLine.shift()
-      return `Currently serving ${firstInLine}.`
+    var firstInLine = katzDeliLine.shift();
+      return `Currently serving ${firstInLine}.`;
   } else {
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
   }
 }
 
 function  currentLine(katzDeliLine) {
-  var numarray = []
+  var numarray = [];
   for (var i = 0; i < katzDeliLine.length; i++) {
-    numarray.push(`${i+1}. ${katzDeliLine[i]}`) 
+    numarray.push(`${i+1}. ${katzDeliLine[i]}`) ;
   }
   
   if (katzDeliLine.length > 0) {
-      var str = numarray.join(`, `)
-      return "The line is currently: " + str 
+      var str = numarray.join(`, `);
+      return "The line is currently: " + str;
   } else {
-    return "The line is currently empty."
+    return "The line is currently empty.";
   }
 }
