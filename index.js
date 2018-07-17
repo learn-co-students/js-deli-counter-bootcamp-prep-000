9de9e59d0ca60b2137a5cb2cd7d8ea4e4d5b22a4
Amy Zhou <aymeighz@gmail.com>
2018-07-17 00:38:08 +0000

    Done.

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  } else {
   var line = [];
   let i = 0;
   while (i < katzDeliLine.length) {
   line.push(" " + [i + 1] + ". " + katzDeliLine[i]);
   i++;
   }
-  if (katzDeliLine.length === 0) {
-    return "The line is currently empty.";
-  } else {
     return "The line is currently:" + line;
 }
 }
