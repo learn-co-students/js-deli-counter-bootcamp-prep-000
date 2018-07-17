09c25913ee89a395b3a4f9e6be75f64d971ea412
Charlie Russo <cparusso@gmail.com>
2018-07-17 21:09:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function takeANumber(katzDeliLine, newPersonsName) {
+  katzDeliLine.push(newPersonsName);
+
+  return `Welcome, ${newPersonsName}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(deliLine) {
+  if (deliLine.length === 0) {
+    return 'There is nobody waiting to be served!';
+  } else {
+    var currentCustomer = deliLine.shift();
+
+    return `Currently serving ${currentCustomer}.`;
+  }
+}
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return 'The line is currently empty.';
+  } else {
+    var newLine = [];
+
+    for (var i = 0; i < line.length; i++) {
+      var currentCustomer = line[i];
+      newLine.push(`${i + 1}. ${currentCustomer}`)
+    }
+
+    return `The line is currently: ${newLine.join(', ')}`
+  }
+}
