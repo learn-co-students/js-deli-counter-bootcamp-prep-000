8e6e0dceb9b7b7f3523b34336706e2dc972594b1
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-07-18 20:29:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 var katzDeli = [];
 
-function takeANumber(KatzDeliLine, name){
-  KatzDeliLine.push(name);
-  return(`Welcome, ${name}. You are number ${KatzDeliLine.length} in line.`);
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 }
 
-function nowServing(KatzDeliLine){
-  if (KatzDeliLine.length > 0){
-    var firstCustomer = KatzDeliLine.shift();
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length > 0){
+    var firstCustomer = katzDeliLine.shift();
     return (`Currently serving ${firstCustomer}.`)
   } else {
     return ('There is nobody waiting to be served!')
 }
 }
 
-function currentLine(KatzDeliLine){
-  if (KatzDeliLine.length === 0){
-    return ('The line is currently empty.')
+function currentLine(katzDeliLine){
+  var s = [];
+  if (katzDeliLine.length > 0){
+    for (let i = 0; i < katzDeliLine.length; i++){
+      s.push(` ${i + 1}` + '. ' + `${katzDeliLine[i]}`);
+    }
+    return 'The line is currently:' + s
   } else {
-    return (`The line is currently: 1. ${KatzDeliLine [0]}, 2. ${KatzDeliLine [1]}, 3. ${KatzDeliLine [2]}`)
+    return (`The line is currently empty.`)
   }
 }
-
+ 
