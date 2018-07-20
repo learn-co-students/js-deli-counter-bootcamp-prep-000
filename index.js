7c608e88d1a3b3bae79f638aa042cd0f2c26a8ec
Jenny K <jkam17@gmail.com>
2018-07-20 04:32:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 var katzDeli = []
 
-function takeANumber(katzDeliLine, n){
-  katzDeliLine.push(n)
+function takeANumber(katzDeliLine, n){  //params: line of ppl array, new person
+  katzDeliLine.push(n)                  //adds new person to end of line
   return ('Welcome, '+n+'. You are number ' +(katzDeliLine.length)+' in line.')
 }
-function nowServing(katzDeliLine){
-  if(katzDeliLine.length===0){
+function nowServing(katzDeliLine){      //line of ppl array
+  if(katzDeliLine.length===0){          //if line is empty, return message
     return 'There is nobody waiting to be served!'
   }
-  else{
+  else{                                 //else remove first person from line and return serving message
     return ('Currently serving '+katzDeliLine.shift()+'.')
   }
 }
 
-function currentLine(katzDeliLine){
+function currentLine(katzDeliLine){     //line of ppl array
   var x = 'The line is currently:'
-  if(katzDeliLine.length===0){
+  if(katzDeliLine.length===0){          //if line is empty, return message
     return('The line is currently empty.')
   }
-  else{
-    for (var i=0;i<katzDeliLine.length;i++){
-      x= x +' '+(i+1)+'. '+katzDeliLine[i]+','
+  else{                                 //else add names and line position to string x
+    for (var i=0;i<katzDeliLine.length;i++){    //loop through line length
+      x= x +' '+(i+1)+'. '+katzDeliLine[i]+','  //add line position+. +name+,
     }
   }
-  x=x.slice(0,x.length-1)
-  return x
+  x=x.slice(0,x.length-1)               //remove comma at end of string
+  return x                              //return string x with line positions and names
 }
