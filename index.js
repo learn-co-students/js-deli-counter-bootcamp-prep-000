230a49e777bb77bc1977fd166d2ca2de5ce93be3
Charlie Russo <cparusso@gmail.com>
2018-07-20 15:18:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-// function takeANumber(katzDeliLine, newPersonsName) {
-//   katzDeliLine.push(newPersonsName);
-//
-//   return `Welcome, ${newPersonsName}. You are number ${katzDeliLine.length} in line.`;
-// }
-//
-// function nowServing(deliLine) {
-//   if (deliLine.length === 0) {
-//     return 'There is nobody waiting to be served!';
-//   } else {
-//     var currentCustomer = deliLine.shift();
-//
-//     return `Currently serving ${currentCustomer}.`;
-//   }
-// }
-//
-// function currentLine(line) {
-//   if (line.length === 0) {
-//     return 'The line is currently empty.';
-//   } else {
-//     var newLine = [];
-//
-//     for (var i = 0; i < line.length; i++) {
-//       newLine.push(`${i + 1}. ${line[i]}`)
-//     }
-//
-//     return `The line is currently: ${newLine.join(', ')}`
-//   }
-// }
-
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
 
