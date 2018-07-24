49f5f13f3b54695b03a2b11d53c6cb30bb745c1c
Taylor J Young <tjy14@scarletmail.rutgers.edu>
2018-07-24 22:17:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function takeANumber(array, customer){
   var katzDeliLine= [];
-  katzDeliLine.push(customer);
-  return katzDeliLine
-  console.log()
+  
+function takeANumber(array, customer){
+  array.push(customer);
+  var welcome = ('Welcome, ' + customer + '. You are number ' + (array.length) + " in line.");
+   return welcome;
   
 }
+
+function nowServing (array){
+  
+    if (array[0]=== undefined){
+    return('There is nobody waiting to be served!');
+    }
+   else
+  var nextCustomer =('Currently serving ' + array[0] +'.' )
+  array.shift();
+  return nextCustomer
+  return array;
+  
+}
+
