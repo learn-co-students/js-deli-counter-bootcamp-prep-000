5e16b7342af431646d5759ecb76f67d54125a52b
Patricia Carter <patricia4194@gmail.com>
2018-07-24 15:45:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-var katzDeliLine = [];
+var katzDeli = [];
 
-function takeANumber(katzDeliLine, newCustomer) {
-  katzDeliLine.push(newCustomer);
-  return `Welcome, ${newCustomer}. You are number ${katzDeliLine.length} in line.`;
+function takeANumber(katzDeli, name) {
+  katzDeli.push(name);
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
 }
 
-function nowServing(katzDeliLine) {
-  if(katzDeliLine.length > 0) {
-    return `Currently serving ${katzDeliLine.shift()}.`;
-  }  else {
+function nowServing(katzDeli) {
+  if(katzDeli.length === 0) {
     return 'There is nobody waiting to be served!';
+  } else {
+    return `Currently serving ${katzDeli.shift()}.`;
   }
 }
 
-function currentLine(katzDeliLine) {
-  var katzDeliCountdown = katzDeliLine.length - 1;
-  var lineList;
-  var countUp = 1;
-  var arrayCountUp = 0;
-  if(katzDeliLine.length > 0) {
-    lineList = `${countUp}. ${katzDeliLine[arrayCountUp]},`;
-    countUp++
-    arrayCountUp++
-    while(katzDeliCountdown > 1) {
-      lineList = `${lineList} ${countUp}. ${katzDeliLine[arrayCountUp]},`;
-      countUp++;
-      arrayCountUp++;
-      katzDeliCountdown--;
+function currentLine(katzDeli) {
+  
+  // It's just a little easier to type 
+  // and we're going to be using this a lot.
+  const lineLength = katzDeli.length
+  
+  if(lineLength === 0) {
+    return 'The line is currently empty.';
+  } else {
+    
+    // First open the list 
+    var list = `The line is currently: 1. ${katzDeli[0]}, `;
+    
+    // Loop through the middle of the array
+    // attaching commas appropriately 
+    for(let i = 1 ; i < lineLength - 1 ; i++) {
+      list = list + `${i+1}. ${katzDeli[i]}, `;
     }
-    lineList = `${lineList} ${countUp}. ${katzDeliLine[arrayCountUp]}`;
-    return `The line is currently: ${lineList}`;
-  } else if(katzDeliLine.length === 0) {
-      return `The line is currently empty.`;
-    }
+    
+    // Attaches the last item seperately so it
+    // doesn't have a comma at the end.
+    list = list + `${lineLength}. ${katzDeli[lineLength - 1]}`;
+    return list;
+  }
 }
