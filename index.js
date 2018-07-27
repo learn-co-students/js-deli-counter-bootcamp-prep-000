cb7334bb83cd40ca8f52bc2f785342e3c13d1bf8
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-07-27 18:15:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeliLine = []
+
+function takeANumber(katzDeliLine, personName) {
+  katzDeliLine.push(personName)
+  return `Welcome, ${personName}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(line) {
+if (line.length > 0) {
+    return `Currently serving ${line.shift()}.`
+  }
+   return "There is nobody waiting to be served!"
+}
+
+function currentLine(line) {
+   if ( line.length === 0 ) {
+     return "The line is currently empty."
+   } else {
+     const theLines = []
+
+     let i = 0
+     while (i < line.length) {
+     theLines.push(` ${[i + 1]}. ${line[i]}`)
+     i++
+  }
+  return "The line is currently:" + theLines
+   }
+}
