4ecf1a1396fc4fa002c0efa8d9f1e4a58645bf88
Joe Muro <jmuro0321@gmail.com>
2018-07-28 17:59:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-var katzDeli = [ ];
-
-function takeANumber(katzDeliLine, customerName) {
-  katzDeliLine.push(customerName);
-  return katzDeliLine.length + 1;
+function takeANumber(deliLine, custName) {
+  deliLine.push(custName);
+  return `Welcome, ${custName}. You are number ${deliLine.length} in line.`;
 }
 
-function nowServing() {
+function nowServing(deliLine) {
   
-  if (katzDeli.length > 0) {
-    return katzDeli.shift();
+  if (deliLine.length > 0) {
+    return `Currently serving ${deliLine.shift()}.`;
   } else {
     return 'There is nobody waiting to be served!'
   }
 }
 
-function currentLine(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-    var prefix = "The line is currently: ";
-    for (let i = 0; i < katzDeliLine.length; i++) {
-      prefix += i ". " katzDeliLine[i] + ","
+function currentLine(deliLine) {
+  
+  if (deliLine.length > 0) {
+    
+    var announce = "The line is currently: ";
+    
+    for (let i = 0; i < deliLine.length; i++) {
+      announce += `${(i + 1)}. ${deliLine[i]}, `
     }
+    
+    return announce.slice(0, -2);
+    
   } else {
     return 'The line is currently empty.'
   }
