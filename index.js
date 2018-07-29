72894eb52bc8c4d62a0bc28400119d7a460eb32c
thomasl.pittman@gmail.com <thomasl.pittman@gmail.com>
2018-07-29 15:41:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 var katzDeli = [];
-
-function takANumber(katzDeliLine, name){
-  katzDeli.push(katzDeliLine, name);
-  return katzDeliLine;
+function takeANumber(line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
+function nowServing(line){
+  if (line.length < 1){
+    return 'There is nobody waiting to be served!';
+  }
+  var customer_name = line.shift(0);
+  return `Currently serving ${customer_name}.`;
+  
+}
+
+function currentLine(line, name){
+  
+  if (line.length < 1){
+    return 'The line is currently empty.';
+  }
+  
+ var namesAndNumbers = []; 
+  for (let i = 0 ; i < line.length; i++){
+    namesAndNumbers.push(`${i + 1}. ${line[i]}`);
+  } 
+    return `The line is currently: ${namesAndNumbers.join(', ')}`;
+
+}
