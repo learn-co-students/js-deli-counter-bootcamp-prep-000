7e5cc2614b5517da78e2db844834162b1604c012
Amir Safoev <amirsafoev0@gmail.com>
2018-07-30 15:53:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var katzDeli = []
 function takeANumber(katzDeliLine, name){
-  
-  return (`Wlecome, $(name). You are number $[katzDeliLine] in line.`)
+  katzDeli.shift
+  return (`Wlecome, $(name). You are number $(katzDeliLine) in line.`)
 }
