4515b704eed7147a7adfbe539a6dfeb0c80d54bf
Billy Tan <btan3@binghamton.edu>
2018-07-31 22:56:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+var katzDeliline = [];
+
+function takeANumber(katzDeliline, newPerson){
+  katzDeliline.push(`${newPerson}`);
+  return (`Welcome, ${newPerson}. You are number ${katzDeliline.length} in line.`);
+}
+
+function nowServing(katzDeliLine){
+  let i = 0; 
+  if (i < katzDeliLine.length){
+    i++;
+  }
+  if (katzDeliLine.length === 0){
+    return (`There is nobody waiting to be served!`);
+  }
+  else
+  return (`Currently serving ${katzDeliLine.shift()}.`);
+}
+
+var line = [];
+
+function currentLine(katzDeliLine){
+  let i = 0;
+  while (i < katzDeliLine.length) {
+    line.push(` `+[i+1]+`. `  + katzDeliLine[i])
+    i++;
+  }
+  if (!katzDeliLine.length){
+    return (`The line is currently empty.`);
+  }
+  return(`The line is currently:` + line);
+}
+
