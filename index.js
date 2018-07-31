ebe7e0aa67a53e02c32de86c136db82e48bac3bc
Noelle Rolwes <nrolwes@gmail.com>
2018-07-31 16:08:55 +0000

    Done.

--- index.js
+++ index.js
@@ -14,9 +14,24 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
-  if (katzDeliLine.length>=1){
+  if (katzDeliLine.length>1){
+    var incrementNumbersWithNames = [];
+  
+    for (let katzDeliLineIndex = 0; katzDeliLineIndex < katzDeliLine.length; katzDeliLineIndex++){
+        incrementNumbersWithNames.push(` ${katzDeliLineIndex + 1}. ${katzDeliLine[katzDeliLineIndex]}`)
+    }
+    return `The line is currently:${incrementNumbersWithNames}`
+  }else{
+    return `The line is currently empty.`;
+  }
+}
+  
+/* took off the '='' sign in line 17 and 20 with '>=' and fixed the '4. undefined error'
+  
+  
+  /*if (katzDeliLine.length>=1){
     return `The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]}`;
   } else {
    return `The line is currently empty.`;
-  }
-}
+  }*/
+
