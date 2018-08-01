b1fb299d22c8ac6b726862fb3bb960af0b98a5c8
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-01 11:32:49 +0000

    Done.

--- index.js
+++ index.js
@@ -20,9 +20,16 @@ function currentLine(line) {
 
      let i = 0
      while (i < line.length) {
+<<<<<<< HEAD
      theLines.push(` ${[i + 1]}. ${line[i]}`)
      i++
   }
   return "The line is currently:" + theLines
+=======
+     theLines.push(`${line[i](i)}. ${line[i]}`)
+     i++
+  }
+  return "The line is currently:  " + theLines
+>>>>>>> 7666acedfb4e282da8561dc1b143afea5eba09b0
    }
 }
