4436b4f4615d862ab4f7b19c7c85e23979ec0277
Kerr Ivan Agapito Cirilo <kerr.ivan29@gmail.com>
2018-08-01 10:00:56 +0000

    Done.

--- index.js
+++ index.js
@@ -34,4 +34,3 @@ function currentLine(katzDeliLine){
     return ("The line is currently:"+line);
    }
 }
-//3. Build a function `currentLine` that returns the current line. For example, if `katzDeliLine` is currently `["Ada", "Grace"]`, `currentLine(katzDeliLine)` would return `"The line is currently: 1. Ada, 2. Grace"`.  If there is nobody in line, it should return `"The line is currently empty."
