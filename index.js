ea3336a156c726df62365b3b717cd66e6c7a13ff
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-08-02 14:04:07 +0000

    Done.

--- index.js
+++ index.js
@@ -18,3 +18,20 @@ function nowServing(customersWaiting){
     }
     return customersWaiting;
 }
+
+// TODO: Build a function currentLine that accepts the current line of people and returns the current line as a string; for example, if 'katzDeliLine' is currently ["Ada", "Grace"], currentLine(katzDeliLine) would return "The line is currently: 1. Ada, 2. Grace". You don't have to use katzDeliLine as a variable or parameter name in your function though, it's just an example of a variable that might be passed to it. If there is nobody in line, it should return "The line is currently empty."
+
+function currentLine(customersWaiting){
+    if(customersWaiting <= 0){
+        return 'The line is currently empty.';
+    } else {
+        let nameList = [];
+        for(let i = 0; i < customersWaiting.length; i++){
+        let currentPosition = customersWaiting.indexOf(customersWaiting[i]) + 1;
+        let customerName = customersWaiting[i];
+        nameList.push(" " + currentPosition + '. ' + customerName); 
+        }
+        return "The line is currently:" + nameList;
+    
+    }
+}
