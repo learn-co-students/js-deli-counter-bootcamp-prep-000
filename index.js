8b2319366fbfeca68adeb768d57f2bf44d443a11
Erin Penner <epenner0522@gmail.com>
2018-08-02 19:13:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+// function to allow a customer to take a number for line position
+function takeANumber(line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
+}
+
+function nowServing(line) {
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else {
+    return `Currently serving ${line.shift()}.`;
+  }
+}
+
+function currentLine(line) {
+  var count = 0;
+  var lineString = "The line is currently";
+  
+  if (line.length === 0) return lineString += " empty.";
+  else {
+    if (count === 0) lineString += ": ";
+    while (count < line.length) {
+      lineString += `${count + 1}. ${line[count]}`;
+      if (count++ < line.length - 1) lineString += ", ";
+    }
+  }
+  return lineString;
+}
