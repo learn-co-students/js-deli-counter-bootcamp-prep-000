161e8f75d61a14e07a749d15c38e1d31a75cf457
Erin Penner <epenner0522@gmail.com>
2018-08-02 21:56:22 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function takeANumber(line, name) {
   return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
+// function to show who will be served 
 function nowServing(line) {
   if (line.length === 0) {
     return "There is nobody waiting to be served!";
@@ -17,13 +18,12 @@ function currentLine(line) {
   var count = 0;
   var lineString = "The line is currently";
   
-  if (line.length === 0) return lineString += " empty.";
-  else {
-    if (count === 0) lineString += ": ";
-    while (count < line.length) {
-      lineString += `${count + 1}. ${line[count]}`;
-      if (count++ < line.length - 1) lineString += ", ";
-    }
-  }
+  lineString += (line.length === 0 ? " empty." : ": ");
+
+  while (count < line.length) {
+    lineString += `${count + 1}. ${line[count]}`;
+    if (count++ < line.length - 1) lineString += ", ";
+  } 
+
   return lineString;
 }
