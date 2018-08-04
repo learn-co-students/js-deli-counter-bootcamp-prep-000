b8c55ddf13d75a235b88ec6ec59bbbd877231b7a
Amir Safoev <amirsafoev0@gmail.com>
2018-08-04 18:21:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-var katzDeli = []
-function takeANumber(katzDeliLine, name){
-  katzDeli.shift
-  return (`Wlecome, $(name). You are number $(katzDeliLine) in line.`)
+
+function takeANumber(line, name){
+line.push(name)
+ return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
+ function nowServing(katzDeliLine){
+   
+    if ( katzDeliLine.length = 0){
+     
+     console.log("There is nobody waiting to be served!");
+  
+   } else {
+     
+   
+   return  `Currently serving ${katzDeliLine[0]}.`;
+   
+   }
+   katzDeliLine.shift();
+   }
+   
+  //function currentLine(line)
+ 
