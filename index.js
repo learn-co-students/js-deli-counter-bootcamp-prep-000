d0b0317aa352c1e9e85192f2255abfe1b90f51b2
Amir Safoev <amirsafoev0@gmail.com>
2018-08-04 19:25:22 +0000

    Done.

--- index.js
+++ index.js
@@ -5,18 +5,37 @@ line.push(name)
 }
  function nowServing(katzDeliLine){
    
-    if ( katzDeliLine.length = 0){
+    if ( katzDeliLine.length === 0){
      
-     console.log("There is nobody waiting to be served!");
+     return "There is nobody waiting to be served!";
   
    } else {
      
    
-   return  `Currently serving ${katzDeliLine[0]}.`;
+   return  `Currently serving ${katzDeliLine.shift(0)}.`;
    
    }
-   katzDeliLine.shift();
+   // katzDeliLine.slice(1);
    }
    
-  //function currentLine(line)
- 
+  function currentLine(katzDeliLine){
+    //line.push(line.length)
+    //return (line.toString())
+    if (katzDeliLine.length === 0){
+      return "The line is currently empty." }
+    //} else {
+    //return ("The line is currently: " + katzDeliLine[]);
+  
+  for (var i = 0; i < katzDeliLine.length; i++  ){
+    return `The line is currently: ${[i]}. ${katzDeliLine[i]}, ${[i+1]}. ${katzDeliLine[i+1]}, ${[i++]}. ${katzDeliLine[i++]}.`
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
