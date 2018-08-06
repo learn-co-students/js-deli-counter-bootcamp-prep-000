2b98f40203602e1da8a64a32d1a5b17d337d3efa
Ali Stone <a.stone1794@gmail.com>
2018-08-06 18:34:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,51 @@
+
+function takeANumber(katzDeliLine, name) {
+  
+  katzDeliLine.push(name);
+  
+  var i = katzDeliLine.length;
+  
+  return `Welcome, ${name}. You are number ${i} in line.`;
+}
+
+
+  
+function nowServing(katzDeliLine) {
+  
+  if (katzDeliLine.length === 0) {
+    
+    return "There is nobody waiting to be served!";
+    
+    } else {
+
+    return `Currently serving ${katzDeliLine.shift()}.`;
+    
+    }
+  }
+  
+
+
+function  currentLine(katzDeliLine) {
+  console.log('katz del =i like', katzDeliLine)
+  
+  
+  if(katzDeliLine.length === 0){
+      return 'The line is currently empty.'
+
+  }
+  let msg = ''
+  
+  for(let i = 0; i < katzDeliLine.length; i++){
+    if(i > 0){
+      msg += ', '
+    }
+    msg += (i + 1) + '. ' + katzDeliLine[i]
+  }
+  
+  return 'The line is currently: ' + msg
+  
+}
+
+
+
+
