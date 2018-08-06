213adcad8f561a2c569feeef0c3072fcc74e0505
Ali Stone <a.stone1794@gmail.com>
2018-08-06 19:20:36 +0000

    Done.

--- index.js
+++ index.js
@@ -23,11 +23,29 @@ function nowServing(katzDeliLine) {
     }
   }
   
-
-
-function  currentLine(katzDeliLine) {
-  console.log('katz del =i like', katzDeliLine)
   
+  function currentLine(katzDeliLine) {
+    
+    if (katzDeliLine.length === 0) {
+      return "The line is currently empty.";
+    }
+    
+    for (let i = 0; i < katzDeliLine.length; i++) {
+      katzDeliLine[i] = `${i+1}. ${katzDeliLine[i]}`;
+    }
+    return `The line is currently: ${katzDeliLine.join(', ')}`;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+/*function  currentLine(katzDeliLine) {
   
   if(katzDeliLine.length === 0){
       return 'The line is currently empty.'
@@ -44,7 +62,7 @@ function  currentLine(katzDeliLine) {
   
   return 'The line is currently: ' + msg
   
-}
+}*/
 
 
 
