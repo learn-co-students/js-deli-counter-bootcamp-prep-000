4c79dcad033cd6af5c166548209cacf973dd6f3b
Ashley Wong <ashwong04@gmail.com>
2018-08-07 23:43:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function takeANumber (line, name){
-  
- return "Welcome, " + name + ". You are number " + line + " in line."
+  let newLine =  line.push(name);
+
+  return "Welcome, " + name + ". You are number " +  line.length +  " in line.";
 }
+
+
+
+
