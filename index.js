5d739d81264c856ec07c353ec6322e824f895c1e
Kevin O'Reilly <molldokor@gmail.com>
2018-08-08 10:17:41 +0000

    Done.

--- index.js
+++ index.js
@@ -9,10 +9,32 @@ function nowServing(currentline) {
     while (currentline.length > 0) {
       var nameServed = currentline[0];
       currentline.shift();
-      return nameServed;
+      return `Currently serving ${nameServed}.`;
     }
   } else {
     return "There is nobody waiting to be served!";
   }
-  
+}
+
+function currentLine(currentline) {
+  if (currentline.length > 0) {
+    var countdown = currentline.length;
+    var response = "The line is currently: ";
+    var list = [];
+    var numName = "";
+    while (countdown > 0) {
+      if (countdown === 1) {
+        numName = `${countdown}. ${currentline[countdown - 1]}`;
+        list.unshift(numName);
+        countdown = countdown - 1;
+      } else {
+        numName = ` ${countdown}. ${currentline[countdown - 1]}`;
+        list.unshift(numName)
+        countdown = countdown  -1;
+      }
+    }
+    return response + list;
+  } else {
+    return "The line is currently empty.";
+  }
 }
