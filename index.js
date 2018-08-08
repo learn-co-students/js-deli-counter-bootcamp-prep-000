5ac6ff7b52285aede2cc0b35b8d96d5081fa6d3a
Tirem Bareno-Sosna <tirempbs@gmail.com>
2018-08-08 16:12:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    var customer = katzDeliLine.shift();
+    return `Currently serving ${customer}.`;
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var line = []
+  let i = 0
+  while (i < katzDeliLine.length) {
+    line.push(` ${i + 1}` + '. ' + katzDeliLine[i]);
+    i++;
+  }
+  if (katzDeliLine.length > 0) {
+    return `The line is currently:` + line;
+  } else {
+    return "The line is currently empty.";
+  }
+}
