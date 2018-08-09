f51dcc3f35053f7a4f0705f643f9f628c51113ed
Akmal Khadir <akmal.a.khadir@gmail.com>
2018-08-09 15:48:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function takeANumber(deliLine, name) {
+
+  deliLine.push(name);
+  return `Welcome, ${name}. You are number ${deliLine.length} in line.`;
+
+}
+
+
+function nowServing(deliLine){
+
+  if (deliLine.length == 0){
+    return `There is nobody waiting to be served!`
+
+  } else {
+    return `Currently serving ${deliLine.shift()}.`;
+
+  }
+}
+
+function currentLine(deliLine){
+  if (deliLine.length == 0){
+    return `The line is currently empty.`;
+  } else {
+    var newArray = []
+  for (let i = 0; i < deliLine.length; i++){
+    newArray.push(`${i + 1}. ${deliLine[i]}`)
+    }
+    return `The line is currently: ${newArray.join(`, `)}`
+  }
+}
