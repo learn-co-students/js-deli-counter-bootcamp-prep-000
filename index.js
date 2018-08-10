90fff97bcf23b71e99ce20df8ce71a9c5d7ebca1
Hank Liu <hanke.liu@gmail.com>
2018-08-10 00:23:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var katzDeliLine = [];
-function takeNumber(katzDeliLine, name) {
 
-  console.log("Welcome, ${name}. You are number ${katzDeliLine.length} in line.")
-}
+function takeNumber(katzDeliLine, name) {
+  do {
+    katzDeliLine.push(name)
+    i++
+  } while ( i >0)
+  console.log(`Welcome, ${name}. You are number ${katzDeliLine.length+1} in line.`)
