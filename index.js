809ea0f7599f2dd3a95cf80aa448a6e268027f74
Hank Liu <hanke.liu@gmail.com>
2018-08-10 00:46:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var katzDeliLine = [];
 
-function takeNumber(katzDeliLine, name) {
-  do {
+function takeANumber(katzDeliLine, name) {
     katzDeliLine.push(name)
-    i++
-  } while ( i >0)
-  console.log(`Welcome, ${name}. You are number ${katzDeliLine.length+1} in line.`)
+    var j = katzDeliLine.length+1;
+ return `Welcome, ${name}. You are number ${j} in line.`
+}
+
+////////////////////////////////////
+
+function nowServing(katzDeliLine) {
+
+  if (katzDeliLine.length === 0) {
+    console.log("There is nobody waiting to be served!")
+  } else {
+    console.log(katzDeliLine[0])
+    katzDeliLine.shift()
+  }
+  return katzDeliLine
+}
