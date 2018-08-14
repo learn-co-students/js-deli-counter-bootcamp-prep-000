0f6146c15e3811cfd7010a26d9bf1413cff3c605
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-08-14 20:01:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function takeANumber(katzDeliLine, newPerson) {
+  katzDeliLine.push(newPerson);
+  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  while (katzDeliLine.length > 0) {
+  var currentlyServing = katzDeliLine.shift();
+  
+  return `Currently serving ${currentlyServing}.`
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var people = [];
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  }
+  
+  for (let i = 0; i < katzDeliLine.length; i++) {
+    
+   people.push(` ${i+1}. ${katzDeliLine[i]}`);
+    
+  
+  }  
+  people.join(',');
+  return `The line is currently:${people}`;
+  
+}
