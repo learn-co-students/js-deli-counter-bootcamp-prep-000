8005dd3e95bef69eeb58de436ba1205adb40abb5
Arlisha2019 <arlisha_hayles@yahoo.com>
2018-08-14 02:20:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function takeANumber(array, name) {
+  array.push(name);
+ return `Welcome, ${name}. You are number ${array.length} in line.`;  
+}
+
+function nowServing(array) {
+if (array.length === 0) {
+  return "There is nobody waiting to be served!";
+} else {
+  return `Currently serving ${array.shift()}.`;
+  }
+}
+
+function currentLine(array) {
+  if (array.length === 0) {
+    return "The line is currently empty."
+  } else {
+    return `The line is currently: 1. ${array.shift()}, 2. ${array.shift()}, 3. ${array.shift()}`;
+  }
+  
+}
