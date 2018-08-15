fb8a3a01df0995a5eb5c62c9b96142819f22627b
Pearl Zeng <zhuzeng0418@gmail.com>
2018-08-15 17:09:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+let katzDeliLine = []
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return `There is nobody waiting to be served!`
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) return `The line is currently empty.`
+  for (let i = 0; i < katzDeliLine.length; i++) {
+    katzDeliLine[i] = `${i + 1}. ${katzDeliLine[i]}`
+  }
+  return `The line is currently: ${katzDeliLine.join(', ')}`
+}
