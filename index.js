57d4c9272d2d7e32560ac5b6c85e883c9a810236
Joshua Vieira <physicsnerd38@gmail.com>
2018-08-16 15:19:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function takeANumber(katzDeliLine, customerName) {
+  katzDeliLine.push(customerName);
+  return `Welcome, ${customerName}. You are number ${katzDeliLine.length} in line.`;
+}
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+      return `Currently serving ${katzDeliLine.shift()}.`;
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var newKatzDeliLine = [];
+   if (katzDeliLine.length === 0) {
+      return "The line is currently empty.";
+   }
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      newKatzDeliLine.push(" " + parseInt(i+1) + "." + " " + katzDeliLine[i]);
+  }
+      return "The line is currently:" + newKatzDeliLine;
+}
