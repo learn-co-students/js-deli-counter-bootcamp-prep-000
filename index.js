226c4c237bec8ee068a834b1b4890260b61ae4da
elaitman1 <elaitman1@gmail.com>
2018-08-17 22:12:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-function takeANumber(katzDeli, newcustomer){
-  katzDeli.push(newcustomer);
-  return(`Welcome, ${newcustomer}. You are number ${katzDeli.length} in line.`);
+function takeANumber(deliLine, name) {
+  deliLine.push(name)
+  return `Welcome, ${name}. You are number ${deliLine.length} in line.`
 }
+
 function nowServing(deliLine){
-  if (deliLine.length === 0){
-    return "There is nobody waiting to be served!"
-  } else {
-    let x = deliLine.shift();
-    return `Currently serving ${x}.`;
-  }
+if (deliLine.length === 0){
+  return "There is nobody waiting to be served!";
+}else{
+  var shifted = deliLine.shift();
+  return `Currently serving ${shifted}.`
 }
+}
+
+
 function currentLine(y){
-    let array = [];
-    for (let x = 0; x < y.length; x++){
-      if (x === y.length - 1){
-      array.push(`${x+1}. ${y[x]}`)
-      }else {
-      array.push(`${x+1}. ${y[x]},`)
-    }
-    }
-      if (array.length === 0){
-        return "The line is currently empty.";
-      }else{ 
-        return "The line is currently: " + array.join(" ");
+  var comment = "The line is currently:"
+  if (y.length === 0){
+    return "The line is currently empty."
+  }else{
+    var i = 1
+    for (var x = 0; x < y.length; x++){
+      comment += ` ${i}. ${y[x]}`
+      if (i !== y.length){
+        comment += ","
+        i++
       }
+    }
+  }
+  return comment
 }
