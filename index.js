e49a40c62325e4bcc83176fbbc6c618fdd867358
Sachit Shetty <sachit.shetty@gmail.com>
2018-08-19 16:24:46 +0000

    Done.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(katzDeliLine) {
     return "The line is currently empty.";
   } 
   else {
-    var currentLine= "The line is currently:"
+    var currentLine= 'The line is currently:'
     for (var i = 0; katzDeliLine.length > 0 ; i++) {
     var currentLineName = katzDeliLine[i];
     currentLine += currentLineName;
