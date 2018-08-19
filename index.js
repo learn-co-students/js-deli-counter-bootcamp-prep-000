073d45a219430f7ff70443692f92364b2c4f8c9c
Sachit Shetty <sachit.shetty@gmail.com>
2018-08-19 16:21:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function takeANumber(katzDeliLine, name) {
   var num = katzDeliLine.length + 1
+  katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${num} in line.`
 }
 
@@ -7,6 +8,22 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-    return katzDeliLine[0]
+    return `Currently serving ${katzDeliLine.shift(0)}.`
+  }
+  katzDeliLine.shift()
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  } 
+  else {
+    var currentLine= "The line is currently:"
+    for (var i = 0; katzDeliLine.length > 0 ; i++) {
+    var currentLineName = katzDeliLine[i];
+    currentLine += currentLineName;
+      
+    }
+  return currentLine    
   }
 }
