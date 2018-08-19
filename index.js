659578a77f533d7bc8578eff256221ea00eed931
Kyle George <k.george21@outlook.com>
2018-08-19 01:23:00 +0000

    Done.

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ line.push(name)
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length = 0) {
+  if (katzDeliLine.length === 0) {
     return  "There is nobody waiting to be served!"
   } else{
     var first = katzDeliLine[0]
     katzDeliLine.shift()
-    return first
+    return `Currently serving ${first}.`
+  }
+}
+
+function currentLine(array, line) {
+  if (array.length === 0) {
+    return "The line is currently empty."
+  }else{
+    var first = array.length
+    
+    var newArray = [];
+    
+    for (let i = 0; i < array.length; i++){
+    
+    newArray.push(`${i + 1}. ${array[i]}`)
+    }
+    return `The line is currently: ${newArray.join(', ')}`
   }
 }
