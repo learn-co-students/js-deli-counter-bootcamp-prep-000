d9688107aa782c56db7dd439022518a0a1bb5a6d
Joon Jang <joon.jang95@gmail.com>
2018-08-19 01:36:56 +0000

    Done.

--- index.js
+++ index.js
@@ -8,19 +8,22 @@ function nowServing(katzDeliLine) {
  if (katzDeliLine.length === 0) {
      return "There is nobody waiting to be served!"
  } else {
-   return katzDeliLine.shift()
+   return "Currently serving " + katzDeliLine.shift() + "."
  }
 }
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
-  } else if (katzDeliLine.length === 1) { 
-    var i = 0
-    var result = console.log('1.' + katzDeliLine[i])
-    return `The line is currently: ${result}`
   } else {
-    var p = katzDeliLine.length + "."
-    return `The line is currently: ${p} 
+  var list = []
+  var i = 0
+  var LineNumber = katzDeliLine.length + "."
+  while (i < katzDeliLine.length) {
+     var results = `${LineNumber}` + katzDeliLine[i];
+     list.push(results);
+     i += 1
+   } 
+   return `The line is currently: ${list}`
   }
 }
