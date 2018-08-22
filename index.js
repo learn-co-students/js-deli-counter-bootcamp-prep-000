250ef9c6e1c22c8d6cee9085b3e097320c813002
Randy Herasme <randyherasme6@gmail.com>
2018-08-22 17:03:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+
 var katzDeli=[];
 
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
-  return "Welcome, "+name+". You are number "+ parseInt(katzDeliLine.length)+ " in line."
+  return "Welcome, "+name+". You are number "+(katzDeliLine.length)+ " in line."
 }
 
 function nowServing(katzDeliLine){
-  if (katzDeliLine.length==0){
+  if (katzDeliLine.length===0){
   return "There is nobody waiting to be served!"
-  }
-  else {
+  } else {
   var output="Currently serving "+katzDeliLine[0]+".";
   katzDeliLine.shift()
   return output;
@@ -18,14 +18,14 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine){
   var output=""
-  if (katzDeliLine.length==0){
+  if (katzDeliLine.length===0){
   return  "The line is currently empty."
-  }  
-  else { 
-  for (var y=0;y<katzDeliLine.length;y++)
-  var output=output+" "+ parseInt(y+1)+". "+ katzDeliLine[y]+ ",";
-}
-var outputs= "The line is currently:"+ output
-outputs=outputs.slice(0,-1)
-return outputs
+  }else{ 
+  for (var y=0;y<katzDeliLine.length;y++){
+  output=output+" "+(y+1)+". "+ katzDeliLine[y]+ ",";
+    }
+  }
+  var finalOutput= "The line is currently:"+ output;
+  finalOutput=finalOutput.slice(0,-1);
+return finalOutput
 }
