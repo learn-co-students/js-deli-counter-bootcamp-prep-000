12bd15e1682520642a0a645be6e7fdca4e85d57d
Robin Tram <robin.tram@gmail.com>
2018-08-22 20:17:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function takeANumber (katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing (katzDeliLine) {
+  if (katzDeliLine.length !== 0) {
+    var currentlyServing = katzDeliLine.shift();
+    return `Currently serving ${currentlyServing}.`;
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine (katzDeliLine) {
+  var chronologicalOrder = [];
+  if (katzDeliLine.length !== 0) {
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      chronologicalOrder.push = `${i + 1}. ${katzDeliLine[i]}`;
+    }
+    return `The line is currently: ${chronologicalOrder}`;
+  } else {
+    return "The line is currently empty.";
+  }
+}
