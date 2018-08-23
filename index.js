f5c2a4728e055827c8051203e8f84b4a4abf5ea8
Adreana Bell <adreanab847@gmail.com>
2018-08-23 07:26:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function takeANumber (katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+}
+function nowServing (katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+  var name = katzDeliLine[0];
+  katzDeliLine.shift(0,1);
+    return "Currently serving " + name + ".";
+  }
+}
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+    return "The line is currently: 1. "+katzDeliLine[0]+", 2. " + katzDeliLine[1] + ", 3. " + katzDeliLine[2];
+  }
+}
