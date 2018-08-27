ebda424421d1abaf9c1fe1debf9df757403f24a2
Steven Rico <intuitive.io.programmer@gmail.com>
2018-08-27 21:23:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return `Currently serving ${katzDeliLine.shift()}.`;
+  }
+  else {
+    return 'There is nobody waiting to be served!';
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return `The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]}`;
+  }
+  else {
+    return 'The line is currently empty.';
+  }
+}
