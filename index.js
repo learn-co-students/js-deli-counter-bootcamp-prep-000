2619e30d34e0d93975c280b84101c75a30e12982
Leslie Turis <lturis@gmail.com>
2018-08-29 15:49:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let deliLine = [];
 
+//takeANumber(deliLine, customer)
+
 function takeANumber(deliLine, customer) {
   deliLine.push(`${customer}`);
   for(let i = 0; i < deliLine.length; i++) {
@@ -8,25 +10,34 @@ function takeANumber(deliLine, customer) {
 }
 
 
+//nowServing(deliLine)
 
 function nowServing(deliLine) {
+
   if(deliLine.length === 0) {
     return 'There is nobody waiting to be served!';
-  } else {
+  }
+  else {
     return `Currently serving ${deliLine.shift()}.`;
   }
 }
 
 
+//currentLine(line)
+
+let line = [];
 
 function currentLine(line) {
-  let currentOrder = [];
-  if (line.length > 0) {
-    for (let i = 0; i < line.length; i++) {
-      currentOrder.push(`${i + 1}. ${line[i]}`);
-    }
-    return `The line is currently: ${currentOrder.join(', ')}`;
-  } else {
-    return 'The line is currently empty.';
+let newLine = [];
+
+  for(let i = 0; i < line.length; i++) {
+    newLine.push(`${i+1}. ${line[i]}`);
   }
+
+    if(line.length > 0) {
+      return `The line is currently: ${newLine.join(', ')}`
+    }
+    else {
+      return 'The line is currently empty.';
+    }
 }
