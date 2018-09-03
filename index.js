c75919e105a6d7e00a3c3ebed1261e0ba620dd71
hansalallu@gmail.com <hansalallu@gmail.com>
2018-09-03 19:49:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-function takeANumber(line, name){
-  line.push(name)
-  return `Welcome, ${name}. You are number ${line.length} in line.`
+function takeANumber(line, person){
+  line.push(person)
+  return `Welcome, ${person}. You are number ${line.length} in line.`
 }
-
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length===0){
   return `There is nobody waiting to be served!`
-   } else { 
-     return `Currently serving ${katzDeliLine.shift("")}.`;
   }
+  else {
+    return `Currently serving ${katzDeliLine.shift()}.`;
 }
-
+}
 
 function currentLine(katzDeliLine){
   var string = "The line is currently: ";
@@ -30,3 +29,4 @@ function currentLine(katzDeliLine){
   }
   return string + storage
 }
+
