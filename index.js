e446ac0291eb86d0ec871997d468beadd80ce15a
Stephen James <steves0089@gmail.com>
2018-09-03 11:34:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-var katzDeliLine = [];
-
 function takeANumber(line, name) {
-  line.push(`${name}`);
-  return(`Welcome, ${name}. You are number ${line.length} in line.`);
+	line.push(name);
+	return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
+
 
 function nowServing(line) {
-  let i = 0;
-  while (i < line.length) {
-    i++;
-  }
-  if (line.length === 0) {
-    return "There is nobody waiting to be served!";
-  }
-  else
-  return (`Currently serving ${line.shift()}.`);
+	if (line.length === 0) {
+		return "There is nobody waiting to be served!";
+	} else {
+		return `Currently serving ${line.shift()}.`;
+	}
 }
 
-var line = [];
-
-function currentLine(katzDeli) {
-
-for (var i = 0; i < katzDeli.length; i++) {
-  line.push(` `+[i+1]+`. `  + katzDeli[i])
-} if (line.length === 0) {
-  return("The line is currently empty.")
-} else
-return(`The line is currently:` + line);
+function currentLine(line) {
+	if (line.length === 0) {
+		return "The line is currently empty.";
+	} else {
+		let sentence = ["The line is currently: "]; 
+		for (let i = 0; i < line.length; i++) {
+			line[line.length - 1] !== line[i] ? sentence.push(`${i + 1}. ${line[i]}, `) : sentence.push(`${i + 1}. ${line[i]}`);
+		}
+		return sentence.join("");
+	}
 }
+
