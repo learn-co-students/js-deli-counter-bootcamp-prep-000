51106e8251b9d7bf306e778f1aa9814eb3ac2682
hansalallu@gmail.com <hansalallu@gmail.com>
2018-09-03 16:07:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function takeANumber(line, name){
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length===0){
+  return `There is nobody waiting to be served!`
+   } else { 
+     return `Currently serving ${katzDeliLine.shift("")}.`;
+  }
+}
+
+
+function currentLine(katzDeliLine){
+  var string = "The line is currently: ";
+  if (katzDeliLine.length===0){
+  return `The line is currently empty.`
+  } else {
+    var i
+    var storage = ""
+    for (i=0; i<=katzDeliLine.length-1; i++){
+      if((i+1) === katzDeliLine.length) {
+        storage = storage + `${i+1}. ${katzDeliLine[i]}`
+      } else {
+        storage = storage + `${i+1}. ${katzDeliLine[i]}, `
+      }
+    }
+  }
+  return string + storage
+}
