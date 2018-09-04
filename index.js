c3452b101432525bb8190936765a65a05a542053
samantha <sstephens614@yahoo.com>
2018-09-04 08:52:25 +0000

    Done.

--- index.js
+++ index.js
@@ -19,19 +19,15 @@ else {
   }
 }
 
-function currentLine(line){
-  if(line.length < 1){
-    return "The line is currently empty."
-  }
-  else { 
-    let text = "The line is currently" 
-    // return line
-    for (let i = 0; i < line.length ; i++) {
-      text = i + i
-      console.log("--", text + " " + (i+1) + "." + line[i])
+function currentLine(x) {
+    var line = []
+    if (x.length === 0) {
+      return "The line is currently empty."
+    } else {
+      for(var i = 0; i < x.length; i++) {
+        line += (i + 1) + ". " + x[i] + ", "
+      }
+      line = line.slice(0, line.length-2)
+      return "The line is currently: " + line
     }
-    // let animals = [dog, cat, bird, frog]
-    // animals[0]
-  }
 }
-  
