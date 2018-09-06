6843254681dc68a64c9b5658ecbf5773f61ce928
Jacob Menashi <jbmenashi@gmail.com>
2018-09-06 15:53:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
-function takeANumber(theLine,newPerson) {
- theLine.push(newPerson);
- return `Welcome, ${newPerson}. You are number ${theLine.length} in line.`;
+function takeANumber(line,newPerson) {
+ line.push(newPerson);
+ return `Welcome, ${newPerson}. You are number ${line.length} in line.`;
 }
 
-//so you're adding a person to the array of people in line, and the # in the line is just the length of the line, which changes every time you add a person
+//so you're adding a person to the array of people in line, and the # in the line is just the length of the line, which changes every time you add a person.
 
-function nowServing(theLine) {
-  if (theLine.length !== 0) {
-   return `Currently serving ${theLine.splice(0,1)}.`;
-  } else return 'There is nobody waiting to be served!';
-  theLine = theLine.slice(1);
-}
-
-//if there's people in line, you want to splice off the first index, and just the 1 name. If there's no one in line, you have the message. Then, you're resetting the parameter so that it starts with the 2nd person in line.
-
-function currentLine(theLine) {
-  if (theLine.length === 0) {
-    return "The line is currently empty.";
-  } else {
-    var oneMoreLine = [];
-    for (let i=0; i < theLine.length; i++) {
-      oneMoreLine.push(` ${i+1}. ${theLine[i]}`);
-    }
-      oneMoreLine = oneMoreLine.slice(0,3);
-      return 'The line is currently:' + oneMoreLine;
+function nowServing(line) {
+  if (line.length === 0) {
+    return 'There is nobody waiting to be served!';
+  }
+  else {
+    return `Currently serving ${line.shift()}.`
   }
 }
 
-//First part is simple, no people then display the message. Then you're making a new variable to get passed. the for loop adds to the new variable the number and name, then you do the slice to get the first 3 names to the list 
+
+//if there's no one in line, return the expected string. Else, remove the first person in line with shift and return that string. The line is mutated with the shfit method.
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return 'The line is currently empty.'
+  }
+  else {
+    let lineAddition = [];
+    for (let i = 0; i < line.length; i++){
+      lineAddition.push(` ${i+1}. ${line[i]}`);
+    }
+    return 'The line is currently:' + lineAddition;
+  }
+}
+
+//If there's no one in line, return the expected string. Then you're making a new array to hold the line. the for loop adds to the new variable the number and name. When you add the array to the string, it is coerced into a string.
