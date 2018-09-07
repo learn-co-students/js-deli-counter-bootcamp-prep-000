bb7c57165ecaeb80fa44be5091c8a503452ede22
Alisa McCormick <muntananuchat@gmail.com>
2018-09-07 00:40:24 +0000

    Done.

--- index.js
+++ index.js
@@ -5,23 +5,22 @@ function takeANumber(line, name) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return "There is nobody waiting to be served!"
-  } 
+    return `There is nobody waiting to be served!`
+  }
   var firstPerson = katzDeliLine.shift()
   return `Currently serving ${firstPerson}.`
 }
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return "The line is currently empty."
+    return `The line is currently empty.`
   }
   var line = `The line is currently:`
   for (var i = 0; i < katzDeliLine.length; i++) {
     if (i === katzDeliLine.length - 1) {
-      line = `${line} ${i + 1}. ${katzDeliLine[i]}`
-    }
-    else {
-      line = `${line} ${i + 1}. ${katzDeliLine[i]},`
+      line = `${line} ${[i + 1]}. ${katzDeliLine[i]}` 
+    } else {
+      line = `${line} ${[i + 1]}. ${katzDeliLine[i]},`
     }
   }
   return line
