f9ad8949094feaf5cd530e707dcdf87067b49654
Kenan Berker <kbbuk90@gmail.com>
2018-09-09 13:10:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-takeANumber(curLine, newName){
-  return `Welcome, ${newName}. You are ${newName.length + 1} in line.`
+
+function takeANumber(curLine, newName){
+  if(curLine.length === 0){
+    curLine[0] = newName
+    return `Welcome, ${newName}. You are number ${curLine.length} in line.`
+  } else if(curLine.length > 0) {
+    curLine.push(newName)
+    return `Welcome, ${newName}. You are number ${curLine.length} in line.`
+  }
+}
+
+
+function nowServing(curLine) {
+  if (curLine.length === 0){
+    return "There is nobody waiting to be served!"
+  } else {
+    return `Currently serving ${curLine.shift()}.`
+  }
+}
+
+
+function currentLine(curLine) {
+  if (curLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+    var announcement = ""
+    do {
+      var i = 0
+      announcement = announcement + `${i + 1}. ${curLine[i]}, `
+      i++
+    } while (i < curLine.length)
+    return `The line is currently: ` + announcement
+  }
 }
