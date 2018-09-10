c98d3f356c6d845f67002d6da2baab322f208959
Jack Ling <j.lingyx@gmail.com>
2018-09-10 23:49:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  var queue = line.length + 1
+  return `Welcome, ${name}. You are number ${queue} in line.`
+}
