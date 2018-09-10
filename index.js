8755cd71cf8ba8a3e65d10bb35a6ba03989db497
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-09-10 18:03:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+//take a Number
+
+var katzDeliLine =[]; 
+
+function takeANumber(katzDeliLine, name){
+  // var number =number++;
+ katzDeliLine = 0;
+ katzDeliLine++;
+
+  
+  
+  return ("Welcome, "+name+". You are number "+katzDeliLine+" in line.")
+}
