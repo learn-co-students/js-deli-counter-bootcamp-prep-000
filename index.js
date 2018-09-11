148f8815f671bb4b2496b3f3e6a65827784d6a0b
Beanie Pies <kgravesjohnson@gmail.com>
2018-09-11 13:54:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-function takeNumber (lineNumber, name) {
-  var realNumber = lineNumber + 1;
-  return `Welcome, ${name}. You are number ${realNumber} in line.`;
+function takeANumber (lineNumber, name) {
+  lineNumber.push(name);
+ 
+       return "Welcome, "+ name + ". You are number " + lineNumber.length +" in line.";
+      
+     
+   }
+  
+ 
+
+function nowServing (num) {
+  if (num.length > 0) {
+    var name = num[0];
+    num.shift();
+    return "Currently serving " + name +".";
+  } else {
+    return "There is nobody waiting to be served!";
+  }
 }
 
-function nowServing 
+function currentLine (num) {
+  var emp = "The line is currently: "
+  if (num.length > 0) {
+    for (var i = 0; i < num.length; i++) {
+      if (i + 1 < num.length) {
+      emp = emp + (i + 1) +". " + num[i] +", "
+      } else {
+        emp = emp + (i + 1) +". " + num[i] 
+      }
+      
+    } return emp;
+  } else { 
+    return "The line is currently empty." }
+}
