c341e885857d755b942e4bca330ad50dc25f9003
persistent-partial-7850 <bhorowitz2014@gmail.com>
2018-09-12 05:52:37 +0000

    Done.

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function nowServing(katzDeliLine) {
   
 function currentLine(line) {
   if (line.length === 0) {
-    return `The line is currently empty.`
+    return `The line is currently empty.`;
   } else if (line.length > 0) {
-    return `The line is currently: 1. ${line[0]}, 2. ${line[1]}, 3. ${line[2]}`
+    return `The line is currently: 1. ${line[0]}, 2. ${line[1]}, 3. ${line[2]}`;
   }
 }
