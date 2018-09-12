6d84d2946dbb2631e4f9855f1b26812cacb6786f
John Higgins <johnhiggins@outlook.ie>
2018-09-12 20:27:51 +0000

    Done.

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function nowServing (katzDeliLine) {
       return String ("There is nobody waiting to be served!");
   }
 }
+
+function currentLine (katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    var newArray = []
+    newArray.push ("1. "+ katzDeliLine[0])
+    for (let i = 1; katzDeliLine.length > i; i++) {
+      newArray.push (" " + (i + 1) + ". " + katzDeliLine[i])
+    }
+    return String ("The line is currently: " + newArray);
+  }else{
+      return String ("The line is currently empty.");
+   }  
+}
