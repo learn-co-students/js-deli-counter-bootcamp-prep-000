e52840b986c8250ed70d275c7ceddd356957200c
Samantha Hyatt <shhyatt@gmail.com>
2018-09-13 20:04:26 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function takeANumber(katzDeli, name) {
     while(i < katzDeliLine.length) {
       i++;
     }
-    if(katzDeliLine === 0) {
+    if(katzDeliLine.length === 0) {
       return "There is nobody waiting to be served!";
       }
       else 
@@ -21,13 +21,13 @@ function takeANumber(katzDeli, name) {
       function currentLine(katzDeli) {
         let i = 0;
         while(i < katzDeli.length) {
-          line.push(` ` +[i+1]+`.` + katzDeli[i]);
+          line.push(` `  +[i+1]+ `. ` +  katzDeli[i]);
           i++;
           }
           if(katzDeli.length === 0) {
             return "The line is currently empty.";
           } else 
-          return (`The line is currently: `  + line);
+          return (`The line is currently:`  + line);
            }
       
       
