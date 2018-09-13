2fb173e952f09975bcdaff0f09d69a643c77ea6b
Gavin Moore <gavizoid16@gmail.com>
2018-09-13 19:45:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-function takeANumber(katzdeli, name) {
-  let i = 0
-  while (i < (katzdeli.length+1)){
-   katzdeli.concat(otherdeli)
-    i++;
-  }
-  return "Welcome, ${name}. You are number ${i} in line."
+function takeANumber(katzDeli, name) {
+    katzDeli.push(name)
+  return ("Welcome, "+ name +". You are number "+(katzDeli.length)+" in line.")
 }
+function nowServing(katzDeliLine) {
+  let i = 0
+ if (i < katzDeliLine.length) {
+   return ("Currently serving "+(katzDeliLine.shift(name))+".")
+ } else {
+   return ("There is nobody waiting to be served!")
+ }}
+ function currentLine(line) {
+  let deliLine = []
+  for (let i=0; i < line.length; i++) {
+    deliLine.push(" "+(i+1)+". "+line[i])
+   } 
+   if(line.length > 0) {
+      return ("The line is currently:"+deliLine)
+   }
+      return ("The line is currently empty.")
+   }
