ac58e86ea3f4d377073089b1a8da07edbc197a7e
Tirem Bareno-Sosna <tirempbs@gmail.com>
2018-09-20 01:08:48 +0000

    Done.

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  var line = []
-  let i = 0
+  var line = [];
+  let i = 0;
   while (i < katzDeliLine.length) {
-    line.push(` ${i + 1}` + '. ' + katzDeliLine[i]);
+    line.push(` ${i + 1}` + ". " + katzDeliLine[i]);
     i++;
   }
   if (katzDeliLine.length > 0) {
-    return `The line is currently:` + line;
+    return "The line is currently:" + line;
   } else {
     return "The line is currently empty.";
   }
