8ccd88f85ab634ef9a415c9795d67758d50efe05
dwolfe1987 <dwolfe1987@gmail.com>
2018-09-26 18:14:14 +0000

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function takeANumber(line, name){
   line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
+
+function nowServing(katzDeliLine){
+  
+  
+  return "There is nobody waiting to be served!"
+}
