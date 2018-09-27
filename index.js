735ac8855e32a23dac13cd7508452233dc768e87
Selcuk Karakus <selchuck.karakus@gmail.com>
2018-09-27 21:38:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-function takeANumber(currentLine, newPersonName ) {
- currentLine.push(newPersonName);
-  return "Welcome, " + newPersonName + ". You are number " + currentLine.length + " in line.";
+function takeANumber(katzDeliLine, newPersonName ) {
+ katzDeliLine.push(newPersonName);
+  return "Welcome, " + newPersonName + ". You are number " + katzDeliLine.length + " in line.";
 }
 
-function nowServing(katzDeliline) {
-  if(katzDeliLine.length === 0){
+takeANumber(katzDeliLine, "Ada");
+takeANumber(katzDeliLine, "Grace");
+takeANumber(katzDeliLine, "Kent");
+currentLine(katzDeliLine);
+nowServing(katzDeliLine);
+currentLine(katzDeliLine);
+takeANumber(katzDeliLine, "Matz"); 
+currentLine(katzDeliLine);
+nowServing(katzDeliLine);
+currentLine(katzDeliLine);
+
+function nowServing(katzDeliLine) {
+  if(!katzDeliLine.length){
     
   return "There is nobody waiting to be served!" ;
 
+  } else {
+    return "Currently serving " + katzDeliLine.shift() + ".";
   }
   
+}
+
+function currentLine(katzDeliLine) {
+  if(!katzDeliLine.legth) {
+  return "The line is currently empty.";
+  }
+  let katz = [];
+  for(var i = 0; i < katzDeliLine.length; i++) {
+    katz.push(i+1 + ". " + katzDeliLine[i]);
+  }   
+  return "The line is currently " + katz;
 }
