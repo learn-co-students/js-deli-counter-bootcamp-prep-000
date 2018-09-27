f5eb8f4e6d4b4196d6cb941cd4f5b094c1f2c340
dwolfe1987 <dwolfe1987@gmail.com>
2018-09-27 16:21:43 +0000

    Done.

--- index.js
+++ index.js
@@ -15,11 +15,18 @@ function nowServing(deliLine){
 
 
 function currentLine(line){
-  for(var i = 0; line.length > 0; i++){
-  return `The line is currently:${line[i]}`
-  }
-  else
-{
+  let currentLine = ""
+  if (line.length === 0) {
     return "The line is currently empty."
+  } else {
+    for(var i = 0; line.length > 0; i++) {
+      currentLine += `${i+1}. ${line[i]}`
+    }
+  
+  return `The line is currently:${currentLine}.`
+  }
 }
-}
+// take in an array (line) and check to see the length.
+// If the length is 0, return the line is currently empty. 
+// otherwise, loop through the array, use index +1 as the position in line. 
+// Concatentate onto a string. Return string.
