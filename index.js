6454d5a919e54e457250a73b2a1545ce4de84833
Selcuk Karakus <selchuck.karakus@gmail.com>
2018-09-27 20:27:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function takeANumber(currentLine, newPersonName ) {
+ currentLine.push(newPersonName);
+  return "Welcome, " + newPersonName + ". You are number " + currentLine.length + " in line.";
+}
+
+function nowServing(katzDeliline) {
+  if(katzDeliLine.length === 0){
+    
+  return "There is nobody waiting to be served!" ;
+
+  }
+  
+}
