8c4cb041ad3fec0b3a51cab290f3e9f73d788dbf
Brian <briandsalemi@gmail.com>
2018-10-01 21:04:08 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ function takeANumber(katzDeliLine, newName) {
 }
 
 function nowServing(katzDeliLine) {
+ if (katzDeliLine.length > 0){
+   return(`Currently serving ${katzDeliLine.shift()}.`); }
+   else {
+    return("There is nobody waiting to be served!");
+} }
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length > 0){
+    return (`The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]}`); }
+  else { 
+    return("The line is currently empty.");
+  }
+}
