842a2ac237a174b4e123d814a284258b71872278
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-03 16:45:20 +0000

    Done.

--- index.js
+++ index.js
@@ -11,16 +11,29 @@ function nowServing(line) {
 }
 
 function currentLine(line) {
-  if (line.length === 0 ) {
+
+  if (!line.length) {
     return "The line is currently empty."
   }
-  var str = "The line is currently:"
-  var i = 0
-  while (i < line.length) {
-    str += ` ${i+1}. ${line[i++]}`
-    if (i < line.length) {
-      str += ","
-    }
+
+  // create the list
+  const list = []
+  for (let i = 0; i < line.length; i++) {
+    list.push(`${i+1}. ${line[i]}`)
   }
-  return str
+
+  return `The line is currently: ${list.join(', ')}`
+
+  // if (line.length === 0 ) {
+  //   return "The line is currently empty."
+  // }
+  // var str = "The line is currently:"
+  // var i = 0
+  // while (i < line.length) {
+  //   str += ` ${i+1}. ${line[i++]}`
+  //   if (i < line.length) {
+  //     str += ","
+  //   }
+  // }
+  // return str
 }
