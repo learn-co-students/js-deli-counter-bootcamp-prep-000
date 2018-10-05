4e5af274950aa1fe52947a13e15673610eb9d52c
naizal Edy <pritiadhikari@gmail.com>
2018-10-05 00:18:36 +0000

    Done.

--- index.js
+++ index.js
@@ -20,8 +20,13 @@ function nowServing(katzDeliLine)
   for(let i=0; i<katzDeliLine.length; i++)
   {
   
+
    return "Currently serving " +katzDeliLine.shift()+"."
    
+
+   return "First person in line is " +katzDeliLine[i]
+   katzDeliLine.shift()
+
   }
   
   }
@@ -31,6 +36,7 @@ function nowServing(katzDeliLine)
 
 function currentLine(katzDeliLine){
   
+
   var line =[];
   
   let i = 0;
@@ -48,5 +54,26 @@ function currentLine(katzDeliLine){
    return "The line is currently:"+line;
    
   }
+  
+
+  
+    if (katzDeliLine.length === 0) {
+  return "The line is currently empty."
+  }
+  
+  else
+  {
+  
+   
+   for(let i=0; i<katzDeliLine.length; i++)
+  {
+    
+   return "The line is currently:"+(i+1)+"." + katzDeliLine[i];
+   
+  }
+  
+  
+}
+
 
 }
