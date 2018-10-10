6e53f87358211cbc9a1cabf82e02d31faf6b3056
Phil Hartley <philh725@gmail.com>
2018-10-10 22:54:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+/*
 function takeANumber(line, name) {
   line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
@@ -30,3 +30,45 @@ function currentLine(line) {
   return message
   }
 }
+*/
+
+
+
+
+
+
+
+
+
+
+//redoing these for practice
+
+
+function takeANumber(line, name) {
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing(line) {
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return `Currently serving ${line.shift()}.`
+  }
+}
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return "The line is currently empty."
+  } else {
+    var message = "The line is currently: "
+    for (var i = 0; i < line.length; i++) {
+      message += `${i+1}. ${line[i]}`
+      if (i < line.length - 1) {
+        message += ", "
+      }
+    }
+    return message
+  }
+}    
+
