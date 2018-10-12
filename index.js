80e1df9e3133404dc67fc35b3f76d616dbf5d205
Phil Hartley <philh725@gmail.com>
2018-10-12 17:57:59 +0000

    Done.

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ function nowServing(line) {
   }
 }
 
+/*
 function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty."
@@ -70,5 +71,16 @@ function currentLine(line) {
     }
     return message
   }
-}    
+}    */
 
+function currentLine(line) {
+  if (line.length === 0) {
+    return "The line is currently empty."
+  } else {
+    var message = "The line is currently: "
+    for (let i = 0; i < line.length; i++) {
+      message += `${i+1}. ${line[i]}, `
+    }
+    return message.slice(0, message.length - 2)
+  }
+}
