1e1d9224a6b01ea6a4fe5d7ef11b822d91f318d7
a lan <bluenowte97@gmail.com>
2018-10-14 14:27:34 +0000

    Done.

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ string = "Welcome, " + name + ". You are number " + currentIndex + " in line.";
   return string;
 }
 
-function nowServing(array) {
-   if (array.length > 0) {
+  function nowServing(array) {
+     if (array.length > 0) {
      var string = "Currently serving " + array[0] + ".";
      array.shift();
      return string;
@@ -21,27 +21,51 @@ function nowServing(array) {
 
 }
 
+//better way than below
+
 function currentLine(array) {
-var newArr = [];
+
   if (array.length === 0) {
     return "The line is currently empty.";
   }
-//body of string built outside as not changing in the for loop
-  var string = "The line is currently: ";
 
+//body of string built outside as not changing in the for loop
+  var output = "The line is currently: ";
+
+//build the string but will have to take care of the period at the end
   for (var i = 0; i < array.length; i++) {
 
-    var currentIndex = i + 1;
+   output += i + 1 + ". " + array[i] + ", ";
 
-     if (i < array.length - 1) {
-      var stringTwo = array[i] + ", ";
-    }
-     else {
-       stringTwo = array[i];
-     }
-    //redefining string as adding the names and no in line each time
-    string = string + currentIndex + ". " + stringTwo;
-    //string = string + array.join("i, , ");
-  }
-return string;
 }
+//last one is array.length - 2 as have a character afterwards for array.length - 1
+  output = output.substring(0, output.length - 2);
+  return output;
+}
+
+
+//function currentLine(array) {
+//var newArr = [];
+  //if (array.length === 0) {
+  //  return "The line is currently empty.";
+  //}
+//<<<<<<< HEAD
+//body of string built outside as not changing in the for loop
+  //var string = "The line is currently: ";
+
+  //for (var i = 0; i < array.length; i++) {
+
+    //var currentIndex = i + 1;
+
+     //if (i < array.length - 1) {
+      //var stringTwo = array[i] + ", ";
+    //}
+     //else {
+       //stringTwo = array[i];
+     //}
+    //redefining string as adding the names and no in line each time
+    //string = string + currentIndex + ". " + stringTwo;
+    //string = string + array.join("i, , ");
+  //}
+//return string;
+//}
