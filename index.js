1f5d959451a048af5a7b636ab86e5815d6bba999
dselca <dselca@gmail.com>
2018-10-19 18:58:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-function takeANumber(num, name) {
-  array.push(num, name)
-  return `Welcome ${array[name]}, You are number ${array[num]} in line.`
+
+function takeANumber(array, name) {
+  array.push(name);
+  return `Welcome, ${name}. You are number ${array.length} in line.`;
+}
+
+function nowServing(array) {
+  if(array.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    for(var i = 0; i < array.length; i++) {
+      return `Currently serving ${array[i]}.`;
+    }
+  }
 }
