83f90d1d9975404e21db9535eb9ba59c4d4ee64c
Harlan Green-Taub <harlan.gt@gmail.com>
2018-10-25 16:58:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 
-function takeANumber(line,name) {
-    line.push(name)
-    return `Welcome, ${name}. You are number ${line.length} in line.`
-}
-
+function takeANumber(line, name) {
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+};
 
 function nowServing(line) {
-  if (!line.length) {
+  if (line.length === 0){
     return "There is nobody waiting to be served!"
   }
 
   return `Currently serving ${line.shift()}.`
+
 };
 
-function currentLine(line) {
-  if (!line.length) {
+function currentLine(line){
+  if (line.length === 0){
     return "The line is currently empty."
   }
-    const numbersAndNames = []
+  let lineStatus = "The line is currently: "
 
-    for (let i = 0, l = line.length; i < l; i++) {
-      numbersAndNames.push(`${i + 1}. ${line[i]}`)
-    }
-    return `The line is currently: ${numbersAndNames.join(', ')}`
+  const comma = []
 
-};
+  for (let i = 0, x = line.length; i < x; i++){
+   comma.push(`${i+1}. ${line[i]}`)
+}
+  return lineStatus + comma.join(", ")
+}
