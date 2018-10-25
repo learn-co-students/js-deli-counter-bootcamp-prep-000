59bd0aa3c213a73ec772429964eff1cc2c166a42
dselca <dselca@gmail.com>
2018-10-25 14:16:49 +0000

    Done.

--- index.js
+++ index.js
@@ -5,11 +5,27 @@ function takeANumber(array, name) {
 }
 
 function nowServing(array) {
+  var name = array[0];
   if(array.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
+    array.shift(name);
+    return `Currently serving ${name}.`;
+  }
+}
+
+function currentLine(array) {
+  var nameString = "The line is currently: ";
+  if(array.length === 0) {
+    return "The line is currently empty.";
+  } else {
     for(var i = 0; i < array.length; i++) {
-      return `Currently serving ${array[i]}.`;
+      if(i == array.length - 1) {
+        nameString += `${[i + 1]}. ${array[i]}`;
+      } else {
+        nameString += `${[i + 1]}. ${array[i]}, `;
+      }
     }
+  return nameString;
   }
 }
