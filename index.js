7170dfa26a7d921ee9f7e6eea6e7941761025cf1
Brooke Gill <brooke.gill25@gmail.com>
2018-10-30 04:08:02 +0000

    Done.

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ function nowServing(line) {
   if (line.length === 0 ) {
     return "There is nobody waiting to be served!";
   } else {
-    return (`Currently serving ${line.shift()}.`) ;
+    return (`Currently serving ${line.shift()}.`);
   }
 }
 
 function currentLine(line) {
   if (line.length === 0) {
-    return "The line is currently empty."
+    return "The line is currently empty.";
   }
 
   const numbers = [];
 
   for (let i = 0; i < line.length; i++) {
-    numbers.push(` ${i + 1}. ${line[i]}`)
+    numbers.push(` ${i + 1}. ${line[i]}`);
   }
 
-  return `The line is currently:${numbers.join()}`
+  return `The line is currently:${numbers.join()}`;
 }
