ddb2218d4226ad87d548bd78f47785d490f5ff34
Pete Hanner <petehanner@gmail.com>
2018-11-03 22:00:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function takeANumber(katzDeliLine, name){
+  //add person to line
+  katzDeliLine.push(name);
+
+  //return welcome message
+  return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return (`There is nobody waiting to be served!`);
+  }
+  else {
+    var serving = katzDeliLine[0];
+    katzDeliLine.shift()
+    return (`Currently serving ${serving}.`);
+  }
+}
+
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return (`The line is currently empty.`);
+  }
+  else {
+    //create variable array to print line later.
+    var print = [];
+    //add each name up to final into array
+    let count = 0;
+    let penultimate = katzDeliLine.length - 1;
+    while (count < penultimate){
+      print.push(` ${count + 1}. ${katzDeliLine[count]},`)
+    }
+    //add final name
+    count++
+    print.push(` ${count + 1}. ${katzDeliLine[count]}`)
+    //return results
+    return (`The line is currently:${print}`);
+  }
+}
