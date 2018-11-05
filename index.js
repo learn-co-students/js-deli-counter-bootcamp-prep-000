24beb151f1456a8d53812feb715d0c18f79c305e
witty-collection-3981 <yisroel96@gmail.com>
2018-11-05 23:13:48 +0000

    Done.

--- index.js
+++ index.js
@@ -27,48 +27,19 @@ return "Currently serving " + line.shift() + "."
 
 
 function currentLine(katzDeliLine) {
-
+  var p =[]
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
+  } else {
+    var k = `${'The line is currently: '}`
 
 
+
+    for (let i = 0; i < katzDeliLine.length; i++) {
+        p.push(`${i+1}`+". " +`${katzDeliLine[i]}`)
+          console.log(p);
 }
-var k = "The line is currently: "
-
-
-for (let i = 0; i < k.length; i++) {
-
-
-   console.log(k[i]);
-
 }
 
-
-
-
-// concatenate the string and var
-/*
-var string1 = "hello"
-undefined
-string1.concat
-ƒ concat() { [native code] }
-string1.concat("world")
-"helloworld"
-string1
-"hello"
-string1 = string1.concat("world")
-"helloworld"
-string1
-"helloworld"
-*/
-
-
-
-
-
-
-
-/* build string off for loop
-take each element out of array off for loop and add to string
-*/
+return k+p.join(", ");
 }
---
+++ note.md
@@ -0,0 +1,25 @@
+// concatenate the string and var
+
+//var string1 = "hello"
+// undefined
+// string1.concat
+// ƒ concat() { [native code] }
+// string1.concat("world")
+// "helloworld"
+// string1
+// "hello"
+// string1 = string1.concat("world")
+// "helloworld"
+// string1
+// "helloworld"
+//
+//
+//
+//
+//
+//
+//
+//
+// build string off for loop
+// take each element out of array off for loop and add to string
+//
