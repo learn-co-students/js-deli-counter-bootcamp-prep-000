4a1321a78cc1f981d2ba0215200aa432202ce618
witty-collection-3981 <yisroel96@gmail.com>
2018-11-09 04:50:13 +0000

    Done.

--- index.js
+++ index.js
@@ -2,44 +2,29 @@ var katzDeliLine = [];
 
 function takeANumber(line, name) {
 
-line.push(name);
+  line.push(name);
 
-
-return `Welcome, ${name}. You are number ${line.length} in line.`
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 };
 
 takeANumber(katzDeliLine, "Ada");
 
 function nowServing(line) {
-
-if (line.length === 0) {
-  return "There is nobody waiting to be served!";
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  return "Currently serving " + line.shift() + "."
 }
-
-
-
-
-return "Currently serving " + line.shift() + "."
-
-
-}
-
-
 
 function currentLine(katzDeliLine) {
-  var p =[]
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
-  } else {
-    var k = `${'The line is currently: '}`
-
-
-
-    for (let i = 0; i < katzDeliLine.length; i++) {
-        p.push(`${i+1}`+". " +`${katzDeliLine[i]}`)
-          console.log(p);
-}
-}
-
-return k+p.join(", ");
+  }
+  var k = `${'The line is currently: '}`
+  var p = []
+  for (let i = 0; i < katzDeliLine.length; i++) {
+    p.push(`${i+1}`+". "+`${katzDeliLine[i]}`)
+    console.log(p);
+  }
+  return k+p.join(", ");
 }
