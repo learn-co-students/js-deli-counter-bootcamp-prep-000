3724775d7de723cc066057804e62718a24c5e2c2
Edwin Lin <edwinlin@gmail.com>
2018-11-09 14:51:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-function takeANumber(currentLine, newPerson){
-  var string ="Welcome, "+ newPerson +". You are number " + (currentLine.length+1).toString() + " in line."
-  currentLine.push(newPerson)
+function takeANumber(deliLine, newPerson){
+  var string ="Welcome, "+ newPerson +". You are number " + (deliLine.length+1).toString() + " in line."
+  deliLine.push(newPerson)
   return string
 }
 
-function nowServing(currentLine){
+function nowServing(deliLine){
   var firstPerson
 
-  if (currentLine.length > 0){
-    firstPerson = currentLine.shift()
+  if (deliLine.length > 0){
+    firstPerson = deliLine.shift()
     return "Currently serving " + firstPerson +"."
   }else{
     return "There is nobody waiting to be served!"
   }
 }
 
-function currentLine(currentLine){
+function currentLine(deliLine){
   var order = ""
   var i = 0
   
-  if (currentLine.length === 0){
+  if (deliLine.length === 0){
     return "The line is currently empty."
   }else{
     do {
       if (i === 0){
-        order += 1 +"."+" "+currentLine[i]
+        order += 1 +"."+" "+deliLine[i]
         i++
       }else{
-        order += ", "+(i+1)+"."+" "+currentLine[i]
+        order += ", "+(i+1)+"."+" "+deliLine[i]
         i++
       }
-    }while (currentLine.length>0 && i<currentLine.length)
+    }while (deliLine.length>0 && i<deliLine.length)
   }
   
   return "The line is currently: " + order 
