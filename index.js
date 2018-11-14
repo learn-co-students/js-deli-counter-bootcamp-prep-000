7cdabf7791aad90d30cce5b474b165378c862617
Liam Roberts <liam_r_123@hotmail.co.uk>
2018-11-14 11:45:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+  }
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  }
+  else{
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+}
+
+function currentLine(katzDeliLine){
+    var newKatzDeliLine = [];
+
+    for (let i = 0; i < katzDeliLine.length; i++){
+      newKatzDeliLine.push(`${i + 1}. ${katzDeliLine[i]}`)
+    }
+    if (katzDeliLine.length === 0){
+      return "The line is currently empty."
+    }
+
+    else{
+      return `The line is currently: ${newKatzDeliLine.join(", ")}`
+  }
+}
