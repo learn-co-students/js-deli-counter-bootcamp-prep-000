c21a71eae67ac61b2a7148bfc78c120858fd3833
Kevin Kang <kevinkangg@gmail.com>
2018-11-20 04:23:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function takeANumber(linePeople, personName){
+  linePeople.push(personName)
+  return "Welcome, " + personName + ". You are number " + linePeople.length + " in line."
+}
+
+function nowServing(currentLine){
+  if (currentLine.length > 0){
+    return "Currently serving " + currentLine.shift() + "."
+  }
+  else{
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(array){
+    if (array.length > 0){
+      var firstPart = "The line is currently: "
+      for (var i=0; i < array.length; i++){
+        if ([i] < array.length - 1){
+          firstPart += [i+1] + ". " + array[i] + ", "
+        }
+        else{
+          firstPart += [i+1] + ". " + array[i]
+        }
+      }
+      return firstPart
+    }
+    else{
+      return "The line is currently empty."
+    }
+  }
