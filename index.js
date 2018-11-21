f26f334626890de4a4a383a227946cbb9351cf54
Vanessa Chu <vanessa.chu@outlook.com>
2018-11-21 21:22:47 +0000

    Done.

--- index.js
+++ index.js
@@ -3,25 +3,20 @@ function takeANumber(currLine, personName){
   return `Welcome, ${personName}. You are number ${currLine.length} in line.`;
 }
 function nowServing(katzDeliLine){
-  var str;
-  if (katzDeliLine.length === 0)
-    str = "There is nobody waiting to be served!";
+  if(katzDeliLine.length == 0)
+    return "There is nobody waiting to be served!"
   else {
-    str = `Currently serving ${katzDeliLine.shift()}.`;
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
-  return str;
 }
-function currentLine(line){
-  var str;
-  if (line.length === 0)
-    str = "The line is currently empty.";
-  else {
-    str = "The line is currently:";
-    for (var i=0; i<line.length; i++){
-      str += ` ${i+1}. ${line[i]}`;
-      if (i != line.length-1)
-        str += ",";
-    }
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0)
+    return "The line is currently empty."
+  var str = "The line is currently:";
+  for (let i=0; i<katzDeliLine.length; i++){
+    str += ` ${i+1}. ${katzDeliLine[i]}`
+    if (!(i === katzDeliLine.length -1))
+      str += ","
   }
   return str;
 }
