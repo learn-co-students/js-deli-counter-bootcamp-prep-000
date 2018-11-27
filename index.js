e24ba3775fb3ceb72d163c036fbc7412db21b925
Naenae L <oneprincesslea@gmail.com>
2018-11-27 18:29:41 +0000

    Done.

--- index.js
+++ index.js
@@ -2,24 +2,33 @@
 var katzDeliLine = ["Jane", "Juan", "Ngugiwa"]; 
 var number = 0; 
 
-function takeANumber (number, name){
+function takeANumber (katzDeliLine, name){
+  number = katzDeliLine.length + 1; 
+  //maybe add incoming customer to array? 
   return `Welcome, ${name}, you are number ${number} in line.`; 
+  
 } 
 
 function nowServing (katzDeliLine) {
-    var firstPerson = katzDeliLine.shift; 
-    if (firstPerson===0){
-        return "There is nobody waiting to be served!"}
-        else { 
-    console.log(`Now serving ${firstPerson}.`)}
+    if (katzDeliLine.length>0){
+      var firstPerson = katzDeliLine.shift; 
+      return `Now serving ${firstPerson}.`; 
+    } else {
+       return "There is nobody waiting to be served!"; 
     }
+}
+    
+    
 function currentLine(katzDeliLine){
-    if (katzDeliLine === 0){
+    if (katzDeliLine.length === 0){
         return "The line is currently empty."; 
     } else {
-        return `The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}`;
+        var text = "The line is currently: "; 
+        for (i=0; i++; i<katzDeliLine.length){
+        text += katzDeliLine[i] } 
+        return text; 
     } 
-
+    
 } 
 
 takeANumber(1, "jane"); 
