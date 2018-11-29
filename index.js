2cf1f2ba3e930e4733b887cade135dd1158939ee
Jonathan Koss <jonathan_koss@yahoo.com>
2018-11-29 04:49:31 +0000

    Done.

--- index.js
+++ index.js
@@ -8,25 +8,31 @@ function nowServing(katzDeli) {
     return "There is nobody waiting to be served!";
   }
   else {
-    return `Currently serving ${katzDeli.shift()}.`;
+    return `Currently serving ${katzDeli.shift()}.` ;
   }
-} 
+}
 
 function currentLine(katzDeli) {
   var array = [];
-  
+  var statement = "The line is currently:";
   if(katzDeli.length === 0) {
     return "The line is currently empty.";
   }
-  
-    for(var i = 0; i < katzDeli.length; i++) {
-    array.push(`${i + 1}. ${katzDeli[i]}`);
+  else {
+     for(var i = 0; i < katzDeli.length; i++) {
+       array.push(`${i + 1}. ${katzDeli[i]}`);
+     }
+      return `The line is currently: ${array.join(", ")}`;
     }
-    return `The line is currently: ${array.join(", ")}`;
-}
+  }
 
 /*
-  it('says who is in line when there are people waiting', () => {
+  describe('currentLine(line)', () => {
+    it('returns "The line is currently empty." if no one is in line', () => {
+      expect(currentLine([])).toEqual("The line is currently empty.");
+    });
+
+    it('says who is in line when there are people waiting', () => {
       expect(currentLine(["Bill", "Jane", "Ann"])).toEqual("The line is currently: 1. Bill, 2. Jane, 3. Ann");
     });
   });
