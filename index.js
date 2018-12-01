7f5aafda423a22afd1db62d6d098f556da275504
Elaina <elainaseki@gmail.com>
2018-12-01 00:49:46 +0000

    Done.

--- index.js
+++ index.js
@@ -6,19 +6,17 @@ var katzDeliLine = [];
 }
  function nowServing(katzDeliLine){
   if (!katzDeliLine.length){return "There is nobody waiting to be served!";
-    } else {  var output = `Currently serving ${katzDeliLine[0]}.`;
-    katzDeliLine.shift();
-    return output; }
+    } else {   return `Currently serving ${katzDeliLine.shift()}.`; }
 }
  function currentLine(katzDeliLine){
    if (!katzDeliLine.length ){ return "The line is currently empty."; }
   else {
-    var line = "The line is currently:";
+    var output = "The line is currently:";
      for(var i=0; i<katzDeliLine.length; i++){
-       line += ` ${i+1}. ${katzDeliLine[i]}`;
-       if (i < katzDeliLine.length - 1){ line += ',';}
+       output += ` ${i+1}. ${katzDeliLine[i]}`;
+       if (i < katzDeliLine.length - 1){ output += ',';}
      }
-    return line;
+    return output;
    }
  }
 
