e6298083673eec7e8dd31b97b8278ab1ac4edfde
Leonardo Peralta <leo.pm15@gmail.com>
2018-12-04 19:13:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-function takeANumber() {
+var katzDeliLine = []
+
+function takeANumber(katzDeliLine, newName ) {
+  katzDeliLine.push(newName);
+
+  return (`Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`)
+}
+
+function nowServing(katzDeliLine) {
+  let i = 0;
+
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+    else return (`Currently serving ${katzDeliLine[0]}.`);
+}
+
+
+function currentLine(katzDeliLine) {
 
 }
