3b306f719dedb612ffd71a3690fb0332f195d097
Jack Rotta <jackrotta12@gmail.com>
2018-12-05 06:41:25 +0000

    Done.

--- index.js
+++ index.js
@@ -3,26 +3,24 @@ deliLine = [];
 function takeANumber(line, name) {
   line.push(name);
   var position = line.indexOf(name);
-  return `Welcome ${name} you are number ${position + 1} in line`;
+  return `Welcome, ${name}. You are number ${position + 1} in line.`;
 }
 
 
 function nowServing(line) {
   if(line.length == 0) {
-    return "There is nobody in line";
+    return "There is nobody waiting to be served!";
 }
   else
-    return `Now serving ${line.shift()}`;
+    return `Currently serving ${line.shift()}.`;
 }
 
 function currentLine(line) {
-  funcArray = [];
+  var funcArray = [];
+  let lame = "";
+  if(line.length === 0) {return "The line is currently empty.";}
   for(var x =0; x < line.length; x++) {
-    funcArray.push(`${x+1}, ${line[x]}`);
+    funcArray.push(` ${x+1}. ${line[x]}` );
   }
-  return "The line is currently: " + array;
+  return "The line is currently:" + funcArray;
 }
-
-
-
-
