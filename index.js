5a9a8d93e6e0ed12e31b72e8c5eeff7b73bec4b4
Elaina <elainaseki@gmail.com>
2018-12-05 17:23:15 +0000

    Done.

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ var katzDeliLine = [];
   katzDeliLine.push(name);
   return  `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+
  function nowServing(katzDeliLine){
   if (!katzDeliLine.length){return "There is nobody waiting to be served!";
-    } else {   return `Currently serving ${katzDeliLine.shift()}.`; }
+    } else {
+return `Currently serving ${katzDeliLine.shift()}.`; }
 }
 
 function currentLine(katzDeliLine) {
