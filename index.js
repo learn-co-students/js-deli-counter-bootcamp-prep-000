78bac2ef3b42e54ebebb50335143f1543cfdc290
Michael Moravetz <moravemv@gmail.com>
2018-12-08 22:46:48 +0000

    Done.

--- index.js
+++ index.js
@@ -19,6 +19,5 @@ function currentLine(line) {
   for (let i = 0; i < line.length; i++) {
     newLine.push(`${ i + 1 }. ${ line[i] }`)
   }
-  
   return `The line is currently: ${ newLine.join(', ') }` 
 }
