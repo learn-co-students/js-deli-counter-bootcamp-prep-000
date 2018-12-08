2495ebdfd8c3d5266890e0a3457f7ae09ba97cf4
Michael Moravetz <moravemv@gmail.com>
2018-12-08 00:35:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function takeANumber(deliLine, name) {
+   deliLine.push(name);
+   return (`Welcome, ${name}. You are number ${deliLine.length} in line.` )
+} 
+
+function nowServing(line) {
+  if(line.length === 0) {
+    return `There is nobody waiting to be served!`
+  }
+  
+  return `Currently serving ${ line.shift() }.`
+}
+
+function currentLine(line) {
+  if(line.length === 0) return `The line is currently empty.`
+  
+  var newLine = []
+  for (let i = 0; i < line.length; i++) {
+    newLine.push(`${ i + 1 }. ${ line[i] }`)
+  }
+  
+  return `The line is currently: ${ newLine.join(', ') }` 
+}
