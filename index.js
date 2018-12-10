7e43ab27f881111ac1f186c0f9209e81ce9de6fa
Lerena Holloway <holloway.lerena@gmail.com>
2018-12-10 22:02:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function takeANumber(currentLine, name){
   currentLine.push(name);
   let place = currentLine.indexOf(name) + 1
-  return `Welcome ${name}, you are number ${place} in line.`
+  return `Welcome, ${name}. You are number ${place} in line.`
+}
+
+function currentLine(line){
+  let cur = []
+  if (line.length === 0){
+    return "The line is currently empty.";
+  } else {
+    for(var i = 0; i < line.length; i++){
+      cur.push(Number(i+1) + '. ' + line[i]);
+    }
+    for (var j = 0; i < cur.length; j++){
+      return `The current line is ${cur[j]}`;
+    }
+  }
 }
