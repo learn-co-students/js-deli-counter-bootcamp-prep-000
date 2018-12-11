69ded3d95402a478c53bb4642595f5d7f63b1603
artificial-engine-5731 <icefire587@gmail.com>
2018-12-11 05:41:38 +0000

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function takeANumber (people, name){
 }
 
 function nowServing (people){
-  if(people.length !== 0){
+  // people.length !== 0
+  if(people.length){ 
     return `Currently serving ${people.shift()}.` 
   } else {
     return "There is nobody waiting to be served!"
@@ -13,15 +14,21 @@ function nowServing (people){
 }
 
 function currentLine(people){
-  var returnLine = 'The line is currently: '
-  if (people.length !== 0 ){
+//  var returnLine = 'The line is currently: '
+//  if (people.length){
+//    for (let i = 0; i < people.length; i++){
+//      returnLine += `${i+1}. ${people[i]}`
+//      if ( i != people.length -1 ){
+//        returnLine += ', '
+//      }
+//    }
+//    return returnLine
+  const returnLine = []
+  if (people.length){
     for (let i = 0; i < people.length; i++){
-      returnLine += `${i+1}. ${people[i]}`
-      if ( i != people.length -1 ){
-        returnLine += ', '
-      }
+      returnLine.push(`${i + 1}. ${people[i]}`)
     }
-    return returnLine
+    return `The line is currently: ${returnLine.join(', ')}`
   } else {
     return "The line is currently empty."
   }
