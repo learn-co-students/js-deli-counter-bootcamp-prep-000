03e25fd0d28c5d5e4c06870a56c553f5b630a510
Isabella de Medici <bellamedici242@gmail.com>
2018-12-12 16:16:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-function takeANumber(line, name) {
-  line.push(name);
-  return `Welcome, ${name}. You are number ${line.length} in line.`
+function takeANumber(deliLine, name) {
+  var length = deliLine.push(name);
+  return `Welcome, ${name}. You are number ${length} in line.`;
 }
 
-function nowServing(line) {
-  if (line.length === 0) {
-     return "There is nobody waiting to be served!"
+function nowServing(deliLine) {
+  if (deliLine.length === 0) {
+    return "There is nobody waiting to be served!";
   }
-  return `Currently serving ${line.shift()}.`
-
+  else {
+    var firstInLine = deliLine.shift();
+    return `Currently serving ${firstInLine}.`;
+  }
 }
 
-//"The line is currently: 1. Ada, 2. Grace"
-function currentLine(line) {
-  
-  if(line.length === 0) {
-    return "The line is currently empty.";
+function currentLine(deliLine) {
+  if (deliLine.length === 0) {
+    return "The line is currently empty."
   }
   
-  let resultStr = "The line is currently: ";
-  
-  for(let i=0; i < line.length; i++) {
-   let person = line[i];
-    
-    if(i > 0) {
-      resultStr += ", ";
+  var line = "The line is currently: ";
+  for(var i = 0; i < deliLine.length; i++){
+    var position = `${i+ 1}. ${deliLine[i]}`;
+    var comma = ', ';
+    if(i === deliLine.length - 1){
+      comma = '';
     }
-    
-    resultStr += `${i + 1}. ${person}`;
+    line = line + position + comma;
   }
-  
-  return resultStr;
+return line;
+
 }
