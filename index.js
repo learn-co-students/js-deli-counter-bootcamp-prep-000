fe49a89846d9d0725873612f21480889d62c8797
Myung Kim <mjk287@cornell.edu>
2018-12-16 14:43:28 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ function takeANumber(lineOfPeople, name){
   lineOfPeople.push(name)
   return `Welcome, ${name}. You are number ${lineOfPeople.length} in line.`
 }
+
+function nowServing(lineOfPeople){
+  if (lineOfPeople.length > 0){
+    var firstPerson = lineOfPeople[0]
+    lineOfPeople = lineOfPeople.slice(1)
+    return firstPerson
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
