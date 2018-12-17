8c932b2c9640a80ec4c15f22e5a93cea5da319c8
Paris Dolfman <parisdolfman@gmail.com>
2018-12-17 20:53:42 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+
+
+function nowServing(katzDeliLine) {
+
+}
