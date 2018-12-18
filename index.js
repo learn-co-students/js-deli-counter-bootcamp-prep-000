1276b7654bb3b5d171f7e5b6125665362e1e5592
brindab50 behal <brindab50@gmail.com>
2018-12-18 16:29:39 +0000

    Done.

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
+
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
   }
   
 function nowServing (line) {
+
+  return `Welcome ${name}. You are number ${katzDeliLine.length} in line.`
+  }
+  
+  function nowServing (line) {
   if (line.length === 0) {
     return "There is nobody waiting to be served!"
     }
@@ -18,15 +24,6 @@ function currentLine(line) {
   if (line.length === 0) {
         return "The line is currently empty."
       }
-      return `katzDeliLine
-      
-      
-      
-      
-      
-      
-      
-      
-      
       return `The line is currently: ${name}.`
+    return `Now serving ${name}.`
     }
