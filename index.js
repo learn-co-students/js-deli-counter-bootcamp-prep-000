bd9893d6a9b50bff82cc85ac773e5e97cb48f09b
brindab50 behal <brindab50@gmail.com>
2018-12-18 17:32:23 +0000

    Done.

--- index.js
+++ index.js
@@ -21,9 +21,13 @@ function nowServing (line) {
     }
     
 function currentLine(line) {
+  var array = [];
   if (line.length === 0) {
         return "The line is currently empty."
       }
-      return `The line is currently: ${name}.`
-    return `Now serving ${name}.`
+      for (var i = 0; i < line.length; i++) {
+        array.push(`${i+1}. ${line[i]}`)
+      }
+    
+     return `The line is currently: ${array.join(", ")}`
     }
