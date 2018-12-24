9309fb256a550b621638124780fbf18a75dc6dbb
Errol W <ewatsonrei@gmail.com>
2018-12-24 17:41:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
-/*  if(katzDeliLine.length === 1) {
-    return(`Welcome, ` + name + `. You are number ` + (katzDeliLine.length + 1) + ` in line.`);
-  }
-*/  
   katzDeliLine.push(name);
   return(`Welcome, ` + name + `. You are number ` + katzDeliLine.length + ` in line.`);
-}
+  }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return(`There is nobody waiting to be served!`);
   } else {
-    katzDeliLine.shift();
+    katzDeliLine.shift(); //somehow this has to change after a return statement...
     return(`Currently serving ` + katzDeliLine[0] +  `.`);
   }
 }
 
 function currentLine(katzDeliLine) {
-
   if (katzDeliLine.length === 0) {
   return(`The line is currently empty.`);
   } else {
-    return(`The line is currently: ` + (katzDeliLine.length - (katzDeliLine.length - 1)) + `.`);
+    return(`The line is currently: ` + katzDeliLine.length + `.`);
   }
 }
