8cd245bd86c125d2ac0707c8a71c8a031364d1d0
Errol W <ewatsonrei@gmail.com>
2018-12-24 03:20:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 //Build a function that a new customer will use when entering the deli.
 
-var katzDeliLine = ['Ada'];
+var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
-  
-  return(`Welcome, ` + name + `. You are number ` + katzDeliLine.length + ` in line.`);
+  katzDeliLine.push(name);
+  return(`Welcome, ` + name + `. You are number ` + (katzDeliLine.length + 1) + ` in line.`);
 }
+// above function is not done, the customer's place in line reflects
 
 function nowServing(katzDeliLine) {
-  
+  if (katzDeliLine.length === 0) {
+    return(`There is nobody waiting to be served!`);
+  } else {
+    katzDeliLine.shift();
+    return(`Currently serving ` + katzDeliLine[0] +  `.`);
+  }
 }
 
 function currentLine(katzDeliLine) {
 
   if (katzDeliLine.length === 0) {
-  return `The line is currently empty.`;
+  return(`The line is currently empty.`);
+  } else {
+    return();
   }
 }
