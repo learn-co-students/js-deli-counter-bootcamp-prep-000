e830630c44f82944d514f1802c4701baa0190a7d
Errol W <ewatsonrei@gmail.com>
2018-12-24 05:34:18 +0000

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
+/*  if(katzDeliLine.length === 1) {
+    return(`Welcome, ` + name + `. You are number ` + (katzDeliLine.length + 1) + ` in line.`);
+  }
+*/  
   katzDeliLine.push(name);
   return(`Welcome, ` + name + `. You are number ` + katzDeliLine.length + ` in line.`);
 }
