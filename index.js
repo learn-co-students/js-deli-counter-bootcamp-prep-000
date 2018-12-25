023082d1ea6e848bd6c33d43307912dbe5deb022
Errol W <ewatsonrei@gmail.com>
2018-12-25 01:02:50 +0000

    Done.

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var katzDeliLine = [];
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return(`Welcome, ` + name + `. You are number ` + katzDeliLine.length + ` in line.`);
-  }
+}
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return(`There is nobody waiting to be served!`);
   } else {
-    return(`Currently serving ` + katzDeliLine.splice(0, 1) +  `.`);
+      return(`Currently serving ` + katzDeliLine.splice(0, 1) +  `.`);
   }
 }
 
@@ -17,8 +17,13 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
   return(`The line is currently empty.`);
   } else {
-/*    return( /* I need to return a function that returns a string with 2 parameters;
-name and index, looped as many times as the current length of katzDeliLine. */
-    return(`The line is currently: ` + katzDeliLine + `.`);
+  var line = katzDeliLine;
+  return(`The line is currently: ` + line);
   }
 }
+
+ /*`The line is currently: ` + katzDeliLine.indexOf(katzDeliLine[i + 1]) + `. ` + katzDeliLine[i] + `, ` ); */
+ 
+ /*    for (var i = 0; i <= katzDeliLine; i++){
+        return(`This worked`);
+        */
