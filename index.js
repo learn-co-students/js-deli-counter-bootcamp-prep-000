03ffe85eeb61824d541af5250d58eebff739f69c
Eric Hardiman <hardimaneric@gmail.com>
2018-12-26 11:14:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+/*Function takeANumber should accept 2 paramaters - current line and name of person.
+Should return a welcome message including the name and position in line.*/
 function takeANumber(line, name){
     line.push(name);
 
     return `Welcome, ${name}. You are number ${line.length} in line.`
 }
+/*Function nowServing accept current line of people, return first person in line.
+Then remove that person from line. */
 function nowServing(line){
     if(line.length > 0){
         return `Currently serving ${line.shift()}.`
