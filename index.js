d6daddfbe2ef632fdf80d08c17fc4b97e182ffcc
Errol W <ewatsonrei@gmail.com>
2018-12-26 17:59:32 +0000

    Done.

--- index.js
+++ index.js
@@ -2,36 +2,35 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return(`Welcome, ` + name + `. You are number ` + katzDeliLine.length + ` in line.`);
+  return `Welcome, ` + name + `. You are number ` + katzDeliLine.length + ` in line.`;
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return(`There is nobody waiting to be served!`);
+    return `There is nobody waiting to be served!`;
   } else {
-      return(`Currently serving ` + katzDeliLine.splice(0, 1) +  `.`);
+      return `Currently serving ` + katzDeliLine.splice(0, 1) +  `.`;
   }
 }
 
-function currentLine(katzDeliLine) {
+/*function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
   return(`The line is currently empty.`);
   } else {
     var e = [2];
         return(`The line is currently: 1. ` + katzDeliLine.join());
   }
-}
+} */
 
 //i am trying to increment e when e is equal to your position in line
 
 // ", " +  + `. `;
 
 
-/*
+
 function currentLine (katzDeliLine){
   if (katzDeliLine.length===0)
   return `The line is currently empty.`;
   else
   return `The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]}`;
 }
-*/
