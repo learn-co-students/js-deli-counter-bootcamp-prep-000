d1820ab7066c907f5595938936c35b79ca1ea15b
Errol W <ewatsonrei@gmail.com>
2018-12-26 18:13:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var katzDeliLine = [];
+var katzDeliLine;
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return `Welcome, ` + name + `. You are number ` + katzDeliLine.length + ` in line.`;
+    return `Welcome, ` + name + `. You are number ` + katzDeliLine.length + ` in line.`;
 }
 
 function nowServing(katzDeliLine) {
@@ -14,9 +14,9 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  if (katzDeliLine.length===0) {
-  return `The line is currently empty.`;
- } else {
-  return `The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]}`;
+  if (katzDeliLine.length === 0) {
+    return `The line is currently empty.`;
+  } else {
+      return `The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]}`;
   }
 }
