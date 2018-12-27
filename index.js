edf62d424f063d7422f571cf03b3c43476568a0e
Marc <marc.troughton@gmail.com>
2018-12-27 22:35:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 
 function takeANumber(queue, newCustomer){
-  var newQueue = queue.length + 1
-  queue[queue.length] = newCustomer
-  
-  return `Welcome, ${newCustomer}. You are number ${newQueue} in line.`
+  queue.push(newCustomer)
+  return `Welcome, ${newCustomer}. You are number ${queue.length} in line.`
 }
 
 function nowServing(currentLine){
-  if (currentLine.length === 0){
+  if (!currentLine.length){
     return "There is nobody waiting to be served!"
   } else{
-    var currentCust = currentLine[0]
-    currentLine.shift()
-    return `Currently serving ${currentCust}.`
+    return `Currently serving ${currentLine.shift()}.`
   }
 }
 
 function currentLine(line){
   
-  if (line.length === 0){
+  if (!line.length){
     return "The line is currently empty."
   } else{
     var lineString = "The line is currently: "
