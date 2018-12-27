cc0872227867c02536f8c43e099fec0dd1c97c94
Salem Qemlas <saqemlas@googlemail.com>
2018-12-27 20:39:02 +0000

    Done.

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ function takeANumber(katzDeliLine,name) {
     return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing(katzDeliLine) {
-  if(katzDeliLine>[]) {
-    return `Currently serving ${katzDeliLine.shift()}.` }
+function nowServing(name) {
+  if (name>[]) {
+    return `Currently serving ${name.shift()}.` }
   else {
     return "There is nobody waiting to be served!" }
 }
 
 function currentLine(katzDeliLine) {
-var line=[];
+var lineNameandNumber=[];
   if(katzDeliLine>[]) {
       for(var i=0;i<katzDeliLine.length;i++) {
-      line.push(` ${i+1}. ${katzDeliLine[i]}`); }
-    return "The line is currently:" + line;}
+      lineNameandNumber.push(`${i+1}.${katzDeliLine[i]}`); }
+    return "The line is currently:" + line; }
   else {
     return "The line is currently empty." }
 }
