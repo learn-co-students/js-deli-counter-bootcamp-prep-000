26be8c0d509677b5e0698e6377b9d815b14f4139
Daniel Gorter <danieljgorter@gmail.com>
2019-01-07 17:58:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function takeANumber(line, name){
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
+}
+
+function nowServing(line){
+  return (line.length===0) ? 'There is nobody waiting to be served!' : `Currently serving ${line.shift()}.`;
+}
+
+function currentLine(line){
+  if (line.length===0) return 'The line is currently empty.';
+  var str = 'The line is currently: ';
+  for (let i = 0; i < line.length; i++){
+     str += `${i+1}. ${line[i]}${(i + 1 < line.length) ? ', ' : ''}`;
+  }
+  return str;
+}
