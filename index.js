b99763319a5671c63d93a3daae4aeb249131ac18
Errol W <ewatsonrei@gmail.com>
2019-01-07 03:46:31 +0000

    Done.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing(katzDeliLine) {
 [])" */
 
 function currentLine(katzDeliLine) {
-  var line = [];
+var line = [];
   if (katzDeliLine.length === 0) {
     return `The line is currently empty.`;
   } else {
