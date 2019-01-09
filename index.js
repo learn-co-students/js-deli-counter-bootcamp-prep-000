2767e44e5c84421066d4ce87cd147034fa74ce67
Josephine Yao <yao.josephine@yahoo.com>
2019-01-09 23:35:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,53 +1,3 @@
-
-var katzDeliLine = [];
-
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name);
-  return ("Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.");
-}
-
-function nowServing (katzDeliLine) {
-   if (katzDeliLine.length >= 1) {
-     var sentence = ("Currently serving " + katzDeliLine[0] + ".");
-     katzDeliLine.splice(0,1);
-     return sentence;
-   } else {
-     return ("There is nobody waiting to be served!");
-   }
- }
- 
-function currentLine(katzDeliLine) {  
-  var line = [];
-if (katzDeliLine.length === 0)  {
-    return "The line is currently empty.";
-    }   
-    else 
-      {
-        for(let i = 0; i<=katzDeliLine.length-1; i++) {
-            line.push(" " + [i+1] + ". " + katzDeliLine[i]);
-        }
-        return "The line is currently:" + line;
-    }
-}
-
- /* function currentLine(katzDeliLine) {
-   var line = [];
-   let i = 0;
-   while (i < katzDeliLine.length) {
-     line.push(" " + [i+1] + ". " + katzDeliLine[i]);
-     i++;
-    }
-   if (katzDeliLine.length === 0) {
-     return "The line is currently empty.";
-    } 
-   else {
-   return "The line is currently:" + line;
-    }
-  }
- */
-
-
-
 /*describe('deli', () => {
   describe('takeANumber', () => {
     var katzDeli;
@@ -77,7 +27,16 @@ if (katzDeliLine.length === 0)  {
     });
   });
 
-  describe('nowServing', () => {
+1. Build a function that a new customer will use when entering the deli. The function, takeANumber, should accept two paramaters: the current line of people, along with the new person's name. The function should return a welcome message including the new person's position in line, such as "Welcome, Ada. You are number 1 in line.". And don't go being too programmer-y and give them their index. These are normal people. If they are 7th in line, tell them that. Don't get their hopes up by telling them they are number 6 in line.*/
+
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
+}
+
+/*describe('nowServing', () => {
     it('returns the line is empty when no one is on line', () => {
       expect(nowServing([])).toEqual("There is nobody waiting to be served!");
     });
@@ -88,8 +47,22 @@ if (katzDeliLine.length === 0)  {
       expect(deliLine).toEqual(["Blake", "Avi"]);
     });
   });
+  
+  2. Build a function nowServing. This function should accept the current line of people (katzDeliLine) and return the first person in line and then remove that individual from the line. If there is nobody in line, it should return "There is nobody waiting to be served!"
+  */
 
-  describe('currentLine(line)', () => {
+function nowServing (katzDeliLine) {
+   if (katzDeliLine.length >= 1) {
+     var sentence = "Currently serving " + katzDeliLine[0] + ".";
+     katzDeliLine.splice(0,1);
+     return sentence;
+     
+   } else {
+     return "There is nobody waiting to be served!";
+   }
+ }
+ 
+/*describe('currentLine(line)', () => {
     it('returns "The line is currently empty." if no one is in line', () => {
       expect(currentLine([])).toEqual("The line is currently empty.");
     });
@@ -98,4 +71,19 @@ if (katzDeliLine.length === 0)  {
       expect(currentLine(["Bill", "Jane", "Ann"])).toEqual("The line is currently: 1. Bill, 2. Jane, 3. Ann");
     });
   });
-})*/
+})
+
+3. Build a function currentLine that accepts the current line of people and returns the current line as a string; for example, if 'katzDeliLine' is currently ["Ada", "Grace"], currentLine(katzDeliLine) would return "The line is currently: 1. Ada, 2. Grace". You don't have to use katzDeliLine as a variable or parameter name in your function though, it's just an example of a variable that might be passed to it. If there is nobody in line, it should return "The line is currently empty."*/
+ 
+function currentLine(katzDeliLine) {  
+  var updatedLine = [];
+if (katzDeliLine.length === 0) {
+  return "The line is currently empty.";
+ } else {
+   for (let i = 0; i <= katzDeliLine.length-1; i++) {
+     updatedLine.push(" " + [i+1] + ". " + katzDeliLine[i]);
+   }
+        return "The line is currently:" + updatedLine;
+    }
+}
+
