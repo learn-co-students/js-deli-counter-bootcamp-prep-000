a690c2e396766b07c9e89025dc672c7e7980866e
Emmanuel Anzele <eanzele@gmail.com>
2019-01-10 11:27:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-function takeANumber(katzDeli, name) {
-  katzDeli.push(`${name}`);
-  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+  
+function nowServing(line) {
+  return line.length === 0 ? "There is nobody waiting to be served!" : `Currently serving ${line.shift()}.`;
 }
 
-
-function nowServing(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
-    i++;
-  }
-  if (katzDeli.length === 0) {
-    return "There is nobody waiting to be served!";
-  }
+function currentLine (katzDeliLine){
+  if (katzDeliLine.length===0)
+  return `The line is currently empty.`;
   else
-  return (`Currently serving ${katzDeli.shift()}.`);
-}
-var line = [];
+  return `The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]}`;
+} 
 
-
-
-
-
-function currentLine(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
-    line.push(` `+[i+1]+`. `  + katzDeli[i]);
-    i++;
-  }
-  if (katzDeli.length === 0) {
-    return "The line is currently empty.";
-  } else
-  return(`The line is currently:` + line);
-}
