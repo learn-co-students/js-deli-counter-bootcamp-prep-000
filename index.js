51ae0938a6daef72acb0f5cefafa7155024aa795
Jaclyn Setina <jaclyn.setina@gmail.com>
2019-01-11 02:12:27 +0000

    Done.

--- index.js
+++ index.js
@@ -17,7 +17,20 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  if( katzDeliLine.length ===0) {
-    return("The line is currently empty")
+  var line = []
+  if(katzDeliLine>[]) {
+    for(var i=0;i<katzDeliLine.length;i++) {
+        line.push(` ${i+1}. ${katzDeliLine[i]}`);
+    }
+    return "The line is currently:" + line;
   }
-}
+  else {
+    return ("The line is currently empty.");
+    }
+  }
+  
+
+    
+    
+    
+  
