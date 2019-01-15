ca4d066bba69b77e9f4c15fa19cbd38d2f8402fa
Matthew Campbell <matjocampbell@gmail.com>
2019-01-15 22:02:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-var katzDeliLine = [];
-
-function takeANumber(currentLine,newPerson) {
-  currentLine.push(newPerson);
-  return `Welcome, ${newPerson}. You are number ${currentLine.length} in line.`; 
+function takeANumber(customerLine, newCustomer) {
+  var position = currentLine.length;
+  return "Welcome, #{newCustomer}. You are number #{position} in line.";
 }
 
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-    var serving = katzDeliLine.shift();
-    return `Currently serving ${serving}.`;
-  } else {
+function nowServing(customerLine) {
+  if (currentLine.length === 0) {
     return "There is nobody waiting to be served!";
+  } else {
+    firstPerson = currentLine.shift();
+    return "Currently serving #{firstPerson}";
   }
 }
 
-function currentLine(line) {
-  var solution = `The line is currently:`;
-  if (line.length>0){
-  for (var i=1; i<line.length;i++){
-    solution = solution + ` ${i}. ${line[i-1]},`;
-  }
-  solution = solution + ` ${line.length}. ${line[line.length-1]}`;
-  return solution;
-  } else {
+function currentLine(customerLine) {
+  if (customerLine.length === 0 ) {
     return "The line is currently empty.";
+  } else {
+    var line = "The line is currently: ";
+    customerLine.forEach(function(index, value){
+      line + "#{index+1}. #{value},";
+    });
   }
 }
