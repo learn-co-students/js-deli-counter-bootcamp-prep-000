e8c8bfabd7deb3b5f176372c7bb00d1ae9738ae2
vyskoutaris <vyskoutaris@hotmail.com>
2019-01-22 15:30:08 +0000

    Done.

--- index.js
+++ index.js
@@ -2,31 +2,31 @@
 var katzDeliLine = [ ];
 
 function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name)
-  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+ katzDeliLine.push(`${name}`);
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 }
 
 
 function nowServing(katzDeliLine) {
  if (katzDeliLine.length > 0) {
-  return ('Currently serving ' + katzDeliLine.shift() + ".");
- }
- else {
+  return (`Currently serving ${katzDeliLine.shift()}.`)
+} else {
+  (katzDeliLine.length === 0);
    return "There is nobody waiting to be served!";
  }
 }
 
 
 function currentLine(katzDeliLine) {
-  var line=[]; 
+var line=[]; 
  if(katzDeliLine>[]) {
-   for(var i=0;i<katzDeliLine.length;i++) {
-   line.push(` ${i+1}. ${katzDeliLine[i]}`); 
-     } 
-     return "The line is currently:" + line; 
-    }
- else {
- katzDeliLine.length === 0;
+   for (var i=0; 
+       i<katzDeliLine.length; 
+       i++) {
+    line.push(` ${i+1}. ${katzDeliLine[i]}`); 
+} 
+  return "The line is currently:" + line; 
+} else {
+   (katzDeliLine.length === 0 );
     return "The line is currently empty." }
-  }
 }
