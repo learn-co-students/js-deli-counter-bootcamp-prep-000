5e7a2d784aa9fa92bc9670be54eba4d1775a42c6
Jonathan Koss <jonathan_koss@yahoo.com>
2019-01-23 19:30:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 function takeANumber(katzDeli, name) {
   katzDeli.push(name);
-  
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
 }
 
+function nowServing(katzDeli) {
+  if(katzDeli.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else{
+    return `Currently serving ${katzDeli.shift()}.`;
+  }
+}
+
+
+
 /*
- it('adds a person to the line', () => {
-      expect(takeANumber(katzDeli, 'Ada')).toEqual("Welcome, Ada. You are number 1 in line.");
-      expect(katzDeli).toEqual(['Ada']);
-    });
-
-    it('appends the person the end of the line if there are already people on it', () => {
-      expect(takeANumber(otherDeli, 'Grace')).toEqual("Welcome, Grace. You are number 4 in line.");
-      expect(otherDeli).toEqual(["Steven", "Blake", "Avi", "Grace"]);
-    });
-
-    it("properly handles multiple people being added", () => {
-      takeANumber(katzDeli, 'Ada');
-      takeANumber(katzDeli, 'Grace');
-      takeANumber(katzDeli, 'Kent');
-
-      expect(katzDeli).toEqual(["Ada", "Grace", "Kent"]);
-    });
-  });
-
-  describe('nowServing', () => {
+describe('nowServing', () => {
     it('returns the line is empty when no one is on line', () => {
       expect(nowServing([])).toEqual("There is nobody waiting to be served!");
     });
