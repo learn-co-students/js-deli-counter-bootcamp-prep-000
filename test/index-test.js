e90b61023fa7db70c763a48157a64206b35a16c8
Jason Velez <jvelez20@yahoo.com>
2019-01-24 17:24:05 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -10,17 +10,17 @@ describe('deli', () => {
       otherDeli = ["Steven", "Blake", "Avi"];
     });
 
-    it('adds a person to the line', () => {
+    console.log('adds a person to the line', () => {
       expect(takeANumber(katzDeli, 'Ada')).toEqual("Welcome, Ada. You are number 1 in line.");
       expect(katzDeli).toEqual(['Ada']);
     });
 
-    it('appends the person the end of the line if there are already people on it', () => {
+    console.log('appends the person the end of the line if there are already people on it', () => {
       expect(takeANumber(otherDeli, 'Grace')).toEqual("Welcome, Grace. You are number 4 in line.");
       expect(otherDeli).toEqual(["Steven", "Blake", "Avi", "Grace"]);
     });
 
-    it("properly handles multiple people being added", () => {
+    console.log("properly handles multiple people being added", () => {
       takeANumber(katzDeli, 'Ada');
       takeANumber(katzDeli, 'Grace');
       takeANumber(katzDeli, 'Kent');
@@ -30,11 +30,11 @@ describe('deli', () => {
   });
 
   describe('nowServing', () => {
-    it('returns the line is empty when no one is on line', () => {
+    console.log('returns the line is empty when no one is on line', () => {
       expect(nowServing([])).toEqual("There is nobody waiting to be served!");
     });
 
-    it('returns an announcement about the person it is serving, and shifts the line', () => {
+    console.log('returns an announcement about the person it is serving, and shifts the line', () => {
       const deliLine = ["Steven", "Blake", "Avi"]
       expect(nowServing(deliLine)).toEqual("Currently serving Steven.");
       expect(deliLine).toEqual(["Blake", "Avi"]);
@@ -42,11 +42,11 @@ describe('deli', () => {
   });
 
   describe('currentLine(line)', () => {
-    it('returns "The line is currently empty." if no one is in line', () => {
+    console.log('returns "The line is currently empty." if no one is in line', () => {
       expect(currentLine([])).toEqual("The line is currently empty.");
     });
 
-    it('says who is in line when there are people waiting', () => {
+    console.log('says who is in line when there are people waiting', () => {
       expect(currentLine(["Bill", "Jane", "Ann"])).toEqual("The line is currently: 1. Bill, 2. Jane, 3. Ann");
     });
   });
