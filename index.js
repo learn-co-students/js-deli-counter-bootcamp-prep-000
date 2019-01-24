240c7ba2e02f027b50da08f53df9b238b7a2d973
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-01-24 01:52:14 +0000

    Done.

--- index.js
+++ index.js
@@ -20,14 +20,3 @@ function currentLine(line) {
     }
     return "The line is currently: "+lineString;
 }
-
-
-function currentLine(line) {
-  if(line.length === 0) {
-    return("The line is currently empty.")}
-    var lineString = [];
-    for(var i = 0; i < line.length; i++){
-     lineString.push(i+ 1 + ". "+ line[i]);
-    }
-    return "The line is currently: "+lineString;
-}
