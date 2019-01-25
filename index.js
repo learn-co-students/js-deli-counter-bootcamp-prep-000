5941fc6426613de50a03895f5fe8386e9fcae3f4
Sajad <31453722+sajad1993gh@users.noreply.github.com>
2019-01-24 22:28:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 var katzDeliLine = [];
 function takeANumber(katzDeliLine, name){
-     katzDeliLine.push(name)
-  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+     katzDeliLine.push(name);
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
 }
 function nowServing(katzDeliLine){
-  var name= katzDeliLine[0]
-  katzDeliLine.shift()
+  var name= katzDeliLine[0];
+  katzDeliLine.shift();
   if(katzDeliLine.length <= 0){
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
   }
   else{
-    return "Currently serving " + name + "."
+    return "Currently serving " + name + ".";
   }
 }
 function currentLine(katzDeliLine){
   if(katzDeliLine.length <= 0){
-    return "The line is currently empty."
+    return "The line is currently empty.";
   }
   else{
-    var lineString ="The line is currently: "
-    for(let i=0 ; i< katzDeliLine.length-1 ; i++){
-    lineString = lineString + (i+1) + ". " + katzDeliLine[i] +", "
+    var array = [];
+    for(let i=0 ; i< katzDeliLine.length ; i++){
+    array.push((i+1) + ". " + katzDeliLine[i]);
     }
-    lineString = lineString + "3. " + katzDeliLine[katzDeliLine.length-1]
-   return lineString
-       }
+   return "The line is currently: " + array.join(", ") ;
+  }
 }
