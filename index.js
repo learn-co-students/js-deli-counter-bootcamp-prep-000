7c88820a2c0d81e5a8205b986991df61847e8f18
kendall willard <kendallwillard@hotmail.com>
2019-01-27 23:54:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, newPerson) {
+  katzDeliLine.push(newPerson)
+  var result = "Welcome, " + newPerson + ". You are number " + katzDeliLine.length + " in line.";
+  return result
+}
+
+function nowServing(katzDeliLine) {
+  var result = []
+  if( katzDeliLine.length <= 0 ) {
+    result.push("There is nobody waiting to be served!")
+  }
+  else {
+    result.push("Currently serving " + katzDeliLine[0] + ".") 
+    katzDeliLine.shift();
+  }
+  return result
+}
+
+function currentLine(katzDeliLine) {
+  var result = ""
+  var i = 0
+  if(katzDeliLine <= 0) {
+    return "The line is currently empty."
+  }
+  else {
+    result += "The line is currently: "
+    for(i = 0; i < katzDeliLine.length; i++ ) {
+      
+      //Check to see if 1 away from last element to remove the comma
+      if( i+1 === katzDeliLine.length) {
+        result += (i+1 + ". " + katzDeliLine[i])
+      }
+      else {
+      result += (i+1 + ". " + katzDeliLine[i] + ", ")
+      }
+    }
+  }
+  
+  return result
+}
