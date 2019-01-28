2d378cd1b23de9e1489550293e4bb7b108b1c3fd
Austin Williams <austinq.williams@gmail.com>
2019-01-28 01:02:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+var katzDeliline = []; 
+
+function takeANumber (katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing (katzDeliLine) {
+  if (katzDeliLine.length === 0) { 
+    return "There is nobody waiting to be served!"
+  } else { 
+    return`Currently serving ${katzDeliLine.shift()}.`
+  }
+}
+
+function currentLine (katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+     var string = "The line is currently:"
+    for (let i = 0 ; i < katzDeliLine.length ; i++) { 
+     string += ` ${i+1}. ${katzDeliLine[i]},` 
+  } 
+  string = string.slice(0, -1);
+  return string 
+  }
+}
