6b200901c472708888ae8fd6330d92ef6b2cc9eb
Diana Utan <dia_utzi@yahoo.com>
2019-02-04 19:40:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
+var katzDeLine = [];
 
-function takeANumber(line, newPerson) {
-  line.push(newPerson);
-  return ("Welcome, " + newPerson + ". You are number " + line.length + " in line.");
-}
-
-function nowServing(line) {
-  if( line.length >0 ) {
-    var removedPerson = line.shift();
-    return `Currently serving ${removedPerson}.`;
+function takeANumber(line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
   }
-  else return `There is nobody waiting to be served!`;
-}
-
-function currentLine(line) {
-  var newLine =[];
-  for( let i= 0; i< line.length; i++) {
-    newLine.push(` ` + [i+1] + `. ` + line[i]) }
-  if ( line.length === 0 ) {
-    return "The line is currently empty.";
-  }
-  else return `The line is currently:` + newLine;
   
-}
+  function nowServing(line) {
+    if( line.length > 0 ) {
+      return `Currently serving ${line.shift()}.`;
+    }
+    else return `There is nobody waiting to be served!`;
+  }
+  
+  function currentLine(line) { 
+    let string = [];
+     for( let i=0; i< line.length; i++) {
+      string.push(` ` + [i+1] + ". " + line[i])}
+   if (line.length > 0) {
+      return `The line is currently: ${string}`; 
+       }
+    else return `The line is currently empty.`;
+     
+  }
