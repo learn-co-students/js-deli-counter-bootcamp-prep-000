0b015a595b3a9ee37dd9aedaebeef5199e3d916e
Henry Koehler <henryk1229@gmail.com>
2019-02-05 19:12:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,4 @@
+var deliLine = []
+function takeANumber(deliLine, newName) { deliLine.push(newName); return `Welcome, ${newName}. You are number ${deliLine.length} in line.` } 
+function nowServing(deliLine) { if (deliLine.length === 0) { return "There is nobody waiting to be served!" } else { return `Currently serving ${deliLine.shift()}.` } }
+function currentLine(deliLine) { if (deliLine.length === 0) { return "The line is currently empty." }  const numbersAndNames = []; for (let i = 0, l = deliLine.length; i < l; i++) { numbersAndNames.push(`${i + 1}. ${deliLine[i]}`) } return `The line is currently: ${numbersAndNames.join(', ')}` }
