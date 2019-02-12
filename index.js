5c688d456a63369aa3b074ad052198859a73c19d
Cynthia LeGolvan <cynthia.nguyen21@gmail.com>
2019-02-12 12:20:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-var katzDeliLine = [name]
 function takeANumber(katzDeliLine, name) {
-  return ("Welcome $ You are number 1 in line.")
+  katzDeliLine.push(name)
+  return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
 }
+function nowServing(katzDeliLine) {
+  if(!katzDeliLine.length) {
+    return "There is nobody waiting to be served!"
+  } return `Currently serving ${katzDeliLine.shift()}.`
+}
+function currentLine(katzDeliLine) {
+  if (!katzDeliLine.length) {
+    return "The line is currently empty."
+  }
+
+ var numbersAndNames = []
+
+  for (let i = 0, l = katzDeliLine.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${katzDeliLine[i]}`)
+  }
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
+
