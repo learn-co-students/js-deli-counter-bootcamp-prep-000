f99db66725dc1db1818c2c6d9b64ef8fe2d3d645
Kyle Graden <kylegraden3@gmail.com>
2019-02-13 18:34:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var katzDeliLine = [];
+
+function takeANumber(line, name) {
+  let number = line.push(name);
+  return `Welcome, ${name}. You are number ${number} in line.`
+
+}
+
+function nowServing(line){
+  let next = line.shift();
+  if (next) {
+    return `Currently serving ${next}.`;
+  }
+  return 'There is nobody waiting to be served!';
+}
+
+
+
+function currentLine(line) {
+  let list = [];
+  for (let i = 0; i < line.length; i++) {
+    list.push(`${i + 1}. ${line[i]}`);
+  }
+  if (list.length === 0) {
+    return 'The line is currently empty.'
+
+  }
+  return `The line is currently: ${list.join(', ')}`;
+}
