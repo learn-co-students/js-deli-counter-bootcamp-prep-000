fb8dca02c42390e6242eff02eb96039c1e8500e6
Christopher Cabell <cccabell@icloud.com>
2019-03-01 23:26:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 function takeANumber(line, name) {
-  line.push(name)
-  return "Welcome, " + name + ". You are number " + line.length + " in line."
+  line.push(name);
+  return "Welcome, " + name + ". You are number " + line.length + " in line.";
 }
 
 function nowServing(line) {
   if (line.length === 0) {
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
   } else {
-    var now = line[0]
-    line.shift()
-    return "Currently serving " + now + "."
+    var now = line[0];
+    line.shift();
+    return "Currently serving " + now + ".";
   }
 }
 
 function currentLine(line) {
   if (line.length === 0) {
-    return "The line is currently empty."
+    return "The line is currently empty.";
   } else {
-    var announce = []
+    var announce = [];
     for (var i = 1; i <= line.length; i++) {
-      announce.push(" " + i + ". " + line[i-1])
+      announce.push(" " + i + ". " + line[i-1]);
     }
-    return "The line is currently:" + announce
+    return "The line is currently:" + announce;
   }
 }
 
