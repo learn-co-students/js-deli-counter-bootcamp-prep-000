66ad6f5700539f99a51e27142fb7df652e4c8a63
Jonathan Koss <jonathan_koss@yahoo.com>
2019-03-07 04:42:20 +0000

    Done.

--- index.js
+++ index.js
@@ -3,28 +3,6 @@ function takeANumber(katzDeli, name) {
   return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
 }
 
-function nowServing(katzDeli) {
-  if(katzDeli.length === 0) {
-    return "There is nobody waiting to be served!";
-  }
-  else{
-    return `Currently serving ${katzDeli.shift()}.`;
-  }
-}
-
-function currentLine(katzDeli) {
-  let array = [];
-  if(katzDeli.length === 0) {
-    return "The line is currently empty.";
-  }
-  else{
-    for(let i = 0; i < katzDeli.length; i++) {
-      array.push(`${i + 1}. ${katzDeli[i]}`);
-    }
-    return `The line is currently: ${array.join(', ')}`;
-  }
-}
-
 /*
  describe('currentLine(line)', () => {
     it('returns "The line is currently empty." if no one is in line', () => {
