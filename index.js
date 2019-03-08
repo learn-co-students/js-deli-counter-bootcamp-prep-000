e5c06a0235cd3a2f45d265ea5d165acd81180b6e
minh nguyen <imnotminh@gmail.com>
2019-03-08 17:59:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-function takeANumber(x,y){
-  x.push(y);
-  var i= 0;
-  var k=0;
-  for(i=0;i<x.length;i++){
-    if(x[i]===y){
-      k=i+1;
-    }
-  } return 'Welcome, '+y+'. You are number '+k+ ' in line.'
+// function takeANumber(x,y){
+//   x.push(y);
+//   var i= 0;
+//   var k=0;
+//   for(i=0;i<x.length;i++){
+//     if(x[i]===y){
+//       k=i+1;
+//     }
+//   } return 'Welcome, '+y+'. You are number '+k+ ' in line.'
+// }
+
+function takeANumber(array,name){
+    array.push(name);
+    return 'Welcome, '+name+'. You are number '+array.length+' in line.'
 }
 
-function nowServing(person){
-  if(person.length===0){
-  return 'There is nobody waiting to be served!'
-  }else{
-    return 'Currently serving '+person.shift()+'.'
-  }
+// function nowServing(person){
+//   if(person.length===0){
+//   return 'There is nobody waiting to be served!'
+//   }else{
+//     return 'Currently serving '+person.shift()+'.'
+//   }
+// }
+
+function nowServing(array){
+    if(array.length===0){
+        return "There is nobody waiting to be served!"
+    } else{
+        return 'Currently serving '+array.shift()+'.'
+    }
 }
 
 function currentLine(line){
