6ed3cef79921b64ff926ee97ac680202b027c3cf
Aiden leeming <aidenjl193@gmail.com>
2019-03-11 16:05:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function takeANumber(line, name) {
+  return `Welcome, ${name}. You are number ${line.push(name)} in line.`
+}
+
+function nowServing(line) {
+  if(line.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return `Currently serving ${line.shift()}.`
+  }
+}
+
+function currentLine(line) {
+  if(line.length === 0) {
+    return "The line is currently empty."
+  } else {
+    var string = "The line is currently: "
+    for(var i = 0; i < line.length; i++) {
+      string += `${i + 1}. ${line[i]}, `
+    }
+    return string.slice(0, string.length - 2)
+  }
+}
