b571a8858389f3b6031363577f18e7993e2b4431
algebraic-torrent-6062 <ashley.e.westcott@gmail.com>
2019-03-11 06:48:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-//var katzDeliLine = []
-
-//function takeANumber (katzDeliLine,name){
-//  katzDeliLine.push(name);
-// return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.` )
-//}
-
-var count = 0
-
 function takeANumber(katzDeliLine,name){
   katzDeliLine.push(name)
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
@@ -31,5 +22,4 @@ var line=[];
       return "The line is currently:" + line;
   }
 }
-
 
