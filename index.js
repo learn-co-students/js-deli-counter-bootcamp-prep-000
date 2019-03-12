c65470489e3c97a350aad05d8c580309bced6058
Mariela Gonzales <mpgonzales14@gmail.com>
2019-03-12 04:40:15 +0000

    Done.

--- index.js
+++ index.js
@@ -14,11 +14,15 @@ function nowServing(katzDeliLine) {
 }
 }
 
-function currentLine(katzDeliLine){
+function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-  return(`The line is currently empty.`);
-  
-}
-}
+    return `The line is currently empty.`;
+  } else {
 
-
+    var newarray = [];
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      newarray.push(` ${i+1}. ${katzDeliLine[i]}`);
+    }
+    return `The line is currently:` + newarray;
+  }
+}
