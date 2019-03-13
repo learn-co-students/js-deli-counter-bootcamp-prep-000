1e8c39a4dc13f34b78f267096c17a64c5e528ad4
Ethan Freire <element55star@gmail.com>
2019-03-13 13:11:30 +0000

    Done.

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ var katzDeliLine = []
 
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name)
-var myString
-myString = `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
-return myString
+  var myString
+  myString = `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+  return myString
 }
 function nowServing(katzDeliLine){
 
 
-if(katzDeliLine.length === 0){
-var noPeople = "There is nobody waiting to be served!"
-return noPeople
-}
+  if(katzDeliLine.length === 0){
+    var noPeople = "There is nobody waiting to be served!"
+    return noPeople
+  }
   var first = katzDeliLine.shift()
   var nowServing = "Currently serving " + first+"."
   return nowServing
@@ -22,11 +22,11 @@ function currentLine(line){
   var currentLineInString = `The line is currently: `
   var newLine = []
   if (line.length === 0){
-  return "The line is currently empty."
-}  else{
+    return "The line is currently empty."
+  }  else{
     for (let i = 0; i < line.length; i++){
-        newLine[i] = `${[i+1]}. ${line[i]}`
-      }
+      newLine[i] = `${[i+1]}. ${line[i]}`
+    }
     currentLineInString += newLine.join(', ')
     return currentLineInString
   }
