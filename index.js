0c06e74ec6e370de7104be04760dd16ea79d90e0
Trevahr Robertson <trevahr@yahoo.com>
2019-03-14 23:24:20 +0000

    Done.

--- index.js
+++ index.js
@@ -16,3 +16,12 @@ function nowServing(katzDeliLine){
      return `Currently serving ${first}.`
       } 
 }
+
+
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return "The line is currently empty.";
+  } 
+  
+  
+}
