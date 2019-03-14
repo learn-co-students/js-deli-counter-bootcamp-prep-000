1a20686f7bf0bc6f18c39b543519d6d505797ea2
Trevahr Robertson <trevahr@yahoo.com>
2019-03-14 22:53:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function takeANumber(katzDeliLine, n){
+  katzDeliLine.push(`${n}`);
+return `Welcome, ${n}. You are number ${(katzDeliLine.length)} in line.`;
+}
+
+
+
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!";
+  } 
+   else {
+     var first = katzDeliLine[0];
+    katzDeliLine.shift() 
+     return `Currently serving ${first}.`
+      } 
+}
