05b185e04ffa0005abebac1a67c3d203c058a38f
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-03-20 01:16:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function takeANumber (arr, newCustomer) {
+  arr.push(newCustomer)
+  return `Welcome, ${newCustomer}. You are number ${arr.length} in line.`
+}
+
+function nowServing (arr) {
+
+  if (!arr.length) {
+    return 'There is nobody waiting to be served!'
+  }
+
+  return `Currently serving ${arr.shift()}.`
+}
+
+function currentLine (arr) {
+
+  if (!arr.length) {
+    return "The line is currently empty."
+  }
+
+  var arrNew = []
+  for (var i = 0, l = arr.length; l > i ; i++) {
+    arrNew.push(` ${ i + 1}. ${arr[i]}`)
+  }
+
+  return `The line is currently:${arrNew}`
+}
