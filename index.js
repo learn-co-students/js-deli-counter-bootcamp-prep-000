080371ba3078d1737def34d2cf3518a89022ca22
Stephen Brewer <stephenbrewer16@gmail.com>
2019-03-20 05:31:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-var katzDeliLine = [];
-
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+var katzDeliLine = []
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
-function nowServing(katzDeliLine) {
+function nowServing(katzDeliLine){
   if (katzDeliLine.length>0) {
     return `Currently serving ${katzDeliLine.shift()}.`
+  } else {
+    return "There is nobody waiting to be served!"
   }
-  else {
-  return "There is nobody waiting to be served!"
 }
+function currentLine(katzDeliLine){
+  var newline = []
+if (katzDeliLine.length>0) {
+  for(var i = 0; i<katzDeliLine.length; ++i){
+  newline.push(` ${i+1}. ${katzDeliLine[i]}`)
 }
-function currentLine(katzDeliLine) {
-  var line = []
-  if (katzDeliLine.length>0) {
-    for(var i = 0; i<katzDeliLine.length; i++){
-      line.push(` ${i + 1}. ${katzDeliLine[i]}`)
-    }
-    return "The line is currently:" + line
-  }
-  else {
-    return "The line is currently empty."
-  }
+return "The line is currently:" + newline
+} else {
+return "The line is currently empty."
+}
 }
