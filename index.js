d290e10941cdb80fa0a14837c47dc0dc8ef62a48
Ariq Holland <001.aqh@gmail.com>
2019-03-20 20:20:19 +0000

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nowServing(katzDeliLine){
 function currentLine(line){
   var theLine=[];
   for (let i = 0; i < line.length; i++){
-    theLine.push(` `+[i+1]+`. `  + line[i])
+    theLine.push(` `+[i+1]+`. `  + line[i]);
   }
   if (line.length === 0) {
     return "The line is currently empty.";
