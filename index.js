449b0e194df5a501efe2e4ae3638f585a55946e0
Ay Has <ahashim983@yahoo.com>
2019-03-21 23:04:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 function takeANumber(katzDeliLine, name){
-  return `Welcome ${name}. You are number ${katzDeliLine} in line.`
+  katzDeliLine.push(name)
+  var position = parseInt(katzDeliLine.indexOf(name))
+  return `Welcome, ${name}. You are number ${position+1} in line.`
 }
 
 function nowServing(katzDeliLine){
-  if katzDeliLine != []{
-    return katzDeliLine[0]
-    katzDeliLine.unshift()
-  } else {
+  if (katzDeliLine.length == 0){
     return "There is nobody waiting to be served!"
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`
+    katzDeliLine
   }
 }
 
 function currentLine(katzDeliLine){
-  if katzDeliLine == []{
+  if (katzDeliLine.length == 0){
     return "The line is currently empty."
   } else {
-    var queue = 0
-     for(var i = 0; i < katzDeliLine.length; i++){
-       queue = queue + 1
-     }
-     return Object.assign({},{[queue] : katzDeliLine})
+    var line_format = []
+    for (var i = 0; i < katzDeliLine.length; i++){
+      var name = katzDeliLine[i]
+
+      line_format.push(` ${i+1}. ${name}`)
+      line_format
+    }
+    return `The line is currently:${line_format}`
 
   }
-
-  }
+}
