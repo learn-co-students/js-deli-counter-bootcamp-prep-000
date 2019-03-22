13ead9fbd43248946bcf0719d995133d8d335325
Josephine Feely <jojofeely@yahoo.co.uk>
2019-03-22 12:45:58 +0000

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, name){
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0){
-    return `There is nobody waiting to be served!`
+    return "There is nobody waiting to be served!"
   } else {
     var name = katzDeliLine[0]
   katzDeliLine.splice(0, 1)
@@ -18,8 +18,8 @@ function currentLine(katzDeliLine){
   if (katzDeliLine.length === 0) {
     return `The line is currently empty.`
   } else {
-    for (var i=0; i < katzDeliLine.length; i++){
-      line += (i + 1) + ". "+ katzDeliLine[i] + ", "
+    for ( var i = 0; i < katzDeliLine.length; i++){
+      line +=(i + 1) + ". "+ katzDeliLine[i] +", "
     }
     line = line.slice(0, line.length-2)
     return "The line is currently: " + line
