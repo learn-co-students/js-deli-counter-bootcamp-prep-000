7d9e7770ff56fd445a3f9bc99f7a1e981b48c7db
elegant-architecture-3891 <katherineortiz313@gmail.com>
2019-03-22 14:32:38 +0000

    Done.

--- index.js
+++ index.js
@@ -20,8 +20,14 @@ function nowServing(katzDeliLine) {
 }
 }
 
-function currentLine(line){
+function currentLine(line) {
   if (line.length === 0){
     return "The line is currently empty."
   }
+  else {
+      for(var i = 0; i < line.length; i++) 
+        line += (i + 1) + ". " + line[i] + ", "
+      }
+      line = line.slice(0, line.length-2)
+      return "The line is currently: " + line
 }
