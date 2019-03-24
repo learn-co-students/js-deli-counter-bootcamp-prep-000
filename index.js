844144f6964d857fb4b2dc8487016f61a4015584
Trevahr Robertson <trevahr@yahoo.com>
2019-03-24 19:42:10 +0000

    Done.

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ function nowServing(katzDeliLine)
   if (katzDeliLine.length === 0){
     return "There is nobody waiting to be served!";
   } 
-   
-     //var first = katzDeliLine[0];
-   // katzDeliLine.shift() 
      return `Currently serving ${katzDeliLine.shift()}.`
 }
 
