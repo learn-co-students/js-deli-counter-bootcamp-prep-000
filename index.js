07404bf0f10d80fe4e895fd3bcf24d9ebb58f837
Gavin O'Connor <gedmund@gmail.com>
2019-03-28 18:32:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 // takeANumber adds customer to line
 var takeANumber = function(katzDeliLine, newCustomer) {
-      katzDeliLine.push(newCustomer)
-      return `Welcome, ${newCustomer}. You are number ${katzDeliLine.length} in line.`
+    katzDeliLine.push(newCustomer)
+    return `Welcome, ${newCustomer}. You are number ${katzDeliLine.length} in line.`
 }
 // nowServing announces customer, removes them from array, returns message if empty
 var nowServing = function(katzDeliLine) {
-    let i = 0
-    while (i < katzDeliLine.length) {
-      i++
-    }
-    if (katzDeliLine.length === 0) {
-      return `There is nobody waiting to be served!`
-    } else {
-      return `Currently serving ${katzDeliLine.shift()}.`
-    }
+  if (katzDeliLine.length === 0) {
+    return `There is nobody waiting to be served!`
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`
   }
+}
 
 // currentLine accepts current line of people and returns string
 var currentLine = function(katzDeliLine) {
   var thisLine = []
+
   for (var i = 0; i < katzDeliLine.length; i++) {
-    thisLine.push(' ' + [i + 1] + '. ' + katzDeliLine[i])
+    thisLine.push(` ${i + 1}. ${katzDeliLine[i]}`)
   }
-  if (katzDeliLine.length === 0) {
+    if (katzDeliLine.length === 0) {
     return `The line is currently empty.`
   } else {
-  return `The line is currently:${thisLine }`
-}
+    return `The line is currently:${thisLine }`
+  }
 }
