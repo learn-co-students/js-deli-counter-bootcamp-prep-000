88859ebd6202541cd1e39987f68225b109f4dee3
Christopher Yuan <chrisyuan7@gmail.com>
2019-03-29 23:58:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return "Welcome, " + name + ". You are number " + (katzDeliLine.indexOf(name) + 1) + " in line."
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var announce = "Currently serving " + katzDeliLine[0] + "."
+    katzDeliLine.shift()
+    return announce
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+    var lineOrder = "The line is currently:"
+    for (let i = 0; i < katzDeliLine.length; i++) {
+      var name = katzDeliLine[i]
+      var newCustomer = " " + (katzDeliLine.indexOf(name) + 1) + ". " + katzDeliLine[i] + ","
+      lineOrder +=  newCustomer
+    }
+    return lineOrder.slice(0, -1)
+  }
+}
