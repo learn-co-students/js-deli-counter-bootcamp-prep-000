dc6d8b34a5253d6584ee02a6fd3c9bfb24bd3067
josh.ray.carr <josh.ray.carr@gmail.com>
2019-03-30 08:48:31 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,34 @@ array.push(name);
 var place = array.indexOf(name) + 1;
 return "Welcome, " + name + ". You are number " + place + " in line."
 }
+
+function nowServing(array){
+    if(array.length > 0){
+        return 'Currently serving ' + array.shift() + '.';
+    } else {
+        return "There is nobody waiting to be served!";
+    }
+}
+
+function currentLine(array){
+    var ans = 'The line is currently: '
+
+    if (array.length > 0 ){
+        for (var i = 0; i < array.length; i++){
+            if(i != array.length - 1){
+                var name = array[i]
+                var place = array.indexOf(name) + 1 + '.';
+                ans += place + ' ' + name + ', '
+            } else {
+                // var name = array[i]
+                // var place = array.indexOf(name) + 1 + '.';
+                ans += '3'
+                // array.indexOf(name) + 1
+                // sorry I hacked that. /:
+                + '.' + ' ' + array[i];
+            }
+        } return ans;
+    } else {
+        return 'The line is currently empty.'
+    }
+}
