64565770ca256e69fdaf3ba5b446634b2fe10fe2
Christopher Yuan <chrisyuan7@gmail.com>
2019-03-30 00:04:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  return "Welcome, " + name + ". You are number " + (katzDeliLine.indexOf(name) + 1) + " in line."
+  return "Welcome, " + name + ". You are number " + (katzDeliLine.length) + " in line."
 }
 
 function nowServing(katzDeliLine) {
