f3a163c8fe4f67c1c39278938796039a8bdcbf58
Hanna Engel <hengel@alumni.berklee.edu>
2019-03-31 22:21:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-function takeANumber (currentLine, name){
-currentLine.push(name)
-  var place = currentLine.length
-  console.log(`Welcome, ${name} You are number ${place} in line.`)
+function takeANumber (katzDeliLine, name){
+  katzDeliLine.push(name)
+  return`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing (katzDeliLine){
+  if(katzDeliLine.length > 0){
+    var serving = katzDeliLine[0]
+    katzDeliLine.shift()
+    return `Currently serving ${serving}.`
+  }else{
+    return "There is nobody waiting to be served!"
+  }
+}
+
+
+function currentLine(line){
+  var newList = []
+  if (line.length===0){
+    return "The line is currently empty.";
+  }for(var i =0; i<line.length; i++){
+    if(i<line.length){
+      newList.push(` ${i+1}. ${line[i]}`)
+    }
+  }
+  return `The line is currently:${newList}`
 }
