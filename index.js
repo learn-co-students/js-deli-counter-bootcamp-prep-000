b9d384939efbd1fd89fc1881a4df8f7d427b93f6
Joseph Reilly <reilly4181@gmail.com>
2019-04-02 05:16:51 +0000

    Done.

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function takeANumber(katzDeliLine, name) {
   
   katzDeliLine.push(name);
   
-var place = katzDeliLine.length;
+ 
 
-  return `Welcome, ${name}. You are number ${place} in line.`
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 
