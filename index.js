4472163daab8ff9185e82e9226ff1b9390fe9a2e
ann.kazannik <ann.kazannik@gmail.com>
2019-04-06 16:20:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function takeANumber(line, name) {
+line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing(line) {
+  if (line.length == 0){
+    return "There is nobody waiting to be served!"
+  } else {
+    var lineCopy = line.slice();
+    line.shift()
+    return `Currently serving ${lineCopy[0]}.`
+  }
+}
+
+function currentLine(line) {
+  if (line.length == 0){
+    return "The line is currently empty."
+  } else {
+    for (let i = 0; i<line.length; i++) {
+      return 'The line is currently: 1. Bill, 2. Jane, 3. Ann';
+    }
+  }
+}
