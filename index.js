60be52f33323285e102ace70b7f9b9c2cec40eb7
Ezra Brand <ezrabrand@gmail.com>
2019-04-07 20:51:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-let katzDeliLine = ['Ada', 'Grace']
+//let katzDeliLine = ['Ada', 'Grace']
 
-// #1 takeANumber
-
-function takeANumber (katzDeliLine, newName) {
-  console.log('Welcome', newName, 'You are number', i+1, 'in line.')
-}
+// // #1 takeANumber
+// //unclear if should mutate/destroy the "current line of peopl" = katzDeliLine. set it to mutate.
+//
+// function takeANumber (katzDeliLine, newPerson) {
+//   katzDeliLine.push(newPerson)
+//   console.log('Welcome', newPerson, '. You are number', katzDeliLine.indexOf(newPerson)+1, 'in line.')
+// }
+//
+// // takeANumber(['Ada', 'Grace'],'Ezra')
+//
+// //there's an extra space after the output, how to fix?
 
 // #2 nowServing
 
-function nowServing (katzDeliLine) {
-  if (Array.isArrary(katzDeliLine) || katzDeliLine.length) {
-  return katzDeliLine[0]
+function nowServing(katzDeliLine) {
+  if (Array.isArray(katzDeliLine) || katzDeliLine.length) {
+  console.log(katzDeliLine[0])
   katzDeliLine.shift()
   } else {
   console.log('There is nobody waiting to be served!')
   }
 }
 
-// #3
+nowServing ('Ezra')
 
-function currentLine(katzDeliLine) {
-  if (Array.isArrary(katzDeliLine) || katzDeliLine.length) {
-  console.log('The line is currently:', katzDeliLine.length)
-  } else {
-  console.log('The line is currently ')
-  }
-}
+// // #3 currentLine
+//
+// function currentLine(katzDeliLine) {
+//   if (Array.isArrary(katzDeliLine) || katzDeliLine.length) {
+//   console.log('The line is currently:', katzDeliLine.length, ' ', katzDeliLine[0])
+//   } else {
+//   console.log('The line is currently empty')
+//   }
+// }
