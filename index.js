78f0a217c1eaefc58046cea4f109e35358c1a16a
Ezra Brand <ezrabrand@gmail.com>
2019-04-07 21:27:34 +0000

    Done.

--- index.js
+++ index.js
@@ -3,27 +3,32 @@
 // // #1 takeANumber
 // //unclear if should mutate/destroy the "current line of peopl" = katzDeliLine. set it to mutate.
 //
-// function takeANumber (katzDeliLine, newPerson) {
-//   katzDeliLine.push(newPerson)
-//   console.log('Welcome', newPerson, '. You are number', katzDeliLine.indexOf(newPerson)+1, 'in line.')
-// }
-//
-// // takeANumber(['Ada', 'Grace'],'Ezra')
+function takeANumber (katzDeliLine, newPerson) {
+  katzDeliLine.push(newPerson)
+  console.log(`Welcome, ${newPerson}. You are number ${katzDeliLine.indexOf(newPerson)+1} in line.`)
+}
+
+// takeANumber(['Ada', 'Grace'],'Ezra')
 //
 // //there's an extra space after the output, how to fix?
 
-// #2 nowServing
+// // #2 nowServing
+//
+// function nowServing(katzDeliLine) {
+//   if (katzDeliLine.length) {
+//   console.log(`Currently serving ${katzDeliLine[0]}.`)
+//   katzDeliLine.shift()
+//   console.log(katzDeliLine)
+//   } else {
+//   console.log('There is nobody waiting to be served!')
+//   }
+// }
+//
+// nowServing (['Ada', 'Grace', 'Ezra'])
 
-function nowServing(katzDeliLine) {
-  if (Array.isArray(katzDeliLine) || katzDeliLine.length) {
-  console.log(katzDeliLine[0])
-  katzDeliLine.shift()
-  } else {
-  console.log('There is nobody waiting to be served!')
-  }
-}
+// // nowServing (['Ada', 'Grace', 'Ezra'])
 
-nowServing ('Ezra')
+// // how to set it up to return when line is empty? getting error "TypeError: Cannot read property 'length' of undefined at nowServing"
 
 // // #3 currentLine
 //
