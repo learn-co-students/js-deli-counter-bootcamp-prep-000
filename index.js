58215933f7ffefcc587053edb43a4bc364a1c7a3
Samir Jasarat <samirjasarat@gmail.com>
2019-04-08 15:24:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Function to welcome and assign customers a place in line
+//Function "takeANumber", which welcomes and assigns customers a place in line
 var katzDeliLine=[];
 function takeANumber(katzDeliLine, name) {
 katzDeliLine.push(name);
@@ -9,14 +9,14 @@ takeANumber(katzDeliLine, "Moe");
 
 console.log(takeANumber(katzDeliLine, "Moe"));
 
-//Function to return the first person in line, and then remove the person from the line.
+//Function "nowServing", which returns the first person in line, and then remove the person from the line.
 //If the line is empty, the function returns "There is nobody waiting to be served".
 
 var katzDeliLine=[];
 
 function nowServing(katzDeliLine) {
   //Initially in the function, an if statement is used to check for the condition of an empty array,
-  //which translates to no customer in line. If this condition returns as false, an else statement is used
+  //which translates to no customer being in line. If this condition returns as false, an else statement is used
   //to identify the first customer in line, return their name, and then remove them from the line utilizing the .shift()
   //method of array manipulation.
 
@@ -30,7 +30,8 @@ function nowServing(katzDeliLine) {
 }
 
 
-
+//Function "currentLine", which uses a for loop to iterate through the katzDeliLine array and use the elements within it to
+//formulate a new array printed out in the desired format (in this case the number in line and name associated with it).
 function currentLine(katzDeliLine){
   if(!katzDeliLine.length) {
     return "The line is currently empty.";
