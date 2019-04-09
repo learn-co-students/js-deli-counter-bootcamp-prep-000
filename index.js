988525e11c737701d9b2c973b41ea94dab35fd18
Alexandre Silva Dias <alexdias53@gmail.com>
2019-04-09 13:15:51 +0000

    Done.

--- index.js
+++ index.js
@@ -5,23 +5,30 @@ function takeANumber (katzDeliline,name) {
 }
 
 function nowServing (katzDeliline) {
-  if (katzDeliline.length === 0)
+  if (katzDeliline.length === 0)  {
   return "There is nobody waiting to be served!"
-} 
-
+} else { 
+    var name = katzDeliline[0];
+    katzDeliline.splice(0,1); // return the first person in line and then remove that individual from the line.
+    return "Currently serving " + name + ".";
+}
+}
 
 function currentLine (Newline){
-  
-  if (Newline.length === 0){
+  var line = []
+  if (Newline.length === 0){ // if the line is empty 
     return "The line is currently empty."
   } else { 
-    for (var i = 0; i < Newline.length; i++){
-    Newline += (i + 1) + ". " + NewLine[i] + ", "
+    for (var i = 0; i < Newline.length; i++){ // compares how many people there are in the line and if there are people executes else loop 
+    
+    line += (i + 1) + ". " + Newline[i] + ", "
   }
   line = line.slice(0, line.length-2)
-  return "The line is currently empty."
+  return "The line is currently: " + line
   }
   
 }
+
+
 
 
