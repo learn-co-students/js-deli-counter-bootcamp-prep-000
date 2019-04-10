72ab8596e664d499463653762d357723cfb13916
Ezra Brand <ezrabrand@gmail.com>
2019-04-10 18:35:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 //let katzDeliLine = ['Ada', 'Grace', 'Ezra']
 
+function welcomeNumber {
+  if (katzDeliLine.length)
+}
+
 // // #1 takeANumber
 // //unclear if should mutate/destroy the "current line of peopl" = katzDeliLine. set it to mutate.
 //
 function takeANumber (katzDeliLine, newPerson) {
   katzDeliLine.push(newPerson)
   return(`Welcome, ${newPerson}. You are number ${katzDeliLine.indexOf(newPerson)+1} in line.`)
+}
+
+let number = 1
+function takeANumberIncrementUp () {
+  katzDeliLine.push(number)
+  return (`Welcome # ${number} on line!`)
+  number ++
 }
 
 // takeANumber(['Ada', 'Grace'],'Ezra')
@@ -33,7 +44,7 @@ function currentLine(katzDeliLine) {
 let str = 'The line is currently:'
   if (katzDeliLine.length) {
     for (let i=0;i<katzDeliLine.length; i++) {
-   (str += ` ${i+1}. ${katzDeliLine[i]},`)
+   str += ` ${i+1}. ${katzDeliLine[i]},`
     } return str.slice(0,-1)
 
   } else {
