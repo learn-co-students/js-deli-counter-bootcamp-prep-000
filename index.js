fb474f0b7de15a6aef4930da81e4a8bdc49b3b97
ann.kazannik <ann.kazannik@gmail.com>
2019-04-11 14:40:44 +0000

    Done.

--- index.js
+++ index.js
@@ -18,8 +18,10 @@ function currentLine(line) {
   if (line.length == 0){
     return "The line is currently empty."
   } else {
-    for (let i = 0; i<line.length; i++) {
-      return 'The line is currently: 1. Bill, 2. Jane, 3. Ann';
+    const currLine = [];
+    for (let i = 0; i < line.length; i++) {
+      currLine.push(`${i+1}. ${line[i]}`);
     }
+    return `The line is currently: ${currLine.join(', ')}`;
   }
 }
