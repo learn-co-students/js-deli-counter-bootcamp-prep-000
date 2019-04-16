6d06d846245fb2e5fa9ad92438c77d1327cac016
Ethan Collins <ethantcollins98@gmail.com>
2019-04-16 18:20:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function takeANumber(line, name) {
-  line.push(name)
+  line.push(name) //adds name to end of array
   return `Welcome, ${name}. You are number ${line.length} in line.`
-}
+} //above line interpolates name and number and returns the string
 
 function nowServing(line) {
-  if (line.length > 0) {
-    var a = line[0]
-    line.shift()
-    return `Currently serving ${a}.`
+  if (line.length > 0) { //if there are people in line
+    var a = line[0] //set a = to value of the first item in array
+    line.shift() //remove the first item from the array
+    return `Currently serving ${a}.` //return the shifted value
   } else {
     return "There is nobody waiting to be served!"
   }
@@ -15,15 +15,15 @@ function nowServing(line) {
 
 function currentLine(line) {
   var str = "The line is currently: "
-  if (line.length > 0) {
+  if (line.length > 0) { //if the array isn't empty
     var i = 0
-    while (line.length > 0) {
-      str += `${i + 1}. ${line[0]}`
+    while (line.length > 0) { //while the array isn't empty
+      str += `${i + 1}. ${line[0]}` //formatting / adding strings
       line.shift()
       if (line.length > 0) {
-        str += ", "
+        str += ", " //formatting / adding strings
       }
-      i++;
+      i++; //variable to track number in line
     }
   } else {
     return "The line is currently empty."
