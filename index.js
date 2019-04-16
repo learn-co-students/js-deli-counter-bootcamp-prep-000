1a7357c8cc84d36ac558f161daa7caec68782ae3
James Hayward <james.hayward6@icloud.com>
2019-04-16 23:25:34 +0000

    Done.

--- index.js
+++ index.js
@@ -19,27 +19,20 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty." //notice that line doesn't become the name of the line in question because it is not inside `${}`
   }
-  if (parseInt(line.length) > 0) {
-    let whoseLeft = [];
+  if (parseInt(line.length) > 0) { // could do else {}
+    let whosLeft = [];
      let i = -1; //had to set to -1 to make the counter work
      while (i++ < line.length-1) { // totally bodged this but it worked.
-         whoseLeft.push(`${i+1}. ${line[i]}`)
-         return `The line is currently: ${whoseLeft[i]}.`
-         //.join(', ');
-//     } return `The line is currently: ${whoseLeft[i]}.`
+         whosLeft.push(`${i+1}. ${line[i]}`)
+       } return `The line is currently: ${whosLeft.join(', ')}`; //note that curly bracket has to precede statement otherwide return cuts while loop
+//     } return `The line is currently: ${whosLeft[i]}.`
 
     // for (let i = 0; i < line.length; i++) {
-    //
-    // return `The line is currently: ${line[i]}.`
-
-
-    // let i = 0
-    // i++
-    // while (i < line.length) {
-    //   return `The line is currently: ${line[i]}.`
-    // }
-
 
     }
   }
-}
+
+// i = 0
+// while (i < line.length) {
+// i++
+// }
