3551ef53088a81ef565777542967e5a75c5eb041
Finn Thye <piqueen@gmail.com>
2019-04-18 14:36:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 function takeANumber(people, newPerson) {
-  return `Welcome, ${newPerson}. You are number ${people.length + 1} in line.`;
+  people.push(newPerson);
+  return `Welcome, ${newPerson}. You are number ${people.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    let currentPerson = katzDeliLine[0]
+    katzDeliLine.shift()
+    return `Currently serving ${currentPerson}.`
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
+    return 'The line is currently empty.'
+  } else {
+    let announcement = REFORMAT NAMES INTO NUMBERED LIST
+    return `Currently serving ${currentPerson}.`
+  }
 }
