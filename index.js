9e4aaa23c270166279b524bd6de1c8b88c965167
Martin Zuleta <mzuleta4@cuc.edu.co>
2019-04-21 02:50:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function takeANumber(arr,name){
+  arr.push(name)
+  return `Welcome, ${name}. You are number ${arr.length} in line.`
+}
+
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  }else{
+
+    var res = 'The line is currently: '
+    for (var i = 1; i <= katzDeliLine.length; i++) {
+      if (i === katzDeliLine.length) {
+          res += `${i}. ${katzDeliLine[i-1]}`
+      }else{
+        res += `${i}. ${katzDeliLine[i-1]}, `
+      }
+    }
+    return res
+  }
+}
+
+function nowServing(arr){
+  if (arr.length > 0) {
+    return `Currently serving ${arr.shift()}.`
+  }else{
+    return "There is nobody waiting to be served!"
+  }
+}
