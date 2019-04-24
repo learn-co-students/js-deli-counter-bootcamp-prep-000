00863ff0d82247e7a119479409af91043f07e5ab
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-04-24 14:31:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-function takeANumber(line, name) {
-  line.push(name);
-  return `Welcome, ${name}. You are number ${line.length} in line.`;
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing(line) {
-  if(line.length === 0) {
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-    return `Currently serving ${line.shift()}.`;
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
 
-function currentLine(line) {
-  if(line.length === 0) {
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
     var temp = "";
-    for(var i = 0; i < line.length; i++) {
+    for(var i = 0; i < katzDeliLine.length; i++) {
       if(i === 0) {
-        temp = `The line is currently: 1. ${line[i]}`;
+        temp = `The line is currently: 1. ${katzDeliLine[i]}`;
       } else {
-        temp = temp + `, ${i+1}. ${line[i]}`;
+        temp = `${temp}, ${i+1}. ${katzDeliLine[i]}`;
       }
     }
     return temp;
