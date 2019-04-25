6dd471ad831b7a9f24614c4d4e0a50fdf8e079d2
Lawrence Yolland <lawrence@sciencedisrupt.com>
2019-04-25 18:42:43 +0000

    Done.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(deliLine){
   var textDisplay = [];
   var baseText = "The line is currently:"
    if (deliLine.length===0){
-    return baseText + ": empty".;
+    return "The line is currently empty.";
   }
   else{
   for (var i=0; i<deliLine.length; i++){
