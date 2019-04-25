6ca91e1ee98f134f944c99f03b7eddc0226245e1
Min Kang <kangm914@gmail.com>
2019-04-25 21:53:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function takeANumber(line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing(line) {
+  var firstperson = line[0];
+  line.shift();
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else {
+    return `Currently serving ${firstperson}.`;
+  }
+}
+
+function currentLine(line){
+  var katzDeliLine = [];
+  var text = "The line is currently:"
+   if (line.length===0){
+    return "The line is currently empty.";
+  }
+  else{
+  for (var i=0; i<line.length; i++){
+    katzDeliLine[i] = ` ${i+1}. ${line[i]}`;
+    }
+    return text + katzDeliLine;
+  }
+}
