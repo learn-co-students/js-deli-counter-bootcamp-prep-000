088c7293a0a888957863591cc0e17eca006e7004
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-04-26 02:41:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function takeANumber(line, name){
+  line.push(name)
+  return "Welcome, " + name + ". You are number " + line.length + " in line."
+}
+
+function nowServing(line){
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return "Currently serving " + line.shift() + "."
+  }
+}
+
+function currentLine(line){
+  if (line.length === 0) {
+    return "The line is currently empty."
+  } else {
+    var string = "The line is currently: ";
+    
+    for (var i = 0; i < line.length; i++){
+      string = string + `${i+1}. ${line[i]}, `      
+    }
+    
+    return string.slice(0,-2)
+  }
+  
+}
