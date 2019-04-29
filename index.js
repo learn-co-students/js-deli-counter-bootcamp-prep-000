ffe8cf91800989fd42809195ae2050fafe2d0f93
Patrick Candlin <pctennislessons@gmail.com>
2019-04-29 22:14:36 +0000

    Done.

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ function takeANumber(katzDeliLine, name){
   return "Welcome, " + name+"." + " You are number " + katzDeliLine.length + " in line.";
 }
 
-function nowServing(array){
-  
+function nowServing(katzDeliLine){
+  if (katzDeliLine[0]===undefined){
+    return 'There is nobody waiting to be served!'}
+    else{ return "Currently serving "+katzDeliLine[0]+"." }
+    return katzDeliLine.shift(0);
 }
 
-function currentLine(array){
+function currentLine(katzDeliLine){
   
 }
