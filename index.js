6d01fd6715e9a01ed0252188c4ec71d1827e2909
Patrick Candlin <pctennislessons@gmail.com>
2019-04-29 20:49:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return "Welcome, " + name+"." + " You are number " + katzDeliLine.length + " in line.";
+}
+
+function nowServing(array){
   
 }
 
 function currentLine(array){
-  
-}
-
-function nowServing(array){
   
 }
