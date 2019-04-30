6cf49cb9ebb4a3b77a5cb35a87cb1e6f4b226ff2
Patrick Candlin <pctennislessons@gmail.com>
2019-04-30 02:57:30 +0000

    Done.

--- index.js
+++ index.js
@@ -13,5 +13,16 @@ function nowServing(katzDeliLine){
 
 
 function currentLine(katzDeliLine){
-  
+  var deliLineString ="The line is currently: ";
+  if(katzDeliLine[0]===undefined){
+    return 'The line is currently empty.';
+  }else{ for(let i = 0; i < katzDeliLine.length; i++) {
+    if(i===katzDeliLine.length-1){
+      deliLineString = deliLineString + parseInt(i+1) +". "+ katzDeliLine[i];
+    }
+     else{ deliLineString = deliLineString + parseInt(i+1) +". "+ katzDeliLine[i]+', ';
+     }
+  }
+    return deliLineString;
+  }
 }
