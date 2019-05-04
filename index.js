c2a0b2a37db8686d2d552956b4b2aae873a813ac
Jonathan Koss <jonathan_koss@yahoo.com>
2019-05-04 03:58:30 +0000

    Done.

--- index.js
+++ index.js
@@ -4,5 +4,25 @@ function takeANumber(line, name) {
 }
 
 function nowServing(line) {
-  
+  if(line.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else{
+    return `Currently serving ${line.shift()}.`; 
+  }
 }
+
+function currentLine(line) {
+  if(line.length === 0) {
+    return "The line is currently empty.";
+  }
+  else{
+    let array = [];
+    for(let i = 0; i < line.length; i++) {
+      array.push(`${i + 1}. ${line[i]}`);
+    }
+    return `The line is currently: ${array.join(', ')}`;
+  }
+}
+
+
