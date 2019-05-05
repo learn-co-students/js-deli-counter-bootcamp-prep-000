6826157d921090d17c5fac8c87bf3ff94d9d0e99
Zach Parker <parkerz410@gmail.com>
2019-05-05 14:01:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+};
+
+function nowServing(katzDeliLine) {
+  let i = 0
+  while (i < katzDeliLine.length) {
+    i++;
+  }
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else
+    return `Currently serving ${katzDeliLine.shift()}.`;
+};
+
+var newLine = [];
+
+function currentLine(katzDeliLine) {
+  for (let i = 0; i < katzDeliLine.length; i++) {
+       newLine.push(' ' + [i+1] + `. ` + katzDeliLine[i])
+     } if (katzDeliLine.length > 0) {
+       return(`The line is currently:` + newLine);
+     } else
+       return 'The line is currently empty.';
+   };
