4a8b29e5efdedc12e9549142cb932e1e3371c4ac
Daniel McClean <dmcclean01@hotmail.com>
2019-05-05 18:44:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-function takeANumber(katzDeliLine, name){
+function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing(katzDeliLine){
+function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) return `Currently serving ${katzDeliLine.shift()}.`;
   else return "There is nobody waiting to be served!";
 }
 
-function currentLine(katzDeliLine){
+function currentLine(katzDeliLine) {
   var str = `The line is currently: `;
   var newLine = [];
   if (katzDeliLine.length === 0) return "The line is currently empty.";
-  else{
-    for (let i = 0; i < katzDeliLine.length; i++){
+  else {
+    for (let i = 0; i < katzDeliLine.length; i++) {
         newLine[i] = `${[i+1]}. ${katzDeliLine[i]}`;
       }
     str += newLine.join(', ');
