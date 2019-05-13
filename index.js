0151dcfb4c47b6298a47723d07323acce5b3d555
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-13 02:25:11 +0000

    Done.

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ function nowServing(katzDeliLine) {
   }
 }
 
-function currentLine(line){
-  if(!line.length) {
+function currentLine(katzDeliLine){
+  if(!katzDeliLine.length) {
     return "The line is currently empty.";
   }
   var lineNamesandNumbers = [];
   
-  for(var i=0; i<line.length; i++) {
-    lineNamesandNumbers.push(i+1 + ". "+ line[i]);
+  for(var i=0; i<katzDeliLine.length; i++) {
+    lineNamesandNumbers.push(i+1 + ". "+ katzDeliLine[i]);
   }
   console.log("The line is currently: " + lineNamesandNumbers)
   return "The line is currently: " + lineNamesandNumbers.join(', ');
