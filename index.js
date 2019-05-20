3d3fa57dbc5661daa38fe1407ecf053699998f07
Ciprian Popa <ciprian_popa2002@yahoo.com>
2019-05-20 23:25:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-function takeANumber(line, newPerson){
-  line.push(newPerson);
-  return `Welcome, ${newPerson}. You are number ${line.length} in line.`;
+function takeANumber(lineOfPeople, newPerson){
+  lineOfPeople.push(newPerson);
+return `Welcome, ${newPerson}. You are number ${lineOfPeople.length} in line.`;
 }
-
-function nowServing(line){
-  if(line.length !== 0){
-    return "Currently serving " + line.shift() +".";
-  } else if (line.length === 0){
-    return "There is nobody waiting to be served!";
-  }
-}
-
-function currentLine(line){
-  if (line.length ===0){
-    return "The line is currently empty."
-  }
-  var linePeople = 'The line is currently: ';
-  for (let i = 0; i < line.length; i++){
-    if(line.length - 1 == i){
-      linePeople += ((i+1) + ". " + line[i]);
-      }else{
-       linePeople += ((i+1) + ". " + line[i] + ", ");
-      }
+function  nowServing(lineOfPeople){
+  if(lineOfPeople.length === 0){
+    return 'There is nobody waiting to be served!';
     }
-  return linePeople;
+  else if(lineOfPeople !== []){
+     return 'Currently serving ' + lineOfPeople.shift() +'.';
+  } 
 }
+function currentLine(lineOfPeople){
+  if(lineOfPeople.length === 0){
+    return 'The line is currently empty.';
+  }
+  var newLine = 'The line is currently: ';
+  var newArray = [];
+  for(let i = 0; i < lineOfPeople.length; i++){
+    if(lineOfPeople.length > 0){
+      
+      newArray.push((i + 1) + '. '  +  (lineOfPeople[i]));
+    }
+  }
+  
+  return newLine + newArray.join(', ');
+}
+
+
