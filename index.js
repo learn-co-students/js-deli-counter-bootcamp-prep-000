0e5b5317f5da49ea83a8be1634dd085067a2dca6
Austin Ruby <austinjruby@gmail.com>
2019-05-29 17:39:26 +0000

    Done.

--- index.js
+++ index.js
@@ -5,24 +5,23 @@ function takeANumber(lineArray, newName) {
 
 function nowServing(lineArray) {
   if(lineArray.length > 0) {
-    var person = lineArray[0];
-    lineArray.shift();
-    return `Currently serving ${person}.`
-  } else {
+    return `Currently serving ${lineArray.splice(0, 1)}.` //could also use shift()
+  } 
+  else {
     return 'There is nobody waiting to be served!';
   }
 }
 
 function currentLine(lineArray) {
-  if(lineArray.length > 0) {
-    return "The line is currently: " + lineArray
+  if (lineArray.length > 0) {
+    return `The line is currently: ${lineArray
                                             .map((currentVal,index) => `${index + 1}. ${currentVal}`)
-                                            .join(", ");
-    /*var newLine = [];
-    for(var i = 0; i < lineArray.length; i++) {
-      newLine.push(`${i + 1}. ${lineArray[i]}`)
-    }
-    return `The line is currently: ${newLine.join(", ")}`;*/
+                                            .join(", ")}`;
+    // const newLine = [];
+    // for (let i = 0; i < lineArray.length; i++) {
+      // newLine.push(`${i + 1}. ${lineArray[i]}`)
+    // }
+    // return `The line is currently: ${newLine.join(", ")}`;
   } else {
     return 'The line is currently empty.';
   }
