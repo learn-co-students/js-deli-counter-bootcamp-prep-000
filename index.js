ac4b54b20410e72380d2f459af1a3a4b3a6d6064
Andrew Tack <andrew.tack.recapllc@gmail.com>
2019-05-30 16:57:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function takeANumber (katzDeliLine, newName) {
+  
+  katzDeliLine.push(newName)  
+
+  return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing (katzDeliLine) {
+  
+  for (let i = 0; i < katzDeliLine.length; i++) {
+  }
+  
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+  
+  return `Currently serving ${katzDeliLine.shift()}.`
+}
+
+function currentLine (katzDeliLine) {
+  var line = []
+  
+  for (let i = 0; i < katzDeliLine.length; i++) {
+    line.push(" " + [i+1] + ". " + katzDeliLine[i])
+  }
+  
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  }
+  
+  return "The line is currently:" + line
+  
+}
