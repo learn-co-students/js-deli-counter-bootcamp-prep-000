e0f6cb06a6c889a8d598e2159009ac661ff7bc57
Aliciea D Suechin <adr330@nyu.edu>
2019-06-05 17:44:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+function nowServing(katzDeliLine) {
+  for(let i = 0; i < katzDeliLine.length; i++){
+    if(katzDeliLine.length=== 0){
+      return "There is nobody waiting to be served!"
+    } else {
+      return `Currently serving ${katzDeliLine.shift()}.`;
+    }
+  }
+}
+function currentLine(katzDeliLine) {
+  // let str = '';
+  if(katzDeliLine.length === 0){
+    return "The line is currently empty.";
+  }
+  let arr = [];
+  for(let i = 0; i < katzDeliLine.length; i++){
+    arr.push(i + 1 +  " . " katzDeliLine[i]);
+    return "The line is currently: " + arr.join(", ")
+  }
+}
+
+
+
+
