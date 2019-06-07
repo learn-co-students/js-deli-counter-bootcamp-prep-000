ea178bdc5efca6247e48adb1d025e7bb98ec1cc2
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-07 03:10:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+var katzDeliLine = []
+function takeANumber(line, name){
+  line.push(name);
+  
+  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
+  
+  return "Welcome, " + name + ". You are number " + line.length + " in line."
+}
+
+function nowServing(line) {
+  if(line !== line.length) {
+    console.log("There is nobody waiting to be served!")
+    return "There is nobody waiting to be served!"
+  } else {
+    console.log("Currently serving " + line.shift());
+    return "Currently serving " + line.shift();
+  }
+}
+
+function currentLine(line){
+  if(line !==line.length) {
+    return "The line is currently empty.";
+  }
+  var nameAndNumber = [];
+  
+  for(var i=0; i<line.length; i++) {
+    nameAndNumber.push(i+1 + ". "+ line[i]);
+  }
+  console.log("The line is currently: " + nameAndNumber)
+  return "The line is currently: " + nameAndNumber.join(', ');
+}
+
+
