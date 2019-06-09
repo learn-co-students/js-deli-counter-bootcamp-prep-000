9f070686a2b0a2b13b763ac1361f56a989fcc383
David Little <littledavid175@gmail.com>
2019-06-09 21:42:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-function takeANumber(katzDeli,Ada){
-  
-  
-}
+function takeANumber(katzDeliLine,Ada){
+katzDeliLine.push(Ada)
+  return`Welcome, ${Ada}. You are number ${katzDeliLine.length} in line.` }
