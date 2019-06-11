eefe87eb5b41b3f7cfb7071ca96010c433c72f1e
David Little <littledavid175@gmail.com>
2019-06-11 01:32:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function takeANumber(katzDeliLine,Ada){
 katzDeliLine.push(Ada)
   return`Welcome, ${Ada}. You are number ${katzDeliLine.length} in line.` }
+function nowServing(katzDeliLine){
+   for (let i =0; i >1; i++){
+     if(i===0){
+       katzDeliLine.push('There is nobody waiting to be served!')}
+     else{
+       katzDeliLine.push(`currently serving ${katzDeliLine.length}`) } }
+  return katzDeliLine
+}
