a4441c3aebf19d7db0e0f97e49193e35b12d489c
Christopher Kennedy <chris94kennedy@gmail.com>
2019-06-19 15:06:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function takeANumber(line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!';
+  } else if (katzDeliLine.length > 0) {
+    var removedIndividual = katzDeliLine.shift(0);
+    return `Currently serving ${removedIndividual}.`;
+  }
+}
+
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return 'The line is currently empty.';
+  }
+  var positionAndName = [];
+  for (var i = 0; i < line.length; i++) {
+    if (i === 0) {
+    positionAndName.push(`${i+1}. ${line[0]}`);
+    } else {
+      positionAndName.push(` ${i+1}. ${line[i]}`);
+    }
+  }
+  return `The line is currently: ${positionAndName}`;
+}
+    
+    
+    
