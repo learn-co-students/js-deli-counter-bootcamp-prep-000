aaa28d3b4ae2d6400e7a62b7022116cb42d34da2
garvin tan <garvintan07@gmail.com>
2019-06-19 06:56:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function takeANumber(array, name) {
+  array.push(name)
+  return `Welcome, ${name}. You are number ${array.length} in line.`
+}
+
+function nowServing (array) {
+  if (array.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    return `Currently serving ${array.shift()}.` 
+      
+  }
+}
+
+function currentLine(array) {
+  if (array.length ===0) {
+    return "The line is currently empty."
+  } else {
+    var str = "The line is currently:"
+    for (let i =0; i < array.length; i+=1) {
+      str += ` ${i+1}. ${array[i]},`
+    }
+    str = str.slice(0,-1)
+    return str
+  }
+}
