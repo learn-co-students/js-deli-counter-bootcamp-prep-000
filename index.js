8320e5f702a0f3912d715126411cf01c33bce3f0
Christopher Kennedy <chris94kennedy@gmail.com>
2019-06-21 06:19:00 +0000

    Done.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return 'There is nobody waiting to be served!';
   } else if (katzDeliLine.length > 0) {
-    var removedIndividual = katzDeliLine.shift(0);
+    var removedIndividual = katzDeliLine.shift();
     return `Currently serving ${removedIndividual}.`;
   }
 }
