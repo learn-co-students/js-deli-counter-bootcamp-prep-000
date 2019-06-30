cdde93ab57bb539fec97b488411a84082f4d6906
Kierra M <k.merriman32@hotmail.com>
2019-06-30 01:53:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-function takeANumber (katzDeliLine, name){
-  var position = katzDeliLine.length+1
+function takeANumber(katzDeliLine,name) {
   katzDeliLine.push(name)
-return "Welcome, " + name +". You are number "+ position +" in line."
-
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
 
-function nowServing (deliLine){
-var name;
-
-if(deliLine.length === 0){
-  return "There is nobody waiting to be served!"
+function nowServing(katzDeliLine,name) {
+ if(katzDeliLine.length===0) {
+ return "There is nobody waiting to be served!"
+ }
+ name= katzDeliLine.shift()
+ return "Currently serving "+ name + "."
 }
 
-name = deliLine.shift()
-return "Currently serving "+ name + "."
-}
-
-function currentLine (katzDeliLine){
-  if(katzDeliLine.length === 0){
-    return "The line is currently empty."
+function currentLine(katzDeliLine,name) {
+  if(katzDeliLine.length===0) {
+   return "The line is currently empty."
   }
-
-  var myString = `The line is currently: 1. ${katzDeliLine[0]}`
-
-  for(let i = 1; i<katzDeliLine.length;i++){
-  myString += `, ${i+1}. ${katzDeliLine[i]}`
-}
-return myString
+  var string =`The line is currently: 1. ${katzDeliLine[0]}`
+  
+   for(let i = 1; i < katzDeliLine.length; i++) {
+   string +=`, ${i+1}. ${katzDeliLine[i]}`
+   }
+ return string;
 }
