77fd5c51e2b9f172efd71fab6f22bbec0080f9a5
Jesus Nunez <jesusnunezxoco@gmail.com>
2019-07-04 16:20:36 +0000

    Done.

--- index.js
+++ index.js
@@ -8,23 +8,27 @@ function takeANumber(deli, name) {
   return `Welcome, ${name}. You are number ${deli.length} in line.`
 }
 
-
  //returns the line is empty if no one is in line
  //otherwise, make announcement 
- 
 function nowServing(deli) {
   if(deli.length > 0) {
-    var first = deli.shift()
-    return `Currently serving ${first}.` 
+    var current = deli.shift()
+    return `Currently serving ${current}.` 
   } else {
     return "There is nobody waiting to be served!"
   }
 }
 
+//return string if empty
+//otherwise, say position first three people in line are
 function currentLine(deli) {
   if(deli.length === 0) {
     return "The line is currently empty."
   } else {
-    return `The line is currently: 1. ${deli[0]}, 2. ${deli[1]}, 3. ${deli[2]}`
+    var newLine = [];
+    for(var i =0; i < deli.length; i++) {
+      newLine.push(`${i+1}. ${deli[i]}`)
+    }
   }
+  return "The line is currently: " + newLine.join(", ")
 }
