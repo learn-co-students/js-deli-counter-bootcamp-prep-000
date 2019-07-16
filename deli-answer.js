f2599ebb875ce840d5f9849055edca4f1f0223de
Alex Morris <alexmorris10x@gmail.com>
2019-07-16 22:29:07 +0000

    Done.

--- deli-answer.js
+++ deli-answer.js
@@ -2,27 +2,24 @@ let katzDeliLine = []
 
 function takeANumber (katzDeliLine,addCustomer) {
   katzDeliLine.push(addCustomer)
-  return ("Welcome, "+ katzDeliLine[katzDeliLine.length-1]
+  console.log("Welcome, "+ katzDeliLine[katzDeliLine.length-1]
   + ". You are number " + katzDeliLine.length + " in line.")
 } 
 
 function currentLine (katzDeliLine) {
   let message = [];
-  if (katzDeliLine.length > 0) {
   for (let i = 0; i < katzDeliLine.length; i++) {
   message.push(i+1 + '. ' + katzDeliLine[i])
   }
-  return ("The line is currently: " + message.join(", "))
-  } else {
-    return ("The line is currently empty.")
-  }
+  console.log("The line is currently: " + message.join(" "))
 }
 
 function nowServing (katzDeliLine) {
   let firstCustomer = katzDeliLine.shift()
   if (katzDeliLine.length > 0 || firstCustomer !== undefined) {
-  return ("Currently serving " + firstCustomer + ".")
+  console.log ("Currently serving: " + firstCustomer)
   } else {
-  return ("There is nobody waiting to be served!")
+  console.log ("There are no more customers to serve")
   }
 }
+
