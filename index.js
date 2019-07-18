a47a521e44771ab81fa3deae8138b1234f59894e
Matthew Chen <mc.2green@gmail.com>
2019-07-18 16:14:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+
+var katzDeliline = [];
+function takeANumber(katzDeliline, name){
+  katzDeliline.push(name);
+  return "Welcome, " + name + ". You are number " +  katzDeliline.length + " in line.";
+  
+}
+
+function nowServing(katzDeliline){
+  if(katzDeliline.length > 0){
+    var current = katzDeliline[0];
+    katzDeliline.splice(0,1);
+    return "Currently serving " + current + ".";
+  }else{
+    return "There is nobody waiting to be served!"
+  }
+}
+function currentLine(katzDeliline){
+  var counter = 0;
+  var sentence = "";
+  if(katzDeliline.length > 0){
+    while (counter < katzDeliline.length){
+      sentence = (counter + 1) + ". " + katzDeliline[counter] + ", ";
+    }
+  }else{
+    return "The line is currently empty.";
+  }
+  return "The line is currently: " + sentence
+}
