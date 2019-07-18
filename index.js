7d854087e716613839203b0721b84ab9e26735f3
Christian Gerace <cgerace8@gmail.com>
2019-07-18 01:34:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 function takeANumber(queue, customer) {
-  return `Welcome, ${customer}. You are number ${queue.size} in line.`
+  queue.push(customer)
+  return `Welcome, ${customer}. You are number ${queue.length} in line.`
 }
 
 function nowServing(queue) {
-  return queue.length === 0 ? "There is nobody waiting to be served" : queue.shift()
+  return queue.length === 0 ? "There is nobody waiting to be served!" : `Currently serving ${queue.shift()}.`
 }
 
 function currentLine(queue) {
-  line = 'The line is currently: '
-  for (let i = 0; i < queue.length; i++) {
-    line += `${i + 1}. ${katzDeliLine[i]}`
+  if (queue.length === 0) {
+    return 'The line is currently empty.'
   }
-  return line
+  else {
+    let line = 'The line is currently: '
+    for (let i = 0; i < queue.length; i++) {
+      line += `${i + 1}. ${queue[i]}${queue.length !== (i+ 1) ? ", " : "" }`
+    }
+    return line
+  }
 }
