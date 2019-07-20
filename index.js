95116ac21d9b5967dce1876954e0e72d75fa5e36
“Patrick <pctennislessons@gmail.com>
2019-07-19 21:55:15 -0600

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name);
-  return "Welcome, " + name+"." + " You are number " + katzDeliLine.length + " in line.";
+function takeANumber(lineArray, name){
+  lineArray.push(name);
+  return `Welcome, ${name}. You are number ${lineArray.length} in line.`;
 }
 
-function nowServing(katzDeliLine){
-  if (katzDeliLine[0]===undefined){
-    return 'There is nobody waiting to be served!'}
-    else{
-      return "Currently serving "+katzDeliLine.shift(0)+".";
-    }
+function nowServing(lineArray){
+  return lineArray.length===0 ? 'There is nobody waiting to be served!' 
+  : `Currently serving ${lineArray.shift(0)}.`;
 }
 
-
-function currentLine(katzDeliLine){
-  var deliLineString ="The line is currently: ";
-  if(katzDeliLine[0]===undefined){
-    return 'The line is currently empty.';
-  }else{ for(let i = 0; i < katzDeliLine.length; i++) {
-    if(i===katzDeliLine.length-1){
-      deliLineString = deliLineString + parseInt(i+1) +". "+ katzDeliLine[i];
-    }
-     else{ deliLineString = deliLineString + parseInt(i+1) +". "+ katzDeliLine[i]+', ';
-     }
-  }
-    return deliLineString;
-  }
+function currentLine(lineArray){
+  var lineString ="The line is currently:";
+  var emptyString = "The line is currently empty."
+  var nameNumberArray = lineArray.map((name, index) => {return `${(index+1)}. ${name}`})
+  return lineArray.length === 0 ? emptyString : `${lineString} ${nameNumberArray.join(', ')}`;
 }
