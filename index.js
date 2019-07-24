a1c28a3dd3739fca6ce42340e9d2a9373f145331
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-07-24 12:56:12 +0000

    Done.

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function nowServing(line) {
     return "There is nobody waiting to be served!"
   } else {
     //console.log("Currently serving " + line.shift());
-    return "Currently serving " + line.shift();
+    return "Currently serving " + line.shift() + ".";
   }
 }
 
