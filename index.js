42806a9104d7eb5ee8e92f7d326143dc8e195548
Ali Pfeffer <alpfefs@gmail.com>
2019-07-25 19:21:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function takeANumber(line, name) {
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing(line) {
+  if (line.length > 0) {
+  string = `Currently serving ${line[0]}.`
+  line.shift()
+  }
+  else {
+    string = 'There is nobody waiting to be served!'
+  }
+  
+  return string
+}
+
+function currentLine(line) {
+  if (line.length > 0) {
+    string = 'The line is currently: '
+    for(var i = 0; i < line.length; i ++) {
+      string += `${i + 1}. ` + line[i] + ', '
+    }
+  }
+  else {
+    return "The line is currently empty."
+  }  
+  return string.slice(0,string.length-2)
+}
