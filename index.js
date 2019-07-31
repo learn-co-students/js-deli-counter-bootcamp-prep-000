5da1777a44e929a8f45e080a5ef54d4986460c03
Barbara Muesing <muesingb@gmail.com>
2019-07-31 14:38:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-function takeANumber(number, name) {
-  return console.log("Welcome, " + `${name}` + ". You are number "+ number +" in line.")
+function takeANumber(array, name) {
+  array.push(name)
+  return "Welcome, " + name + ". You are number "+ array.length +" in line."
 };
 function nowServing(katzDeliLine) {
-
+  if (katzDeliLine.length < 1) {return "There is nobody waiting to be served!"}
+  else {return "Currently serving " + katzDeliLine[0]}
+  katzDeliLine.shift()
 };
-function currentLine() {
-
+function currentLine(array) {
+  var a = []
+  if (array.length < 1) {return "The line is currently empty."}
+  else {for (var x=0; x < array.length; x++) {a.push(" " + (x+1) + ". " + array[x])}}
+  return "The line is currently:" + a
 };
