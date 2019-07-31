66459a65f964ccae24712533e8f95b2a06e57a56
Barbara Muesing <muesingb@gmail.com>
2019-07-31 17:51:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(array, name) {
   array.push(name)
-  return "Welcome, " + name + ". You are number "+ array.length +" in line."
+  return "Welcome, " + name + ". You are number " + array.length + " in line."
 };
 function nowServing(array) {
   if (array.length < 1) {return "There is nobody waiting to be served!"}
