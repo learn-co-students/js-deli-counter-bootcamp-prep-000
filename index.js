9ec3a81030d430b6952e2035606a90cc550edcff
Priscilla Firpo <priscillayf@gmail.com>
2019-07-31 16:00:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function takeANumber(currentLine, newPerson) {
+  currentLine.push(newPerson);
+  var person = currentLine.length;
+  var welcome = `Welcome, ${newPerson}. You are number ${person} in line.`;
+  return welcome;
+}
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return "Currently serving " + katzDeliLine.shift() + ".";
+  } else return "There is nobody waiting to be served!";  
+}
+
+function currentLine(line) {
+   var empty = "The line is currently: ";
+   var people = 0;
+   if (line.length > 0) {
+     while (people < line.length) {
+       empty = empty + `${people + 1}. ` + line[people] + ", ";
+       people++;
+     } return empty.slice(0, empty.length - 2)
+  } else return "The line is currently empty."
+
+}
