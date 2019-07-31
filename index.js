92db1afa47fe990d2b882f38d6c6524e88d56fc1
Barbara Muesing <muesingb@gmail.com>
2019-07-31 13:19:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function takeANumber(number, name) {
+  return console.log("Welcome, " + `${name}` + ". You are number "+ number +" in line.")
+};
+function nowServing(katzDeliLine) {
+
+};
+function currentLine() {
+
+};
