53f768318835e500ca292db91647189cdac71396
Bailey Wood <woodbmae@gmail.com>
2019-08-05 23:56:32 +0000

    Done.

--- index.js
+++ index.js
@@ -2,31 +2,27 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
 }
 
-function currentLine(x) {
-  var line = []
-    if (x.length === 0) {
-      return "The line is currently empty."
-    } else {
-      for(var i = 0; i < x.length; i++) {
-        line += (i + 1) + ". " + x[i] + ", "
-      }
-      line = line.slice(0, line.length-2)
-      return "The line is currently: " + line
-    }
-}
-function nowServing(x) {
-  if (x.length === 0) {
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-    var name = x[0];
-    x.splice(0, 1);
-    return "Currently serving " + name + ".";
+    var name = katzDeliLine.shift();
+    return (`Currently serving ${name}.`)
   }
 }
 
-
-
-
+function currentLine(people) {
+  var line = ""
+    if (people.length === 0) {
+      return (`The line is currently empty.`)
+    } else {
+      for(var i = 0; i < people.length; i++) {
+        line += `${i + 1}. ${people[i]}, `
+      }
+      line = line.slice(0, line.length - 2)
+      return (`The line is currently: ${line}`)
+    }
+}
