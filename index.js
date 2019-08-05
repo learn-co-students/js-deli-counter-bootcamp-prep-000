82e1decd648ee09ae6bf7d2879a15d9efb18f13d
Michael Adair <riada125@hotmail.com>
2019-08-05 17:48:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-//function that a new customer uses when entering the deli
-
 function takeANumber(line, name){
  
-  //add the new customer's name to the line, adding to the end of the array and changing it
-  
+ //add the new customer's name to the line, adding to the end of the array 
   line.push(name);
   
   //position in line for the new customer is dictated by the indexOf names + 1
    let position = line.indexOf(name) + 1;
   
-  //Returns the below string + name and position. NB can improve with ` and $ identifier
- return("Welcome, " + name + ". You are number " + position + " in line.");
- 
+  //Returns the below string + name and position.
+ return(`Welcome, ${name}. You are number ${position} in line.`);
 }
 
 function nowServing(line){
@@ -35,7 +31,6 @@ else {
 //create a function that accepts the current line of people and accepts the current line as a string
 function currentLine(line){
   
-  //creating a variable which contains an empty array (for people in the line?)
   var katzDeliLine = []
 
   for (let i = 0; i<line.length; i++) {
@@ -44,7 +39,6 @@ function currentLine(line){
       katzDeliLine.push(t)
     }
 
-//if line is 0, return the below string
    if (line.length === 0) {
     return 'The line is currently empty.';
   } 
@@ -53,7 +47,6 @@ function currentLine(line){
   else {
     var string1 = 'The line is currently:';
     
-    //returns string1 + merges the KatzDeliLine array above
     return string1.concat(katzDeliLine);
     }
 
