3fc6b66efd35625c31cf53aa293e12485077a280
Peter Sampson <petersampson010@gmail.com>
2019-08-12 14:52:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function takeANumber(currentLine, name) {
+  currentLine.push(name)
+  return `Welcome, ${name}. You are number ${currentLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length===0) {
+    return `There is nobody waiting to be served!`
+  } else { 
+    var userName = katzDeliLine.shift()
+    return `Currently serving ${userName}.`
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length===0) {
+    return `The line is currently empty.`
+  } else {
+    for (let i=1; katzDeliLine.length>0; i++) {
+      var newArray = []
+      newArray.push(`${i}. ${katzDeliLine[i]}`)
+    }
+    return `The line is currently: ${newArray}`
+  }
+}
