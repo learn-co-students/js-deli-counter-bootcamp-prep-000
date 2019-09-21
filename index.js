60068744532eee7743fc36a170806a9a2994febe
Mark Kauffman <markkauffman22@gmail.com>
2019-09-21 21:14:59 +0000

    Done.

--- index.js
+++ index.js
@@ -9,34 +9,34 @@ function takeANumber (curLinePeople, newPersonName) {
 }
 
 function nowServing (katzDeliLine) {
-  currentLine(katzDeliLine);
 
   if (katzDeliLine.length === 0) {
      return ("There is nobody waiting to be served!");
   }
-  // return the first person in line and remove that individual from the line
+  // run the currentLine function to 'process' the katzDeliLine queue (array)
+  currentLine(katzDeliLine);
   var curPerson =  katzDeliLine[0];
+  
+  // return the first person in line and remove that individual from the line
   katzDeliLine.shift();
   return ("Currently serving " + curPerson + ".");
 }
 
-function currentLine (line) {
+function currentLine(line) {
   var outputStr = "The line is currently: ";
   
-   if (line.length === 0) {
+  if (line.length === 0) {
      return ("The line is currently empty.");
-   }
-   else {
-     
-     for (var i = 0; i < line.length; i++) {
-       var pos = i + 1;
-       outputStr = outputStr + pos + ". " + line[i] + " " ;
-       
-       //expect(currentLine(["Bill", "Jane", "Ann"])).toEqual("The line is currently: 1. Bill, 2. Jane, 3. Ann");
-     return (outputStr );
-       
-     }
-   }
-}  
+  }
+  for (var i = 0; i < line.length; i++) {
+    var pos = i + 1;
+    outputStr = outputStr + pos + ". " + line[i] ;
+    if (pos != line.length) {
+      outputStr = outputStr + ", "
+    }
+    //expect(currentLine(["Bill", "Jane", "Ann"])).toEqual("The line is currently: 1. Bill, 2. Jane, 3. Ann");
+  }
+    return (outputStr );
+}
 
  
