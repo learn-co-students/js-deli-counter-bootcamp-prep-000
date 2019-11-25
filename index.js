e57e191aa4e71d9e91c9b9bfb07ad2f913e92a32
Gian Moran <gpmoran_@outlook.com>
2019-11-25 17:15:58 +0000

    Done.

--- index.js
+++ index.js
@@ -2,29 +2,24 @@ function takeANumber(current,name){
   current.push(name)
   return `Welcome, ${name}. You are number ${current.length} in line.`
 }
-function nowServing(katzDeliLine){
-  var count = 0
-  while(count < katzDeliLine.length){
-    count++
+function nowServing(current){
+  if(current.length === 0){
+    return `There is nobody waiting to be served!`
   }
-  if(katzDeliLine.length === 0){
-    return "There is nobody waiting to be served!"
-  }
-  else
-  return `Currently serving ${katzDeliLine.shift()}.`
+for(var i = 0; i < current.length; i++){
+  return `Currently serving ${current.shift()}.`
 }
-function currentLine(katzDeliLine){
-  var count = 0
-  var line = []
-  while(count < katzDeliLine.length){
-   
-    line.push(` ` + [count+1] + `.` + ` `+  katzDeliLine[count])
-    count++
+ 
+}
+function currentLine(current){
+  if(current.length === 0){
+    return `The line is currently empty.`
   }
-   if(katzDeliLine.length === 0){
-      return `The line is currently empty.`
-    }
-    else
-    return `The line is currently:` + line
-    
+  var i = 0;
+  var line = []
+  while(i < current.length){
+    line.push(` ` + [i+1] +`.`+ ` `+current[i])
+    i++
+  }
+  return `The line is currently:` + line
 }
