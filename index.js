49888f333a945e4e649f883aee69403972942867
favorable-principle-2048 <musta7188@hotmail.it>
2019-12-02 15:34:36 +0000

    Done.

--- index.js
+++ index.js
@@ -24,18 +24,15 @@ return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 
 function currentLine(katzDeli,line){
 
-  var i = 0;
-
 var line = [];
 
-   while (katzDeli.length > 0){
+for (let i = 0; i < katzDeli.length; i++ ){
 
-  line.push(`The current line is:` + [i+1] + ' ' + `${katzDeli[i]},`) ;
- i++;
+line.push([i+1] + '. '+ katzDeli[i]);
 
-  } if (katzDeli.length === 0){
+} if (katzDeli.length === 0) {
     return `The line is currently empty.`;
-  }
+  } else {
 
-
+return `The line is currently: ${line.join(', ')}`};
 }
