b92d3d5a70c62261324770231f1d7f6852b44012
nsabir <awesomeartist15@gmail.com>
2019-12-26 17:43:02 +0000

    Done.

--- index.js
+++ index.js
@@ -16,11 +16,41 @@ function nowServing(katzDeliLine){
     }
   }
 
+// function currentLine(line){
+//   let numbers = []
+//   for(var i = 0; i < line.length; i++){
+//     //create a string with number and person's name
+//     var nums = `${i + 1}. ${line}`
+//     //put name into our numbers array 
+//     numbers.push(nums)
+//     if (line.length > 0){
+//       return `The line is currently: ${numbers}.`
+//     }
+//     else {
+//       return `The line is currently empty.`
+//     }
+//   }
+//   return numbers
+// }
+
 function currentLine(line){
-    if (line.length > 0){
-      return `The line is currently ` + ` ${line}.`
+  let numbers = []
+  if (line.length === 0){
+    return `The line is currently empty.`
+  }
+  for(var i = 0, l= line.length; i < l; i++){
+    var nums = `${i + 1}. ${line[i]}`
+    numbers.push(nums)
     }
-    else {
-      return `The line is currently empty.`
-    }
+  return `The line is currently: ${numbers.join(', ')}`
 }
+
+
+// function currentLine(line){
+//     if (line.length > 0){
+//       return `The line is currently: ${line}.`
+//     }
+//     else {
+//       return `The line is currently empty.`
+//     }
+//   }
