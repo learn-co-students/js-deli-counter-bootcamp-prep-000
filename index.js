301949eb347add4dcc76b1fd42f7a011c5f6b6ed
Avinash Nepal <nepalavinash@gmail.com>
2019-12-26 23:37:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var katzDeliLine = []
+
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+function nowServing(katzDeliLine){
+    if (katzDeliLine.length > 0){
+    return `Currently serving ${katzDeliLine.shift()}.`
+    } else {
+      return "There is nobody waiting to be served!"
+    }
+
+}
+function currentLine(katzDeliLine){
+  var array = []
+  for (var i = 0; i < katzDeliLine.length; i++){
+  `${i+1}.${katzDeliLine[]}.`
+}
+
+}
