49c22bd18ce0a43eae749ef7b407cd376e17560f
Rashid Jama <rashidjama5@gmail.com>
2019-12-29 09:36:17 +0000

    Done.

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ function takeANumber(deliLine, customerName) {
 }
 
 function nowServing(katzDeli) {
-  for(let i=0;i<katzDeli.length;i++) {
-
-  }
   if(katzDeli.length === 0) {
     return 'There is nobody waiting to be served!'
   } else {
@@ -16,15 +13,16 @@ function nowServing(katzDeli) {
 }
 
 function currentLine(katzDeli) {
-  let line = []
+
+  if(katzDeli.length === 0) 
+    return 'The line is currently empty.';
+
+  let line = "";
   for (let i = 0; i < katzDeli.length; i++) {
-     line.push([i+1] + ". " + katzDeli[i])
+    line += i + 1 + ". " + katzDeli[i] + ", ";
   }
-  if(katzDeli.length === 0) {
-    return 'The line is currently empty.'
-  } else {
-  return(`The line is currently: ${line.join(", ")}`);
+  
+    line = line.substring(0, line.length - 2);
+  
+  return `The line is currently: ${line}`;
 }
-}
-
-
