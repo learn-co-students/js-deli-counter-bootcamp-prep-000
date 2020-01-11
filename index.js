3bc0d5a86a417d500319be60d763fee513cd6ad2
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 07:04:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+var katzDeliLine = [];
+var newPerson = ["Ada", "Grace", "Kent"];
+var currentLine = [1, 2, 3];
+
+function takeANumber(newPerson, currentLine){
+ var i = 0;
+ if i = 0
+ return console.log(`Welcome, ${newPerson[i]}. You are number ${currentLine[i]} in line.`);
+}
