468348ca2ae2f4a33a54bbba3a7a30f82cb4eee2
Lili Rodriguez <lilimrod@gmail.com>
2020-02-17 00:10:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
 var katzDeliLine = [];
 
-function takeANumber(katzDeliLine, name) {
+function takeANumber(katzDeliLine,name){
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine){
-  if(katzDeliLine.length === 0) {
+  if(katzDeliLine.length === 0){
     return "There is nobody waiting to be served!"
-  }
-  else {
+  } else{
     return 'Currently serving ' + katzDeliLine.shift() + '.'
   }
 }
 
 var newLine = [];
-
-function currentLine(katzDeliLine){
+function currentLine(katzDeliLine) {
   let i = 0
-  while (i <katzDeliLine.length){
+  while (i < katzDeliLine.length){
     newLine.push(' ' + (i+1) + '. ' + katzDeliLine[i])
     i++;
   }
-  if (katzDeliLine.length === 0) {
-    return "The line is currently empty."
-  }
-  else{
+  if (katzDeliLine.length === 0){
+    return 'The line is currently empty.'
+  }else {
     return `The line is currently:${newLine}`
   }
 }
