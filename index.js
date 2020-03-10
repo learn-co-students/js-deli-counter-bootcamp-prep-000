24bb828a6d414dc23d9e1b3d6c23d5c1d2a63cb9
RAJAN GAUCHAN <rgrgrg711@gmail.com>
2020-03-10 02:00:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-//var katzDeliLine = [];
+var katzDeliLine = [];
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
+function nowServing(katzDeliLine){
+    if (katzDeliLine.length === -1){ 
+      return "There is nobody waiting to be served!" 
+      
+    } else {
+    return `Currently serving  ${katzDeliLine.shift()}.`
+  }
+}
