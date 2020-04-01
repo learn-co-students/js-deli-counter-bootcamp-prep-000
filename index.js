af29d6cf72f934ae83cfccb55f1efcf32043a6ba
zaid85j <zaid85j@gmail.com>
2020-04-01 18:50:07 +0000

    Done.

--- index.js
+++ index.js
@@ -6,5 +6,11 @@ function takeANumber(katzDeliLine,newPersonName){
   console.log(`Welcome,${newPersonName}. You are number ${i} in line.`)
 ++i
  return katzDeliLine.push(newPersonName)
-
+}
+function nowServing(katzDeliLine){
+if(katzDeliLine.length>0){
+for(var i=0;i<katzDeliLine.length;i++){
+console.log(katzDeliLine.shift())}
+return katzDeliLine[i];
+}else{console.log("There is nobody waiting to be served!")}
 }
