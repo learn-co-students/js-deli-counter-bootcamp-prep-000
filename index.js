ed4b5dcc7fd5b5214d7dc59bd266a69cd2143580
zaid85j <zaid85j@gmail.com>
2020-04-01 16:19:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var katzDeliLine = [];
 function takeANumber(katzDeliLine,newPersonName){
-  katzDeliLine.push(newPersonName)
-  return `Welcome,${newPersonName}. You are number 1 in line.`
+ return katzDeliLine.push(newPersonName)
+  console.log(`Welcome,${newPersonName}. You are number 1 in line.`)
 }
