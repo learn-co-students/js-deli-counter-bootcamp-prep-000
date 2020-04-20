28aeb3bfcf047b923ef1be26d29888f2cb7135f6
Scott Singer <ssinger303@gmail.com>
2020-04-20 14:36:46 +0000

    Done.

--- index.js
+++ index.js
@@ -12,25 +12,6 @@ function nowServing(katzDeliLine){
   return `Currently serving ${katzDeliLine.shift()}.`;
 }
 
-/*takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
-takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
-takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
-
-currentLine(katzDeliLine); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
-
-nowServing(katzDeliLine); // "Currently serving Ada."
-
-currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent"
-
-takeANumber(katzDeliLine, "Matz"); // "Welcome, Matz. You are number 1 in line."
-
-currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent, 3. Matz"
-
-nowServing(katzDeliLine); // "Currently serving Grace."
-
-currentLine(katzDeliLine); // "The line is currently: 1. Kent, 2. Matz"
-``` */
-
 function currentLine(katzDeliLine){
   var string=`The line is currently:`;
   if (katzDeliLine.length===0)
