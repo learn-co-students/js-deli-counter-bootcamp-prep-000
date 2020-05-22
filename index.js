25ec995b478fa23c193ff97e1efa41424b742ad5
David MacPhail <david.macphail@icloud.com>
2020-05-22 17:12:37 +0000

    Done.

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function currentLine(line){
   if(!line.length){
     return "The line is currently empty."
   }
-  var numbersAndNames = []
+  var namenumber = []
 
   for (let i = 0, l = line.length; i < l; i++) {
-    numbersAndNames.push(`${i+1}. ${line[i]}`)
-  } return `The line is currently: ${numbersAndNames.join(', ')}`
+    namenumber.push(`${i+1}. ${line[i]}`)
+  } return `The line is currently: ${namenumber.join(', ')}`
 }
