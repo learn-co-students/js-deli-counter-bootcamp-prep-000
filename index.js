102ffb35e8c923fe9fc302a5f6ed5c53f1d7503d
Daniel Gould <dmgould310@gmail.com>
2020-06-16 15:16:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return "Currently serving " + katzDeliLine.shift() + ".";
+  } else {
+      return "There is nobody waiting to be served!";
+  }
+}
+function currentLine(katzDeliLine){
+    if (katzDeliLine.length === 0){ return "The line is currently empty."; }
+    else {
+    	return katzDeliLine.reduce(function(acc,val,index){
+	    let placeName = `${index+1}. ${val}`;
+	    if (index < katzDeliLine.length - 1){ return acc + placeName + ', '; }
+	    else { return acc + placeName; }
+	}, "The line is currently: ");
+    }
+}
