018b99666049010fd6e22bbf42886f7f86ea9f79
Gabrielle Piacentile <gabriellepiacentile98@gmail.com>
2020-12-19 18:17:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-function takeANumber(num,name){
-  array.push('Welcome, Ada. You are number 1 in line.')
+function takeANumber(line,name){
+  line.push(name)
+  let lineNum= line.length
+  var message = (`Welcome, ${name}. You are number ${lineNum} in line.`)
+  return message
+}
+
+function nowServing(line){
+  var next =  line.shift()
+  if (typeof(next) === 'string'){
+    return `Currently serving ${next}.`
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine (line) {
+  var message = "The line is currently:"
+  if (line.length === 0){
+    return "The line is currently empty."
+  } else {
+    let num = 1
+    while (num < line.length){
+      line.push(`${num}. ${name}`)
+    }
+  }
 }
